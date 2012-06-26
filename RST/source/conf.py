61b2db44d1db845286670e5c6df75aa483ac9db9
Cliff Shaffer <shaffer@vt.edu>
2012-06-26 12:12:33 -0400

    Update Makefile for csslint settings; update css files to pass csslint validation. Fix path in RST conf.py file.

--- RST/source/conf.py
+++ RST/source/conf.py
@@ -26,7 +26,7 @@ import sys, os
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 
-sys.path.append(os.path.abspath('../SAextensions/odsa/avembed'))
+sys.path.append(os.path.abspath('../ODSAextensions/odsa/avembed'))
 sys.path.append(os.path.abspath('../ODSAextensions/odsa/avmetadata'))
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'avembed', 'avmetadata']
 
