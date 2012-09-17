740f94b507eacdeb284961e46a91ed9ed4b8c0c4
efouh <ericfouh@gmail.com>
2012-09-17 14:11:48 -0400

    Issue #101. Added a contact tab slide on all the page for contact and feedback information. We are using jquery tabslideout plugin.

--- RST/source/_static/ODSA.js
+++ RST/source/_static/ODSA.js
@@ -88,6 +88,21 @@ $(document).ready(function() {
 		});
 	});
 
+
+
+        $('.slide-out-div').tabSlideOut({
+        tabHandle: '.handle',                              //class of the element that will be your tab
+        pathToTabImage: '_static/Images/contact_tab.gif',          //path to the image for the tab *required*
+        imageHeight: '122px',                               //height of tab image *required*
+        imageWidth: '40px',                               //width of tab image *required*    
+        tabLocation: 'left',                               //side of screen where tab lives, top, right, bottom, or left
+        speed: 300,                                        //speed of animation
+        action: 'click',                                   //options: 'click' or 'hover', action to trigger animation
+        topPos: '200px',                                   //position from the top
+        fixedPosition: false                               //options: true makes it stick(fixed position) on scroll
+        });   
+
+
 	// Brings up the login box if the user clicks 'Login' and
 	// logs the user out if they click their username
 	$('a.login-window').click(function() {
