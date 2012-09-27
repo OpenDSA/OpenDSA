bd1abb4f0efdf47b61ccff1b42888fd8af9ea61e
efouh <ericfouh@gmail.com>
2012-09-27 13:08:55 -0400

    Make sure that we only process messages comming from legit opendsa site.

--- RST/source/_static/ODSA.js
+++ RST/source/_static/ODSA.js
@@ -2,7 +2,7 @@
 var server_url = "http://opendsa.cc.vt.edu:8080"; // opendsa.cc.vt.edu:8080
 // Dan's desktop: 128.173.54.186:8000
 // Eric's desktop: 128.173.55.223:8080
-
+var odsa_url = "http://algoviz.org"; 
 // Contains a list of all AVs on the page
 var avList = [];
 
@@ -204,6 +204,8 @@ $(document).ready(function() {
 
         // Listen to message from child window
         eventer(messageEvent,function(e) {
+            if (e.origin !== odsa_url)
+                 return;
             msg_ka = jQuery.parseJSON( e.data );  
             updateProfDispStat(msg_ka.exercise, msg_ka.proficient);   
         },false);    
