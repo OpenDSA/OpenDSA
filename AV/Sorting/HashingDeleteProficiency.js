9f56272f3e714ccaacf70592b535aecbfd2f3ce6
Ville Karavirta <vkaravir@gmail.com>
2012-10-24 16:29:41 -0400

    Improvements to Hash delete proficiency

--- AV/Sorting/HashingDeleteProficiency.js
+++ AV/Sorting/HashingDeleteProficiency.js
@@ -11,6 +11,9 @@
 
   jsav.recorded();
 
+  // setup used for hashing. changing the probe and hashfunction functions, this
+  // exercise should be easily modifiable to other collision resolution methods
+  // and hashfunctions
   var Setup = {
     probe: function(i) {
       return i;
@@ -21,43 +24,65 @@
   }; // end setup
 
   function randomizeData() {
-    var initValues = JSAV.utils.rand.numKey(6, 8),
+    var initValues = JSAV.utils.rand.numKey(7, 8), // some randomization on size
         initArr = [],
-        randVal, inspos, i, j, valVals,
-        deleted = [], hashdata = [],
+        randMod, randVal, inspos, i, j, valVals,
+        deletions = [],
         input = [];
     initArr.length = arr_size;
-    hashdata.length = arr_size;
+
+    // insert initial data to the hashtable
     while (initValues > 0) {
-      randVal = JSAV.utils.rand.numKey(0, arr_size - 1);
-      valVals = JSAV.utils.rand.numKey(1, 3);
-      inspos = randVal;
+      randMod = JSAV.utils.rand.numKey(0, arr_size - 1); // the index we target
+      valVals = JSAV.utils.rand.numKey(2, 4); // how many values with that modulo
+      inspos = randMod;
       i = 0;
       for (j = 0; j < valVals; j++) {
-        while (initArr[inspos]) {
+        while (initArr[inspos]) { // find the right position to insert the value
           i++;
-          inspos = Setup.hashfunction(randVal + Setup.probe(i));
+          inspos = Setup.hashfunction(randMod + Setup.probe(i));
         }
-        initArr[inspos] = JSAV.utils.rand.numKey(10, 99) * 10 + randVal;
+        // randomize the value for the given mod randMod
+        randVal = JSAV.utils.rand.numKey(10, 99) * 10 + randMod;
+        while (initArr.indexOf(randVal) !== -1) { // make sure not to have duplicates
+          randVal = JSAV.utils.rand.numKey(10, 99) * 10 + randMod;
+        }
+        initArr[inspos] = randVal;
         initValues--;
-        console.log(hashdata[randVal] || []);
-        if (hashdata[randVal]) { hashdata[randVal].push(initArr[inspos]); }
-        else { hashdata[randVal] = [initArr[inspos]]; }
       }
+      deletions.push(initArr[inspos]); // add the last item with this module to the list of items to be deleted
     }
-    console.log(hashdata);
-    input.push(["delete", initArr[inspos]]);
-    input.push(["insert", JSAV.utils.rand.numKey(10, 99) * 10]);
-    input.push(["insert", JSAV.utils.rand.numKey(10, 99) * 10]);
+    for (i = 0; i < deletions.length; i++) { // add the deletions to the required operations
+      input.push(["delete", deletions[i]]);
+    }
+    // add some insertions
+    input.push(["insert", JSAV.utils.rand.numKey(10, 99) * 10 + Setup.hashfunction(deletions[0])]);
+    input.push(["insert", JSAV.utils.rand.numKey(10, 99) * 10 + Setup.hashfunction(deletions[1])]);
+    input.push(["insert", deletions[1] + JSAV.utils.rand.numKey(0, 29)]);
+
+    // check which values have not been deleted from the hashtable already
+    var values = $.map(initArr, function(item) {
+      return (deletions.indexOf(item) !== -1)?null:item;
+    });
+    // .. and delete all the initial values still in hashtable
+    for (i = 0; i < values.length; i++) {
+      input.push(["delete", values[i]]);
+    }
+    // .. and finish with a couple of random inserts
+    input.push(["insert", JSAV.utils.rand.numKey(100, 999)]);
+    input.push(["insert", JSAV.utils.rand.numKey(100, 999)]);
+
+    // return the data
     return {array: initArr, input: input };
   }
 
   function init() {
     if (jsavArr) {
+      // remove existing array and variables
       jsavArr.clear();
       task.element.remove();
       jsavInputPos.element.remove();
-    } // remove existing array
+    }
     jsav.end();
     jsav.displayInit(); // remove old animation
     initData = randomizeData();
@@ -75,7 +100,11 @@
         size = modelArr.size();
     jsavInputPos.value(inputPos.value());
     for (var i = 0; i < size; i++) {
-      var val = modelArr.value(i);
+      var val = modelArr.value(i),
+          bgColor = modelArr.css(i, "background-color");
+      if (jsavArr.css(i, "background-color") !== bgColor) { // fix background color
+        jsavArr.css(i, {"background-color": bgColor});
+      }
       if (val !== jsavArr.value(i)) { // fix values
         jsavArr.value(i, val);
       }
@@ -85,7 +114,6 @@
   }
 
   function model(modeljsav) {
-    console.log("model", modeljsav);
     var modelArr = modeljsav.ds.array(initData.array, {indexed: true}),
         inputPos = modeljsav.variable(0);
     modeljsav.displayInit();
@@ -96,14 +124,17 @@
           pos = Setup.hashfunction(val + Setup.probe(probeIndex));
       modelArr.highlight(pos);
       modeljsav.umsg((inputElem[0] === "delete" ? "Deleting key ":"Inserting key ") +
-          val + ". We start by checking position " + pos);
-      modeljsav.step();
+          val + ". We start by inspecting index " + pos + ".");
       if (inputElem[0] === "delete") {
         while (modelArr.value(pos) != val && modelArr.value(pos) !== "") {
+          modeljsav.stepOption("grade", true);
+          modeljsav.step();
           probeIndex++;
           pos = Setup.hashfunction(val + Setup.probe(probeIndex));
-          modeljsav.umsg("Since it is not the value we are looking for, continue to " + pos);
+          modeljsav.umsg("Since it is not " + inputElem[1] + ", continue to index " + pos + ".");
           modelArr.highlight(pos);
+        }
+        if (probeIndex > 0) {
           modeljsav.step();
         }
         modeljsav.umsg("Found the key we are looking for. Delete and mark it with a tombstone.");
@@ -117,9 +148,9 @@
           modeljsav.step();
         }
         if (modelArr.value(pos) === "") {
-          modeljsav.umsg("It is empty, so we insert the key");
+          modeljsav.umsg("The index is empty, so we insert the key.");
         } else { // has value [del]
-          modeljsav.umsg("It contains a tombstone, so we insert the key");
+          modeljsav.umsg("The index contains a tombstone, so we insert the key.");
         }
         modelArr.value(pos, val);
       }
@@ -130,7 +161,7 @@
     }
     return [modelArr, inputPos];
   }
-  exercise = jsav.exercise(model, init, {}, {
+  exercise = jsav.exercise(model, init, [{css: "background-color"}, {}], {
     controls: $('.jsavexercisecontrols'),
     feedback: "continuous",
     fixmode: "fix",
@@ -143,7 +174,7 @@
     if (pos < inputData.length) {
       task.text((inputData[pos][0] === "delete" ? "Delete key ":"Insert key ") + inputData[pos][1]);
     } else {
-      task.text("DONE");
+      task.text("No more input");
     }
   }
 
@@ -151,11 +182,17 @@
     var pos = jsavInputPos.value(),
         input = inputData[pos];
     if (input[0] === "delete") {
-      jsavArr.value(index, "[del]");
+      if (jsavArr.value(index) != input[1]) {
+        jsavArr.highlight(index);
+      } else {
+        jsavArr.unhighlight(true);
+        jsavArr.value(index, "[del]");
+        jsavInputPos.value(pos + 1);
+      }
     } else {
       jsavArr.value(index, input[1]);
+      jsavInputPos.value(pos + 1);
     }
-    jsavInputPos.value(pos + 1);
     updateStepTask();
     exercise.gradeableStep();
   }
