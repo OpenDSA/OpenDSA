770a01fb386c9019d626de7b2198a2c348eb0579
X455u <kassu7@gmail.com>
2013-07-12 10:42:45 +0300

    Refactored ClickHandler

--- AV/Development/ClickHandler.js
+++ AV/Development/ClickHandler.js
@@ -22,18 +22,18 @@
 			select: "click",			//click, first, last
 			drop: "click",				//click, first, last
 			keep: false,				//don't allow selecting the last node
-			onSelect: function() {},	//called by array when array is selected
-			onDrop: function() {}		//called by array when value has been changed
+			onSelect: function () {},	//called by array when array is selected
+			onDrop: function () {}		//called by array when value has been changed
 		};
 
 		this.jsav = jsav;
 		this.exercise = exercise;
 		this.selStruct = jsav.variable(-1);
 		this.selIndex = jsav.variable(-1);
-		this.selNode;	//only valid when selStruct != -1 and selIndex = -1
+		this.selNode = null;	//only valid when selStruct != -1 and selIndex = -1
 		this.ds = [];
 		this.options = $.extend({}, defaults, options);
-	}
+	};
 
 	ClickHandler.prototype = {
 		//get the index of a structure
@@ -88,22 +88,24 @@
 			var ch = this;
 
 			//add click handler
-			array.click(function(index) {
+			array.click(function (index) {
 				//move the values from the JSAV variables into regulas js vars
 				var sStruct = ch.selStruct.value();
 				var sIndex = ch.selIndex.value();
 				//changed to true if the step should be graded
-	            var grade = false;
+				var grade = false;
 
 				if (sStruct === -1) {
 					//select empty nodes only if the options allow it
-					if (!options.selectEmpty && this.value(index) === "")
+					if (!options.selectEmpty && this.value(index) === "") {
 						return;
+					}
 					//call onSelect function
 					var continueSelect = options.onSelect.call(this, index);
 					//return if onSelect returns false
-					if (typeof continueSelect !== "undefined" && !continueSelect)
+					if (typeof continueSelect !== "undefined" && !continueSelect) {
 						return;
+					}
 					//mark as selected
 					this.addClass(index, ch.options.selectedClass);
 					//set sStruct and sIndex values
@@ -111,16 +113,17 @@
 					sIndex = index;
 				} else if (sStruct === ch.getDsIndex(this)) {
 					//swap with empty nodes only if the options allow it
-					if (!options.selectEmpty && this.value(index) === "" && ch.options.effect === "swap")
+					if (!options.selectEmpty && this.value(index) === "" && ch.options.effect === "swap") {
 						return;
+					}
 					//unselect
 					this.removeClass(sIndex, ch.options.selectedClass);
 					if (sIndex !== index) {
 						//move/copy/swap within the array
 						valueEffect(ch, {
-							from: ch.getDs(sStruct), 
-							fromIndex: sIndex, 
-							to: ch.getDs(sStruct), 
+							from: ch.getDs(sStruct),
+							fromIndex: sIndex,
+							to: ch.getDs(sStruct),
 							toIndex: index,
 							effect: options.effect
 						});
@@ -140,8 +143,9 @@
 				} else {
 					//move/copy/swap from an another structure
 					//swap with empty nodes only if the options allow it
-					if (!options.selectEmpty && this.value(index) === "" && ch.options.effect === "swap")
+					if (!options.selectEmpty && this.value(index) === "" && ch.options.effect === "swap") {
 						return;
+					}
 					if (sIndex === -1) {
 						//from node
 						//unselect node
@@ -149,7 +153,7 @@
 						//move value
 						valueEffect(ch, {
 							from: ch.selNode,
-							to: this, 
+							to: this,
 							toIndex: index,
 							effect: options.effect
 						});
@@ -159,32 +163,31 @@
 						ch.getDs(sStruct).removeClass(sIndex, ch.options.selectedClass);
 						//move value
 						valueEffect(ch, {
-							from: ch.getDs(sStruct), 
-							fromIndex: sIndex, 
-							to: this, 
+							from: ch.getDs(sStruct),
+							fromIndex: sIndex,
+							to: this,
 							toIndex: index,
 							effect: options.effect
 						});
 					}
 					//call onDrop function
-						grade = options.onDrop.call(this, index);
-						if (typeof grade === "undefined") {
-							//set true if nothing was returned
-							grade = true;
-						} else {
-							//convert to boolean
-							grade = !!grade;
-						}					
+					grade = options.onDrop.call(this, index);
+					if (typeof grade === "undefined") {
+						//set true if nothing was returned
+						grade = true;
+					} else {
+						//convert to boolean
+						grade = !!grade;
+					}
 					//set sStruct and sIndex values					
 					sStruct = -1;
 					sIndex = -1;
 				}
-	            //move the values back to the JSAV variables
+				//move the values back to the JSAV variables
 				ch.selStruct.value(sStruct);
 				ch.selIndex.value(sIndex);
 				//grade if grade is true
-				if (options.gradeable && grade)
-					ch.exercise.gradeableStep();
+				if (options.gradeable && grade) {ch.exercise.gradeableStep(); }
 			});
 		},
 
@@ -198,36 +201,39 @@
 			var ch = this;
 
 			//add click handler
-			list.click(function() {
+			list.click(function () {
 				//move the values from the JSAV variables into regulas js vars
 				var sStruct = ch.selStruct.value();
 				var sIndex = ch.selIndex.value();
 				//changed to true if the step should be graded
-	            var grade = false;
+				var grade = false;
 
 				if (sStruct === -1) {
 					//select empty nodes only if the options allow it
-					if (!options.selectEmpty && this.value() === "" && options.select === "click")
+					if (!options.selectEmpty && this.value() === "" && options.select === "click") {
 						return;
+					}
 					//don't allow to select the last node if keep is true
-					if (options.keep && list.size() === 1)
+					if (options.keep && list.size() === 1) {
 						return;
+					}
 					//select
 					switch (options.select) {
-						case "first":
-							ch.selNode = list.first();
-							break;
-						case "last":
-							ch.selNode = list.last();
-							break;
-						default: //"click"
-							ch.selNode = this;
+					case "first":
+						ch.selNode = list.first();
+						break;
+					case "last":
+						ch.selNode = list.last();
+						break;
+					default: //"click"
+						ch.selNode = this;
 					}
 					//call onSelect function
 					var continueSelect = options.onSelect.call(ch.selNode);
 					//return if onSelect returns false
-					if (typeof continueSelect !== "undefined" && !continueSelect)
+					if (typeof continueSelect !== "undefined" && !continueSelect) {
 						return;
+					}
 					//mark as selected
 					ch.selNode.addClass(ch.options.selectedClass);
 					//set sStruct and sIndex values
@@ -238,27 +244,27 @@
 					ch.selNode.removeClass(ch.options.selectedClass);
 					var to;
 					switch (options.drop) {
-						case "first":
-							if (options.select === "first" || this === ch.selNode) {
-								to = list.first();
-								break;
-							}
-							to = list.addFirst().first();
+					case "first":
+						if (options.select === "first" || this === ch.selNode) {
+							to = list.first();
 							break;
-						case "last":
-							if (options.select === "last" || this === ch.selNode) {
-								to = list.last();
-								break;
-							}
-							to = list.addLast().last();
+						}
+						to = list.addFirst().first();
+						break;
+					case "last":
+						if (options.select === "last" || this === ch.selNode) {
+							to = list.last();
 							break;
-						default: //"click"
-							to = this;
+						}
+						to = list.addLast().last();
+						break;
+					default: //"click"
+						to = this;
 					}
 					if (to !== ch.selNode && this !== ch.selNode) {
 						//move/copy/swap within the list
 						valueEffect(ch, {
-							from: ch.selNode, 
+							from: ch.selNode,
 							to: to,
 							effect: options.effect
 						});
@@ -280,14 +286,14 @@
 					//move/copy/swap from an another structure
 					var to;
 					switch (options.drop) {
-						case "first":
-							to = list.addFirst().first();
-							break;
-						case "last":
-							to = list.addLast().last();
-							break;
-						default: //"click"
-							to = this;
+					case "first":
+						to = list.addFirst().first();
+						break;
+					case "last":
+						to = list.addLast().last();
+						break;
+					default: //"click"
+						to = this;
 					}
 					if (sIndex === -1) {
 						//from node
@@ -306,35 +312,34 @@
 						ch.getDs(sStruct).removeClass(sIndex, ch.options.selectedClass);
 						//move value
 						valueEffect(ch, {
-							from: ch.getDs(sStruct), 
-							fromIndex: sIndex, 
+							from: ch.getDs(sStruct),
+							fromIndex: sIndex,
 							to: to,
 							effect: options.effect
 						});
 						list.layout();
 					}
 					//call onDrop function
-						grade = options.onDrop.call(to);
-						if (typeof grade === "undefined") {
-							//set true if nothing was returned
-							grade = true;
-						} else {
-							//convert to boolean
-							grade = !!grade;
-						}					
+					grade = options.onDrop.call(to);
+					if (typeof grade === "undefined") {
+						//set true if nothing was returned
+						grade = true;
+					} else {
+						//convert to boolean
+						grade = !!grade;
+					}
 					//set sStruct and sIndex values					
 					sStruct = -1;
 					sIndex = -1;
 				}
-	            //move the values back to the JSAV variables
+				//move the values back to the JSAV variables
 				ch.selStruct.value(sStruct);
 				ch.selIndex.value(sIndex);
 				//grade if grade is true
-				if (options.gradeable && grade)
-					ch.exercise.gradeableStep();
+				if (options.gradeable && grade) {ch.exercise.gradeableStep(); }
 			});
 		}
-	}
+	};
 
 	/*
 	 * moves, copies or swaps the elements
@@ -344,21 +349,22 @@
 		var args = valueEffectArguments(options);
 
 		switch (options.effect) {
-			case "move":
-				ch.jsav.effects.moveValue.apply(this, args);
-				break;
-			case "copy":
-				ch.jsav.effects.copyValue.apply(this, args);
-				break;
-			case "swap":
-				ch.jsav.effects.swapValues.apply(this, args);
-				break;
-			case "toss":
-				//remove value from "from structure"
-				if (typeof options.fromIndex === "number")
-					//remove from array
-					ch.getDs(ch.selStruct.value()).value(ch.selIndex.value(), "");
-				break;
+		case "move":
+			ch.jsav.effects.moveValue.apply(this, args);
+			break;
+		case "copy":
+			ch.jsav.effects.copyValue.apply(this, args);
+			break;
+		case "swap":
+			ch.jsav.effects.swapValues.apply(this, args);
+			break;
+		case "toss":
+			//remove value from "from structure"
+			if (typeof options.fromIndex === "number") {
+				//remove from array
+				ch.getDs(ch.selStruct.value()).value(ch.selIndex.value(), "");
+			}
+			break;
 		}
 
 		if (ch.options.removeNodes &&
@@ -373,8 +379,9 @@
 				for (i = 0; i < list.size(); i++) {
 					if (list.get(i) === options.from) {
 						list.remove(i);
-						if (i !== 0)
+						if (i !== 0) {
 							options.from.hide();
+						}
 						break;
 					}
 				}
@@ -409,6 +416,6 @@
 	}
 
 	if (window) {
-    	window.ClickHandler = ClickHandler;
-  	}
+		window.ClickHandler = ClickHandler;
+	}
 }(jQuery));
