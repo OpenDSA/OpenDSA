420dded989c6e5558a2230a69299f6087e4cc9e8
junyangchen <junyang@vt.edu>
2013-08-14 04:12:49 +0800

    Change hint area text from 'Show solution' to 'Show hints'.

--- ODSAkhan-exercises-new/interface.js
+++ ODSAkhan-exercises-new/interface.js
@@ -486,8 +486,8 @@ function updateHintButtonText() {
 
     if (hintsAreFree) {
         $hintButton.val(hintsUsed ?
-                $._("Show next step (%(hintsLeft)s left)", {hintsLeft: hintsLeft}) :
-                $._("Show solution"));
+                $._("Show next hint (%(hintsLeft)s left)", {hintsLeft: hintsLeft}) :
+                $._("Show hints"));
     } else {
         $hintButton.val(hintsUsed ?
                 $.ngettext("I'd like another hint (1 hint left)",
