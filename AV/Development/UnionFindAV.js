334c6f73206d069deec0d5ca3180dad9c5880f7d
mfseddik <mfs2004_465@hotmail.com>
2013-09-11 00:49:01 -0400

    Setting up the layout

--- AV/Development/UnionFindAV.js
+++ AV/Development/UnionFindAV.js
@@ -3,66 +3,87 @@
 
 (function ($) {
   var jsav;
-  var tree;
-  var distances;
+  var trees;
+  var parents;
   var labels;
-  var arr;     //Used to initialize the distance and labels arrays.
+  var arr;     //Used to initialize the parents and labels arrays.
+  var graph;   //To hold the nodes and edges before running the union find
 
   function runit() {
     var i;
     ODSA.AV.reset(true);
     jsav = new JSAV($('.avcontainer'));
-    tree = jsav.ds.tree({width: 600, height: 400, layout: "manual", directed: true});
-    //initGraph();
-    //graph.layout();
-    arr = new Array(5);
+    trees = new Array(10);
+    arr = new Array(10);
+	//Initializing the parent pointers
     for (i = 0; i < arr.length; i++) {
-      arr[i] = Infinity;
+      arr[i] = null;
     }
-    parents = jsav.ds.array(arr, {layout: "vertical", left: 200, top: 20});
+	parents = jsav.ds.array(arr, {left: 300, top: 0});
+	//Initializing the labels
+	for(i = 0; i < 10; i++){
+	  arr[i] = String.fromCharCode(i + 65);
+	}
+	labels = jsav.ds.array(arr, {left: 300, top: 47});
+	
+	//Rendering the trees on the container
+	initTrees();
+	//Defining the graph
+	initGraph();
   }
-  // Mark a node in the graph.
-  function markIt(node) {
-    node.addClass("visited");
-    jsav.umsg("Add node " + node.value() + " to the MST");
-    distances.highlight(gnodes.indexOf(node));
-    labels.highlight(gnodes.indexOf(node));
-    node.highlight();
-    jsav.step();
+  function initTrees(){
+	var pixelOffset = 0;
+    for(var i = 0; i < 10; i++){
+	  if(i < 5){
+	    trees[i] = jsav.ds.tree({left:50 + pixelOffset,top:150});
+	    trees[i].newNode(labels.value(i));
+	    trees[i].show();
+		pixelOffset += 100;
+	  }
+	  else if(i === 5){
+	    pixelOffset = 0;
+	  }
+	  if (i >= 5){
+	    trees[i] = jsav.ds.tree({left:50 + pixelOffset,top:220});
+	    trees[i].newNode(labels.value(i));
+	    trees[i].show();
+		pixelOffset+= 100;
+	  }	  
+	}
+  }
+  function initGraph(){
+    graph = jsav.ds.graph();
+    var a = graph.addNode("A");
+    var b = graph.addNode("B");
+    var c = graph.addNode("C");
+    var d = graph.addNode("D");
+    var e = graph.addNode("E");
+    var f = graph.addNode("F");
+	var g = graph.addNode("G");
+	var h = graph.addNode("H");
+	var i = graph.addNode("I");
+	var j = graph.addNode("J");
+	
+	graph.addEdge(a, b);
+    graph.addEdge(a, h);
+    graph.addEdge(a, c);
+    graph.addEdge(b, h);
+    graph.addEdge(c, h);
+    graph.addEdge(d, f);
+    graph.addEdge(d, e);
+    graph.addEdge(e, f);
+	graph.addEdge(e, g);
+	graph.addEdge(f, g);
+	graph.addEdge(f, i);
+	graph.addEdge(h, e);
+	//Make the Graph invisible
+	graph.hide();
   }
 
   function about() {
-    var mystring = "Union Find Data Structure Visualization\nWritten by Mohammed Fawzy and Cliff Shaffer\nCreated as part of the OpenDSA hypertextbook project.\nFor more information, see http://algoviz.org/OpenDSA\nWritten during Spring, 2013\nLast update: March, 2013\nJSAV library version " + JSAV.version();
+    var mystring = "Union Find Data Structure Visualization\nWritten by Mohammed Fawzi and Cliff Shaffer\nCreated as part of the OpenDSA hypertextbook project.\nFor more information, see http://algoviz.org/OpenDSA\nWritten during Spring, 2013\nLast update: March, 2013\nJSAV library version " + JSAV.version();
     alert(mystring);
-  }
-
-  // Initialize the graph.
-  function initGraph() {
-
-    //Nodes of the original graph
-    var a = graph.addNode("A", {"left": 10, "top": 25});
-    var b = graph.addNode("B", {"left": 100, "top": 25});
-    var c = graph.addNode("C", {"left": 50, "top": 50});
-    var d = graph.addNode("D", {"left": 50, "top": 70});
-    var e = graph.addNode("E", {"left": 0, "top": 300});
-    var f = graph.addNode("F", {"left": 325, "top": 250});
-	var g = graph.addNode("G", {"left": 325, "top": 250});
-	var h = graph.addNode("H", {"left": 325, "top": 250});
-	var i = graph.addNode("I", {"left": 325, "top": 250});
-	var j = graph.addNode("J", {"left": 325, "top": 250});
-	
-    //Original graph edges
-    graph.addEdge(a, c);
-    graph.addEdge(a, e);
-    graph.addEdge(c, b);
-    graph.addEdge(c, d);
-    graph.addEdge(c, f);
-    graph.addEdge(f, b);
-    graph.addEdge(d, f);
-    graph.addEdge(e, f);
-
-    gnodes = graph.nodes();
-  }
+  }  
   // Connect action callbacks to the HTML entities
   $('#about').click(about);
   $('#runit').click(runit);
