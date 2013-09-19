bb514a1f686ec68e625426afb6b52c3d75294888
thomasw4 <thomasw4@vt.edu>
2013-09-19 18:26:39 -0400

    graphEditorFiles

--- AV/Development/graphEditor.js
+++ AV/Development/graphEditor.js
@@ -3,14 +3,21 @@
 	
 	var click = false;
 	var cll = isChecked();
-	jsav = new JSAV($('.avcontainer'));  
-	var g = jsav.ds.graph({width: 800, height: 500, layout: "manual", directed: cll});
+  var message = new JSAV($('.avcontainer'));
+	var jsav = new JSAV($('.avcontainer')); 
+  var jsav2 = new JSAV($('.avcontainer2'));
   
-  //jsav.umsg(
-	var i = 0; 
-	var weight = 10;
+  
+	var g = jsav.ds.graph({layout: "manual", directed: cll, height:"500px", width:"600px"});
+  var g2 = jsav.ds.graph();
+ // var g2 = jsav.ds.graph(); 
+  var size;  var i = 0; 
+	
+  
 	var graphNodes = new Array();
 	var preClick = new Array();
+  var nodeOne = null;
+  var nodeTwo = null;
 	var insideBox = true;
 	var addNodes = false;
 	var addEdges = false;
@@ -21,13 +28,13 @@
   var nextClick = true;
   var edges;
   var nodeNumber;
+   var moveNode;
   var switchState = true;
   var stateCount = 0;
   var stateCountTwo = 0;
   
     // Struct type container to hold id and x,y position of nodes   
-	function node(id, x, y) {
-    console.log("in node");
+	function node(id, x, y) {   
 		this.id = id;
 		this.x = x;
 		this.y = y;
@@ -38,68 +45,183 @@
     this.x = x;
 		this.y = y;
 	}
+  function showControls() {
+    $('#graphControls').show();
+    
+  }
+  ///////////////////////////////////////////////////////////////////////////////
+function runit() {
+  size = g.nodeCount();
+  
+  markIt(g.nodes()[0]);
+  dfs(g.nodes()[0]);
+ 
+  jsav.step(); 
+
+ // jsav.displayInit();
+  jsav.recorded();
+  
+}
+
+// Mark the nodes when visited and highlight it to 
+// show it has been marked
+function markIt(node) {
+  node.addClass("marked");
+  jsav.umsg("Mark node " + node.value());
+  node.highlight();
+  jsav.step();
+}
+
+
+function preVisit(node, prev) {
+  jsav.umsg("Add " + node.value() + " to the stack ");
+ // arr.css({"left": "600px", "bottom": "500px", "width": "30px"});
+  arr.value(size, node.value());
+  size--;
+  if (prev) {
+    node.edgeFrom(prev).css({"stroke-width":"4", "stroke":"red"}); // highlight
+    //prev.edgeFrom(node).css("stroke-width", "4" );
+  }
+  jsav.step();
+}
+
+function postVisit(node) {
+  jsav.umsg("Pop " + node.value() + " off of stack");
+  size++;
+  arr.value(size, " ");
+  jsav.step();
+  
+}
+
+// Recursive depth first search algorithmn for searching
+// the graph
+function dfs(start, prev) {
+  var adjacent;
+  var next;
+  preVisit(start, prev);
+  adjacent = start.neighbors();
+
+
+  for (next = adjacent.next(); next; next = adjacent.next()) {
+    jsav.umsg("Process (" + start.value() + "," + next.value() + ")");
+      if(next.hasClass("marked")) {
+        jsav.umsg("Node " + next.value() + " already marked");
+      }
+  
+    jsav.step();
+    if (!next.hasClass("marked")) {
+      jsav.umsg("Print (" + start.value() + "," + next.value() + ") and call depth first search on " + next.value());
+      jsav.step();
+      markIt(next);
+      dfs(next, start);
+      jsav.step();
+    }
+  }
+  postVisit(start);
+}
+  
+  
+  
+  
+  
+  /////////////////////////////////////////////////////////////////////////
 	
-	function getGraphNodeOne(graphNodes, preClick, x, y) {
+  function done() {
+  console.log("DONE");
+  $('#graphControls').toggle();
+  g2 = g;
+  g.clear();
+   console.log("done " + g.nodes()[0].value());
+   message.clearumsg();
+ // $('.avcontainer').clear();  
+  
+   
+    
+    }
+    
+    
+    function reshow() {
+    
+    console.log("reshow " + g2.nodes()[0].value());
+   
+   
+    g2.layout();
+     jsav.displayInit();
+    
+    
+    
+        
+      
+    }
+ 
+	function getGraphNodeOne(x, y) {
 		var	clickedX = preClick[0].x;
 		var clickedY = preClick[0].y;
 		for(var i = 0; i < graphNodes.length; i++) {
 			realX = graphNodes[i].x;
 			realY = graphNodes[i].y;
 				if(realX  + 25 >= clickedX && realX - 25 <= clickedX && realY + 25 >= clickedY && realY - 25 <= clickedY) {
-		    		//console.log("X: " + realX + "  Y: " + realY);
-					//g.nodes()[graphNodes[i].id].highlight();
-					//alert("now I am in node one");
-				
-					return graphNodes[i].id;
+          return graphNodes[i].id;
 				}
 		}
 		return null;
 	}
 	
 	
-    function getGraphNodeTwo(graphNodes, x, y) {
+  // function getGraphNode(x, y) {
+		// var realX;
+		// var realY;
+		// for(var i = 0; i < graphNodes.length; i++) {
+			// realX = graphNodes[i].x;
+			// realY = graphNodes[i].y;
+			
+				// if(realX + 25 >= x && realX - 25 <= x && realY + 25 >= y && realY - 25 <= y) {		    
+					// return graphNodes[i].id;
+				// }
+		// }
+		// return null;
+		
+  // }
+  
+  
+  function getGraphNode(x, y) {
 		var realX;
 		var realY;
-		for(var i = 0; i < graphNodes.length; i++) {
-			realX = graphNodes[i].x;
-			realY = graphNodes[i].y;
+		for(var i = 0; i < g.nodes().length; i++) {
+			realX = g.nodes()[i].options.left;
+			realY = g.nodes()[i].options.top;
 			
-				if(realX + 25 >= x && realX - 25 <= x && realY + 25 >= y && realY - 25 <= y) {
-		    		//alert("now I am in node one");
-					//g.nodes()[graphNodes[i].id].highlight();
-          console.log("GET GRAPH NODE:   X: " + x + "  Y: " + y);
-          console.log("GET GRAPH NODE:   realX: " + realX + "  realY: " + realY);
-					return graphNodes[i].id;
+				if(realX + 25 >= x && realX - 25 <= x && realY + 25 >= y && realY - 25 <= y) {		    
+					return g.nodes()[i];
 				}
 		}
 		return null;
 		
-    }
+  }
     
 	
 	function isChecked(){
-   var checkbox = document.getElementById('chkbox');
-     if (checkbox.checked) {
-       return true;
-     } else { 
-       return false;
-     }
-   
+   return document.getElementById('#chkbox');
+  // var checkbox = document.getElementById('chkbox');
+   // console.log(checkbox.checked);
+     // if (checkbox.checked) {
+       // return true;
+     // } else { 
+       // return false;
+     // }   
 	}
 	
 	function addNodeMode() {
-   
-      addNodes = true;
-      addEdges = false;
-      addWeights = false;
-      move = false;
-      console.log("Inside add node mode()");
-      jsav.umsg("You are currently in add node mode");
     
-	}
-	
+    addNodes = true;
+    addEdges = false;
+    addWeights = false;
+    move = false;    
+    message.umsg("You are currently in add node mode");
+    
+	}	
 	function addEdgeMode() {
-   console.log(stateCount);
+   
    if(stateCount <= 0) {
       
       addEdges = true;
@@ -108,8 +230,8 @@
       move = false;
       stateCountTwo++;
       stateCount--;
-      console.log("Inside add edge mode()");
-      jsav.umsg("You are currently in add edge mode");
+  
+      message.umsg("You are currently in add edge mode");
    }
    else {
       alert("Can not change from add edges to add weighte edges");
@@ -119,28 +241,26 @@
 	
 	function addWeightedEdgeMode() {
    
-   if(stateCountTwo<=0) {
-    
-    addWeights = true;
-		addEdges = false;
-		addNodes = false;
-		move = false;
-    stateCount++;
-    stateCountTwo--;
-    console.log("Inside add weighted edge mode()");
-		jsav.umsg("You are currently in add weighted edge mode.  Click on two nodes to add the weighted edge.");
-  }else {
-     alert("Can not change from add weight edges to add edges");
-	}
+    if(stateCountTwo<=0) {    
+      addWeights = true;
+		  addEdges = false;
+		  addNodes = false;
+		  move = false;
+      stateCount++;
+      stateCountTwo--;
+     
+		  message.umsg("You are currently in add weighted edge mode.  Click on two nodes to add the weighted edge.");
+    }else {
+      alert("Can not change from add weight edges to add edges");
+	  }
   }
   
   function moveNodeMode() {
     move = true;
     addEdges = false;
 		addNodes = false;
-		addWeights = false;
-    console.log("Inside move node mode()" + move);
-		jsav.umsg("You are currently in move node mode.  Click on a node and then click on new position.");
+		addWeights = false;    
+		message.umsg("You are currently in move node mode.  Click on a node and then click on new position.");
   }
     
 	
@@ -158,193 +278,108 @@
 	
  // START MAKING CHANGES HERE... ATTEMPT TO CAPTURE CLICK AND DRAW A NODE.  
   
-	jQuery(document).ready(function(){$('.jsavcanvas').click(function(e)
+	jQuery(document).ready(function(){$('.avcontainer').click(function(e)
 	{
-  
-    
-      
-  
- // function test() {
- 
-    // var self = $(this);
-    // var dx = e.pageX;
-    // var dy = e.pageY;
 
-    // var x = dx - self.offset().left ;
-    // var y = dy - self.offset().top ;
-		
-			var x = e.pageX - 86;       //x click coordinate
-			var y = e.pageY - 195; 		// y click coordinate
-			console.log("moveNode in clicker " + move);
-			//if click is outside of the box that contains the graph
-			// if(y < 250) {
-				
-				// alert("You can only add nodes inside the blue box");
-				// insideBox = !insideBox;
-			// }
-      
-      
-        
-			/* Add the node to the canvas and store the id (i) and coordinates (x and y) in an array. */
- 			if(insideBox && addNodes) {
-        console.log("I should add node to screen here");
-				//g.addNode(i, {"left": x - 88, "top": y - 180});
-        g.addNode(i, {"left": x, "top": y});
-				graphNodes[i] = new node(i, x, y);
-        console.log("i " + i);
-				i++;
+  // g.mouseenter(function() { this.highlight(); })
+    // .mouseleave(function() { this.unhighlight(); });
+   console.log('Nodes  ' + g.nodes());
+		var x = e.pageX - 40;       //x click coordinate 150
+		var y = e.pageY - 210; 		// y click coordinate   190
+				/* Add the node to the canvas and store the id (i) and coordinates (x and y) in an array. */
+ 			if(addNodes) {
+        g.addNode(i, {"left": x, "top": y}); 
+        g.layout();
+       	i++;
 			}
       /*Move does not work correctly now -- disconnected handler */
-      if(move) {
-      console.log("in moveNode");
+      if(move) {     
         if(!click) {
-          console.log("moveNodeMode and !nextClick");
-          
-          var moveNode = getGraphNodeTwo(graphNodes, x, y);
-         // edges = g.nodes()[moveNode].edges();
-       //  (g.nodes()[moveNode]).hide();
-         
-         var xX=graphNodes[moveNode].x;
-         var yY=graphNodes[moveNode].y;
-         console.log("x " + x + "  y " + y);
-         console.log("xX " + xX + "   yY " + yY);
-         jsav.g.circle(xX + 68, yY + 23, 24);
-       //  jsav.displayInit();
-         // g.removeNode(g.nodes()[moveNode]);
-          nodeNumber = moveNode;
+          moveNode = getGraphNode(x, y);
+          console.log("moving node " +  moveNode.options.left);
           click = !click;
-        } else {
-          console.log("move node mode  and nextclick");
-          g.addNode(nodeNumber, {"left": x, "top": y});
-          console.log(graphNodes[nodeNumber].x);
-         
-          
+        }else{
+          var temp = moveNode.value();
+       //   console.log("move " + temp.left);
+          g.removeNode(moveNode);
+          g.layout();
+          moveNode.options.left = x;
+          moveNode.options.top = y;
+          g.addNode(temp, {"left": x, "top": y}); 
+          console.log('x : ' + x + '  y: ' + y);
+          test = getGraphNode(x, y);
+          console.log("coords of moved x:" + test.options.left + '   y:   ' + test.options.top);
+        
           click = !click;
+         // g.layout();
         }
       }
-			
       
 			/* Add Edges */
 			if(addEdges && g.nodeCount() > 1) {
-				if(nodeOneHigh!=null && nodeTwoHigh!=null) {
-					g.nodes()[nodeOneHigh].unhighlight();
-					g.nodes()[nodeTwoHigh].unhighlight();
-				}
-				
-				
-			
-				//Need to store a click location to identify the first node selected
-				if(!click){
-				   	preClick[0] = new storeClick(x,y);
-          /*Highlight broken  
-					var nodeOne = getGraphNodeOne(graphNodes, preClick, x, y);
-					nodeOneHigh = nodeOne;
-					console.log(nodeOneHigh);**/
-					
-          
-					click = !click;
+				if(!click){        
+          nodeOne = getGraphNode(x,y);
+         	click = !click;
 				}
 				// First node has been selected, now retrieve second selected node.
 				else if(click) {
-					var nodeOne = getGraphNodeOne(graphNodes, preClick, x, y);
-					//nodeOneHigh = nodeOne;
-					//g.nodes()[nodeOne].highlight();
-					var nodeTwo = getGraphNodeTwo(graphNodes, x, y);
-				//	nodeTwoHigh = nodeTwo;
-				//	g.nodes()[nodeTwo].highlight();
-					
-					//validateNodeClick(nodeOne, nodeTwo);
-					
-					//g.nodes()[nodeOne].highlight();
+					nodeTwo = getGraphNode(x, y);
+          
 					if(nodeOne === null || nodeTwo === null) {
-						alert("Must click on a node -- Try again");
-						click = !click;
-						
+            alert("Must click on a node -- Try again");
+            click = !click;
 					}
 					//Also need to check if there is already an edge connecting two nodes
 					
-					if (g.nodes()[nodeOne].value() === g.nodes()[nodeTwo].value()) {
+					if (nodeOne.value() === nodeTwo.value()) {
 						jsav.umsg("Can not connect a node to itself");
 						click = !click;
 					}
-					else if (g.hasEdge(g.nodes()[nodeOne],g.nodes()[nodeTwo])) {
-						alert("Graph already has a edge between " + g.nodes()[nodeOne].value() +
-							" and " + g.nodes()[nodeTwo].value());
+					else if (g.hasEdge(nodeOne,nodeTwo)) {
+						alert("Graph already has a edge between " + nodeOne.value() +
+							" and " + nodeTwo.value());
 							click = !click;
-					}
-					
+					}					
 					else {
-						g.addEdge(g.nodes()[nodeOne],g.nodes()[nodeTwo]);
-						
-						jsav.umsg("Connected node " + g.nodes()[nodeOne].value() + " to node " + g.nodes()[nodeTwo].value());
-						g.layout();
-						click = !click;
-					
+           	g.addEdge(nodeOne,nodeTwo);
+          	jsav.umsg("Connected node " + nodeOne.value() + " to node " + nodeTwo.value());		
+            g.layout();
+						click = !click;					
 					}
-				}
-				else {
-					preClick[0] = new storeClick(x,y);
-					click = !click;
-				}
+				}				
 			}		
       
 			if(addWeights && g.nodeCount() > 1) {
         if(!click){
-				  preClick[0] = new storeClick(x,y);
-				//	var nodeOne = getGraphNodeOne(graphNodes, preClick, x, y);
-				//	nodeOneHigh = nodeOne;
-				//	console.log(nodeOneHigh);
-					//g.nodes()[nodeOne].highlight();
+          nodeOne = getGraphNode(x, y);
 					click = !click;
-				}
-				
-				else if(click) {
-					var nodeOne = getGraphNodeOne(graphNodes, preClick, x, y);
-				//	nodeOneHigh = nodeOne;
-					//g.nodes()[nodeOne].highlight();
-					var nodeTwo = getGraphNodeTwo(graphNodes, x, y);
-				//	nodeTwoHigh = nodeTwo;
-				//	g.nodes()[nodeTwo].highlight();
-					
-					//validateNodeClick(nodeOne, nodeTwo);
-					
-					//g.nodes()[nodeOne].highlight();
+				}        
+				else if(click) {		
+					nodeTwo = getGraphNode(x, y);
 					if(nodeOne === null || nodeTwo === null) {
 						alert("Must click on a node -- Try again");
-						click = !click;
-						
+						click = !click;						
 					}
-					//Also need to check if there is already an edge connecting two nodes
-					
-					if (g.nodes()[nodeOne].value() === g.nodes()[nodeTwo].value()) {
+					//Also need to check if there is already an edge connecting two nodes					
+					if (nodeOne === nodeTwo) {
 						jsav.umsg("Can not connect a node to itself");
 						click = !click;
 					}
-					else if (g.hasEdge(g.nodes()[nodeOne],g.nodes()[nodeTwo])) {
-						alert("Graph already has a edge between " + g.nodes()[nodeOne].value() +
-							" and " + g.nodes()[nodeTwo].value());
+					else if (g.hasEdge(nodeOne,nodeTwo)) {
+						alert("Graph already has a edge between " + nodeOne.value() +
+							" and " + nodeTwo.value());
 							click = !click;
-					}
-					
+					}					
 					else {
             var weight = window.prompt("Input weight of edge","");
-						//var edge = g.getEdge(g.nodes()[nodeOne], g.nodes()[nodeTwo]);
-						//edge.weight(weight);
-						// jsav.displayInit();
-						// click = !click;
-						g.addEdge(g.nodes()[nodeOne],g.nodes()[nodeTwo],{"weight": weight});
-					//	jsav.container.trigger("jsav-updaterelative");
-						jsav.umsg("Connected node " + g.nodes()[nodeOne].value() + " to node " + g.nodes()[nodeTwo].value());
+						g.addEdge(nodeOne,nodeTwo,{"weight": weight});					
+						jsav.umsg("Connected node " + nodeOne.value() + " to node " + nodeTwo.value());
             g.layout();
-						jsav.displayInit();
-						click = !click;
-					
+					//	jsav.displayInit();
+						click = !click;					
 					}
 				}
-				else {
-					preClick[0] = new storeClick(x,y);
-					click = !click;
-				}
+				
       
       
 				// if(!click){
@@ -357,7 +392,7 @@
 				// }else  {
 					// console.log("in weights: ");
 					// var nodeOne = getGraphNodeOne(graphNodes, preClick, x, y);
-					// var nodeTwo = getGraphNodeTwo(graphNodes, x, y);
+					// var nodeTwo = getGraphNode(graphNodes, x, y);
 					// validateNodeClick(nodeOne, nodeTwo);
 					
 					// if(!g.hasEdge(g.nodes()[nodeOne], g.nodes()[nodeTwo])) {
@@ -372,13 +407,12 @@
 					// }
 				// }
 			}
-			if(!insideBox){
-				insideBox=!insideBox;
-			}
-			
+	  
       
   }
+
                                                                     )
+                                                                    
                                   }
 	//}
 		
@@ -386,12 +420,16 @@
     //}
 //	)
 // Connect action callbacks to the HTML entities
-$('#about').click(about);
-$('#help').click(help);
+
 $('#addNodeMode').click(addNodeMode);
 $('#addEdgeMode').click(addEdgeMode);
 $('#addWeightedEdgeMode').click(addWeightedEdgeMode);
 $('#moveNodeMode').click(moveNodeMode);
+$('#createGraph').click(showControls);
+$('#reshowGraph').click(reshow);
+
+$('#done').click(done);
+
 
 
 
