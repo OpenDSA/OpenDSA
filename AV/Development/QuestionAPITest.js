9d26d1fc4de87e357ab0beb9fedd28ad35f11b69
Ville Karavirta <vkaravir@gmail.com>
2013-11-07 18:05:35 +0200

    Updated the question API example

--- AV/Development/QuestionAPITest.js
+++ AV/Development/QuestionAPITest.js
@@ -7,20 +7,32 @@
   function runit() {
     var i;
     jsav = new JSAV($('.avcontainer'));
-	jsav.umsg('In slide 1');
-	jsav.step();
-	jsav.umsg('In slide 2');
-	var q = jsav.question("MS", "Life is good?");
-    q.addChoice("Of course", {correct: true});
-    q.addChoice("Certainly", {correct: true});
-    q.addChoice("No way!");
-    q.show();
-	jsav.step();
-	jsav.umsg('In slide 3');
-	jsav.step();
-	jsav.recorded();
+  	jsav.umsg('In slide 1');
+    jsav.displayInit();
+    jsav.umsg('In slide 2, multiple-select question');
+    var q = jsav.question("MS", "Life is good?")
+                .addChoice("Of course", {correct: true})
+                .addChoice("Certainly", {correct: true})
+                .addChoice("No way!")
+                .show();
+    jsav.step();
+    jsav.umsg('In slide 3, true/false question');
+    var q2 = jsav.question("TF", "True or false?", {correct: true})
+                 .show();
+    jsav.step();
+    jsav.umsg('In slide 4, multiple-choice question');
+    var q3 = jsav.question("MC", "Yes or No?")
+                 .addChoice("Yes", {correct: true})
+                 .addChoice("No", {correct: false})
+                 .show();
+    jsav.step();
+    jsav.umsg('In slide 5, iframe question');
+    jsav.question("IFRAME", "../../Exercises/Background/IntroSumm.html", {height: "800", title: "Answer me this:"})
+        .show();
+
+    jsav.recorded();
   }
-  
+
   // Connect action callbacks to the HTML entities
   $('#about').click(about);
   $('#runit').click(runit);
