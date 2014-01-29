7f20d15c75c408621d297c6a5dd244cf7025bf3c
samanthafisher <samf22@vt.edu>
2014-01-29 14:15:53 -0500

    Figure 13.2.1 is not a jsav diagram

---
+++ AV/Development/dynamicCON.js
@@ -0,0 +1,16 @@
+"use strict";
+
+(function ($) {
+  var av = new JSAV("freelistCON", {"animationMode": "none"});
+  var l = av.g.line(0, 0, 470, 0);
+  var l2 = av.g.line(0, 75, 470, 75);
+  
+  var rect1 = av.g.rect(20, 0, 60, 75).css({"fill": "lightgray"});
+  var rect2 = av.g.rect(80, 0, 70, 75).css({"fill": "white"});
+  var rect3 = av.g.rect(150, 0, 100, 75).css({"fill": "lightgray"}); 
+  var rect4 = av.g.rect(250, 0, 50, 75).css({"fill": "white"});
+  var rect5 = av.g.rect(300, 0, 80, 75).css({"fill": "lightgray"});
+  var rect6 = av.g.rect(380, 0, 40, 75).css({"fill": "white"});
+  var rect7 = av.g.rect(420, 0, 30, 75).css({"fill": "lightgray"});
+  
+  }(jQuery));
