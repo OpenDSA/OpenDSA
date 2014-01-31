73cb241b3ee76690f80a8e2da01594a79d4aebb2
Elmer Landaverde <elmer@vt.edu>
2014-01-31 09:36:16 -0500

    Added comments to the 2-3 tree css and js files

--- AV/Development/twoThreeTreeCON.js
+++ AV/Development/twoThreeTreeCON.js
@@ -36,8 +36,8 @@
 	"use strict";
 	var jsav = new JSAV("simpleInsertCON");
 
+	/* 1st Slide *************************************************************/
 	jsav.umsg("Simple insert into the 2-3 tree.");
-
 	// Create all the arrays that represent the nodes in the 2-3 tree.
 	var arr1 = jsav.ds.array([18, 13], {left: "240px", top: "0px"});
 	var arr2 = jsav.ds.array([12, ""], {left: "40px", top: "80px"});
@@ -51,7 +51,6 @@
 	var arr10 = jsav.ds.array([45, 47], {left: "400px", top: "160px"});
 	var arr11 = jsav.ds.array([50, 52], {left: "480px", top: "160px"});
 	var arr12 = jsav.ds.array([14], {left: "180px", top: "0px", visible: false});
-
 	// Create lines that connect all the nodes.
 	var properties = {"stroke-width": 1.5};
 	jsav.g.line(243, 28, 73, 81, properties);
@@ -64,34 +63,40 @@
 	jsav.g.line(304, 110, 354, 160, properties);
 	jsav.g.line(437, 110, 434, 160, properties);
 	jsav.g.line(468, 111, 514, 160, properties);
-
+	// set initial display for first slide.
 	jsav.displayInit();
 
+	/* 2nd Slide *************************************************************/
 	jsav.umsg("The value 14 is inserted into the tree. The value is first compared agains the root node.\nSince 14 is less than the left value of the root node it follows the left child node.");
 	arr12.show();
 	arr1.highlight(0);
 	jsav.step();
 
+	/* 3rd Slide *************************************************************/
 	jsav.umsg("This node has only one element, and 14 is greater than 12 so the center child is followed next.");
 	arr1.unhighlight(0);
 	arr12.css({left: "40px", top: "40px"});
 	arr2.highlight(0);
 	jsav.step();
 
+	/* 4th Slide *************************************************************/
 	jsav.umsg("A leaf node has being reached. Since the leaf node has an empty space the new node can be inserted here.");
 	arr2.unhighlight(0);
 	arr12.css({left: "80px", top: "200px"});
 	arr6.highlight(0);
 	jsav.step();
 
+	/* 5th Slide *************************************************************/
 	jsav.umsg("The new key 14 is less than 15 so the key 15 has to be moved to the right to make room for the new key.");
 	arr6.unhighlight(0);
 	arr6.swap(0, 1, {arrow: false});
 	jsav.step();
 
+	/* 6th Slide *************************************************************/
 	jsav.umsg("Finally the key 14 is inserted into the 2-3 tree.");
 	arr6.value(0, 14);
 	arr12.hide();
+	// Mark the slide show as finished.
 	jsav.recorded();
 
 }(jQuery));
