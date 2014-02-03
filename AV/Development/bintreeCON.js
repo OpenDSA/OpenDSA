7a530c29692d0988a2e32e34bcee64f116a4877d
Anthony Rinaldi <rinaldi1@vt.edu>
2014-02-03 00:35:50 -0500

    Continued progress on bintree example

---
+++ AV/Development/bintreeCON.js
@@ -0,0 +1,10 @@
+"use strict";
+
+(function ($) {
+  var av = new JSAV("bintreeCONBTEX", {"animationMode": "none"});
+
+  av.label("Hello World! ~Anthony Rinaldi", {visible: true}).show;
+  //av.umsg("Hello World! ~Anthony Rinaldi", {"color": "black"});
+
+
+}(jQuery));
