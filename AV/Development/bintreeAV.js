c5caade0dc89c9c85a13d69fed9e0e8b609a1ff5
Anthony Rinaldi <rinaldi1@vt.edu>
2014-02-19 12:13:01 -0500

    Worked on Bintree visualization and static images

--- AV/Development/bintreeAV.js
+++ AV/Development/bintreeAV.js
@@ -1,3 +1,80 @@
+"use strict";
+
+  var av = new JSAV("bintreeAV", {"animationMode": "none"});
+
+  JSAV.umsg("This is a test...");
+
+// Setup the tree
+  var bt = av.ds.bintree();
+  bt.root('root');
+  var rt = bt.root();
+
+  rt.left('');
+  rt.left().left('A');
+  rt.left().right('B');
+
+  // Process About button: Pop up a message with an Alert
+function about() {
+  var mystring = "Bintree Visualization\nJSAV library version " + JSAV.version();
+  alert(mystring);
+}
+
+(function($) {
+  var context = $("#ssperform");
+
+  // create a new settings panel and specify the link to show it
+  var settings = new JSAV.utils.Settings($(".jsavsettings"));
+  // Connect action callbacks to the HTML entities
+}(jQuery));
+
+/* (function($) {
+  var comp = function(a, b) {
+    return a - b;
+  };
+
+  JSAV._types.ds.BinTree.prototype.insert = function(value) {
+    // helper function to recursively insert
+    var ins = function(node, insval) 
+    {
+      var val = node.value();
+      if (!val || val === "jsavnull") 
+      { // no value in node
+        node.value(insval);
+      } 
+      else if (comp(val, insval) > 0) 
+      { // go left
+        if (node.left()) {
+          ins(node.left(), insval);
+        } else {
+          node.left(insval);
+        }
+      } 
+      else 
+      { // go right
+        if (node.right()) 
+        {
+          ins(node.right(), insval);
+        } else 
+        {
+          node.right(insval);
+        }
+      }
+    };
+    if ($.isArray(value)) 
+    { // array of values
+      for (var i = 0, l = value.length; i < l; i++) 
+      {
+        ins(this.root(), value[i]);
+      }
+    } 
+    else 
+    {
+      ins(this.root(), value);
+    }
+    return this;
+  };
+}*/
+
 /*
 "use strict";
 
