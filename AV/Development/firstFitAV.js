e955deda0aa540005b5392d2c172f9c6b7dec078
trsims7 <trsims7@vt.edu>
2014-03-02 14:58:43 -0500

    Attempted to get visualization to appear

--- AV/Development/firstFitAV.js
+++ AV/Development/firstFitAV.js
@@ -1,9 +1,9 @@
 (function ($) {
   "use strict";
   var jsav,              // JSAV
-      arr,				// for the JSAV array
-   	  defCtrlState, 	// Stores the default state of the controls
-   	  nextStep = new Queue();
+      arr,        // for the JSAV array
+      defCtrlState,   // Stores the default state of the controls
+      nextStep = new Queue();
 
 //  jsav = new JSAV($('.avcontainer'));
  jsav = new JSAV($('.avcontainer'));
@@ -65,13 +65,13 @@
   function setDefaultControlState() {
     defCtrlState = {};
     defCtrlState.fitAlgorithm = 0;
-	
+  
     var params = JSAV.utils.getQueryParameter();
 
     if(params.fitAlgorithm) {
       if(params.fitAlgorithm > 0 && params.fitAlgorithm <= 5) {
-	    defCtrlState.fitAlgorithm = params.fitAlgorithm;
-	  }
+      defCtrlState.fitAlgorithm = params.fitAlgorithm;
+    }
     }
   }
 
@@ -161,88 +161,88 @@
 
   function OriginalMemBlock() {
   
-	  var used1 = jsav.g.rect(85, 200, 30, 80).css({"fill": "red"});
-	  var used2 = jsav.g.rect(220, 200, 75, 80).css({"fill": "red"});
-	  var used3 = jsav.g.rect(391, 200, 54, 80).css({"fill": "red"});
-	  var used4 = jsav.g.rect(580, 200, 30, 80).css({"fill": "red"});
-	  
-	  var free1 = jsav.g.rect(10, 200, 75, 80).css({"fill": "blue"});
-	  var free2 = jsav.g.rect(115, 200, 105, 80).css({"fill": "blue"});
-	  var free3 = jsav.g.rect(295, 200, 96, 80).css({"fill": "blue"});
-	  var free4 = jsav.g.rect(445, 200, 135, 80).css({"fill": "blue"});
-	  
-	  var free1Start = 10;
-	  var free2Start = 115;
-	  var free3Start = 295;
-	  var free4Start = 445;
-	  
-	  var freeStartArray = new Array();
-	  freeStartArray[0] = free1Start;
-	  freeStartArray[1] = free2Start;
-	  freeStartArray[2] = free3Start;
-	  freeStartArray[3] = free4Start;
-	  
-	  var free1Finish = 85;
-	  var free2Finish = 220;
-	  var free3Finish = 391;
-	  var free4Finish = 580;
-	  
-	  var freeFinArray = new Array();
-	  freeFinArray[0] = free1Finish;
-	  freeFinArray[1] = free2Finish;
-	  freeFinArray[2] = free3Finish;
-	  freeFinArray[3] = free4Finish;
-	  
-	  
-	  
-	  var usedRec = jsav.g.rect(430, 50, 50, 50).css({"fill": "red"});
-	  var freeRec = jsav.g.rect(510, 50, 50, 50).css({"fill": "blue"});
-	  
-	  var usedLabel = jsav.label("Used Space", {left :  430, top:  105});
-	  var freeLabel = jsav.label("Free Space", {left :  510, top:  105});
-	  
-	  var usedNum = 63;
-	  var freeNum = 137;
-	  
-	  var usedAmountLabel = jsav.label(usedNum, {left :  450, top:  65});
-	  var freeAmountLabel = jsav.label(freeNum, {left :  530, top:  65});
-	  
-	  
-	  
-	   
-	  
-	  var freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "lightgrey"});
-	  var freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "lightgrey"});
-	  var freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "lightgrey"});
-	  var freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "lightgrey"});
-	  
-	  
-	  var block1 = 25;
-	  var block2 = 35;
-	  var block3 = 32;
-	  var block4 = 45;
-	 
-	  var freeArray = new Array();
-	  freeArray[0] = block1;
-	  freeArray[1] = block2;
-	  freeArray[2] = block3;
-	  freeArray[3] = block4;
-	 
-	  var block1Label = jsav.label(block1, {left :  22, top:  420});
-	  var block2Label= jsav.label(block2, {left :  47, top:  420});
-	  var block3Label = jsav.label(block3, {left :  72, top:  420});
-	  var block4Label = jsav.label(block4, {left :  97, top:  420});
-	  var freeLabel = jsav.label("Free List", {left : 15, top: 455});
-	 
-	  var connect1 = jsav.g.line(22.5, 400, 47.5, 280);
-	  var connect2 = jsav.g.line(47.5, 400, 167.5, 280);
-	  var connect3 = jsav.g.line(72.5, 400, 343, 280);
-	  var connect4 = jsav.g.line(97.5, 400, 512.5, 280);
+    var used1 = jsav.g.rect(85, 200, 30, 80).css({"fill": "red"});
+    var used2 = jsav.g.rect(220, 200, 75, 80).css({"fill": "red"});
+    var used3 = jsav.g.rect(391, 200, 54, 80).css({"fill": "red"});
+    var used4 = jsav.g.rect(580, 200, 30, 80).css({"fill": "red"});
+    
+    var free1 = jsav.g.rect(10, 200, 75, 80).css({"fill": "blue"});
+    var free2 = jsav.g.rect(115, 200, 105, 80).css({"fill": "blue"});
+    var free3 = jsav.g.rect(295, 200, 96, 80).css({"fill": "blue"});
+    var free4 = jsav.g.rect(445, 200, 135, 80).css({"fill": "blue"});
+    
+    var free1Start = 10;
+    var free2Start = 115;
+    var free3Start = 295;
+    var free4Start = 445;
+    
+    var freeStartArray = new Array();
+    freeStartArray[0] = free1Start;
+    freeStartArray[1] = free2Start;
+    freeStartArray[2] = free3Start;
+    freeStartArray[3] = free4Start;
+    
+    var free1Finish = 85;
+    var free2Finish = 220;
+    var free3Finish = 391;
+    var free4Finish = 580;
+    
+    var freeFinArray = new Array();
+    freeFinArray[0] = free1Finish;
+    freeFinArray[1] = free2Finish;
+    freeFinArray[2] = free3Finish;
+    freeFinArray[3] = free4Finish;
+    
+    
+    
+    var usedRec = jsav.g.rect(430, 50, 50, 50).css({"fill": "red"});
+    var freeRec = jsav.g.rect(510, 50, 50, 50).css({"fill": "blue"});
+    
+    var usedLabel = jsav.label("Used Space", {left :  430, top:  105});
+    var freeLabel = jsav.label("Free Space", {left :  510, top:  105});
+    
+    var usedNum = 63;
+    var freeNum = 137;
+    
+    var usedAmountLabel = jsav.label(usedNum, {left :  450, top:  65});
+    var freeAmountLabel = jsav.label(freeNum, {left :  530, top:  65});
+    
+    
+    
+     
+    
+    var freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "lightgrey"});
+    var freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "lightgrey"});
+    var freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "lightgrey"});
+    var freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "lightgrey"});
+    
+    
+    var block1 = 25;
+    var block2 = 35;
+    var block3 = 32;
+    var block4 = 45;
+   
+    var freeArray = new Array();
+    freeArray[0] = block1;
+    freeArray[1] = block2;
+    freeArray[2] = block3;
+    freeArray[3] = block4;
+   
+    var block1Label = jsav.label(block1, {left :  22, top:  420});
+    var block2Label= jsav.label(block2, {left :  47, top:  420});
+    var block3Label = jsav.label(block3, {left :  72, top:  420});
+    var block4Label = jsav.label(block4, {left :  97, top:  420});
+    var freeLabel = jsav.label("Free List", {left : 15, top: 455});
+   
+    var connect1 = jsav.g.line(22.5, 400, 47.5, 280);
+    var connect2 = jsav.g.line(47.5, 400, 167.5, 280);
+    var connect3 = jsav.g.line(72.5, 400, 343, 280);
+    var connect4 = jsav.g.line(97.5, 400, 512.5, 280);
   }
  
   function newRec(startX, sizeX)
   {
-	var newRec = av.g.rect(stratX, 50, sizeX, 50).css({"fill": "red"});
+  var newRec = av.g.rect(stratX, 50, sizeX, 50).css({"fill": "red"});
   }
  
   function updadateLabels()
@@ -264,85 +264,86 @@
   }
  
   function firstFit(inputVal) {
-	
-	//var input = params;
-	if (inputVal < 0 || inputVal > 99999 || isNaN(inputVal)) {
+  
+  //var input = params;
+  if (inputVal < 0 || inputVal > 99999 || isNaN(inputVal)) {
       error("Please enter a number in the range of 0-99999");
       // Return error
       return 1;
-	else
-	{
-		jsav.umsg("The request has been scheduled\n Algorithm: First Fit\n Size Required: " + input + "\n");
-		var rec2 = jsav.g.rect(10, 300, input * 3, 80).css({"fill": "lightblue"}); //creates lightblue rectangle
-		jsav.step();
-		rec2 = jsav.g.rect(0, 0, 0, 0).css({"fill": "white"}); //deletes lightblue rectangle after step
-		var stop = 0;
-		var i = 0;
-		for(i = 0; i <= 4; i++)
-		while(i <= 4 && stop != 1)
-		{ 
-			if(i == 0)
-			{
-				connect1 = jsav.g.line(22.5, 400, 47.5, 280, {'stroke-width' : 3}); //sets line 1 to bold
-				freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "yellow"}); //sets 1 to yellow
-			}
-			else if(i == 1)
-			{
-				connect2 = jsav.g.line(47.5, 400, 167.5, 280, {'stroke-width' : 3});
-				connect1 = jsav.g.line(22.5, 400, 47.5, 280); //sets line 1 back to original color
-				freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "lightgrey"}); //sets 1 back to grey
-				freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "yellow"}); //sets 2 to yellow
-				
-			}
-			else if(i == 2)
-			{
-				connect3 = jsav.g.line(72.5, 400, 343, 280, {'stroke-width' : 3});
-				connect2 = jsav.g.line(47.5, 400, 167.5, 280); //sets line 2 back to original color
-				freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "lightgrey"}); //sets 2 back to grey
-				freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "yellow"}); //sets 3 to yellow
-			}
-			else if(i == 3)
-			{
-				connect4 = jsav.g.line(97.5, 400, 512.5, 280, {'stroke-width' : 3});
-				connect3 = jsav.g.line(72.5, 400, 343, 280); //sets line 3 back to original color
-				freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "lightgrey"}); //turns 3 back to grey
-				freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "yellow"}); //turns 4 yellow
-			}
-			else if (i == 4){
-			connect4 = jsav.g.line(97.5, 400, 512.5, 280); //sets 4 back to non bold
-			freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "lightgrey"}); //sets 4 back to grey
-			jsav.umsg("End of freelistreached\n Try a smaller size!\n");
-			
-			}
-			
-			jsav.umsg("Freelist's " + i + "th block size " + freeArray[i] + "\n");
-			
-			
-			if(input <= freeArray[i])
-			{
-				jsav.umsg("Appropriate block size found!\n");
-				jsav.umsg("Freelist's " + i + "th block size " + freeArray[i] + "\n");
-				jsav.step();
-				newRec(freeStartArray[i], freeStartArray[i] + input);
-				freeArray[i] = freeArray[i] - input;
-				freeStartArray[i] = freeStartArray[i] + input;
-				usedNum = UsedNum + input;
-				freeNum = freeNum - input;
-				updateLabels();
-				updateLines();
-				stop = 1;
-				jsav.step();
-				
-				
-			}
-			jsav.step();
-		
-		}
-	}
-	
+  }
+  else
+  {
+    jsav.umsg("The request has been scheduled\n Algorithm: First Fit\n Size Required: " + input + "\n");
+    var rec2 = jsav.g.rect(10, 300, input * 3, 80).css({"fill": "lightblue"}); //creates lightblue rectangle
+    jsav.step();
+    rec2 = jsav.g.rect(0, 0, 0, 0).css({"fill": "white"}); //deletes lightblue rectangle after step
+    var stop = 0;
+    var i = 0;
+    //for(i = 0; i <= 4; i++)
+    while(i <= 4 && stop != 1)
+    { 
+      if(i == 0)
+      {
+        connect1 = jsav.g.line(22.5, 400, 47.5, 280, {'stroke-width' : 3}); //sets line 1 to bold
+        freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "yellow"}); //sets 1 to yellow
+      }
+      else if(i == 1)
+      {
+        connect2 = jsav.g.line(47.5, 400, 167.5, 280, {'stroke-width' : 3});
+        connect1 = jsav.g.line(22.5, 400, 47.5, 280); //sets line 1 back to original color
+        freeListRect = jsav.g.rect(10, 400, 25, 50).css({"fill": "lightgrey"}); //sets 1 back to grey
+        freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "yellow"}); //sets 2 to yellow
+        
+      }
+      else if(i == 2)
+      {
+        connect3 = jsav.g.line(72.5, 400, 343, 280, {'stroke-width' : 3});
+        connect2 = jsav.g.line(47.5, 400, 167.5, 280); //sets line 2 back to original color
+        freeListRect2 = jsav.g.rect(35, 400, 25, 50).css({"fill": "lightgrey"}); //sets 2 back to grey
+        freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "yellow"}); //sets 3 to yellow
+      }
+      else if(i == 3)
+      {
+        connect4 = jsav.g.line(97.5, 400, 512.5, 280, {'stroke-width' : 3});
+        connect3 = jsav.g.line(72.5, 400, 343, 280); //sets line 3 back to original color
+        freeListRect3 = jsav.g.rect(60, 400, 25, 50).css({"fill": "lightgrey"}); //turns 3 back to grey
+        freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "yellow"}); //turns 4 yellow
+      }
+      else if (i == 4){
+      connect4 = jsav.g.line(97.5, 400, 512.5, 280); //sets 4 back to non bold
+      freeListRect4 = jsav.g.rect(85, 400, 25, 50).css({"fill": "lightgrey"}); //sets 4 back to grey
+      jsav.umsg("End of freelistreached\n Try a smaller size!\n");
+      
+      }
+      
+      jsav.umsg("Freelist's " + i + "th block size " + freeArray[i] + "\n");
+      
+      
+      if(input <= freeArray[i])
+      {
+        jsav.umsg("Appropriate block size found!\n");
+        jsav.umsg("Freelist's " + i + "th block size " + freeArray[i] + "\n");
+        jsav.step();
+        newRec(freeStartArray[i], freeStartArray[i] + input);
+        freeArray[i] = freeArray[i] - input;
+        freeStartArray[i] = freeStartArray[i] + input;
+        usedNum = UsedNum + input;
+        freeNum = freeNum - input;
+        updateLabels();
+        updateLines();
+        stop = 1;
+        jsav.step();
+        
+        
+      }
+      jsav.step();
+    
+    }
+  }
+  
   }
  
- }
+ 
 
   $(document).ready(function () {
 
