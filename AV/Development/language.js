edb1ab93796010165d41763f005daaa2e1ff67fd
X455u <kassu7@gmail.com>
2014-03-12 15:03:38 +0200

    Removed umsg override from language.js because it's now part of JSAV.

--- AV/Development/language.js
+++ AV/Development/language.js
@@ -1,19 +1,3 @@
-
-var umsg = JSAV.ext.umsg;
-
-JSAV.ext.umsg = function (msg, options) {
-	if (options && options.fill) {
-		for (var label in options.fill) {
-			var reg = new RegExp("{"+label+"}", "g");
-			console.log(reg.toString());
-			msg = msg.replace(reg, options.fill[label]);
-			console.log(msg);
-		}
-	}
-	// console.log(msg);
-	return umsg.apply(this, [msg, options]);
-}
-
 function getInterpreter(langJSON, selectedLanguage) {
     var trans;
 
