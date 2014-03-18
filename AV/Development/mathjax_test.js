3d8b5313d7eaef2748818cc238f6e9a6e28756ae
Cliff Shaffer <shaffer@vt.edu>
2014-03-18 13:55:13 -0400

    Minor change to mathjax_test.js

--- AV/Development/mathjax_test.js
+++ AV/Development/mathjax_test.js
@@ -3,17 +3,19 @@
 
 (function ($) {
   var av;
-    av = new JSAV($(".avcontainer"));
-    MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
-	$(".avcontainer").on("jsav-message", function() {
-      // invoke MathJax to do conversion again
-      MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-    });
-	av.umsg('Test with JSAV: The running time is $n\\log{n}$');
-	av.displayInit();
-	av.umsg('Test with JSAV: The running time is $\\theta(n^2)$');
-	av.recorded();
-	
+  av = new JSAV($(".avcontainer"));
+  MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']]}});
+  $(".avcontainer").on("jsav-message", function() {
+    // invoke MathJax to do conversion again
+    MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
+  });
+  av.umsg('Test with JSAV: The running time is $n \\log n$');
+  av.displayInit();
+  av.umsg('Again Test with JSAV: The running time is $n \\log n$');
+  av.step();
+  av.umsg('Test with JSAV: The running time is $\\theta(n^2)$');
+  av.recorded();
+
   // Connect action callbacks to the HTML entities
   $('#reset').click(ODSA.AV.reset);
 }(jQuery));
