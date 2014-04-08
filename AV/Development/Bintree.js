4cb86be86a509b10a21306cb96acaa756a685afc
Rich Episcopo <rich4756@vt.edu>
2014-04-07 22:13:07 -0400

    Add additional minute Bintree changes

--- AV/Development/Bintree.js
+++ AV/Development/Bintree.js
@@ -1,30 +1,48 @@
 /* Bintree Constructor */
-function Bintree(av, root) {
+(function () {
+    function Bintree(av, root, min, max) {
 
-    var t = av.ds.bintree({nodegap: 25});
-    var r = t.root();
+        this.t = av.ds.bintree({nodegap: 25});
+        this.t.layout();
+        this.r = this.t.root(root);
+        this.t.layout();
 
-    this.insert = function(val) {
+        this.insert = function(val) {
+            
+            // check if valid bounds
+            if (val < min || val > max) {
+                return undefined;
+            }
 
+            this.r = insertHelp(this.r, val);
+        }
+
+        this.remove = function(val) {
+
+        };
+
+        this.layout = function() {
+            t.layout();
+        };
+
+        this.insertHelp = function(node, val) {
+            
+            if (typeof node.value() === "undefined") {
+                return undefined;
+            }
+            if (this.r.value() > val) {
+                if (typeof this.r.left() === "undefined") {
+
+                }
+            }        
+        };
+
+        this.removeHelp = function(node, val) {
+      
+        };
     }
 
-    this.remove = function(val) {
-
-    }
-
-    this.insertHelp = function(node, val) {
-    
-    };
-
-    this.removeHelp = function(node, val) {
-  
-    };
-}
-
-var insert = function(node, val) {
-
-};
-
-var remove = function(node, val) {
-
-};
+    var tree = {};
+    tree.Bintree = Bintree;
+    window.tree = tree;
+}());
