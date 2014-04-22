e9bd37c709ec54c91638050921a3e73218ee51b3
Elmer Landaverde <elmer@vt.edu>
2014-04-22 18:30:30 -0400

    Removed unnecessary code from js file

--- AV/Development/treeIndexingCON.js
+++ AV/Development/treeIndexingCON.js
@@ -1,91 +1,29 @@
+
 (function () {
   "use strict";
-
-  /**
-   * Generate a colorized binary search tree.
-   * @param root The root of the tree.
-   * @param max_level The level depth to stop at.
-   * @param level The current level of depth.
-   * @param colors Array of colors from which to select the background.
-   */
-  function generateBSTNodes(root, max_level, level, colors) {
-    if (level <= max_level) {
-      var color_index;
-      // Determine if left node will be created.
-      if (Math.random() > window.ODSA.probability_threshold) {
-        // Create new node.
-        var new_left = root.left("");
-        // Determine node background color.
-        color_index = Math.floor((Math.random() * 100) % colors.length);
-        new_left.css({"background-color": colors[color_index]});
-        // Give node chosen property
-        new_left.is_chosen = false;
-        // Add node to bottom layer if level is last.
-        if (level === max_level) {
-          window.ODSA.bottom_layer.push(new_left);
-        }
-        generateBSTNodes(new_left, max_level, level + 1, colors);
-      }
-
-      // Determine if right node will be created.
-      if (Math.random() > window.ODSA.probability_threshold) {
-        // Determine if left node will be created.
-        var new_right = root.right("");
-        // Determine node background color.
-        color_index = Math.floor((Math.random() * 100) % colors.length);
-        new_right.css({"background-color": colors[color_index]});
-        // Give node chosen property
-        new_right.is_chosen = false;
-        // Add node to bottom layer if level is last.
-        if (level === max_level) {
-          window.ODSA.bottom_layer.push(new_right);
-        }
-        generateBSTNodes(new_right, max_level, level + 1, colors);
-      }
-    }
-  }
-
-  /**
-   * Examines a colorized binary search tree and determines the number of disk
-   * accesses necessary to reach the selected node.
-   * @param root The root of the tree.
-   * @param disk_accesses The current number of disk access to get to the given
-   *        tree node 'root'.
-   */
-  function getAnswer(root, disk_accesses) {
-    if (root.is_chosen === true) {
-      window.ODSA.disk_accesses = disk_accesses;
-    } else {
-      // Explore left child.
-      if (typeof(root.left()) !== "undefined" && root.left() !== null) {
-        if (root.css("background-color") !== root.left().css("background-color")) {
-          getAnswer(root.left(), disk_accesses + 1);
-        } else {
-          getAnswer(root.left(), disk_accesses);
-        }
-      }
-
-      // Explore right child.
-      if (typeof(root.right()) !== "undefined" && root.right() !== null) {
-        if (root.css("background-color") !== root.right().css("background-color")) {
-          getAnswer(root.right(), disk_accesses + 1);
-        } else {
-          getAnswer(root.right(), disk_accesses);
-        }
-      }
-    }
-  }
 
   /**
    * Toggles the highlighting of a node.
    * @param node The node to highlight/un-highlight
    */
   function toggleNodeHighlight(node) {
+
     if (node.css(node_highlight_property) === node_highlight_default) {
+//      highlightNode(node);
       node.css(node_highlight_add);
     } else {
       node.css(node_highlight_remove);
     }
+  }
+
+  /**
+   * Adds a box-shadow effect to the node of the same color as the background.
+   * @param node The node to be highlighted.
+   */
+  function highlightNode(node) {
+    var color = node.css("background-color");
+    color = "rgba(" + color.substring(4, color.length - 1) + ", 1.0)";
+    node.css({"box-shadow": "0 0 0 10px " + color});
   }
 
   /**
@@ -107,74 +45,17 @@
   // The default CSS property.
   var node_highlight_default;
   // The CSS to add the highlighting effect.
-  var node_highlight_add = {"box-shadow": "0 0 15px 5px #2B92FF"};
+  var node_highlight_add = {"box-shadow": "0 0 12px 8px rgba(0, 0, 0, 0.5)"};
   // The CSS to remove the highlighting effect.
   var node_highlight_remove = {"box-shadow": "node"};
 
   var ODSA = {};
-  ODSA.generateBSTNodes = generateBSTNodes;
-  ODSA.getAnswer = getAnswer;
-  ODSA.bottom_layer = [];
-  ODSA.disk_accesses = 0;
-  ODSA.probability_threshold = 0.3;
   ODSA.toggleNodeHighlight = toggleNodeHighlight;
   ODSA.setNodeHighlightDefault = setNodeHighlightDefault;
   window.ODSA = ODSA;
 }());
 
-/**
- * Demo: Randomized creation of a colorized binary search tree.
- *
- * Container ID: colorizedCON
- */
-(function () {
-  "use strict";
 
-  var jsav = new JSAV("colorizedBST");
-
-  var bst = jsav.ds.binarytree({nodegap: 15});
-
-  // Create color array
-  var colors = ["#7BFF95", "#77CCFF", "#FF6F52", "#FFDE70", "#E39BCF"];
-
-  // Create disk rectangles.
-  var width = 100;
-  var height = 50;
-  var x = 50;
-  var y = 210;
-  for (var i = 0; i < colors.length; i++) {
-    jsav.g.rect(x, y, width, height, {"fill": colors[i]});
-    x += width;
-  }
-
-  // Get color for right and level 1 nodes.
-  var color_root = Math.floor((Math.random() * 100) % colors.length);
-  var color_right = Math.floor((Math.random() * 100) % colors.length);
-  var color_left = Math.floor((Math.random() * 100) % colors.length);
-
-  // Create tree.
-  bst.root("").css({"background-color": colors[color_root]});
-  bst.root().right("").css({"background-color": colors[color_right]});
-  bst.root().left("").css({"background-color": colors[color_left]});
-
-  // Randomly generate three more levels of nodes.
-  while (window.ODSA.bottom_layer.length === 0) {
-    window.ODSA.generateBSTNodes(bst.root().left(), 2, 0, colors);
-    window.ODSA.generateBSTNodes(bst.root().right(), 2, 0, colors);
-  }
-  bst.layout();
-
-  // Pick bottom layer node to be chosen one.
-  var chosen_index = Math.floor((Math.random() * 100) % window.ODSA.bottom_layer.length);
-  var chosen_node = window.ODSA.bottom_layer[chosen_index];
-  chosen_node.css({"border": "1px solid white"});
-  chosen_node.css({"box-shadow": "0 0 10px 3px black"});
-  chosen_node.is_chosen = true;
-
-  // Find the number of accesses to get to chosen one.
-  window.ODSA.getAnswer(bst.root(), 1);
-  jsav.label("Number of disk accesses: " + window.ODSA.disk_accesses, {top: "280px", left: "220px"});
-}());
 
 /**
  * Creates a diagram to demonstrate how a binary search tree might be laid out on disk.
