f7bcfb7c0b63a21400d3c2bf57b492c15759eefc
breakid <breakid@vt.edu>
2014-05-25 10:11:35 -0400

    Changed loadLangData() to take an object containing optional arguments as a parameter

--- AV/Sorting/InsertionSortP3CON.js
+++ AV/Sorting/InsertionSortP3CON.js
@@ -3,7 +3,7 @@
 (function ($) {
   $(document).ready(function () {
     var av_name = "InsertionSortP3CON";
-    var interpret = ODSA.UTILS.loadLangData(av_name).interpreter;
+    var interpret = ODSA.UTILS.loadLangData({'av_name': av_name}).interpreter;
 
     var theArray3 =  [10, 15, 20, 54, 55, 11, 78, 14];
     var LIGHT = "rgb(215, 215, 215)";  // For "greying out" array elements
