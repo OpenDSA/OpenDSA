7c483a062bbaa4b92fa493366786e2962e32e72c
Hannah <hannah@I.am.computer>
2014-07-02 12:46:57 +0200

    Minor changes to Mergesort. Added new file MergesortOpt.

--- SourceCode/C++/Sorting/MergesortOpt.cpp
+++ SourceCode/C++/Sorting/MergesortOpt.cpp
@@ -1,50 +1,100 @@
-// Mergesort implementation and timing test driver
-// Basic Mergesort version
+#include "utils.h"
+#include "Comparable.cpp"
+#include "KVPair.cpp"
+#include "Int.cpp"
+#include "Checkorder.cpp"
 
-#include "book.h"
-
-// Include comparator functions
-#include "compare.h"
-
-// Standard insertion sort implementation
-template <typename E, typename Comp>
-void inssort(E A[], int n) { // Insertion Sort
-  for (int i=1; i<n; i++)       // Insert i'th record
-    for (int j=i; (j>0) && (Comp::prior(A[j], A[j-1])); j--)
+void swap(Comparable *A[], int i, int j) {
+  Comparable* tmp = A[i];
+  A[i] = A[j];
+  A[j] = tmp;
+}
+//Insertionsort algorithm
+void inssort(Comparable* A[], int n) { // Insertion Sort
+  for (int i = 1; i < n; i++) { // Insert i'th record
+    for (int j = i; (j > 0) && (*A[j] < *A[j-1]); j--) {
       swap(A, j, j-1);
-}
-
-extern int THRESHOLD;
-
-/* *** ODSATag: MergesortOpt *** */
-template <typename E, typename Comp>
-void mergesortOpt(E A[], E temp[], int left, int right) {
-  if ((right-left) <= THRESHOLD) { // Small list
-    inssort<E,Comp>(&A[left], right-left+1);
-    return;
+    }
   }
-  int i, j, k, mid = (left+right)/2;
-  mergesort<E,Comp>(A, temp, left, mid);
-  mergesort<E,Comp>(A, temp, mid+1, right);
-  // Do the merge operation.  First, copy 2 halves to temp.
-  for (i=mid; i>=left; i--) temp[i] = A[i];
-  for (j=1; j<=right-mid; j++) temp[right-j+1] = A[j+mid];
-  // Merge sublists back to A
-  for (i=left,j=right,k=left; k<=right; k++)
-    if (Comp::prior(temp[i], temp[j])) A[k] = temp[i++];
-    else A[k] = temp[j--];
 }
+// With KVPair
+/* *** ODSATag: MergesortOpt *** */
+
+// Merge sort optional algorithm ****Hannah
+void mergesortOpt(Comparable* A[], Comparable* temp[], int left, int right, int n) {
+  int THRESHOLD = 50; //
+  if ((right-left) <= THRESHOLD) {     // Small list
+    inssort(A, right-left+1);
+      return;
+  }
+  int i, j, k, mid = (left+right)/2;  // Select the midpoint
+  mergesortOpt(A, temp, left, mid, n);
+  mergesortOpt(A, temp, mid+1, right, n);
+  // Do the merge operation. First, copy 2 halves to temp.
+  for (i = mid; i >= left; i--) *temp[i] = *A[i];
+  for (j = 1; j <= right-mid; j++) *temp[right-j+1] = *A[j+mid];
+  // Merge sublists back to A
+  for (i = left, j = right, k = left; k <= right; k++)
+    if (*temp[i] < *temp[j]) *A[k] = *temp[i++];
+    else *A[k] = *temp[j--];
+}
+
 /* *** ODSAendTag: MergesortOpt *** */
 
-template <typename E, typename Comp>
-void sort(E* array, int n) {
-  static E* temp = NULL;
-  if (temp == NULL) temp = new E[n];  // Declare temp array
-  mergesort<E,Comp>(array, temp, 0, n-1);
+// With KVPair
+
+bool sorttest(int array[], int n, int threshold) {
+  Comparable* A[n];
+  Comparable* temp[n];
+  int i;
+
+  /* Sort an array of Ints */
+  for (i = 0; i < n; ++i) {
+    A[i] = new Int(array[i]);
+  }
+  for (int i = 0; i < n; ++i) {
+    temp[i] = new Int(0);
+  }
+
+  //  for (i = 0; i < n; ++i) {
+  //    cout << *A[i] << " ";
+  //  }
+  //  cout << std::endl;
+  
+  mergesortOpt(A, temp, 0, n-1, n);
+
+  if (!checkorder(A, n)) return false;
+
+  for (i = 0; i < n; ++i) {
+    delete A[i];
+  }
+   for (int i = 0; i < n; ++i) {
+      delete temp[i];
+    }
+
+  /* Sort an array of KVPairs */
+  
+  for (i = 0; i < n; ++i) {
+    A[i] = new KVPair(array[i], &array[i]);
+  }
+  for (int i = 0; i < n; ++i) {
+      temp[i] = new KVpair(0, 0);
+   }
+
+  mergesortOpt(A, temp, 0, n-1, n);
+
+  if (!checkorder(A, n)) return false;
+
+  for (i = 0; i < n; ++i) {
+    delete A[i];
+  }
+  for (int i = 0; i < n; ++i) {
+      delete temp[i];
+    }
+  
+  delete[] array;
+
+  return true;
 }
 
-#include "sortmain.cpp"
-
-int main(int argc, char** argv) {
-  return sortmain<minintCompare>(argc, argv);
-}
+#include "SortTest.cpp"
