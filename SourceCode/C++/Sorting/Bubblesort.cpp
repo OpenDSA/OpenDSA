3f224806252851287dbf8ad29f5d9843daf530d6
hannahborje <hannah.borjesson@gmail.com>
2014-07-08 15:24:23 +0200

    Minor fixes.

--- SourceCode/C++/Sorting/Bubblesort.cpp
+++ SourceCode/C++/Sorting/Bubblesort.cpp
@@ -7,13 +7,10 @@
 // With KVPair
 /* *** ODSATag: Bubblesort *** */
 void bubblesort(Comparable* A[], int n) {
-  for (int i = 0; i < n-1; i++) {
-    for (int j = 0; j < n-1; j++) {
-      if (*A[j] > *A[j+1]) {
+  for (int i = 0; i < n-1; i++) 
+    for (int j = 0; j < n-1; j++) 
+      if (*A[j] > *A[j+1]) 
         swap(A, j, j+1);
-      }
-    }
-  }
 }
 /* *** ODSAendTag: Bubblesort *** */
 
