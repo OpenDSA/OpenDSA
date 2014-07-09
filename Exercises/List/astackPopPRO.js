d63d804ca4bd04d07b1033d1f8f7e0ec908f09dd
Cliff Shaffer <shaffer@vt.edu>
2014-07-08 20:27:08 -0400

    Updated exercises for array-based stack

--- Exercises/List/astackPopPRO.js
+++ Exercises/List/astackPopPRO.js
@@ -1,15 +1,16 @@
 "use strict";
-var av,               // The JSAV object
-    answerArr = [],   // The (internal) array that stores the correct answer
-    cloneArr = [],    // Copy of (internal) array at start of exercise for reset
-    maxSize,          // Size of the JSAV array.
-    jsavArr,          // The JSAV array that the user manipulates
-    topPointer,       // "top" pointer
-    pointerIndex,     // Index of array that the pointer points to
-    pSelected,        // Boolean: Tells if pointer is highlighted
-    selectedIndex,    // Array position that has been selected by user
-    inValue,          // insertion value
-    userInput;        // Boolean: Tells if user ever did anything
+var av,             // The JSAV object
+    answerArr = [], // The (internal) array that stores the correct answer
+    cloneArr = [],  // Copy of (internal) array at start of exercise for reset
+    returnVal,      // The correct value that should be returned
+    jsavArr,        // The array that the user manipulates (JSAV object)
+    returnArr,      // Holds the value to be returned
+    labelReturn,    // Label for returnArr
+    topPointer,     // "top" pointer
+    pSelected,      // Boolean: Tells if pointer is highlighted
+    pointerIndex,   // The index of array that topPointer points to.
+    userInput,      // Boolean: Tells us if user ever did anything
+    selectedIndex;  // Position that has been selected by user for swap
 
 JSAV._types.Pointer.prototype.click = function (fn) {
   var pointer = this;
@@ -33,7 +34,7 @@ function pclick(pointer) {
   userInput = true;
 }
 
-// Click event handler on the array
+// Click event handler on the array "jsavArr"
 function clickHandler(index, e) {
   if (selectedIndex === -1) { // nothing currently selected
     if (pSelected) { // He's re-setting the pointer
@@ -60,90 +61,97 @@ function clickHandler(index, e) {
   userInput = true;
 }
 
-// reset function definition
-function f_reset(max_size, curr_size) {
-  var leftMargin = 20, topMargin = 50;
-  selectedIndex = -1;
-  pSelected = false;
-
-  if ($("#astackPushPRO")) {
-    $("#astackPushPRO").empty();
+// Click event handler on the array "returnArr"
+function copyHandler(index, e) {
+  if (selectedIndex !== -1) {
+    av.effects.moveValue(jsavArr, selectedIndex, returnArr, 0);
+    jsavArr.css(selectedIndex, { "background-color": "#ddd" });
+    jsavArr.css(selectedIndex, {"font-size": "100%"});
+    jsavArr.unhighlight(selectedIndex);
+    selectedIndex = -1;
+    userInput = true;
   }
-  av = new JSAV("astackPushPRO");
-  jsavArr = av.ds.array(cloneArr, {indexed: true, center: false,
-                                   left: leftMargin, top : topMargin});
-    
+}
+
+
+// reset function definition
+function f_reset(max_size, arr_size) {
+  selectedIndex = -1;
+  if ($("#astackPopPRO")) {
+    $("#astackPopPRO").empty();
+  }
+  av = new JSAV("astackPopPRO");
+  jsavArr = av.ds.array(cloneArr, {indexed: true, center: false, top : 50});
+  returnArr = av.ds.array([""], {left: 350, top : 50});
+  labelReturn = av.label("return", {left: 305, top : 55});
+  pointerIndex = arr_size;
+
   if (typeof topPointer !== "undefined") {
     topPointer.element.remove();
     topPointer.arrow.element.remove();
   }
-
-  topPointer = av.pointer("top", jsavArr, {targetIndex: curr_size});
+  topPointer = av.pointer("top", jsavArr, {targetIndex : arr_size});
   topPointer.click(pclick);
-  pointerIndex = curr_size;
+  pSelected = false;
 
   av.recorded();
   av.forward();
-
   // Bind the clickHandler to handle click events on the array
   jsavArr.click(clickHandler);
+  returnArr.click(copyHandler);
   userInput = false;
 }
 
 // Initialise the exercise
-function initJSAV(max_size, curr_size, insertValue) {
+function initJSAV(max_size, arr_size, insertValue) {
   var i, j;
-
-  //global variables
   userInput = false;
-  maxSize = max_size;
   selectedIndex = -1;
-  pSelected = false;
-  inValue = insertValue;
   answerArr.length = 0; // Out with the old
 
   // Give random numbers in range 0..999
-  for (i = 0; i < curr_size; i++) {
+  for (i = 0; i < arr_size; i++) {
     answerArr[i] = Math.floor(Math.random() * 1000);
   }
-  for (i = 0; i < max_size - curr_size; i++) {
+  for (i = 0; i < max_size - arr_size; i++)
+  {
     answerArr.push.apply(answerArr, [""]);
   }
   // Now make a copy
   cloneArr = answerArr.slice(0);
 
-  f_reset(max_size, curr_size);
+  f_reset(max_size, arr_size);
 
   // correct answer
-  answerArr.splice(curr_size, 0, inValue);
-  answerArr.splice(curr_size + 1, 1);
+  if (arr_size === 0) {
+    returnVal = "";
+  } else {
+    returnVal = answerArr[arr_size - 1];
+  }
 
-  // Handler for insert button
-  $("#insert").click(function () {
-    if (pSelected) { // Unhighlight the pointer
-      pSelected = false;
-      topPointer.element.removeClass("highlight");
-    }
-    if (selectedIndex !== -1) {
-      jsavArr.value(selectedIndex, inValue);
-      jsavArr.css(selectedIndex, { "background-color": "#ddd" });
-      jsavArr.css(selectedIndex, {"font-size": "100%"});
-      jsavArr.unhighlight(selectedIndex);
-      selectedIndex = -1;
-      userInput = true;
-    }
-  });
+  answerArr.splice(arr_size - 1, 0, "");
+  answerArr.splice(arr_size, 1);
 
   // Set up handler for reset button
-  $("#reset").click(function () { f_reset(max_size, curr_size); });
+  $("#reset").click(function () { f_reset(max_size, arr_size); });
 }
 
-// Check user's answer for correctness: User's array must match answer
-function checkAnswer(max_size, curr_size) {
-  if (pointerIndex !== curr_size + 1) {
+// Check user's answer for correctness
+function checkAnswer(max_size, arr_size) {
+  if (returnArr.value(0) !== returnVal) {
     return false;
   }
-  for (var i = 0; i < max_size; i++) {
+  if (arr_size === 0) {
+    if (pointerIndex !== 0) {
+      return false;
+    }
+  } else {
+    if (pointerIndex !== arr_size - 1) {
+      return false;
+    }
+  }
+  var i;
+  for (i = 0; i < max_size; i++) {
     if (jsavArr.value(i) !== answerArr[i]) {
       return false;
     }
