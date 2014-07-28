aefb9474dbc64c4b1f99a0c4571a6defe0c23642
Cliff Shaffer <shaffer@vt.edu>
2014-07-28 14:58:28 -0400

    Updated selection sort

--- AV/Sorting/selectionsortAV.js
+++ AV/Sorting/selectionsortAV.js
@@ -1,97 +1,61 @@
-/*global alert: true, ODSA */
-(function ($) {
-  "use strict";
-  var jsav, // for JSAV av
-      arr,  // for the JSAV array
-      pseudo; // for the pseudocode display
-
-  // check query parameters from URL
-  var params = JSAV.utils.getQueryParameter();
-  if ("array" in params) { // set value of array pick if it is a param
-    $('#arrayValues').val(params.array).prop("disabled", true);
-  }
-  
-  // create a new settings panel and specify the link to show it
-  var settings = new JSAV.utils.Settings($(".jsavsettings"));
-
-  // add the layout setting preference
-  var arrayLayout = settings.add("layout", {"type": "select",
-                      "options": {"bar": "Bar", "array": "Array"},
-                      "label": "Array layout: ", "value": "bar"});
-
-  var LIGHT = "rgb(215, 215, 215)";  // For "greying out" array elements
-
-  // Initialize the arraysize dropdown list
-  ODSA.AV.initArraySize(5, 16, 8);
-
+"use strict";
+/*global alert: true, ODSA, console */
+$(document).ready(function () {
   // Process About button: Pop up a message with an Alert
   function about() {
-    alert("Selection Sort Algorithm Visualization\nWritten by Cliff Shaffer and Mauricio De La Barra\nCreated as part of the OpenDSA hypertextbook project\nFor more information, see http://algoviz.org/OpenDSA\nSource and development history available at\nhttps://github.com/cashaffer/OpenDSA\nCompiled with JSAV library version " + JSAV.version());
+    alert(ODSA.AV.aboutstring(interpret(".avTitle"), interpret("av_Authors")));
   }
-
-  var setBlue = function (index) {
-    arr.css(index, {"background-color": "#ddf" });
-  };
-
-  var setGreen = function (index) {
-    arr.css(index, {"background-color": "#00FF00" });
-  };
-
-  var setGray = function (index) {
-    arr.css(index, {"background-color": "#eee"});
-  };
 
   // Selection sort
   function selsort() {
     var i, j, bigindex;
-    jsav.umsg("For each pass, we will move left to right looking for the next largest value. Once that is found, it will be swapped into its final position (these will be shown in lighter color).");
-    pseudo.setCurrentLine(0);
-    jsav.step();
+    av.umsg(interpret("av_c3"));
+    pseudo.setCurrentLine("sig");
+    av.step();
     for (i = 0; i < arr.size() - 1; i++) {
-      jsav.umsg("Starting pass " + i);
-      pseudo.setCurrentLine(1);
-      jsav.step();
-      jsav.umsg("Initialize bigindex");
-      pseudo.setCurrentLine(2);
+      av.umsg(interpret("av_c4") + i);
+      pseudo.setCurrentLine("outloop");
+      av.step();
+      av.umsg(interpret("av_c5"));
+      pseudo.setCurrentLine("initbig");
       bigindex = 0;
-      setGreen(0);
-      jsav.step();
-      jsav.umsg("For each element moving through the list: the biggest seen so far is always green");
-      pseudo.setCurrentLine(3);
-      jsav.step();
+      arr.addClass(0, "special");
+      av.step();
+      av.umsg(interpret("av_c6"));
+      pseudo.setCurrentLine("inloop");
+      av.step();
       for (j = 1; j < arr.size() - i; j++) {
-        setBlue(j);
-        jsav.umsg("Compare to biggest seen so far");
-        pseudo.setCurrentLine(4);
-        jsav.step();
+        arr.addClass(j, "processing");
+        av.umsg(interpret("av_c7"));
+        pseudo.setCurrentLine("compare");
+        av.step();
         if (arr.value(j) > arr.value(bigindex)) {
-          jsav.umsg("Found something bigger, so switch value of bigindex");
-          setGray(bigindex);
-          pseudo.setCurrentLine(5);
+          av.umsg(interpret("av_c8"));
+          arr.removeClass(bigindex, "special");
+          pseudo.setCurrentLine("setbig");
           bigindex = j;
-          setGreen(j);
-          jsav.step();
+          arr.addClass(bigindex, "special");
+          av.step();
         }
-        else {
-          setGray(j);
-        }
+        arr.removeClass(j, "processing");
       }
-      jsav.umsg("Now swap the next biggest element into place");
-      pseudo.setCurrentLine(6);
-      jsav.step();
+      av.umsg(interpret("av_c9"));
+      pseudo.setCurrentLine("swap");
+      av.step();
       if (bigindex !== (arr.size() - i - 1)) {
         arr.swap(bigindex, arr.size() - i - 1); // swap the two indices
-        setGray(bigindex);
-        setGreen(arr.size() - i - 1);
+        arr.removeClass(bigindex, "special");
+        arr.addClass(arr.size() - i - 1, "special");
       }
-      jsav.step();
-      jsav.umsg("Done this pass");
-      setGray(arr.size() - i - 1);
-      arr.css([arr.size() - i - 1], {"color": LIGHT});
-      jsav.step();
+      av.step();
+      av.umsg(interpret("av_c10"));
+      arr.removeClass(arr.size() - i - 1, "special");
+      arr.addClass(arr.size() - i - 1, "deemph");
+      av.step();
     }
-    jsav.umsg("Done Sorting!");
-    pseudo.setCurrentLine(8);
+    av.umsg(interpret("av_c2"));
+    arr.addClass(0, "deemph");
+    pseudo.setCurrentLine("end");
   }
 
   // Execute the "Run" button function
@@ -101,17 +65,17 @@
     // If arrValues is null, the user gave us junk which they need to fix
     if (arrValues) {
       ODSA.AV.reset(true);
-      jsav = new JSAV($('.avcontainer'));
+      av = new JSAV($('.avcontainer'));
       
       // Create a new array using the layout the user has selected
-      arr = jsav.ds.array(arrValues, {indexed: true, layout: arrayLayout.val()});
-      pseudo = jsav.code({url: "../../SourceCode/Processing/Sorting/Selectionsort.pde",
-          startAfter: "/* *** ODSATag: Selectionsort *** */",
-          endBefore: "/* *** ODSAendTag: Selectionsort *** */"});
-      jsav.displayInit();
+      arr = av.ds.array(arrValues, {indexed: true, layout: arrayLayout.val()});
+
+      // Create the pseudocode display object
+      pseudo = av.code(code);
+      av.umsg(interpret("av_c1"));
+      av.displayInit();
       selsort();
-      setGray();
-      jsav.recorded(); // mark the end
+      av.recorded(); // mark the end
     }
   }
 
@@ -125,4 +89,33 @@
     runIt();
   });
   $('#reset').click(ODSA.AV.reset);
-}(jQuery));
+
+  //////////////////////////////////////////////////////////////////
+  // Start processing here
+  //////////////////////////////////////////////////////////////////
+  var av,     // for JSAV av
+      arr,    // for the JSAV array
+      pseudo; // for the pseudocode display
+
+  // Load the config object with interpreter and code created by odsaUtils.js
+  var config = ODSA.UTILS.loadLangData(),
+      interpret = config.interpreter,       // get the interpreter
+      code = config.code;                   // get the code object
+
+  // Placeholder text translation needs to be set explicitly
+  $("#arrayValues").attr("placeholder", interpret("av_arrValsPlaceholder"));
+
+  // Placeholder text translation needs to be set explicitly
+  $("#arrayValues").attr("placeholder", interpret("av_arrValsPlaceholder"));
+
+  // create a new settings panel and specify the link to show it
+  var settings = new JSAV.utils.Settings($(".jsavsettings"));
+
+  // add the layout setting preference
+  var arrayLayout = settings.add("layout",
+          {"type": "select", "options": {"bar": "Bar", "array": "Array"},
+           "label": "Array layout: ", "value": "bar"});
+
+  // Initialize the arraysize dropdown list
+  ODSA.AV.initArraySize(5, 16, 8);
+});
