19b0167e772ced888a73cc0cc0e53c139f59d898
Elmer Landaverde <elmer@vt.edu>
2014-09-01 14:24:02 -0400

    Modify exmple to show bug
    Replicated bug when positioning nodes in a tree. It appears that hidden
    nodes affect the layout of the tree.

--- AV/Development/arrayTreeCON.js
+++ AV/Development/arrayTreeCON.js
@@ -5,37 +5,69 @@
 
   // Tree array testing function
   function init($) {
+    // Toggle between these two for common tree and array tree.
     createArrayTree();
-//    createOther();
+    // createCommonTree();
   }
 
   function createArrayTree() {
     var jsav = new JSAV("arrayTree");
     var tree = new jsav.ds.arraytree();
 
-    var n1 = tree.root([1, 1, 1]);
+    // Add all nodes
+    var r = tree.root([1,1]);
+    r.addChild([2,2]);
+    r.addChild([3,3]);
+    r.addChild([4,4]);
 
-    var n2 = n1.addChild([2,2,2]).child(0);
-    var n3 = n1.addChild([3,3,3,3]).child(1);
-    var n4 = n1.addChild([4,4,4]).child(2);
-    var n5 = n1.addChild([5,5,5]).child(3);
+    r.child(0).addChild([5,5]);
+    r.child(0).addChild([6,6]);
 
-    var n6 = n2.addChild([6,6,6]).child(0);
-    var n7 = n2.addChild([7,7,7]).child(1);
-    var n8 = n2.addChild([8,8,8]).child(2);
-    var n9 = n2.addChild([9,9,9]).child(3);
+    r.child(1).addChild([7,7]);
+    r.child(1).addChild([8,8]);
 
-    n6.value([6, 5, 4]);
+    r.child(2).addChild([9,9]);
+    r.child(2).addChild([10,10]);
 
-    n6.value([6, 5, 4, 3]);
+    // Remove node at index 2 from root
+    r.child(2, null);
 
-    n6.edgeToParent().addClass("superclass");
+    // Add nodes back plus an extra blank node
+    r.addChild(["", ""]);
+    r.addChild([4,4]);
+    r.child(3).addChild([11, 11]);
+    r.child(3).addChild([12, 12]);
 
     tree.layout();
   }
 
-  function createOther() {
+  function createCommonTree() {
     var jsav = new JSAV("other");
-    var arr = new jsav.ds.array([1, 2, 3, 4], {element: $('#hello')});
+    var tree = new jsav.ds.tree();
+
+    var r = tree.root(1);
+    r.addChild(2);
+    r.addChild(3);
+    r.addChild(4);
+
+    r.child(0).addChild(5);
+    r.child(0).addChild(6);
+
+    r.child(1).addChild(7);
+    r.child(1).addChild(8);
+
+    r.child(2).addChild(9);
+    r.child(2).addChild(10);
+
+    // Remove node at index 2 from root
+    r.child(2, null);
+
+    // Add nodes back plus an extra blank node
+    r.addChild("");
+    r.addChild(4);
+    r.child(3).addChild(11);
+    r.child(3).addChild(12);
+
+    tree.layout();
   }
 }(jQuery));
