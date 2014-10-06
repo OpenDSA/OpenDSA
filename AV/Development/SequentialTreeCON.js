c211276b80ddd581c539a59ee12ff34ab712b30f
Kevin Ellis <kevellis@vt.edu>
2014-10-06 10:17:37 -0400

    Pointer works for all nodes but the root.

--- AV/Development/SequentialTreeCON.js
+++ AV/Development/SequentialTreeCON.js
@@ -11,7 +11,7 @@ $(document).ready(function () {
   var a = bt.root();
   bt.layout();
 
-  var ptr = av.pointer("rt", a, {anchor: "left top", top: -10});
+  var ptr = av.pointer("rt", a, {anchor: "middle top", top: -10});
 
   //Slide 1
   av.umsg("We begin with the first node in the string 'A' which will be the root node");
@@ -25,8 +25,8 @@ $(document).ready(function () {
   av.umsg("We insert B as the left child of A");
   a.left('B'); //<--- HERE IS THE LINE IN QUESTION
   var b = a.left();
-  ptr.target(b);
   bt.layout();
+  ptr.target(b);
   av.step();
 
   //Slide 4
@@ -36,8 +36,8 @@ $(document).ready(function () {
   //Slide 5
   av.umsg("The next token 'D' is the right child of 'B' ");
   var d = a.left().right('D');
-  ptr.target(d);
   bt.layout();
+  ptr.target(d);
   //point to D
   av.step();
 
@@ -60,24 +60,24 @@ $(document).ready(function () {
   //Slide 9
   av.umsg("The next character in the string represents A's right child 'C' ");
   var c = a.right('C');
-  ptr.target(c);
   bt.layout();
+  ptr.target(c);
   //point to C
   av.step();
 
   //Slide 10
   av.umsg("'E' must be C's left child");
   var e = a.right().left('E');
-  ptr.target(e);
   bt.layout();
+  ptr.target(e);
   //point to E
   av.step(); 
 
   //Slide 11
   av.umsg("The next character 'G' represents E's left child");
   var g = a.right().left().left('G');
-  ptr.target(g);
   bt.layout();
+  ptr.target(g);
   //point to G
   av.step();
 
@@ -108,28 +108,29 @@ $(document).ready(function () {
   //Slide 17
   av.umsg("F represents C's right child");
   var f = a.right().right('F');
-  ptr.target(f);
   bt.layout();
+  ptr.target(f);
   //point to F
   av.step();
 
   //Slide 18
   av.umsg("H represents F's left child");
   var h = a.right().right().left('H');
-  ptr.target(h);
   bt.layout();
+  ptr.target(h);
   //point to H
   av.step();
 
   //Slide 19
   av.umsg("Double slash implies H is a leaf node with no children. Pop back up to F");
+  ptr.target(f);
   av.step();
 
   //Slide 20
   av.umsg("I must be the right child of F");
   var i = a.right().right().right('I');
-  ptr.target(i);
   bt.layout();
+  ptr.target(i);
   //point to I
   av.step();
 
