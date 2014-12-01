d972b31b02164105ed0cf801cfb6cafd954cc565
Cliff Shaffer <shaffer@vt.edu>
2014-12-01 13:43:28 -0500

    Update selection sort

--- SourceCode/C++/Sorting/Selectionsort.cpp
+++ SourceCode/C++/Sorting/Selectionsort.cpp
@@ -7,12 +7,12 @@
 // With KVPair
 /* *** ODSATag: Selectionsort *** */
 void selectionsort(Comparable* A[], int n) {
-  for (int i = 0; i < n-1; i++) {
-    int bigindex = i;
-    for (int j = i+1; j < n; j++) 
-      if (*A[j] < *A[bigindex])  
-        bigindex = j;    
-      swap(A, bigindex, i);    
+  for (int i = 0; i < n-1; i++) { // Select i'th biggest record
+    int bigindex = 0;             // Current biggest index
+    for (int j = 1; j < n-i; j++) // Find the max value
+      if (*A[j] > *A[bigindex])   // Found something bigger
+        bigindex = j;             // Remember bigger index    
+      swap(A, bigindex, n-i-1);   // Put it into place
   }
 }
 /* *** ODSAendTag: Selectionsort *** */
