1c250ef20d5f7c6ea7a67b7f9146fdda23744d43
Nabanita Maji <nabanita@vt.edu>
2015-02-28 18:17:25 -0500

    Intermediate checkin

--- Exercises/Development/graphUtilsHC.js
+++ Exercises/Development/graphUtilsHC.js
@@ -1,14 +1,11 @@
 (function ($) {
   "use strict";
-  var actualEdges=0;
-  var enforcedHC=false;
-  var hcpermute=[];
+
+  var actualEdges;
   function generateRandomEdges(nNodes, nEdges, weighted) {
     // Utility funciton to check whether the edge already exists 
     function isEligibleEdge(startIndex, endIndex) {
       if ((startIndex === endIndex) ||
-          (enforcedHC && hcpermute.indexOf(endIndex) 
-                         === (hcpermute.indexOf(startIndex)+1)%nNodes)  ||
           (adjacencyMatrix[startIndex][endIndex] === 1) ||
           (adjacencyMatrix[endIndex][startIndex] === 1))
       {
@@ -21,6 +18,7 @@
         adjacencyMatrix,
         index1,
         index2,
+        remaining,
         i, j;
     //Create the adjacencyMatrix
     adjacencyMatrix = new Array(nNodes);
@@ -33,8 +31,42 @@
         adjacencyMatrix[i][j] = 0;
       }
     }
-    var ind = nEdges - nNodes;  
-    for (i = 0; i < nEdges - nNodes; i++) {
+   var curr =0 ;
+   var enforcedHC = (Math.floor((Math.random() * 100))%2==0);
+
+    if(enforcedHC){
+       var hcpermute = new Array(nNodes);
+       for(var idx = 0; idx < nNodes; idx++)
+             hcpermute[idx] = idx;
+       for(var idx = 0; idx < nNodes; idx++)
+       {
+         var swpIdx = idx + Math.floor(Math.random() * (nNodes - idx));
+         var tmp = hcpermute[idx];
+         hcpermute[idx] = hcpermute[swpIdx];
+         hcpermute[swpIdx] = tmp;
+         
+       }
+       for(var idx = 0; idx < nNodes; idx++){
+           index1=hcpermute[idx];
+           index2=hcpermute[(idx+1)%nNodes];
+       	   edges[curr] = {
+             startIndex: index1,
+             endIndex: index2
+      	   };
+      	   if (weighted) {
+             edges[curr].weight = 1 + Math.floor((Math.random() * 9));
+      	   }
+           curr = curr + 1;
+           adjacencyMatrix[index1][index2] = 1;
+       }
+       remaining = nEdges;
+       actualEdges = nEdges;
+    }
+    else
+       remaining = nEdges-nNodes;
+
+
+    for (var i = curr;i<remaining ;i++) {
       do {
         index1 = Math.floor((Math.random() * nNodes));
         index2 = Math.floor((Math.random() * nNodes));
@@ -50,57 +82,60 @@
       adjacencyMatrix[index1][index2] = 1;
       // adjacencyMatrix[index2][index1] = 1;
     }
-    for (i = 0; i < nNodes; i++) {
-      var rEmpty=1, cEmpty=1;
-      for (j = 0; j < nNodes; j++) {
-        if(adjacencyMatrix[i][j]!=0){
-		rEmpty=0; 
-		break;
+    if(!enforcedHC){
+      var ind = nEdges - nNodes;
+      for (var i = 0; i < nNodes; i++) {
+        var rEmpty=1, cEmpty=1;
+        for (var j = 0; j < nNodes; j++) {
+          if(adjacencyMatrix[i][j]!=0){
+                rEmpty=0;
+                break;
+          }
         }
-      }
-      if(rEmpty==1){
-	for(var k=0;k<nNodes;k++){
-	  index2=k;
-          if(isEligibleEdge(i,index2)){
+        if(rEmpty==1){
+          for(var k=0;k<nNodes;k++){
+            index2=k;
+            if(isEligibleEdge(i,index2)){
               edges[ind] = {
                 startIndex: i,
                 endIndex: index2
-            };
-            if (weighted) {
-               edges[ind].weight = 1 + Math.floor((Math.random() * 9));
+              };
+              if (weighted) {
+                 edges[ind].weight = 1 + Math.floor((Math.random() * 9));
+              }
+              adjacencyMatrix[i][index2] = 1;
+              ind++;
+              break;
             }
-            adjacencyMatrix[i][index2] = 1;
-            ind++;
-            break;
+          }
+        }
+        for (j = 0; j < nNodes; j++) {
+          if(adjacencyMatrix[j][i]!=0){
+                cEmpty=0;
+                break;
+          }
+        }
+        if(cEmpty==1){
+          for(var k=0;k<nNodes;k++){
+            index1=k;
+            if(isEligibleEdge(k,i)){
+                edges[ind] = {
+                  startIndex: index1,
+                  endIndex: i
+                };
+                if (weighted) {
+                   edges[ind].weight = 1 + Math.floor((Math.random() * 9));
+                }
+                adjacencyMatrix[index1][i] = 1;
+                ind++;
+                break;
+            }
           }
         }
       }
-      for (j = 0; j < nNodes; j++) {
-        if(adjacencyMatrix[j][i]!=0){
-		cEmpty=0; 
-		break;
-        }
-      }
-      if(cEmpty==1){
-	for(var k=0;k<nNodes;k++){
-	  index1=k;
-          if(isEligibleEdge(k,i)){
-              edges[ind] = {
-                startIndex: index1,
-                endIndex: i
-            };
-            if (weighted) {
-               edges[ind].weight = 1 + Math.floor((Math.random() * 9));
-            }
-            adjacencyMatrix[index1][i] = 1;
-            ind++;
-            break;
-          }
-	}
-      }
-	
-    }
-    actualEdges = ind;
+      actualEdges = ind;
+
+     }
 
     return edges;
   }
@@ -125,47 +160,24 @@
     var opts = $.extend(defaultOptions, options),
         weighted = opts.weighted,
         nNodes = opts.nodes,
-        nEdges = opts.edges + nNodes,
+        nEdges = opts.edges,
         nodes = new Array(nNodes),
         edges,
         i;
-
-    enforcedHC = (Math.floor((Math.random() * 100))%2==0); 
 
     // Generate the node values
     for (i = 0; i < nNodes; i++) {
       nodes[i] = String.fromCharCode(i + 65);
     }
     // Generate edges
-    if(enforcedHC)
-      edges = generateRandomEdges(nNodes, nEdges-nNodes, weighted);
-    else
-      edges = generateRandomEdges(nNodes, nEdges, weighted);
+    edges = generateRandomEdges(nNodes, nEdges, weighted);
     // Add the nodes to the graph
     for (i = 0; i < nNodes; i++) {
       graph.addNode(nodes[i]);
     }
-
-    if(enforcedHC){
-       var tmpnodes = graph.nodes()
-       var tmpnodes2 = graph.nodes()
-       hcpermute = new Array(nNodes);
-       for(var idx = 0; idx < tmpnodes.length; idx++)
-       {
-         var swpIdx = idx + Math.floor(Math.random() * (tmpnodes.length - idx));
-         var tmp = tmpnodes[idx];
-         tmpnodes[idx] = tmpnodes[swpIdx];
-         tmpnodes[swpIdx] = tmp;
-       }
-       for(var idx = 0; idx < tmpnodes.length; idx++){
-         hcpermute[idx] = tmpnodes2.indexOf(tmpnodes[idx]);
-       }     
-       for(i=0;i<nNodes;i++)
-         graph.addEdge(tmpnodes[i],tmpnodes[(i+1)%nNodes]);
-    }
-
     // Add the edges to the graph
-    for (i = 0; i < actualEdges; i++) {
+    for (i = 0; i <actualEdges; i++) {
+    //for (i = nEdges-nNodes; i < nEdges; i++) {
       var gNodes  = graph.nodes(),
           start   = gNodes[edges[i].startIndex],
           end     = gNodes[edges[i].endIndex],
