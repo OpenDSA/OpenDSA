a8b5c6a9d98427fbdb4ee25a64b069ded883caf1
David Furcy <furcyd@uwosh.edu>
2015-03-23 09:48:10 -0500

    The applicative- and normal- order proficiency exercises now guarantee that each randomly selected expression will be reduced differently with the two strategies

--- AV/PL/profexercises/applicativeNormalOrderPro.js
+++ AV/PL/profexercises/applicativeNormalOrderPro.js
@@ -1,9 +1,12 @@
+"use strict";
+
+/* global PARAMS, LAMBDA, alert */
 
 $(document).ready(function () {
 
-    var normalOrder = PARAMS["strategy"] === "normal";
-    
+    var normalOrder = PARAMS.strategy === "normal";
     var L = LAMBDA;
+    var jsavArray;
     var settings = new JSAV.utils.Settings($(".jsavsettings"));
     var av = new JSAV($('.avcontainer'), {settings: settings});
     av.recorded();
@@ -36,7 +39,7 @@ $(document).ready(function () {
 	var step;
 	for(var i=0; i<correct.length; i++) {
 	    step = LAMBDA.mySplit(correct[i]);
-	    if (i==0) {
+	    if (i===0) {
 		step.unshift("Initial &lambda; exp: ");
 	    } else{
 		step.unshift("Step " + i + ":");
@@ -76,102 +79,120 @@ $(document).ready(function () {
     
     function init() {
 	var vars = "uvxyz";
-	var numSteps = 3;  // number of reductions in this exercise
+	var numSteps = 3;  // minimum number of reductions in this exercise
 	var e, numCols;
 	if (normalOrder) {
 	    $("#strategy").html("normal-order");
 	}
-	correct = [];
-	while (correct.length-1 < numSteps ||
-	       correct.length-1 > numSteps+1) {
-	    e = L.getRndExp(1,2,5,vars,"");
-	    correct = L.reduceToNormalForm(e,
-			   normalOrder ? "normal" : "applicative");
-	    numCols = correct.map(function (x) { return x[0].length; }).reduce(function(a,b) { return Math.max(a,b);} , -1);
-	    if (correct[0].length > 1  || // eliminate infinite loops
-                numCols > 50) {           // eliminate long expressions
-		correct = [];
+	function infiniteLoop(reduction) { return reduction[0].length > 1; }
+	function tooLong(reduction) { 
+	    return 35 < reduction.map(function (x) { return x[0].length; })
+		.reduce(function(a,b) { return Math.max(a,b);} , -1);
+	}
+	function sameReduction(r1,r2) {
+	    if (r1.length !== r2.length) {
+		return false;
+	    } else {
+		for(var i=1; i<r1.length; i++) {
+		    if (r1[i][0] !== r2[i][0]) {
+			return false;
+		    }
+		}
+		return true;
 	    }
 	}
-	console.log(numCols);
-
+	correct = [];
+	var normalRed, applicativeRed;
+	while (correct.length < numSteps+1 || correct.length > numSteps+2) {
+	    e = L.getRndExp(1,2,6,vars,"");
+	    normalRed = L.reduceToNormalForm(e,"normal");
+	    applicativeRed = L.reduceToNormalForm(e,"applicative");
+	    
+	    if ( infiniteLoop(normalRed) || infiniteLoop(applicativeRed) ||
+		 sameReduction(normalRed,applicativeRed) ||
+		 tooLong(normalRed) || tooLong(applicativeRed) ) {
+		correct = [];
+	    } else {
+		correct = normalOrder ? normalRed : applicativeRed;
+	    }
+	}
 	correct = correct.map(function(a) { return a[0]; });
 	$theExpression.html(correct[0]);
         correctTrimmed = correct.map(function (x) { return x.replace(/\s+/g,''); }); 
-        correctTrimmed = correctTrimmed.map(function (x) { return x.replace(/\u03BB/g,'^')}); 
+        correctTrimmed = correctTrimmed.map(function (x) { return x.replace(/\u03BB/g,'^');}); 
 	step = 1;
 	// jsavArray is used for grading each step
 	jsavArray = av.ds.array( correctTrimmed, {visible: false});
 	return jsavArray;
     }
 	
-	function help() {
-	    alert("At each step of the " +
-		  (normalOrder ? "normal" : "applicative") +
-		  "-order reduction, you must pick the leftmost " +
-		  (normalOrder ? "outermost" : "innermost") +		  
-		  " \u03B2-redex and reduce it.");
-	}
+    function help() {
+	alert("At each step of the " +
+	      (normalOrder ? "normal" : "applicative") +
+	      "-order reduction, you must pick the leftmost " +
+	      (normalOrder ? "outermost" : "innermost") +		  
+	      " \u03B2-redex and reduce it.");
+    }
 
-	function about() {
-	    alert("This exercise was developed by David Furcy.\n\nSolve it" +
-		  " to demonstrate that you have mastered the process of " +
-		  (normalOrder ? "normal" : "applicative") +
-		  "-order reduction in the" +
-		  " \u03BB-calculus.");
-	}
+    function about() {
+	alert("This exercise was developed by David Furcy.\n\nSolve it" +
+	      " to demonstrate that you have mastered the process of " +
+	      (normalOrder ? "normal" : "applicative") +
+	      "-order reduction in the" +
+	      " \u03BB-calculus.");
+    }
 
-	function submit() {
-	    var answer = $('#answer').val().replace(/\s+/g, '');
-	    $('#answer').val("");
-	    if(step < correct.length) {
-		if(answer === correctTrimmed[step]) {
-		    jsavArray.highlight(step);
-		    jsavArray.unhighlight(step-1);
-		    exercise.gradeableStep();
-		    $theExpression.html(correct[step]);
-		    step++;
-		} else {
-		    exercise.gradeableStep();
-		}
-	    } else {
-		exercise.gradeableStep();
-	    }
-	}
-		
-	function done() {
-	    if(step < correct.length) {
-		alert("There are still more reductions to perform.");
-		exercise.gradeableStep();
-	    } else {
-		jsavArray.highlight();
-		alert("Correct! There are no more reductions to perform.");
-		exercise.gradeableStep();
-
-	    }
-	}
-	
-	// Function to fix exercise if an incorrect submission is entered.
-	function fixState(modeljsav) {
-	    if(step < correct.length) {
+    function submit() {
+	var answer = $('#answer').val().replace(/\s+/g, '');
+	$('#answer').val("");
+	if(step < correct.length) {
+	    if(answer === correctTrimmed[step]) {
 		jsavArray.highlight(step);
 		jsavArray.unhighlight(step-1);
+		exercise.gradeableStep();
 		$theExpression.html(correct[step]);
 		step++;
 	    } else {
-		alert("There are no more reductions to be done!");
+		exercise.gradeableStep();
 	    }
+	} else {
+	    exercise.gradeableStep();
 	}
+    }
+    
+    function done() {
+	if(step < correct.length) {
+	    alert("Watch out! There are more reductions to perform.");
+	    exercise.gradeableStep();
+	} else {
+	    jsavArray.highlight();
+	    alert("Correct! There are no more reductions to perform.");
+	    exercise.gradeableStep();
+	    
+	}
+    }
 	
-	var exercise = 	av.exercise(modelSolution, init, 
-				    { compare: {class: "jsavhighlight"},
-				      controls: $('.jsavexercisecontrols'), 
-				      fix: fixState });
-	exercise.reset();
+    // Function to fix exercise if an incorrect submission is entered.
+    function fixState(modeljsav) {
+	if(step < correct.length) {
+	    jsavArray.highlight(step);
+	    jsavArray.unhighlight(step-1);
+	    $theExpression.html(correct[step]);
+	    step++;
+	} else {
+	    alert("There are no more reductions to perform.");
+	}
+    }
+	
+    var exercise = av.exercise(modelSolution, init, 
+			       { compare: {class: "jsavhighlight"},
+				 controls: $('.jsavexercisecontrols'), 
+				 fix: fixState });
+    exercise.reset();
 
-	$('#help').click(help);
-	$('#about').click(about);
-	$('#submit').click(submit);
-	$('#done').click(done);
+    $('#help').click(help);
+    $('#about').click(about);
+    $('#submit').click(submit);
+    $('#done').click(done);
 });
 
