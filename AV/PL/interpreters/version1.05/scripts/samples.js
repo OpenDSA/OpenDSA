ed139232ace6a9795721a729fa36d8d61ca8424c
David Furcy <furcyd@uwosh.edu>
2015-04-03 13:38:16 -0500

    Updated test suite in versions 1.04 and 1.05 of SLang interpreter

--- AV/PL/interpreters/version1.05/scripts/samples.js
+++ AV/PL/interpreters/version1.05/scripts/samples.js
@@ -13,7 +13,7 @@ var samples = [
 /* 4 */   "lambda  . 5",
 /* 5 */   "lambda x . x",
 /* 6 */   "lambda a,b,c . y",
-/* 7 */   "(lambda  . 5 y)",
+/* 7 */   "(lambda x . 5 y)",
 /* 8 */   "(lambda z . z *(y, add1(x)))",
 /* 9 */   "(lambda a,b,c . y 1 2 3)",
 /* 10 */  "(lambda x . *(x,x) +(x,y))",
