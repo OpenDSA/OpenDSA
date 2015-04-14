bcad5552629410ffbfe18c0d9f8c7fe14ce3864d
X455u <kassu7@gmail.com>
2015-04-14 15:10:38 +0300

    Changed the way nodes are split in B+ tree insert.

--- AV/Development/bPlusTreeInsertPRO.js
+++ AV/Development/bPlusTreeInsertPRO.js
@@ -6,7 +6,7 @@
       tree,
       stack,
       nodeSize = parseInt(PARAMS.nodesize || 3, 10),
-      insertSize = Math.min(20, parseInt(PARAMS.insertsize || nodeSize * 4, 10)),
+      insertSize = Math.min(20, parseInt(PARAMS.insertsize || nodeSize * 6, 10)),
 
       // Configurations
       config = ODSA.UTILS.loadConfig({av_container: "jsavcontainer"}),
@@ -18,10 +18,10 @@
       // Create a JSAV instance
       av = new JSAV($("#jsavcontainer"), {settings: settings});
 
-  if (nodeSize < 3) {
+  if (nodeSize < 2) {
     window.alert("Split doesn't work correctly for internal nodes when" +
-      " nodesize is less than 3.\n Nodesize has been set to 3.");
-    nodeSize = 3;
+      " nodesize is less than 2.\n Nodesize has been set to 2.");
+    nodeSize = 2;
   }
 
   av.recorded(); // we are not recording an AV with an algorithm
@@ -54,13 +54,8 @@
     tree.layout();
 
     tree.click(function (index) {
-      if (isFull(this)) {
-        // click on non-leaf node -> split this node
-        splitNode(this);
-      } else {
-        // insert value to node
-        insertValueToNode(this, stack);
-      }
+      if (this.childnodes.length) { return; }
+      insertAndSplit(this, stack);
     });
 
     return tree;
@@ -75,43 +70,21 @@
     jsav.displayInit();
 
     function keyFilter(v) { return v && v <= val; }
-    function checkAndSplit(node) {
-      if (isFull(node)) {
-        jsav.umsg(interpret("av_ms_split"));
-        splitNode(node);
-        return true;
-      }
-      return false;
-    }
+
     while (modelStack.size()) {
       // the value we are inserting
       var val = modelStack.first().value();
       jsav.umsg(interpret("av_ms_search"), {fill: {val: val}});
       // find the node
       var node = modelTree.root();
-      do {
-        if (!node.hasClass("ms-highlight")) {
-          node.addClass("ms-highlight");
-        }
-        jsav.step();
-        // split the node if it is full
-        if (checkAndSplit(node)) {
-          node = node.parent();
-        }
-        if (node.childnodes.length) {
-          // the position of the next child we want to explore
-          var pos = node.value().filter(keyFilter).length;
-          node.removeClass("ms-highlight");
-          node = node.child(pos);
-          node.addClass("ms-highlight");
-        }
-      } while (node.childnodes.length || isFull(node));
+      while (node.childnodes.length) {
+        // the position of the next child we want to explore
+        var pos = node.value().filter(keyFilter).length;
+        node = node.child(pos);
+      }
       // insert the value into the found node
       jsav.umsg(interpret("av_ms_insert"), {fill: {val: val}});
-      insertValueToNode(node, modelStack);
-      if (node !== tree.root()) {
-        node.removeClass("ms-highlight");
-      }
+      insertAndSplit(node, modelStack);
     }
 
     return modelTree;
@@ -134,15 +107,47 @@
     return node.value().every(function (v) { return typeof v === "number"; });
   }
 
-  function splitNode(node) {
+  function updateStack(stack) {
+    if (!stack) { return; }
+    stack.removeFirst();
+    stack.layout();
+  }
+
+  // TODO: shorten or split this function
+  function insertAndSplit(node, stackOrValue) {
+    var stack, val;
+    if (typeof stackOrValue === "number") {
+      val = stackOrValue;
+    } else {
+      stack = stackOrValue;
+      val = stack.first().value();
+    }
+    // return if stack is empty
+    if (stack && !stack.size()) { return; }
+    // set variable values
     var tree = node.container,
         av = tree.jsav,
         nodeSize = tree.options.nodesize,
         parent = node.parent(),
-        arr = node.value(),
-        sliceInd = Math.ceil(nodeSize / 2),
-        left = arr.slice(0, sliceInd),
-        right = arr.slice(sliceInd);
+        newValues = node.value().concat(val).sort(function (a, b) {
+          if (a === "") { a = 1000; }
+          if (b === "") { b = 1000; }
+          return a - b;
+        }),
+        sliceInd = (stack ? Math.floor(nodeSize / 2) + 1 : Math.ceil((nodeSize + 2) / 2)),
+        left = newValues.slice(0, sliceInd),
+        right = newValues.slice(sliceInd);
+    // if there is empty space in the node
+    // insert the value and return
+    if (!isFull(node)) {
+      node.value(newValues);
+      if (stack) { // if we are in a leaf node
+        updateStack(stack);
+        av.gradeableStep();
+      }
+      return;
+    }
+    // else -> node is full, start splitting
     // if there is no parent we are in the root node
     // -> create new root node and set 'node' as child of new root
     if (!parent) {
@@ -150,11 +155,11 @@
       tree.root(parent, {hide: false});
       parent.addChild(node);
     }
-    // create a new node and give the right half to it
+    // create a new node and give the right half of the values to it
     var newNode = tree.newNode(right, parent),
         parentChildIndex = parent.childnodes.indexOf(node),
-        newParentValues = parent.value().slice(0, -1),
-        newParentChildNodes = parent.childnodes.slice(0);
+        newParentChildNodes = parent.childnodes.slice(0),
+        newParentValue;
     // add the new node to the parents child node array
     newParentChildNodes.splice(parentChildIndex + 1, 0, newNode);
     // set the new child nodes to the parent
@@ -162,7 +167,7 @@
     // if the split node was a non-leaf node
     if (node.childnodes.length) {
       // give half of the child nodes to the new node
-      var childSliceInd = Math.floor((nodeSize + 1) / 2),
+      var childSliceInd = Math.ceil((nodeSize + 2) / 2),
           leftChildren = node.childnodes.slice(0, childSliceInd),
           rightChildren = node.childnodes.slice(childSliceInd);
       node._setchildnodes(leftChildren);
@@ -171,40 +176,28 @@
       });
       newNode._setchildnodes(rightChildren);
       // give new values for the parent node
-      newParentValues.splice(parentChildIndex, 0, left[sliceInd - 1]);
+      newParentValue = left[sliceInd - 1];
       // remove the value that was given to the parent node from 'node'
       left[sliceInd - 1] = "";
     } else {
       // give new values for the parent node
-      newParentValues.splice(parentChildIndex, 0, right[0]);
+      newParentValue = right[0];
     }
     // give the left half of the values to node
     node.value(left);
-    // set new values for the parent node
-    parent.value(newParentValues);
     // position the new node on top of node
     newNode.element.position({of: node.element});
-    // move ms-highlight class to parent node if it exists
-    if (node.hasClass("ms-highlight")) {
-      node.removeClass("ms-highlight");
-      node.parent().addClass("ms-highlight");
+    // insert new values for the parent node
+    insertAndSplit(parent, newParentValue);
+    // if we are in a leaf node
+    if (stack) {
+      // update the stack
+      updateStack(stack);
+      // call the layout function
+      tree.layout();
+      // end step and make it gradeable
+      av.gradeableStep();
     }
-    // call the layout function
-    tree.layout();
-    av.gradeableStep();
-  }
-
-  function insertValueToNode(node, stack) {
-    if (!stack.size()) { return; } // return if stack is empty
-    var newValues = node.value().slice(0, -1).concat(stack.first().value()).sort(function (a, b) {
-      if (a === "") { a = 1000; }
-      if (b === "") { b = 1000; }
-      return a - b;
-    });
-    node.value(newValues);
-    stack.removeFirst();
-    stack.layout();
-    stack.jsav.gradeableStep();
   }
 
   // create exercise and reset it
