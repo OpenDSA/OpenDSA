1b42d9aecd9474b4cd52244f7df7f0ea3fc97ea5
bbcrabb <bbcrabb@vt.edu>
2015-04-14 12:05:13 -0400

    creating a visualization to match slideshows

---
+++ AV/Development/GIntroDef2_2CON.js
@@ -0,0 +1,9 @@
+"use strict";
+$(document).ready(function () {
+	var av = new JSAV("GIntroDef2_2CON", {"animationMode" : "none"});
+
+	av.umsg("A graph G=(V,E) consists of a set of vertices V and a set of edges E, such that each edge " + 
+        "in E is a connection between a pair of vertices in V. Here is an undirected graph. A graph whose edges are not " +
+        "directed is called an <i>undirected graph<i/>");
+
+});
