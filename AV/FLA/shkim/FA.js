46457a7bdd6a5cbb91fa558888a12c354ceee25b
s-hkim <sungspot@gmail.com>
2015-06-04 17:14:13 -0400

    Made changes to editing

--- AV/FLA/shkim/FA.js
+++ AV/FLA/shkim/FA.js
@@ -4,11 +4,11 @@
 
 //inefficient: should create a new state object to put in the arrays, or use css classes
 
-  var convertToDFA = function(graph) {
-    $('button').hide();
-    $('input').hide();
-    jsav.label("Converted:");
-    var g = jsav.ds.fa($.extend({width: '90%', height: 440}, {layout: 'automatic'})),
+  var convertToDFA = function(jsav, graph, opts) {
+    // $('button').hide();
+    // $('input').hide();
+    // jsav.label("Converted:");
+    var g = jsav.ds.fa($.extend({layout: 'automatic'}, opts)),
         alphabet = Object.keys(graph.alphabet),
         startState = graph.initial,
         newStates = [];
@@ -54,42 +54,43 @@
     for (var next = nodes.next(); next; next = nodes.next()) {
       next.stateLabel(next.value());
       next.stateLabelPositionUpdate();
-      next.hide();
-      next._stateLabel.hide();
+      //next.hide();
+      //next._stateLabel.hide();
     }
     g.updateNodes();
-    var edges = g.edges();
-    for (next = edges.next(); next; next = edges.next()) {
-      next.hide();
-    }
-    graph.hide();
+    return g;
+    // var edges = g.edges();
+    // for (next = edges.next(); next; next = edges.next()) {
+    //   next.hide();
+    // }
+    // graph.hide();
 
-    jsav.displayInit();
-    var bfs = [],
-        visited = [];
-    bfs.push(g.initial);
-    visited.push(g.initial);
-    while (bfs.length > 0) {
-      var cur = bfs.shift();
-      cur.show();
-      cur._stateLabel.show();
-      var successors = cur.neighbors();
-      for (var next = successors.next(); next; next = successors.next()) {
-        if (!_.contains(visited, next)) {
-          bfs.push(next);
-          visited.push(next);
-        }
-      }
-      jsav.step();
-    }
-    for (var i = 0; i < visited.length; i++) {
-      var outgoing = visited[i].getOutgoing();
-      for (var j = 0; j < outgoing.length; j++) {
-        outgoing[j].show();
-      }
-      jsav.step();
-    }
-    jsav.recorded();
+    // jsav.displayInit();
+    // var bfs = [],
+    //     visited = [];
+    // bfs.push(g.initial);
+    // visited.push(g.initial);
+    // while (bfs.length > 0) {
+    //   var cur = bfs.shift();
+    //   cur.show();
+    //   cur._stateLabel.show();
+    //   var successors = cur.neighbors();
+    //   for (var next = successors.next(); next; next = successors.next()) {
+    //     if (!_.contains(visited, next)) {
+    //       bfs.push(next);
+    //       visited.push(next);
+    //     }
+    //   }
+    //   jsav.step();
+    // }
+    // for (var i = 0; i < visited.length; i++) {
+    //   var outgoing = visited[i].getOutgoing();
+    //   for (var j = 0; j < outgoing.length; j++) {
+    //     outgoing[j].show();
+    //   }
+    //   jsav.step();
+    // }
+    // jsav.recorded();
     //could move the values into mouseover and rename all of the nodes
 
   };
