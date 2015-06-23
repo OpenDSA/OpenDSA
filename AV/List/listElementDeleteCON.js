ac9ff80949ae482ccb9650d72a4ab386f3007d71
Cliff Shaffer <shaffer@vt.edu>
2015-06-23 00:56:43 -0400

    Updates to listElement text and AVs

--- AV/List/listElementDeleteCON.js
+++ AV/List/listElementDeleteCON.js
@@ -1,109 +1,89 @@
-// Garbage collection of lists
-(function ($) {
-  var jsav = new JSAV('listElementDeleteCON');
+/*global ODSA */
+"use strict";
+// Written by Jun Yang and Cliff Shaffer
+// Deleting lists
+$(document).ready(function () {
+  var av_name = "listElementDeleteCON";
+  var config = ODSA.UTILS.loadConfig({"av_name": av_name}),
+      interpret = config.interpreter;       // get the interpreter
+  var av = new JSAV(av_name);
+
   // Relative offsets
   var leftMargin = 200;
-  var topMargin = 25;
+  var topMargin = 35;
+
   // JSAV list
-  var list1 = jsav.ds.list({
-      'left': leftMargin,
-      'top': topMargin
-    });
-  list1.addFirst('null').addFirst('').addFirst('').addFirst('').addFirst('').addFirst('null');
+  var list1 = av.ds.list({left: leftMargin, top: topMargin});
+  list1.addFirst("null").addFirst("").addFirst("").addFirst("").addFirst("").addFirst("null");
   list1.layout();
-  list1.hide();
 
-  var list2 = jsav.ds.list({
-      'left': leftMargin,
-      'top': topMargin + 100
-    });
-  list2.addFirst('null').addFirst('').addFirst('').addFirst('').addFirst('').addFirst('null');
-  list2.layout();
-  list2.hide();
-  var curr = setPointer('curr', list1.get(0));
-  var head = setPointer('head', list1.get(2));
-  var tail = setPointer('tail', list1.get(5));
+  var head = av.pointer("head", list1.get(0));
+  var curr = av.pointer("curr", list1.get(2));
+  var tail = av.pointer("tail", list1.get(5));
 
-
-  jsav.umsg('The third issue that users of a list implementation must face is primarily of concern when programming in languages that do not support automatic garbage collection. ');
-  jsav.displayInit();
-  list1.show();
-  var bigData = jsav.ds.array([
-      'ID : 546457',
-      'Name : Jake',
-      'Phone : 5405642511',
-      'Email : example@vt.edu',
-      'Office : 212'
-    ], {
-      layout: 'vertical',
-      top: 140,
-      left: 40
-    });
+  var bigData = av.ds.array([
+      "ID : 546457",
+      "Name : Jake",
+      "Phone : 5405642511",
+      "Email : example@vt.edu",
+      "Office : 212"
+    ], {layout: "vertical", left: 40, top: 135});
   bigData.addClass(true, "widerecord");
 
-  var bigData1 = jsav.ds.array([
-      'ID : 546213',
-      'Name : Mike',
-      'Phone : 5405642513',
-      'Email : example@vt.edu',
-      'Office : 212'
-    ], {
-      layout: 'vertical',
-      top: 140,
-      left: 240
-    });
+  var bigData1 = av.ds.array([
+      "ID : 546213",
+      "Name : Mike",
+      "Phone : 5405642513",
+      "Email : example@vt.edu",
+      "Office : 212"
+    ], {layout: "vertical", left: 240, top: 135});
   bigData1.addClass(true, "widerecord");
 
-  var bigData2 = jsav.ds.array([
-      'ID : 546805',
-      'Name : John',
-      'Phone : 5405642552',
-      'Email : example@vt.edu',
-      'Office : 212'
-    ], {
-      layout: 'vertical',
-      top: 140,
-      left: 440
-    });
+  var bigData2 = av.ds.array([
+      "ID : 546805",
+      "Name : John",
+      "Phone : 5405642552",
+      "Email : example@vt.edu",
+      "Office : 212"
+    ], {layout: "vertical", left: 440, top: 135});
   bigData2.addClass(true, "widerecord");
 
-  var bigData3 = jsav.ds.array([
-      'ID : 546991',
-      'Name : Lucy',
-      'Phone : 5405642568',
-      'Email : example@vt.edu',
-      'Office : 212'
-    ], {
-      layout: 'vertical',
-      top: 140,
-      left: 640
-    });
+  var bigData3 = av.ds.array([
+      "ID : 546991",
+      "Name : Lucy",
+      "Phone : 5405642568",
+      "Email : example@vt.edu",
+      "Office : 212"
+    ], {layout: "vertical", left: 640, top: 135});
   bigData3.addClass(true, "widerecord");
 
-  var listP1 = jsav.g.line(320, 55, 120, 150,
-                 {'arrow-end': 'classic-wide', 'stroke-width': 2});
-  var listP2 = jsav.g.line(410, 55, 320, 150,
-                 {'arrow-end': 'classic-wide', 'stroke-width': 2});
-  var listP3 = jsav.g.line(500, 55, 520, 150,
-                 {'arrow-end': 'classic-wide', 'stroke-width': 2});
-  var listP4 = jsav.g.line(590, 55, 720, 150,
-                 {'arrow-end': 'classic-wide', 'stroke-width': 2});
-  jsav.umsg('That is how to deal with the memory of the objects stored on the list when the list is deleted or the clear method is called. In C++ for example, list destructor and the clear method are problematic in that there is a potential that they will be misused. ');
-  jsav.step();
+  var listP1 = av.g.line(305, 65, 120, 150,
+                 {"arrow-end": "classic-wide", "stroke-width": 2});
+  var listP2 = av.g.line(388, 65, 320, 150,
+                 {"arrow-end": "classic-wide", "stroke-width": 2});
+  var listP3 = av.g.line(471, 65, 520, 150,
+                 {"arrow-end": "classic-wide", "stroke-width": 2});
+  var listP4 = av.g.line(554, 65, 720, 150,
+                 {"arrow-end": "classic-wide", "stroke-width": 2});
 
+  // Slide 1
+  av.umsg(interpret("av_c1"));
+  av.displayInit();
+
+  // Slide 2
   listP1.hide();
   listP2.hide();
   listP3.hide();
   listP4.hide();
   list1.hide();
+  curr.hide();
+  head.hide();
+  tail.hide();
   bigData.highlight();
   bigData1.highlight();
   bigData2.highlight();
   bigData3.highlight();
 
-  jsav.umsg('Deleting listArray in the array-based implementation, or deleting a link node in the linked list implementation, might remove the only reference to an object, leaving its memory space inaccessible. ');
-  jsav.step();
-
-  jsav.umsg('Unfortunately, there is no way for the list implementation to know whether a given object is pointed to in another part of the program or not. Thus, the user of the list must be responsible for deleting these objects when that is appropriate.');
-  jsav.recorded();
-}(jQuery));
+  av.umsg(interpret("av_c2"));
+  av.recorded();
+});
