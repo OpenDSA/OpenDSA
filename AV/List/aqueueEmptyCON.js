da9919a5ea73b14c1b14a9e243805eceba21e345
Cliff Shaffer <shaffer@vt.edu>
2015-06-29 16:00:25 -0400

    Fixed aqueue exercises

--- AV/List/aqueueEmptyCON.js
+++ AV/List/aqueueEmptyCON.js
@@ -16,14 +16,14 @@ $(document).ready(function () {
   var cir = av.circular(cx, cy, r1, r2, 12, {"stroke-width" : 2});
 
   // Slide 1
-  av.umsg("There remains one more serious, though subtle, problem to the array-based queue implementation. How can we recognize when the queue is empty or full?");
+  av.umsg(interpret("av_c1"));
   av.displayInit();
 
   // Slide 2
   cir.value(10, "12");
   cir.highlight(10);
   var frontP = cir.pointer("front,rear", 10);
-  av.umsg("Assume that front stores the array index for the front element in the queue, and rear stores the array index for the rear element. If both front and rear have the same position, then with this scheme there must be one element in the queue.");
+  av.umsg(interpret("av_c2"));
   av.step();
 
   // Slide 3
@@ -33,11 +33,11 @@ $(document).ready(function () {
   frontP.label.hide();
   var frontP1 = cir.pointer("front", 11);
   var rearP1 = cir.pointer("rear", 10);
-  av.umsg("Thus, an empty queue would be recognized by having rear be <b>one less</b> than front (taking into account the fact that the queue is circular, so position <code>size-1</code> is actually considered to be one less than position 0).");
+  av.umsg(interpret("av_c3"));
   av.step();
 
   // Slide 4
-  av.umsg("But what if the queue is completely full? In other words, what is the situation when a queue with $n$ array positions available contains $n$ elements? In this case, if the front element is in position 0, then the rear element is in position <code>size-1</code>.");
+  av.umsg(interpret("av_c4"));
   frontP1.arrow.hide();
   frontP1.label.hide();
   rearP1.arrow.hide();
@@ -50,18 +50,18 @@ $(document).ready(function () {
   av.step();
 
   // Slide 5
-  av.umsg("But this means that the value for rear is one less than the value for front when the circular nature of the queue is taken into account. In other words, the full queue is indistinguishable from the empty queue!");
+  av.umsg(interpret("av_c5"));
   av.step();
 
   // Slide 6
-  av.umsg("You might think that the problem is in the assumption about front and rear being defined to store the array indices of the front and rear elements, respectively, and that some modification in this definition will allow a solution. ");
+  av.umsg(interpret("av_c6"));
   av.step();
 
   // Slide 7
-  av.umsg(" Unfortunately, the problem cannot be remedied by a simple change to the definition for front and rear, because of the number of conditions or states that the queue can be in.");
+  av.umsg(interpret("av_c7"));
   av.step();
 
   // Slide 8
-  av.umsg("Ignoring the actual position of the first element, and ignoring the actual values of the elements stored in the queue, how many different states are there? There can be no elements in the queue, one element, two, and so on. At most there can be $n$ elements in the queue if there are $n$ array positions. This means that there are $n+1$ different states for the queue (0 through $n$ elements are possible).");
+  av.umsg(interpret("av_c8"));
   av.recorded();
 });
