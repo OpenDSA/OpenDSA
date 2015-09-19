251c40ab23e5cedc9c65dbcb4c38d9a4c441e1b3
Cliff Shaffer <shaffer@vt.edu>
2015-09-18 22:05:24 -0400

    Replace MathJAX with katex in GrowthRatesPRO

--- Exercises/AlgAnal/GrowthRatesPRO.js
+++ Exercises/AlgAnal/GrowthRatesPRO.js
@@ -1,12 +1,13 @@
-/*global console, JSAV, window, MathJax */
+/*global JSAV, window, katex */
 (function() {
   "use strict";
   var av,           // The JSAV object
-      answerArr = [], // The (internal) array that stores the correct answer
-      cloneArr = [],  // Copy of the original (internal) array for reset
-      functions = [], // The list of functions to choose from
-      jsavArr,        // The array that the user manipulates (JSAV object)
-      selected_index; // Position that has been selected by user for swap
+      answerIndex = [], // The current order for the user's functions
+      cloneArr = [],    // The display functions, in original order
+      cloneIndex = [],  // Original order of the indices for the functions
+      functions = [],   // The list of functions to choose from
+      jsavArr,          // The array that the user manipulates (JSAV object)
+      selected_index;   // Position that has been selected by user for swap
 
   var growthRatesPRO = {
     userInput: null,      // Boolean: Tells us if user ever did anything
@@ -20,18 +21,10 @@
         jsavArr.swap(selected_index, index);
         jsavArr.css(index, {"font-size": "100%"});
         jsavArr.css(selected_index, {"font-size": "100%"});
+        growthRatesPRO.swap(answerIndex, selected_index, index);
         selected_index = -1;  // Reset to nothing selected
       }
       growthRatesPRO.userInput = true;
-    },
-
-    // reset function definition
-    f_reset: function() {
-      jsavArr.clear();             // Re-initialize the displayed array object
-      jsavArr = av.ds.array(cloneArr, {indexed: false, center: false, left: -5});
-      jsavArr.click(growthRatesPRO.clickHandler); // Rebind click handler after reset
-      growthRatesPRO.userInput = false;
-      selected_index = -1;         // Reset to nothing selected
     },
 
     // swap two values in array
@@ -41,70 +34,60 @@
       arr[j] = temp;
     },
 
+    // reset function definition
+    f_reset: function() {
+      if (jsavArr) { jsavArr.clear(); }
+      jsavArr = av.ds.array(cloneArr, {indexed: false, center: false, left: -5});
+      // Bind the clickHandler to handle click events on the array
+      jsavArr.click(growthRatesPRO.clickHandler); // Rebind click handler after reset
+      answerIndex = cloneIndex.slice(0);
+      growthRatesPRO.userInput = false;
+      selected_index = -1;         // Reset to nothing selected
+    },
+
     // Initialise the exercise
     initJSAV: function(arr_size) {
-      var i, j;
-      growthRatesPRO.userInput = false;
-      selected_index = -1;
       functions = [
-        "$5 \\log \\log n$",
-        "$2 \\log n$",
-        "$\\log^2 n$",
-        "$2 \\log^3 n$",
-        "$7 \\sqrt{n}$",
-        "$\\sqrt{n} \\log n$",
-        "$n \\log \\log n$",
-        "$9 n \\log n$",
-        "$n \\log^2 n$",
-        "$n^{4/3}$",
-        "$n^2$",
-        "$n^4$",
-        "$2^{\\log n}$",
-        "$2^{\\sqrt{n}}$",
-        "$2^n$",
-        "$2^{n^2}$"
+        "5 \\log \\log n",
+        "2 \\log n",
+        "\\log^2 n",
+        "2 \\log^3 n",
+        "7 \\sqrt{n}",
+        "\\sqrt{n} \\log n",
+        "n \\log \\log n",
+        "9 n \\log n",
+        "n \\log^2 n",
+        "n^{4/3}",
+        "n^2",
+        "n^4",
+        "2^{\\log n}",
+        "2^{\\sqrt{n}}",
+        "2^n",
+        "2^{n^2}"
       ];
-      answerArr.length = 0; // Out with the old
+
+      // Out with the old
+      cloneIndex.length = 0;
+      cloneArr.length = 0;
 
       av = new JSAV("GrowthRatesPRO");
       av.recorded();
 
-      MathJax.Hub.Config({tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]], displayMath: [["$$", "$$"], ["\\[", "\\]"]], processEscapes: true}});
-
       growthRatesPRO.randomize(arr_size, functions.length);
-      jsavArr = av.ds.array([], {indexed: false, center: false, left: -5});
-      for (i = 0; i < arr_size; i++) {
-        jsavArr.value(i, answerArr[i]);
-        console.log(answerArr[i]);
-      }
-      MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 
-      // Now make a copy
-      cloneArr = answerArr.slice(0);
+      growthRatesPRO.f_reset();
 
-      // Compute the correct Answer
-      for (i = 0; i < arr_size - 1; i++) {
-        for (j = 1; j < arr_size - i; j++) {
-          if (functions.indexOf(answerArr[j - 1]) > functions.indexOf(answerArr[j])) {
-            growthRatesPRO.swap(answerArr, j - 1, j);
-          }
-        }
-      }
-
-      // Bind the clickHandler to handle click events on the array
-      jsavArr.click(growthRatesPRO.clickHandler);
       // Set up handler for reset button
       $("#reset").click(function() {
         growthRatesPRO.f_reset();
-        MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
       });
     },
 
     // Check student's answer for correctness: User's array must match answer
     checkAnswer: function(arr_size) {
       var i;
-      for (i = 0; i < arr_size; i++) {
-        if (jsavArr.value(i) !== answerArr[i]) {
+      for (i = 0; i < (arr_size - 1); i++) {
+        if (answerIndex[i] > answerIndex[i + 1]) {
           return false;
         }
       }
@@ -116,8 +99,10 @@
       var randomIndex;
       while (count < arr_size) {
         randomIndex = Math.floor(Math.random() * numfuncs);
-        if (answerArr.indexOf(functions[randomIndex]) === -1) {
-          answerArr.push(functions[randomIndex]);
+        if (functions[randomIndex] !== "") {
+          cloneArr.push(katex.renderToString(functions[randomIndex]));
+          cloneIndex.push(randomIndex);
+          functions[randomIndex] = "";
           count++;
         }
       }
