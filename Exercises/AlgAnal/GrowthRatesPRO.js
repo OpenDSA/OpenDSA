49cb54bec9b10ae12e89b9d0c000a371b8fe34a6
Cliff Shaffer <shaffer@vt.edu>
2015-09-18 21:19:01 -0400

    lint'ing GrowthRates

--- Exercises/AlgAnal/GrowthRatesPRO.js
+++ Exercises/AlgAnal/GrowthRatesPRO.js
@@ -1,10 +1,10 @@
-/*global console,  MathJax */
+/*global console, JSAV, window, MathJax */
 (function() {
   "use strict";
-  var jsav,           // The JSAV object
+  var av,           // The JSAV object
       answerArr = [], // The (internal) array that stores the correct answer
-      cloneArr = [],  // A copy of the (internal) array at the start of the exercise for reset
-      functions = [],
+      cloneArr = [],  // Copy of the original (internal) array for reset
+      functions = [], // The list of functions to choose from
       jsavArr,        // The array that the user manipulates (JSAV object)
       selected_index; // Position that has been selected by user for swap
 
@@ -12,12 +12,11 @@
     userInput: null,      // Boolean: Tells us if user ever did anything
 
     // Click event handler on the array
-    clickHandler: function (index, e) {
+    clickHandler: function(index) {
       if (selected_index === -1) { // if nothing currently selected
         jsavArr.css(index, {"font-size": "130%"});
         selected_index = index;
-      }
-      else {
+      } else {
         jsavArr.swap(selected_index, index);
         jsavArr.css(index, {"font-size": "100%"});
         jsavArr.css(selected_index, {"font-size": "100%"});
@@ -27,24 +26,24 @@
     },
 
     // reset function definition
-    f_reset: function () {
+    f_reset: function() {
       jsavArr.clear();             // Re-initialize the displayed array object
-      jsavArr = jsav.ds.array(cloneArr, {indexed: false, center: false, left: -5});
+      jsavArr = av.ds.array(cloneArr, {indexed: false, center: false, left: -5});
       jsavArr.click(growthRatesPRO.clickHandler); // Rebind click handler after reset
       growthRatesPRO.userInput = false;
       selected_index = -1;         // Reset to nothing selected
     },
 
     // swap two values in array
-    swap: function (arr, i, j) {
+    swap: function(arr, i, j) {
       var temp = arr[i];
       arr[i] = arr[j];
       arr[j] = temp;
     },
 
     // Initialise the exercise
-    initJSAV: function (arr_size) {
-      var i, j, value;
+    initJSAV: function(arr_size) {
+      var i, j;
       growthRatesPRO.userInput = false;
       selected_index = -1;
       functions = [
@@ -67,14 +66,14 @@
       ];
       answerArr.length = 0; // Out with the old
 
-      jsav = new JSAV("GrowthRatesPRO");
-      jsav.recorded();
+      av = new JSAV("GrowthRatesPRO");
+      av.recorded();
 
-      MathJax.Hub.Config({tex2jax: {inlineMath: [['$','$'], ['\\(','\\)']], displayMath: [ ['$$','$$'], ["\\[","\\]"] ], processEscapes: true}});
+      MathJax.Hub.Config({tex2jax: {inlineMath: [["$", "$"], ["\\(", "\\)"]], displayMath: [["$$", "$$"], ["\\[", "\\]"]], processEscapes: true}});
 
       growthRatesPRO.randomize(arr_size, functions.length);
-      jsavArr = jsav.ds.array([], {indexed: false, center: false, left: -5});
-      for(i = 0; i < arr_size; i++){
+      jsavArr = av.ds.array([], {indexed: false, center: false, left: -5});
+      for (i = 0; i < arr_size; i++) {
         jsavArr.value(i, answerArr[i]);
         console.log(answerArr[i]);
       }
@@ -85,9 +84,9 @@
 
       // Compute the correct Answer
       for (i = 0; i < arr_size - 1; i++) {
-        for(j = 1; j < arr_size - i; j++) {
-          if (functions.indexOf(answerArr[j-1]) > functions.indexOf(answerArr[j])) {
-            growthRatesPRO.swap(answerArr, j-1, j);
+        for (j = 1; j < arr_size - i; j++) {
+          if (functions.indexOf(answerArr[j - 1]) > functions.indexOf(answerArr[j])) {
+            growthRatesPRO.swap(answerArr, j - 1, j);
           }
         }
       }
@@ -95,14 +94,14 @@
       // Bind the clickHandler to handle click events on the array
       jsavArr.click(growthRatesPRO.clickHandler);
       // Set up handler for reset button
-      $("#reset").click(function () {
+      $("#reset").click(function() {
         growthRatesPRO.f_reset();
         MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
       });
     },
 
     // Check student's answer for correctness: User's array must match answer
-    checkAnswer: function (arr_size) {
+    checkAnswer: function(arr_size) {
       var i;
       for (i = 0; i < arr_size; i++) {
         if (jsavArr.value(i) !== answerArr[i]) {
@@ -112,12 +111,12 @@
       return true;
     },
 
-    randomize: function (arr_size, numfuncs) {
+    randomize: function(arr_size, numfuncs) {
       var count = 0;
       var randomIndex;
-      while(count < arr_size){
+      while (count < arr_size) {
         randomIndex = Math.floor(Math.random() * numfuncs);
-        if(answerArr.indexOf(functions[randomIndex]) === -1) {
+        if (answerArr.indexOf(functions[randomIndex]) === -1) {
           answerArr.push(functions[randomIndex]);
           count++;
         }
