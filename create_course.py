c0c30620fc0b057a397f5eca8103af5913bcbb2d
Hosam Shahin <hosamlshahin@gmail.com>
2015-10-03 10:33:15 -0400

    Add create_course.py

--- create_course.py
+++ create_course.py
@@ -1,33 +1,24 @@
 import json, sys, requests
-
-# print(sys.argv[1])
-# print(sys.argv[2])
+'''
+This program sends the required information to OpenDSA-server api to create canvas
+course and to register the book.
+'''
 
 book_json_file = sys.argv[1]
 conf_json_file = sys.argv[2]
+
 f = open(book_json_file, 'r')
 book_json = json.loads(f.read())
 f.close()
+
 f = open(conf_json_file, 'r')
 conf_json = json.loads(f.read())
 f.close()
 
-# book_json= json.loads(sys.argv[1])
-# conf_json= json.loads(sys.argv[2])
-
-# req = urllib2.Request('http://abc.com/api/posts/create')
-# req.add_header('Content-Type', 'application/json')
-
 conf_json['book_json'] = book_json
-# print(json.dumps(conf_json))
 
-# response = urllib2.urlopen(req, json.dumps(data))
-# use requests
 url = 'https://ltitest.cs.vt.edu:8443/api/v1/module/createcourse/'
-# payload = {'some': 'data'}
 headers = {'content-type': 'application/json'}
 
 response = requests.post(url, data=json.dumps(conf_json), headers=headers, verify=False)
-# response = requests.post(url, data=conf_json, headers=headers, verify=False)
-# response = requests.post(url, data=conf_json, verify=False)
 print response.content
