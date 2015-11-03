bebe019ce1a187c3e84a4bf731fd8e6692cd8170
Hosam Shahin <hosamlshahin@gmail.com>
2015-11-02 21:59:12 -0500

    Add dispModComp to global scope

--- tools/ODSA_Config.py
+++ tools/ODSA_Config.py
@@ -27,7 +27,7 @@ error_count = 0
 required_fields = ['chapters', 'code_lang', 'module_origin', 'title']
 
 optional_fields = ['allow_anonymous_credit', 'assumes', 'av_origin', 'av_root_dir', 'build_cmap', 'build_dir', 'build_JSAV', 'code_dir', 'exercise_origin', 'exercises_root_dir', 'exercise_server',
-                   'glob_mod_options', 'glob_exer_options', 'lang', 'logging_server', 'req_full_ss', 'score_server', 'start_chap_num', 'suppress_todo', 'tabbed_codeinc', 'theme', 'theme_dir', 'LTI']
+                   'glob_mod_options', 'glob_exer_options', 'lang', 'logging_server', 'req_full_ss', 'score_server', 'start_chap_num', 'suppress_todo', 'tabbed_codeinc', 'theme', 'theme_dir', 'LTI', 'dispModComp']
 
 lang_file = os.path.abspath('tools/language_msg.json')
 
@@ -371,6 +371,9 @@ def set_defaults(conf_data):
         conf_data['glob_mod_options'] = {}
 
     # If no global exercise options are specified, defer to exercise-specific options or the defaults in odsaUtils.js
+    if 'dispModComp' not in conf_data:
+        conf_data['dispModComp'] = True
+
     if 'glob_exer_options' not in conf_data:
         conf_data['glob_exer_options'] = {}
 
