8c725e107f886415ad423416d88e7c2ece092081
Cliff Shaffer <shaffer@vt.edu>
2015-11-07 19:59:18 -0500

    More C++ utils testing

--- SourceCode/C++/Utils/PermuteTest.cpp
+++ SourceCode/C++/Utils/PermuteTest.cpp
@@ -2,15 +2,7 @@
 #include <cstdlib>
 #include <time.h>
 #include "utils.h"
-
-/* *** ODSATag: Permute *** */
-//Randomly permute the values in array A
-void permute(int A[], int n) {
-  for (int i = n; i > 0; i--) // for each i
-    swap(A, i-1, int(Random(i)));    //   swap A[i-1] with a random
-                                     //   position in the range 0 to i-1.
-}
-/* *** ODSAendTag: Permute *** */
+#include "Permute.h"
 
 int main(void) {
   const int n = 10;
