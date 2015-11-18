fe9397431322cc4c29413b9d827a8e9374b26d5d
Cliff Shaffer <shaffer@vt.edu>
2015-11-18 16:45:13 -0500

    Update to RecurTutor AVs

--- AV/RecurTutor/recurTraceSum2CON.js
+++ AV/RecurTutor/recurTraceSum2CON.js
@@ -4,29 +4,32 @@
 $(document).ready(function () {
   "use strict";
   var av_name = "recurTraceSum2CON";
+  var config = ODSA.UTILS.loadConfig({"av_name": av_name}),
+      interpret = config.interpreter,       // get the interpreter
+      code = config.code;           
   var av = new JSAV(av_name);
 
   // Slide 1
-  av.umsg("The process must eventually reach a base case.");
-  var pseudo = av.code({url: "../../../SourceCode/Java/RecurTutor/WrtSumV4.java",
-                       lineNumbers: false,});
+  av.umsg(interpret("av_c1"));
+  var pseudo = av.code(code);
+  
   pseudo.highlight(2);
   av.displayInit();
   av.step();
 
   // Slide 2
-  av.umsg("Thus, the value returned by the base case is important.");
+  av.umsg(interpret("av_c2"));
   pseudo.unhighlight(2);
   pseudo.highlight(3);
   av.step();
 
   // Slide 3
-  av.umsg("To keep track of what is going on, it can be helpful to label recursive calls. A recursive call, like any other function call, eventually returns back to the point of being called. However, since you are calling the same function, it is easy to make mistakes when tracing the code.");
+  av.umsg(interpret("av_c3"));
   pseudo.unhighlight(3);
   pseudo.highlight(5);
   av.step();
   
   // Slide 4
-  av.umsg("Recursion involves a 'winding' phase where the calls are progressively getting closer to the base case, and you are getting to smaller and smaller problems, and an 'unwinding' phase, when you begin to return back to the original call. It is usually in the 'unwinding' phase where the solution is generated.");
+  av.umsg(interpret("av_c4"));
   av.recorded();
 });
