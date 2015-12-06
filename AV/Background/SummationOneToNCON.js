6e41b05a85eb71a9bd5cbfb25de355cd31d31efe
mfseddik <mfs2004_465@hotmail.com>
2015-12-05 23:25:07 -0500

    Coloring equation terms

--- AV/Background/SummationOneToNCON.js
+++ AV/Background/SummationOneToNCON.js
@@ -12,10 +12,8 @@ $(document).ready(function() {
   var rectWidth = 50;
   var leftAlign = 300;
   var topAlign = 110;
-  var set;
 
   av = new JSAV(av_name);
-  set = av.g.set();
 
   //Slide 1
   av.umsg(interpret("av_c1"));
@@ -28,50 +26,65 @@ $(document).ready(function() {
   //Slide 3
   av.umsg(interpret("av_c3.1"));
   av.umsg(interpret("av_c3.2"), {preserve: true});
-  set.push(av.g.rect(leftAlign, topAlign, rectWidth, rectHeight));
+  var rect1 = av.g.rect(leftAlign, topAlign, rectWidth, rectHeight).css({"fill":"red"});
   av.label("$i = 1$",  {top: topAlign + 12, left: leftAlign + 10});
   av.step();
 
   //Slide 4
   av.umsg(interpret("av_c4.1"));
   av.umsg(interpret("av_c4.2"), {preserve: true});
-  set.push(av.g.rect(leftAlign + rectWidth, topAlign, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + rectWidth, topAlign - rectHeight, rectWidth, rectHeight));
+  rect1.css({"fill":"none"});
+  var rect21 = av.g.rect(leftAlign + rectWidth, topAlign, rectWidth, rectHeight).css({"fill":"red"});
+  var rect22 = av.g.rect(leftAlign + rectWidth, topAlign - rectHeight, rectWidth, rectHeight).css({"fill":"red"});
   av.label("$i = 2$",  {top: topAlign + 12, left: leftAlign + rectWidth + 10});
   av.step();
 
   //Slide 5
   av.umsg(interpret("av_c5.1"));
   av.umsg(interpret("av_c5.2"), {preserve: true});
-  set.push(av.g.rect(leftAlign + 2 * rectWidth, topAlign, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 2 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 2 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight));
+  rect21.css({"fill":"none"});
+  rect22.css({"fill":"none"});
+  var rect31 = av.g.rect(leftAlign + 2 * rectWidth, topAlign, rectWidth, rectHeight).css({"fill":"red"});
+  var rect32 = av.g.rect(leftAlign + 2 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect33 = av.g.rect(leftAlign + 2 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
   av.label("$i = 3$",  {top: topAlign + 12, left: leftAlign + 2 * rectWidth + 10});
   av.step();
 
   //Slide 6
   av.umsg(interpret("av_c6.1"));
   av.umsg(interpret("av_c6.2"), {preserve: true});
-  set.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign - 3 * rectHeight, rectWidth, rectHeight));
+  rect31.css({"fill":"none"});
+  rect32.css({"fill":"none"});
+  rect33.css({"fill":"none"});
+  var rect41 = av.g.rect(leftAlign + 3 * rectWidth, topAlign, rectWidth, rectHeight).css({"fill":"red"});
+  var rect42 = av.g.rect(leftAlign + 3 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect43 = av.g.rect(leftAlign + 3 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect44 = av.g.rect(leftAlign + 3 * rectWidth, topAlign - 3 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
   av.label("$i = 4$",  {top: topAlign + 12, left: leftAlign + 3 * rectWidth + 10});
   av.step();
 
   //Slide 7
   av.umsg(interpret("av_c7.1"));
   av.umsg(interpret("av_c7.2"), {preserve: true});
-  set.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign - 3 * rectHeight, rectWidth, rectHeight));
-  set.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign - 4 * rectHeight, rectWidth, rectHeight));
+  rect41.css({"fill":"none"});
+  rect42.css({"fill":"none"});
+  rect43.css({"fill":"none"});
+  rect44.css({"fill":"none"});
+  var rect51 = av.g.rect(leftAlign + 4 * rectWidth, topAlign, rectWidth, rectHeight).css({"fill":"red"});
+  var rect52 = av.g.rect(leftAlign + 4 * rectWidth, topAlign - rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect53 = av.g.rect(leftAlign + 4 * rectWidth, topAlign - 2 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect54 = av.g.rect(leftAlign + 4 * rectWidth, topAlign - 3 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
+  var rect55 = av.g.rect(leftAlign + 4 * rectWidth, topAlign - 4 * rectHeight, rectWidth, rectHeight).css({"fill":"red"});
   av.label("$i = 5$",  {top: topAlign + 12, left: leftAlign + 4 * rectWidth + 10});
   av.step();
 
   //Slide 8
   av.umsg(interpret("av_c8"));
+  rect51.css({"fill":"none"});
+  rect52.css({"fill":"none"});
+  rect53.css({"fill":"none"});
+  rect54.css({"fill":"none"});
+  rect55.css({"fill":"none"});
   av.label("|---------------- $n$ ------------------|",
            {top: topAlign + 25, left: leftAlign + 20}).css({"font-size": "16px", "text-align": "center"});
   av.label("|------- $n$ -------|",
