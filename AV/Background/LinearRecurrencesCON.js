e37483915633e9b6b201beba3b90580a09250f0b
Cliff Shaffer <shaffer@vt.edu>
2015-12-05 23:58:57 -0500

    Polish Background chapter slides

--- AV/Background/LinearRecurrencesCON.js
+++ AV/Background/LinearRecurrencesCON.js
@@ -1,136 +1,107 @@
 /*global ODSA */
-"use strict";
 // Written by Mohammed Farghally and Cliff Shaffer
 // Linear Recurrences
-$(document).ready(function () {
+$(document).ready(function() {
+  "use strict";
   var av_name = "LinearRecurrencesCON";
   // Load the config object with interpreter and code created by odsaUtils.js
-  var config = ODSA.UTILS.loadConfig({"av_name": av_name}),
-      interpret = config.interpreter,       // get the interpreter
-      code = config.code;                   // get the code object
+  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
+      interpret = config.interpreter;       // get the interpreter
   var av;
   var graph;
   var leftAlign = 10;
-  var topAlign = 10;
+  var topAlign = 0;
   var nodeGap = 50;
   var nodeHeight = 40;
   var nodeWidth = 40;
-  var labelShift = 10;
   var labelSet;
-  
+
   av = new JSAV(av_name);
   labelSet = [];
-	
-  //Slide 1	
-  av.umsg(interpret("Slide 1"));
+
+  // Slide 1
+  av.umsg(interpret("av_c1"));
   av.displayInit();
 
-	//Slide 2
-  av.umsg(interpret("Slide 2.1"));
-  av.umsg(interpret("Slide 2.2"), {"preserve": true});
-  // labelSet.push(av.label("<b><u>Amount of Work<b><u>", {"top": topAlign - 10 , "left": leftAlign + 200}));
-  // labelSet.push(av.label("----------------------------------", {"top": topAlign + 0.5 * nodeHeight, "left": leftAlign + nodeWidth + labelShift}));
-  // labelSet.push(av.label("1", {"top": topAlign + 0.5 * nodeHeight , "left": leftAlign + nodeWidth + labelShift * 20}));
-  graph = av.ds.graph({"left": leftAlign, "top": topAlign, layout: "manual", directed: false});
-  var n = graph.addNode("n", {"left": leftAlign, "top": topAlign});
-  var nMinusOne = graph.addNode("n-1", {"left": leftAlign + nodeWidth + nodeGap, "top": topAlign});
-  var oneTwo = graph.addEdge(n, nMinusOne, {"weight": "<b>1 +</b>"});
+  // Slide 2
+  av.umsg(interpret("av_c2.1"));
+  av.umsg(interpret("av_c2.2"), {preserve: true});
+  graph = av.ds.graph({left: leftAlign, top: topAlign, layout: "manual", directed: false});
+  var n = graph.addNode("n", {left: leftAlign, top: topAlign});
+  var nMinusOne = graph.addNode("n-1", {left: leftAlign + nodeWidth + nodeGap, top: topAlign});
+  var oneTwo = graph.addEdge(n, nMinusOne, {weight: "<b>1 +</b>"});
   n.highlight();
-  oneTwo.css({"stroke":"green"});
-  nMinusOne.css({"background-color":"lightgreen"});
+  oneTwo.css({stroke: "green"});
+  nMinusOne.css({"background-color": "lightgreen"});
   graph.layout();
-
   av.step();
 
-  //Slide 3
-  av.umsg(interpret("Slide 3.1"));
-  av.umsg(interpret("Slide 3.2"), {"preserve": true});
-  // labelSet.push(av.label("----------------------------------", {"top": topAlign + 2 * nodeHeight + nodeGap , "left": leftAlign + nodeWidth + labelShift}));
-  // labelSet.push(av.label("1", {"top": topAlign + 2 * nodeHeight + nodeGap , "left": leftAlign + nodeWidth + labelShift * 20}));
-  
-  var nMinusTwo = graph.addNode("n-2", {"left": leftAlign + 2*nodeGap + 2* nodeWidth, "top": topAlign});
-  var twoThree = graph.addEdge(nMinusOne, nMinusTwo, {"weight": "<b>1 +</b>"});
+  // Slide 3
+  av.umsg(interpret("av_c3.1"));
+  av.umsg(interpret("av_c3.2"), {preserve: true});
+  var nMinusTwo = graph.addNode("n-2", {left: leftAlign + 2 * nodeGap + 2 * nodeWidth, top: topAlign});
+  var twoThree = graph.addEdge(nMinusOne, nMinusTwo, {weight: "<b>1 +</b>"});
   n.unhighlight();
-  nMinusOne.css({"background-color":"white"});
+  nMinusOne.css({"background-color": "white"});
   nMinusOne.highlight();
-  oneTwo.css({"stroke":"black"});
-  twoThree.css({"stroke":"green"});
-  nMinusTwo.css({"background-color":"lightgreen"});;
-  
+  oneTwo.css({stroke: "black"});
+  twoThree.css({stroke: "green"});
+  nMinusTwo.css({"background-color": "lightgreen"});
   graph.layout();
   av.step();
 
-  //Slide 4
-  av.umsg(interpret("Slide 4.1"));
-  av.umsg(interpret("Slide 4.2"), {"preserve": true});
-  // labelSet.push(av.label("----------------------------------", {"top": topAlign + 3.5 * nodeHeight + 2 * nodeGap , "left": leftAlign + nodeWidth + labelShift}));
-  // labelSet.push(av.label("1", {"top": topAlign + 3.5 * nodeHeight + 2 * nodeGap , "left": leftAlign + nodeWidth + labelShift * 20}));
-  
-  var nMinusThree = graph.addNode("n-3", {"left": leftAlign + 3*nodeGap + 3* nodeWidth, "top": topAlign});
-  var threeFour = graph.addEdge(nMinusTwo, nMinusThree, {"weight": "<b>1 +</b>"});
+  // Slide 4
+  av.umsg(interpret("av_c4.1"));
+  av.umsg(interpret("av_c4.2"), {preserve: true});
+  var nMinusThree = graph.addNode("n-3", {left: leftAlign + 3 * nodeGap + 3 * nodeWidth, top: topAlign});
+  var threeFour = graph.addEdge(nMinusTwo, nMinusThree, {weight: "<b>1 +</b>"});
   nMinusOne.unhighlight();
   nMinusTwo.highlight();
-  twoThree.css({"stroke":"black"});
-  threeFour.css({"stroke":"green"});
-  nMinusThree.css({"background-color":"lightgreen"});
-
+  twoThree.css({stroke: "black"});
+  threeFour.css({stroke: "green"});
+  nMinusThree.css({"background-color": "lightgreen"});
   graph.layout();
-
-
   av.step();
 
-  //Slide 5
-  av.umsg(interpret("Slide 5.1"));
-  av.umsg(interpret("Slide 5.2"), {"preserve": true});
-  // labelSet.push(av.label("----------------------------------", {"top": topAlign + 5 * nodeHeight + 3 * nodeGap , "left": leftAlign + nodeWidth + labelShift}));
-  // labelSet.push(av.label("1", {"top": topAlign + 5 * nodeHeight + 3 * nodeGap , "left": leftAlign + nodeWidth + labelShift * 20}));
-  
-  var nMinusFour = graph.addNode("n-4", {"left": leftAlign + 4*nodeGap + 4* nodeWidth, "top": topAlign});
-  var fourFive = graph.addEdge(nMinusThree, nMinusFour, {"weight": "<b>1 +</b>"});
+  // Slide 5
+  av.umsg(interpret("av_c5.1"));
+  av.umsg(interpret("av_c5.2"), {preserve: true});
+  var nMinusFour = graph.addNode("n-4", {left: leftAlign + 4 * nodeGap + 4 * nodeWidth, top: topAlign});
+  var fourFive = graph.addEdge(nMinusThree, nMinusFour, {weight: "<b>1 +</b>"});
   nMinusTwo.unhighlight();
-  nMinusTwo.css({"background-color":"white"});
+  nMinusTwo.css({"background-color": "white"});
   nMinusThree.highlight();
-  threeFour.css({"stroke":"black"});
-  fourFive.css({"stroke":"green"});
-  nMinusFour.css({"background-color":"lightgreen"});
+  threeFour.css({stroke: "black"});
+  fourFive.css({stroke: "green"});
+  nMinusFour.css({"background-color": "lightgreen"});
   graph.layout();
   av.step();
 
-  //Slide 6
-  av.umsg(interpret("Slide 6.1"));
-  av.umsg(interpret("Slide 6.2"), {"preserve": true});
-  // labelSet.push(av.label("----------------------------------", {"top": topAlign + 8.5 * nodeHeight + 4 * nodeGap , "left": leftAlign + nodeWidth + labelShift}));
-  // labelSet.push(av.label("1", {"top": topAlign + 8.5 * nodeHeight + 4 * nodeGap , "left": leftAlign + nodeWidth + labelShift * 20}));
-  
-  var last = graph.addNode("1", {"left": leftAlign + 6 * nodeGap + 6 * nodeWidth, "top": topAlign});
+  // Slide 6
+  av.umsg(interpret("av_c6.1"));
+  av.umsg(interpret("av_c6.2"), {preserve: true});
+  var last = graph.addNode("1", {left: leftAlign + 6 * nodeGap + 6 * nodeWidth, top: topAlign});
   graph.layout();
-
-  var lastEdge = av.g.line(leftAlign + 4 * nodeGap + 5.25 * nodeWidth, 
-    topAlign + nodeHeight + 6, 
-    leftAlign + 5 * nodeGap + 5.25 * nodeWidth + 90, 
-    topAlign +  nodeHeight + 6);
+  var lastEdge = av.g.line(leftAlign + 4 * nodeGap + 5 * nodeWidth + 14,
+                           topAlign + nodeHeight - 4,
+                           leftAlign + 5 * nodeGap + 5 * nodeWidth + 104,
+                           topAlign +  nodeHeight - 4);
   lastEdge.addClass("dashed");
-
   nMinusThree.unhighlight();
-  nMinusThree.css({"background-color":"white"});
-  fourFive.css({"stroke":"black"});
-  
-  nMinusFour.css({"background-color":"white"});
-  last.css({"background-color":"lightgreen"});
-  
-  av.step();	
-
-  //Slide 7
-  last.css({"background-color":"white"});
-  av.umsg(interpret("Slide 7"));
-  labelSet.push
-  (av.label("|--------------------------------------------------- $\\displaystyle\\sum_{i=1}^{n}1$ ---------------------------------------------------|", {"top": topAlign + 2*nodeHeight , "left": leftAlign + 0.5 * nodeWidth}));
+  nMinusThree.css({"background-color": "white"});
+  fourFive.css({stroke: "black"});
+  nMinusFour.css({"background-color": "white"});
+  last.css({"background-color": "lightgreen"});
   av.step();
 
-  //Slide 8
-  av.umsg(interpret("Slide 8"));
+  // Slide 7
+  last.css({"background-color": "white"});
+  av.umsg(interpret("av_c7"));
+  labelSet.push(av.label("|--------------------------------------------------- $\\displaystyle\\sum_{i=1}^{n}1$ ---------------------------------------------------|",
+            {top: topAlign + 1.5 * nodeHeight, left: leftAlign + 0.5 * nodeWidth}));
   av.step();
-		
+
+  // Slide 8
+  av.umsg(interpret("av_c8"));
   av.recorded();
-
 });
--- AV/Background/SummationTwoPowerICON.js
+++ AV/Background/SummationTwoPowerICON.js
@@ -13,12 +13,16 @@ $(document).ready(function() {
   var leftAlign = 1;
   var topAlign = 30;
   var labelShift = 5;
-  var set2; //To hold rectangles of i = 2
   var set1; //To hold rectangles of i = 1
+  var set2; //To hold rectangles of i = 2
+  var set3; //To hold rectangles of i = 3
+  var set4; //To hold rectangles of i = 4
 
   av = new JSAV(av_name);
-  set2 = av.g.set();
   set1 = av.g.set();
+  set2 = av.g.set();
+  set3 = av.g.set();
+  set4 = av.g.set();
 
   //Slide 1
   av.umsg(interpret("av_c1"));
@@ -31,7 +35,7 @@ $(document).ready(function() {
   //Slide 3
   av.umsg(interpret("av_c3.1"));
   av.umsg(interpret("av_c3.2"), {preserve: true});
-  var rect0 = av.g.rect(leftAlign, topAlign, rectWidth, rectHeight);
+  var rect0 = av.g.rect(leftAlign, topAlign, rectWidth, rectHeight).css({fill: "red"});
   var label0 = av.label("$i = 0$", {top: topAlign - 0.5 * rectHeight,
                                     left: leftAlign + rectWidth + labelShift});
   av.step();
@@ -39,8 +43,10 @@ $(document).ready(function() {
   //Slide 4
   av.umsg(interpret("av_c4.1"));
   av.umsg(interpret("av_c4.2"), {preserve: true});
+  rect0.css({fill: "none"});
   set1.push(av.g.rect(leftAlign, topAlign + rectHeight, rectWidth, rectHeight));
   set1.push(av.g.rect(leftAlign + rectWidth, topAlign + rectHeight, rectWidth, rectHeight));
+  set1.css({fill: "red"});
   var label1 = av.label("$i = 1$",  {top: topAlign - 0.5 * rectHeight + rectHeight,
                                      left: leftAlign + 2 * rectWidth + labelShift});
   av.step();
@@ -48,10 +54,12 @@ $(document).ready(function() {
   //Slide 5
   av.umsg(interpret("av_c5.1"));
   av.umsg(interpret("av_c5.2"), {preserve: true});
+  set1.css({fill: "none"});
   set2.push(av.g.rect(leftAlign, topAlign + 2 * rectHeight, rectWidth, rectHeight));
   set2.push(av.g.rect(leftAlign + rectWidth, topAlign + 2 * rectHeight, rectWidth, rectHeight));
   set2.push(av.g.rect(leftAlign + 2 * rectWidth, topAlign + 2 * rectHeight, rectWidth, rectHeight));
   set2.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign + 2 * rectHeight, rectWidth, rectHeight));
+  set2.css({fill: "red"});
   var label2 = av.label("$i = 2$",  {top: topAlign - 0.5 * rectHeight + 2 * rectHeight,
                                      left: leftAlign + 4 * rectWidth + labelShift});
   av.step();
@@ -59,14 +67,16 @@ $(document).ready(function() {
   //Slide 6
   av.umsg(interpret("av_c6.1"));
   av.umsg(interpret("av_c6.2"), {preserve: true});
-  av.g.rect(leftAlign, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 2 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 3 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 4 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 5 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 6 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 7 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight);
+  set2.css({fill: "none"});
+  set3.push(av.g.rect(leftAlign, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 2 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 3 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 4 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 5 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 6 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.push(av.g.rect(leftAlign + 7 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight));
+  set3.css({fill: "red"});
   var label3 = av.label("$i = 3$",  {top: topAlign - 0.5 * rectHeight + 3 * rectHeight,
                                      left: leftAlign + 8 * rectWidth + labelShift});
   av.step();
@@ -74,22 +84,24 @@ $(document).ready(function() {
   //Slide 7
   av.umsg(interpret("av_c7.1"));
   av.umsg(interpret("av_c7.2"), {preserve: true});
-  av.g.rect(leftAlign, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +      rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  2 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  3 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  4 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  5 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  6 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  7 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  8 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign +  9 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 10 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 11 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 12 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 13 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 14 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
-  av.g.rect(leftAlign + 15 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight);
+  set3.css({fill: "none"});
+  set4.push(av.g.rect(leftAlign, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +      rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  2 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  3 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  4 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  5 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  6 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  7 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  8 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign +  9 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 10 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 11 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 12 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 13 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 14 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.push(av.g.rect(leftAlign + 15 * rectWidth, topAlign + 4 * rectHeight, rectWidth, rectHeight));
+  set4.css({fill: "red"});
   var label4 = av.label("$i = 4$",  {top: topAlign - 0.5 * rectHeight + 4 * rectHeight,
                                      left: leftAlign + 16 * rectWidth + labelShift});
   av.step();
@@ -97,6 +109,7 @@ $(document).ready(function() {
   //Slide 8
   av.umsg(interpret("av_c8.1"));
   av.umsg(interpret("av_c8.2"), {preserve: true});
+  set4.css({fill: "none"});
   label0.hide();
   label1.hide();
   label2.hide();
@@ -118,6 +131,10 @@ $(document).ready(function() {
 
   //Slide 12
   av.umsg(interpret("av_c12"));
+  av.label("-1",  {top: topAlign - 0.5 * rectHeight + 3 * rectHeight,
+                                     left: leftAlign + 16 * rectWidth + labelShift});
+  av.label("|--------------------------------------------------------------------------- $2^4$  ---------------------------------------------------------------------------|",  {top: topAlign - 0.5 * rectHeight + 2 * rectHeight,
+                                     left: leftAlign + labelShift});
   av.g.rect(leftAlign + 15 * rectWidth, topAlign + 3 * rectHeight, rectWidth, rectHeight).css({fill: "black"});
   av.step();
 
