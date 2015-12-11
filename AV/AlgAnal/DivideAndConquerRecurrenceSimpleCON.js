af2ceb75c01f9f0b9899b0e99adc6922e733e2c6
Cliff Shaffer <shaffer@vt.edu>
2015-12-11 00:52:16 -0500

    Update AlgAnal slideshow

--- AV/AlgAnal/DivideAndConquerRecurrenceSimpleCON.js
+++ AV/AlgAnal/DivideAndConquerRecurrenceSimpleCON.js
@@ -1,13 +1,13 @@
 /*global ODSA */
-"use strict";
 // Written by Mohammed Farghally and Cliff Shaffer
 // Linear Recurrences
-$(document).ready(function () {
+$(document).ready(function() {
+  "use strict";
   var av_name = "DivideAndConquerRecurrenceSimpleCON";
   // Load the config object with interpreter and code created by odsaUtils.js
-  var config = ODSA.UTILS.loadConfig({"av_name": av_name}),
-      interpret = config.interpreter,       // get the interpreter
-      code = config.code;                   // get the code object
+  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
+      interpret = config.interpreter;       // get the interpreter
+
   var av;
   var graph;
   var leftAlign = 10;
@@ -15,23 +15,22 @@ $(document).ready(function () {
   var nodeGap = 50;
   var nodeHeight = 40;
   var nodeWidth = 40;
-  var labelShift = 10;
   var labelSet;
-  
+
   av = new JSAV(av_name);
   labelSet = [];
-	
-  //Slide 1	
+
+  //Slide 1
   av.umsg(interpret("Slide 1"));
   av.displayInit();
 
 	//Slide 2
   av.umsg(interpret("Slide 2.1"));
-  av.umsg(interpret("Slide 2.2"), {"preserve": true});
-  graph = av.ds.graph({"left": leftAlign, "top": topAlign, layout: "manual", directed: false});
-  var n = graph.addNode("n", {"left": leftAlign, "top": topAlign});
-  var nOverTwo = graph.addNode("n/2", {"left": leftAlign + nodeWidth + nodeGap, "top": topAlign});
-  var oneTwo = graph.addEdge(n, nOverTwo, {"weight": "<b>1 +</b>"});
+  av.umsg(interpret("Slide 2.2"), {preserve: true});
+  graph = av.ds.graph({left: leftAlign, top: topAlign, layout: "manual", directed: false});
+  var n = graph.addNode("n", {left: leftAlign, top: topAlign});
+  var nOverTwo = graph.addNode("n/2", {left: leftAlign + nodeWidth + nodeGap, top: topAlign});
+  var oneTwo = graph.addEdge(n, nOverTwo, {weight: "<b>1 +</b>"});
   n.highlight();
   oneTwo.addClass("subProblemEdge");
   nOverTwo.addClass("subProblemNode");
@@ -40,9 +39,9 @@ $(document).ready(function () {
 
   //Slide 3
   av.umsg(interpret("Slide 3.1"));
-  av.umsg(interpret("Slide 3.2"), {"preserve": true});
-  var nOverFour = graph.addNode("n/4", {"left": leftAlign + 2*nodeGap + 2* nodeWidth, "top": topAlign});
-  var twoThree = graph.addEdge(nOverTwo, nOverFour, {"weight": "<b>1 +</b>"});
+  av.umsg(interpret("Slide 3.2"), {preserve: true});
+  var nOverFour = graph.addNode("n/4", {left: leftAlign + 2 * nodeGap + 2 * nodeWidth, top: topAlign});
+  var twoThree = graph.addEdge(nOverTwo, nOverFour, {weight: "<b>1 +</b>"});
   n.unhighlight();
   nOverTwo.removeClass("subProblemNode");
   nOverTwo.highlight();
@@ -54,9 +53,9 @@ $(document).ready(function () {
 
   //Slide 4
   av.umsg(interpret("Slide 4.1"));
-  av.umsg(interpret("Slide 4.2"), {"preserve": true});
-  var nOverEight = graph.addNode("n/8", {"left": leftAlign + 3*nodeGap + 3* nodeWidth, "top": topAlign});
-  var threeFour = graph.addEdge(nOverFour, nOverEight, {"weight": "<b>1 +</b>"});
+  av.umsg(interpret("Slide 4.2"), {preserve: true});
+  var nOverEight = graph.addNode("n/8", {left: leftAlign + 3 * nodeGap + 3 * nodeWidth, top: topAlign});
+  var threeFour = graph.addEdge(nOverFour, nOverEight, {weight: "<b>1 +</b>"});
   nOverTwo.unhighlight();
   nOverFour.highlight();
   twoThree.removeClass("subProblemEdge");
@@ -67,9 +66,9 @@ $(document).ready(function () {
 
   //Slide 5
   av.umsg(interpret("Slide 5.1"));
-  av.umsg(interpret("Slide 5.2"), {"preserve": true});
-  var nOverSixteen = graph.addNode("n/16", {"left": leftAlign + 4*nodeGap + 4* nodeWidth, "top": topAlign});
-  var fourFive = graph.addEdge(nOverEight, nOverSixteen, {"weight": "<b>1 +</b>"});
+  av.umsg(interpret("Slide 5.2"), {preserve: true});
+  var nOverSixteen = graph.addNode("n/16", {left: leftAlign + 4 * nodeGap + 4 * nodeWidth, top: topAlign});
+  var fourFive = graph.addEdge(nOverEight, nOverSixteen, {weight: "<b>1 +</b>"});
   nOverFour.unhighlight();
   nOverFour.removeClass("subProblemNode");
   nOverEight.highlight();
@@ -81,12 +80,12 @@ $(document).ready(function () {
 
   //Slide 6
   av.umsg(interpret("Slide 6.1"));
-  av.umsg(interpret("Slide 6.2"), {"preserve": true});
-  var last = graph.addNode("1", {"left": leftAlign + 6 * nodeGap + 6 * nodeWidth, "top": topAlign});
+  av.umsg(interpret("Slide 6.2"), {preserve: true});
+  var last = graph.addNode("1", {left: leftAlign + 6 * nodeGap + 6 * nodeWidth, top: topAlign});
   graph.layout();
-  var lastEdge = av.g.line(leftAlign + 4 * nodeGap + 5.25 * nodeWidth, 
-    topAlign + nodeHeight + 6, 
-    leftAlign + 5 * nodeGap + 5.25 * nodeWidth + 90, 
+  var lastEdge = av.g.line(leftAlign + 4 * nodeGap + 5.25 * nodeWidth,
+    topAlign + nodeHeight + 6,
+    leftAlign + 5 * nodeGap + 5.25 * nodeWidth + 90,
     topAlign +  nodeHeight + 6);
   lastEdge.addClass("dashed");
   nOverEight.unhighlight();
@@ -99,14 +98,12 @@ $(document).ready(function () {
   //Slide 7
   av.umsg(interpret("Slide 7"));
   last.removeClass("subProblemNode");
-  labelSet.push
-  (av.label("|----------------------------------------------- $\\displaystyle\\sum_{i=0}^{\\log{n}}1$ -----------------------------------------------|", {"top": topAlign + 2*nodeHeight , "left": leftAlign + 0.5 * nodeWidth}));
+  labelSet.push(av.label("|----------------------------------------------- $\\displaystyle\\sum_{i=0}^{\\log{n}}1$ -----------------------------------------------|", {top: topAlign + 2 * nodeHeight, left: leftAlign + 0.5 * nodeWidth}));
   av.step();
 
   //Slide 8
   av.umsg(interpret("Slide 8"));
   av.step();
-	
-  av.recorded();
 
+  av.recorded();
 });
