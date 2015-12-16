dcf48567aba64e2fa5d6f2b76824f6cb6ec712e6
Sally Hamouda <sally@algoviz.cc.vt.edu>
2015-12-16 14:25:21 -0500

    addclass instead of js .css for TOH AV

--- AV/RecurTutor/recurTraceTOHCON.js
+++ AV/RecurTutor/recurTraceTOHCON.js
@@ -12,19 +12,17 @@ $(document).ready(function() {
   var label1 = av.label("1", {left: 180, top: 102});
   var label2 = av.label("2", {left: 195, top: 132});
   var label3 = av.label("3", {left: 210, top: 162});
-  var rectver0 = av.g.rect(105, 10, 10, 190).css({fill: "brown"});
+  var rectver0 = av.g.rect(105, 10, 10, 190).addClass("polecolor");
   rectver0.show();
-  var rectver1 = av.g.rect(375, 10, 10, 190).css({fill: "brown"});
+  var rectver1 = av.g.rect(375, 10, 10, 190).addClass("polecolor");
   rectver1.show();
-  var rectver2 = av.g.rect(605, 10, 10, 190).css({fill: "brown"});
+  var rectver2 = av.g.rect(605, 10, 10, 190).addClass("polecolor");
   rectver2.show();
 // The moving ones
-  var rect0 = av.g.rect(65, 90, 90, 20).css({fill: "grey"});
-  var rect1 = av.g.rect(50, 120, 120, 20).css({fill: "yellow"});
-  var rect2 = av.g.rect(35, 150, 150, 20).css({fill: "purple"});
-  var rect3 = av.g.rect(20, 180, 180, 20).css({fill: "green"});
-//  var rect4 = av.g.rect(5, 150, 210, 20).css({fill: "red"});
-//  var rect5 = av.g.rect(-10, 180, 240, 20).css({fill: "blue"});
+  var rect0 = av.g.rect(65, 90, 90, 20).addClass("disk0color");
+  var rect1 = av.g.rect(50, 120, 120, 20).addClass("disk1color");
+  var rect2 = av.g.rect(35, 150, 150, 20).addClass("disk2color");
+  var rect3 = av.g.rect(20, 180, 180, 20).addClass("disk3color");
 // Labels for each tower
   var labela = av.label("A", {left: 90, top: -18});
   labela.show();
@@ -59,7 +57,7 @@ $(document).ready(function() {
   label0.hide();
   var label2122 = av.label("0", {left: 660, top: 163});
   label2122.show();
-  var rect06 = av.g.rect(565, 180, 90, 20).css({fill: "grey"});
+  var rect06 = av.g.rect(565, 180, 90, 20).addClass("disk0color");
   av.step();
   //slide 6
   pseudo.unhighlight(11);
@@ -70,7 +68,7 @@ $(document).ready(function() {
   labelCallStack0.hide();
   av.umsg(interpret("av_c7"));
   var label17 = av.label("1", {left: 440, top: 163});
-  var rect17 = av.g.rect(315, 180, 120, 20).css({fill: "yellow"});
+  var rect17 = av.g.rect(315, 180, 120, 20).addClass("disk1color");
   av.step();
   //slide 7
   av.umsg(interpret("av_c8"));
@@ -79,9 +77,9 @@ $(document).ready(function() {
   //slide 8
   pseudo.highlight(10);
   rect06.hide();
-  label0.hide();
+  label2122.hide();
   var label068 = av.label("0", {left: 425, top: 130});
-  var rect068 = av.g.rect(330, 150, 90, 20).css({fill: "grey"});
+  var rect068 = av.g.rect(330, 150, 90, 20).addClass("disk0color");
   av.step();
   //slide 9
   pseudo.unhighlight(10);
@@ -91,7 +89,7 @@ $(document).ready(function() {
   labelCallStack1.hide();
   av.umsg(interpret("av_c9"));
   var label29 = av.label("2", {left: 698, top: 163});
-  var rect29 = av.g.rect(540, 180, 150, 20).css({fill: "purple"});
+  var rect29 = av.g.rect(540, 180, 150, 20).addClass("disk2color");
   av.step();
   //slide 10
   av.umsg(interpret("av_c10"));
@@ -104,7 +102,7 @@ $(document).ready(function() {
   rect068.hide();
   label068.hide();
   var label06810 = av.label("0", {left: 165, top: 132});
-  var  rect06810 = av.g.rect(65, 150, 90, 20).css({fill: "grey"});
+  var  rect06810 = av.g.rect(65, 150, 90, 20).addClass("disk0color");
   av.step();
   //slide 12
   labelCallStack0.hide();
@@ -114,7 +112,7 @@ $(document).ready(function() {
   rect17.hide();
   label17.hide();
   var label1711 = av.label("1", {left: 686, top: 132});
-  var rect1711 = av.g.rect(550, 150, 120, 20).css({fill: "yellow"});
+  var rect1711 = av.g.rect(550, 150, 120, 20).addClass("disk1color");
   av.step();
   //slide 13
   av.umsg(interpret("av_c12"));
@@ -124,7 +122,7 @@ $(document).ready(function() {
   label06810.hide();
   labelCallStack0.show();
   var label0681012 = av.label("0", {left: 660, top: 102});
-  var rect0681012 = av.g.rect(565, 120, 90, 20).css({fill: "grey"});
+  var rect0681012 = av.g.rect(565, 120, 90, 20).addClass("disk0color");
   av.step();
   //slide 14
   av.umsg(interpret("av_c13"));
@@ -143,7 +141,7 @@ $(document).ready(function() {
   label3.hide();
   var label313 = av.label("3", {left: 480, top: 162});
   label313.show();
-  var rect313 = av.g.rect(295, 180, 180, 20).css({fill: "green"});
+  var rect313 = av.g.rect(295, 180, 180, 20).addClass("disk3color");
   rect313.show();
   av.step();
   //slide 17
@@ -163,7 +161,7 @@ $(document).ready(function() {
   rect0681012.hide();
   label0681012.hide();
   var label068101214 = av.label("0", {left: 428, top: 132});
-  var  rect068101214 = av.g.rect(330, 150, 90, 20).css({fill: "grey"});
+  var  rect068101214 = av.g.rect(330, 150, 90, 20).addClass("disk0color");
   av.step();
   //slide 21
   pseudo.unhighlight(11);
@@ -174,7 +172,7 @@ $(document).ready(function() {
   //av.step();
   //slide 22
   var label171115 = av.label("1", {left: 180, top: 162});
-  var rect171115 = av.g.rect(50, 180, 120, 20).css({fill: "yellow"});
+  var rect171115 = av.g.rect(50, 180, 120, 20).addClass("disk1color");
   labelCallStack0.show();
   av.step();
   //slide 23
@@ -184,7 +182,7 @@ $(document).ready(function() {
   label068101214.hide();
   labelCallStack0.hide();
   var label015  = av.label("0", {left: 165, top: 132});
-  var rect015 = av.g.rect(65, 150, 90, 20).css({fill: "grey"});
+  var rect015 = av.g.rect(65, 150, 90, 20).addClass("disk0color");
   av.step();
   //slide 24
   labelCallStack1.hide();
@@ -198,7 +196,7 @@ $(document).ready(function() {
   label29.hide();
   var label216 = av.label("2", {left: 468, top: 132});
   label216.show();
-  var rect216 = av.g.rect(310, 150, 150, 20).css({fill: "purple"});
+  var rect216 = av.g.rect(310, 150, 150, 20).addClass("disk2color");
   rect216.show();
   av.step();
   //slide 26
@@ -215,7 +213,7 @@ $(document).ready(function() {
   rect015.hide();
   label015.hide();
   var label017  = av.label("0", {left: 660, top: 162});
-  var rect017 = av.g.rect(565, 180, 90, 20).css({fill: "grey"});
+  var rect017 = av.g.rect(565, 180, 90, 20).addClass("disk0color");
   av.step();
   //slide 29
   pseudo.unhighlight(11);
@@ -225,7 +223,7 @@ $(document).ready(function() {
   labelCallStack0.hide();
   var label118 = av.label("1", {left: 450, top: 102});
   label118.show();
-  var rect118 = av.g.rect(320, 120, 120, 20).css({fill: "yellow"});
+  var rect118 = av.g.rect(320, 120, 120, 20).addClass("disk1color");
   rect118.show();
   av.step();
   //slide 30
@@ -240,7 +238,7 @@ $(document).ready(function() {
   label017.hide();
   var label019 = av.label("0", {left: 440, top: 72});
   label019.show();
-  var rect019 = av.g.rect(340, 90, 90, 20).css({fill: "grey"});
+  var rect019 = av.g.rect(340, 90, 90, 20).addClass("disk0color");
   rect019.show();
   labelCallStack0.hide();
   av.step();
