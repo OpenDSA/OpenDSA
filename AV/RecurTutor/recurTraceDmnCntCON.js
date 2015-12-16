78fa110f49d676654707e0922b901d332a618f6d
Sally Hamouda <sally@algoviz.cc.vt.edu>
2015-12-16 14:06:39 -0500

    addclass insted of js .css

--- AV/RecurTutor/recurTraceDmnCntCON.js
+++ AV/RecurTutor/recurTraceDmnCntCON.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
       interpret = config.interpreter,       // get the interpreter
       code = config.code;                   // get the code object
   var av = new JSAV(av_name);
-  var rect = av.g.rect(150, 30, 50, 90).css({fill: "grey"});
+  var rect = av.g.rect(150, 30, 50, 90).addClass("dominocolor");
   var dot =   av.g.circle(270, 75, 2);
   var dots0 = av.g.circle(320, 75, 2);
   var dots1 = av.g.circle(370, 75, 2);
@@ -15,10 +15,9 @@ $(document).ready(function() {
   dots0.show();
   dots1.show();
   dots2.show();
-  var rect3 = av.g.rect(500, 30, 50, 90).css({fill: "grey"});
-  var rect4 = av.g.rect(600, 30, 50, 90).css({fill: "grey"});
-  var rect5 = av.g.rect(700, 30, 50, 90).css({fill: "grey"});
-
+  var rect3 = av.g.rect(500, 30, 50, 90).addClass("dominocolor");
+  var rect4 = av.g.rect(600, 30, 50, 90).addClass("dominocolor");
+  var rect5 = av.g.rect(700, 30, 50, 90).addClass("dominocolor");
   av.umsg(interpret("av_c1"));
   var  pseudo = av.code(code);
   av.displayInit();
@@ -27,9 +26,9 @@ $(document).ready(function() {
   av.step();
   av.umsg(interpret("av_c3"));
   rect5.hide();
-  var rect10 = av.g.rect(675, 34, 50, 95).css({fill: "lightgray"});
+  var rect10 = av.g.rect(675, 34, 50, 95).addClass("tilteddominocolor");
   rect10.rotate(-55);
-  av.label("1's digit",  {top: "6px", left: "700px"}).css({"font-size": "15px", "text-align": "center"});
+  av.label("1's digit",  {top: "6px", left: "700px"}).addClass("digitstyle");
   pseudo.highlight(3);
   av.step();
   av.umsg(interpret("av_c4"));
@@ -38,18 +37,18 @@ $(document).ready(function() {
   av.step();
   av.umsg(interpret("av_c5"));
   rect4.hide();
-  var rect11 = av.g.rect(575, 34, 50, 95).css({fill: "lightgray"});
+  var rect11 = av.g.rect(575, 34, 50, 95).addClass("tilteddominocolor");
   rect11.rotate(-55);
-  av.label("10's digit",  {top: "6px", left: "600px"}).css({"font-size": "15px", "text-align": "center"});
+  av.label("10's digit",  {top: "6px", left: "600px"}).addClass("digitstyle");
   av.step();
   rect3.hide();
-  var rect12 = av.g.rect(475, 34, 50, 95).css({fill: "lightgray"});
+  var rect12 = av.g.rect(475, 34, 50, 95).addClass("tilteddominocolor");
   rect12.rotate(-55);
-  av.label("100's digit",  {top: "6px", left: "500px"}).css({"font-size": "15px", "text-align": "center"});
+  av.label("100's digit",  {top: "6px", left: "500px"}).addClass("digitstyle");
   av.step();
   rect.hide();
-  var rect13 = av.g.rect(125, 34, 50, 95).css({fill: "lightgray"});
+  var rect13 = av.g.rect(125, 34, 50, 95).addClass("tilteddominocolor");
   rect13.rotate(-55);
-  av.label("10^n's digit",  {top: "6px", left: "150px"}).css({"font-size": "15px", "text-align": "center"});
+  av.label("10^n's digit",  {top: "6px", left: "150px"}).addClass("digitstyle");
   av.recorded();
 });
