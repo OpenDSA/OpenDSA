5dd107e3f8a290f81e3e13c25389bd52db1ff4d9
Sally Hamouda <sally@algoviz.cc.vt.edu>
2015-12-16 13:28:46 -0500

    AVs fixed for the eslint

--- AV/RecurTutor/recurTraceFactCON.js
+++ AV/RecurTutor/recurTraceFactCON.js
@@ -1,42 +1,30 @@
 /*global ODSA */
 // Written by Sally Hamouda and Cliff Shaffer
-// Visualization of factorial with copies model 
-$(document).ready(function () {
+// Visualization of factorial with copies model
+$(document).ready(function() {
   "use strict";
   var av_name = "recurTraceFactCON";
-  var config = ODSA.UTILS.loadConfig({"av_name": av_name}),
+  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
       interpret = config.interpreter,       // get the interpreter
       code = config.code;                   // get the code object
   var av = new JSAV(av_name);
- 
   var pseudo = av.code($.extend({lineNumbers: false}, code[0]));
-
+  pseudo.show();
   // Slide 1
   av.umsg(interpret("av_c1"));
   av.displayInit();
- 
   // Slide 2
   av.umsg(interpret("av_c2"));
-  var pseudo2 = av.code($.extend({lineNumbers: false, top: 150, left: 0}, code[1])); 
-  
-  var pseudo3 = av.code($.extend({lineNumbers: false, top: 150, left: 260}, code[2])); 
-  
-  var pseudo4 = av.code($.extend({lineNumbers: false, top: 150, left: 520}, code[3])); 
-  
-  var pseudo5 = av.code($.extend({lineNumbers: false, top: 300, left: 125}, code[4])); 
-  
-  var pseudo6 = av.code($.extend({lineNumbers: false, top: 300, left: 393}, code[5])); 
-  
-  
+  var pseudo2 = av.code($.extend({lineNumbers: false, top: 150, left: 0}, code[1]));
+  var pseudo3 = av.code($.extend({lineNumbers: false, top: 150, left: 260}, code[2]));
+  var pseudo4 = av.code($.extend({lineNumbers: false, top: 150, left: 520}, code[3]));
+  var pseudo5 = av.code($.extend({lineNumbers: false, top: 300, left: 125}, code[4]));
+  var pseudo6 = av.code($.extend({lineNumbers: false, top: 300, left: 393}, code[5]));
  // After the return:
-  var pseudo7 = av.code($.extend({lineNumbers: false, top: 150, left: 125}, code[6])); 
-  
-  var pseudo8 = av.code($.extend({lineNumbers: false, top: 150, left: 385}, code[7])); 
-  
-  var pseudo9 = av.code($.extend({lineNumbers: false, top: 300, left: 125}, code[8])); 
-  
-  var pseudo10 = av.code($.extend({lineNumbers: false, top: 300, left: 385}, code[9])); 
-  
+  var pseudo7 = av.code($.extend({lineNumbers: false, top: 150, left: 125}, code[6]));
+  var pseudo8 = av.code($.extend({lineNumbers: false, top: 150, left: 385}, code[7]));
+  var pseudo9 = av.code($.extend({lineNumbers: false, top: 300, left: 125}, code[8]));
+  var pseudo10 = av.code($.extend({lineNumbers: false, top: 300, left: 385}, code[9]));
   pseudo2.hide();
   pseudo3.hide();
   pseudo4.hide();
@@ -50,40 +38,34 @@ $(document).ready(function () {
   pseudo2.highlight(1);
   pseudo2.highlight(4);
   av.step();
-
   // Slide 3
   av.umsg(interpret("av_c3"));
   pseudo3.show();
   pseudo3.highlight(1);
   pseudo3.highlight(4);
   av.step();
-  
   // Slide 4
   av.umsg(interpret("av_c4"));
   pseudo4.show();
   pseudo4.highlight(1);
   pseudo4.highlight(4);
   av.step();
-
   // Slide 5
   av.umsg(interpret("av_c5"));
   pseudo5.show();
   pseudo5.highlight(1);
   pseudo5.highlight(4);
   av.step();
-
   // Slide 6
   av.umsg(interpret("av_c6"));
   pseudo6.show();
   pseudo6.highlight(2);
   av.step();
-
   // Slide 7
   av.umsg(interpret("av_c7"));
   pseudo6.unhighlight(2);
-  pseudo6.highlight(3); 
+  pseudo6.highlight(3);
   av.step();
-
   // Slide 8
   av.umsg(interpret("av_c8"));
   pseudo2.hide();
@@ -94,19 +76,16 @@ $(document).ready(function () {
   pseudo7.show();
   pseudo7.highlight(4);
   av.step();
-
   // Slide 9
   av.umsg(interpret("av_c9"));
   pseudo8.show();
   pseudo8.highlight(4);
   av.step();
-  
   // Slide 10
   av.umsg(interpret("av_c10"));
   pseudo9.show();
   pseudo9.highlight(4);
   av.step();
-  
   // Slide 11
   av.umsg(interpret("av_c11"));
   pseudo10.show();
