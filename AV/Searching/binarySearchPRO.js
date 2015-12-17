bc633e7b2903e1902928f431a0c3483cc092d20e
Kasper Hellström <kasper.hellstrom@aalto.fi>
2015-12-17 13:34:03 +0200

    BinarySearchPRO: Use JSAV to log key.

--- AV/Searching/binarySearchPRO.js
+++ AV/Searching/binarySearchPRO.js
@@ -18,6 +18,11 @@ $(document).ready(function () {
 
     //generate a random key, with value between the min and max of the array
     key = Math.ceil(5 * (initialArray[0] + initialArray[arraySize - 1]) / 7);
+    // log the key
+    av.logEvent({
+      type: "exercise-data",
+      key: key
+    });
 
     // clear old elements
     if (keyholder) {
