e9efe5bcdff141b460d406df8239d896d9749828
David Furcy <furcyd@uwosh.edu>
2016-02-13 22:50:19 -0600

    Add randomization options to RP 8 part 3

--- Exercises/PL/RP8part3.js
+++ Exercises/PL/RP8part3.js
@@ -1,8 +1,9 @@
 /* global console fp  */
 (function() {
   "use strict";
-    
+   
     var randomDigit;
+    var randomDigit2;
 
     function generateRandomList () {
 	var result = [];
@@ -47,7 +48,7 @@ function helper(ns,a) {
 }
     ],
 
-    /* 1: add all elements */
+    /* 1: add all elements + init acc. */
     [ "<span class='string'>ns</span> is a (flat) list of integers and " +
       "<span class='string'>a</span> is an integer accumulator",
       1, // index of main function 
@@ -94,24 +95,142 @@ function helper(ns,a) {
 	return helper(fp.tl(ns),fp.sub(a,fp.hd(ns)));
     }
 }
-    ]
+    ],
+
+    /* 4: copy then reverse */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return fp.cons(fp.hd(ns),
+		       helper(fp.tl(ns),
+			      fp.cons(fp.hd(ns),a)));
+    }
+}
+    ],
+
+    /* 5: add all elements + init acc + randomDigit * input length */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is an integer accumulator",
+      1, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return fp.add(randomDigit,helper(fp.tl(ns),fp.add(fp.hd(ns),a)));
+    }
+}
+    ],
+
+    /* 6: skip second, fourth, etc., elements then reverse
+          "error" when the input has odd length
+    */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return helper(fp.tl(fp.tl(ns)),fp.cons(fp.hd(ns),a));
+    }
+}
+    ],
+
+
+    /* 7: add second, fourth, etc. elements 
+          "error" if input has odd length
+    */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is an integer accumulator",
+      1, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return helper(fp.tl(fp.tl(ns)),fp.add(fp.hd(ns),a));
+    }
+}
+    ],
+
+    /* 8: identity */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return fp.cons(fp.hd(ns),helper(fp.tl(ns),a));
+    }
+}
+    ],
+
+    /* 9: duplicate each element */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else {
+	return fp.cons(fp.hd(ns),fp.cons(fp.hd(ns),helper(fp.tl(ns),a)));
+    }
+}
+    ],
+
+    /* 10: remove all elements GT a random digit then reverse*/
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else if (fp.isGT(fp.hd(ns),randomDigit)) {
+	return helper(fp.tl(ns),a);
+    } else {
+	return helper(fp.tl(ns),fp.cons(fp.hd(ns),a));
+    }
+}
+    ],
+
+
+    /* 11: reverse then remove all elements LT a random digit */
+    [ "<span class='string'>ns</span> is a (flat) list of integers and " +
+      "<span class='string'>a</span> is a list accumulator",
+      0, // index of main function 
+function helper(ns,a) {
+    if (fp.isNull(ns)) {
+	return a;
+    } else if (fp.isLT(fp.hd(ns),randomDigit)) {
+	return helper(fp.tl(ns),a);
+    } else {
+	return helper(fp.tl(ns),fp.cons(fp.hd(ns),a));
+    }
+}
+    ],
 
 	    ];// functions array
 
 	    // pick a random input
-	    var randomDigit =  Math.floor(Math.random() * 10); 
+	    var randomDigit =  Math.floor(Math.random() * 10);
+	    var randomDigit2 =  Math.floor(Math.random() * 10); 
 	    var list = generateRandomList();
 	    //console.log(list);
 	    var integer = Math.floor(Math.random() * 10); 
 	    // pick a random function
 	    var functionNumber = 
 		Math.floor(Math.random() * helperFunctions.length); 
-	    functionNumber = 3;
+	    //functionNumber = 11;
 	    var helper = helperFunctions[ functionNumber ][2];
 	    var main = mainFunctions[helperFunctions[functionNumber][1]];
 	    var params = helperFunctions[ functionNumber ][0];
-	    var helperStr = helper.toString();	    
-	    var mainStr = main.toString().replace(/randomDigit/g,randomDigit);
+	    var helperStr = helper.toString()
+		.replace(/randomDigit/g,randomDigit);
+	    var mainStr = main.toString().replace(/randomDigit/g,randomDigit2);
 	    var answer;
 	    try {
 		answer = JSON.stringify(
@@ -129,7 +248,13 @@ function helper(ns,a) {
 	},// init
 
 	checkAnswer: function (studentAnswer) {
-	    return this.answer === studentAnswer.replace(/\s+/g,"");
+	    if (this.answer.match(/^-?\d+$/) ||
+		this.answer === "error") {
+		// correct answer is an integer or "error"
+		return this.answer === studentAnswer.trim();
+	    } else {
+		return this.answer === studentAnswer.replace(/\s+/g,"");
+	    }
 	}
     };// RP8part3  
 
