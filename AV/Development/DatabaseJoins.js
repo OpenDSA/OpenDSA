ac77ad3021247d08a3a84e4f3158ce38f89c71c0
beardsocool <gottsj36@uwosh.edu>
2016-02-15 16:03:22 -0600

    bugfix in js

--- AV/Development/DatabaseJoins.js
+++ AV/Development/DatabaseJoins.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
 
     JSAV.init();
 
-    var av = new JSAV("container");
+    var av = new JSAV("DatabaseJoins");
     var theArray = [20, 30, 44, 54, 55, 11, 78, 14, 13, 79, 12, 98];
     var arr = av.ds.array(theArray, {indexed: true});
     av.umsg("Text before displayInit()");
