0f97761c2ac947057f09a6a17f3d6aa063de1d0e
beardsocool <gottsj36@uwosh.edu>
2016-02-15 16:13:25 -0600

    anuddah fix

--- AV/Development/DatabaseJoins.js
+++ AV/Development/DatabaseJoins.js
@@ -5,20 +5,32 @@ $(document).ready(function () {
 
     JSAV.init();
 
-    var av = new JSAV("DatabaseJoins");
-    var theArray = [20, 30, 44, 54, 55, 11, 78, 14, 13, 79, 12, 98];
+    //create the JSAV object
+    var av = new JSAV("container");
+    //initialize the array
+    var theArray = [];
+    //insert random integers in the array
+    for(i = 0; i < 11; i++){
+      theArray.push(Math.floor(Math.random() * 100));
+    }
+    //sort the array to prepare for randomizer. 
+    theArray.sort();
     var arr = av.ds.array(theArray, {indexed: true});
-    av.umsg("Text before displayInit()");
+    av.umsg("This is presorted and random array visualization. The numbers "
+      +"below the structure indicate array indices.");
     // Note: av.displayInit() will not affect the number of slides.
     // All that it will do is affect what you get to see on the
     // initial slide.
     av.displayInit();
-    // We are now starting a new slide (#2)
-    av.umsg("... and text after displayInit()", {preserve: true});
-    av.step();
-    arr.swap(0,1, highlight=true);
-    // We are now starting a new slide (#3)
-    av.umsg("Text after av.step()");
+    //slide (#2)
+    av.umsg("Each step in the randization is a swap of two indices. Every index is visited and swapped with a randomly generated index from Math.random().", {preserve: false});
+    
+    //slide (#3)
+    for(i = 0; i < 11; i++){
+      av.step();
+      var swapWith = parseInt((Math.random() * 11));
+      arr.swap(parseInt(i),swapWith, highlight=true);
+      av.umsg("In this step the swap was for index " + i + "  and this it swapped with " + swapWith);
+    }
     av.recorded();
-
 });
