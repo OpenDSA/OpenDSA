f580d8b1e680f5ee5b48e5b90b4abdf6bf9574ac
mfseddik <mfseddik@gmail.com>
2016-03-01 21:18:38 +0000

    Aligning pivot label and extending length marker

--- AV/Development/QuickSortWorstCaseCON.js
+++ AV/Development/QuickSortWorstCaseCON.js
@@ -16,16 +16,16 @@ $(document).ready(function() {
   // Slide 2
   av.umsg(interpret("Slide 2"));
   av.g.rect(100, 0, 400, 30);
-  av.label("$n$",  {top: "-12px", left: "300px"}).addClass("mediumLabel");
+  av.label("$n$",  {top: "-8px", left: "300px"}).addClass("mediumLabel");
   av.step();
 
   // Slide 3
   av.umsg(interpret("Slide 3"));
   av.g.rect(100, 80, 400, 30);
   av.g.rect(100, 80, 20, 30);
-  av.label("|-----------------------------  $> A[pivot]$  ---------------------------|",  {top: "40px", left: "120px"}).addClass("mediumLabel2");
-  av.label("pivot",  {top: "74px", left: "98px"}).addClass("smallLabel rotated");
-  av.label("$n-1$",  {top: "67px", left: "270px"}).addClass("mediumLabel");
+  av.label("|---------------------------------------  $> A[pivot]$  -------------------------------------|",  {top: "40px", left: "120px"}).addClass("mediumLabel2");
+  av.label("pivot",  {top: "76px", left: "98px"}).addClass("smallLabel rotated");
+  av.label("$n-1$",  {top: "71px", left: "270px"}).addClass("mediumLabel");
   av.label("$n-1$",  {top: "67px", left: "600px"}).addClass("mediumLabel");
   av.label(interpret("lab1"),  {top: "-17px", left: "580px"}).addClass("mediumLabel");
   av.step();
@@ -34,9 +34,9 @@ $(document).ready(function() {
   av.umsg(interpret("Slide 4"));
   av.g.rect(120, 160, 380, 30);
   av.g.rect(120, 160, 20, 30);
-  av.label("|---------------------------  $> A[pivot]$  -------------------------|",  {top: "120px", left: "140px"}).addClass("mediumLabel2");
-  av.label("pivot",  {top: "154px", left: "118px"}).addClass("smallLabel rotated");
-  av.label("$n-2$",  {top: "147px", left: "290px"}).addClass("mediumLabel");
+  av.label("|------------------------------------- $> A[pivot]$  ----------------------------------|",  {top: "120px", left: "140px"}).addClass("mediumLabel2");
+  av.label("pivot",  {top: "156px", left: "118px"}).addClass("smallLabel rotated");
+  av.label("$n-2$",  {top: "151px", left: "290px"}).addClass("mediumLabel");
   av.label("$n-2$",  {top: "147px", left: "600px"}).addClass("mediumLabel");
   av.step();
 
@@ -44,9 +44,9 @@ $(document).ready(function() {
   av.umsg(interpret("Slide 5"));
   av.g.rect(140, 240, 360, 30);
   av.g.rect(140, 240, 20, 30);
-  av.label("|-------------------------  $> A[pivot]$  -----------------------|",  {top: "200px", left: "160px"}).addClass("mediumLabel2");
-  av.label("pivot",  {top: "234px", left: "138px"}).addClass("smallLabel rotated");
-  av.label("$n-3$",  {top: "227px", left: "300px"}).addClass("mediumLabel");
+  av.label("|----------------------------------  $> A[pivot]$  --------------------------------|",  {top: "200px", left: "160px"}).addClass("mediumLabel2");
+  av.label("pivot",  {top: "236px", left: "138px"}).addClass("smallLabel rotated");
+  av.label("$n-3$",  {top: "231px", left: "300px"}).addClass("mediumLabel");
   av.label("$n-3$",  {top: "227px", left: "600px"}).addClass("mediumLabel");
   av.step();
 
@@ -57,7 +57,7 @@ $(document).ready(function() {
   av.g.rect(460, 315, 40, 30);
   av.g.rect(460, 315, 20, 30);
   av.label("pivot",  {top: "309px", left: "458px"}).addClass("smallLabel rotated");
-  av.label("$1$",  {top: "302px", left: "485px"}).addClass("mediumLabel");
+  av.label("$1$",  {top: "306px", left: "485px"}).addClass("mediumLabel");
   av.label("$1$",  {top: "302px", left: "620px"}).addClass("mediumLabel");
   av.step();
 
