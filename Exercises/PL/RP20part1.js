003d8c75eb1fe685dd55cc6e3782e9d0f4130a72
David Furcy <furcyd@uwosh.edu>
2016-03-15 19:49:50 -0500

    Complete RP 20 part1

--- Exercises/PL/RP20part1.js
+++ Exercises/PL/RP20part1.js
@@ -1,9 +1,8 @@
-/* global console, PLutils */
+/* global console,  SLang */
 (function() {
   "use strict";
 
     var question = {};
-
     var RP20part1 = {    
 
 
@@ -12,153 +11,115 @@
 	    var vs = "uvxyz";
 	    var maxDepth = 5;
 	    var minDepth = 4;
-	    var exp;
-	    var numCorrect = 0;
+	    var numCorrect = SL.absyn.getRnd(0,4);
 	    var exps = [ ];
-	    var isCorrect;
 	    var options = [ 0, 1, 2, 3, 4];
-	    var expStr = 
-		SL.printExp(
-		    SL.absyn.getProgramExp(
-			SL.absyn.generateRandomSLang1Program(0,2,5,"xyz","")));
+	    var exp, badExp;
 
-	    while (expStr.length < 15 ||  expStr.length>50) {
-		expStr = 
+	    function getRndExp() {
+		var exp =
 		    SL.printExp(
 			SL.absyn.getProgramExp(
-			    SL.absyn.generateRandomSLang1Program(0,2,5,"xyz","")));
+			    SL.absyn.generateRandomSLang1Program(
+				0,2,5,"xyz","")));
+		
+		while (exp.length < 10 ||  exp.length>35) {
+		    exp = 
+			SL.printExp(
+			    SL.absyn.getProgramExp(
+				SL.absyn.generateRandomSLang1Program(
+				    0,2,5,"xyz","")));
+		}
+		return exp;		
+	    }// getRndExp function
+
+	    // replace a with b in exp
+	    function replaceWith(exp,a,b) {
+		var index, lastIndex, rnd;
+		index = exp.indexOf(a); 
+		lastIndex = exp.lastIndexOf(a); 
+		rnd = SL.absyn.getRnd(1,3);
+		if (index > -1) {
+		    switch (rnd) {
+		    case 1: // replace first a
+			exp = exp.substring(0,index) + b +
+			    exp.substring(index+1);
+			break;
+		    case 2: // replace last a
+			exp = exp.substring(0,lastIndex) + b +
+			    exp.substring(lastIndex+1);
+			break;
+		    case 3: // replace all bs
+			exp = exp.replace(new RegExp(a,'g'),b);
+			break;
+		    }
+		}// there is at least one blank space		
+		return exp;
+	    }// replaceWith method
+
+
+	    function addSyntaxError(exp) {
+		var type = SL.absyn.getRnd(1,4);
+		var rnd, index, lastIndex;
+		type =4;
+		switch (type) {
+		    case 1:  // replace space(s) with comma
+		    return replaceWith(exp," ",",");
+
+		    case 2:  // replace comma(s) with space
+		    return replaceWith(exp,","," ");
+
+		    case 3: // replace "=>" with " "
+		    index = exp.indexOf("=>");
+		    if (index > -1) {
+			if (SL.absyn.getRnd(0,1) === 0) {
+			    index = exp.lastIndexOf("=>");
+			}
+			return exp.substring(0,index) + " " +
+			    exp.substring(index+2);			    
+		    } 
+		    break;
+		    case 4: // replace " " with "=>"
+		    index = exp.indexOf(" ");
+		    if (index > -1) {
+			if (SL.absyn.getRnd(0,1) === 0) {
+			    index = exp.lastIndexOf(" ");
+			}
+			return exp.substring(0,index) + "=>" +
+			    exp.substring(index+1);			    
+		    } 
+		}// switch
+		return exp;
+	    }// addSyntaxError
+
+	    // add valid programs
+	    while (exps.length < numCorrect) {
+		exp = getRndExp();
+		if (exps.indexOf(exp) === -1) {
+		    exps.push(exp);
+		}
 	    }
-	    //console.log("========================");
-	    //console.log(exp);
-	    //console.log("========================");
-	    console.log(expStr.length);
-	    this.expressions = expStr;
-	    this.answer = "answer";
+
+	    // add non-programs
+	    while (exps.length < 4) {
+		exp = getRndExp();
+		badExp = addSyntaxError( exp );
+		if (exp !== badExp && exps.indexOf(exp) === -1) {
+		    exps.push(badExp);
+		}
+	    }
+
+	    this.expressions = exps.join("<br />");
+	    this.answer = numCorrect + "";
+	    options.splice(numCorrect,1);
+	    //console.log(numCorrect);
 	    this.option1 = options[0];
 	    this.option2 = options[1];
 	    this.option3 = options[2];
 	    this.option4 = options[3];
+	}// init function
+    };
 
-/*	    function pickRndCharacter(c,s) {
-		var list = s.split("")
-		    .map(function (e,i) { return (e===c ? i : -1) ; });
-		list = list.filter(function (x) { return x >= 0; });
-		return list[L.getRnd(0,list.length-1)];		       
-	    }
-
-	    function findMatchingParen(s,index) {
-		s = s.split("");
-		var count = 0;
-		for(var i=index+1; i<s.length; i++) {
-		    if (s[i] === ')') {
-			if (count === 0) {
-			    return i;
-			} else {
-			    count--;
-			}
-		    } else {
-			if (s[i] === '(') {
-			    count++;
-			}
-		    }
-		}
-		throw new Error("Could not find closing paren for the one " +
-				"at position " + index + " in " + s);
-	    }
-	    function removeParenPair(s) {
-		var openParen = pickRndCharacter('(',s);
-		var closeParen = findMatchingParen(s,openParen);
-		return s.substring(0,openParen) + 
-		    s.substring(openParen+1,closeParen) + 
-		    s.substring(closeParen+1);
-	    }
-	    function removeDot(s) {
-		var dot = pickRndCharacter('.',s);
-		return s.substring(0,dot) + " " + s.substring(dot+1);
-	    }
-	    function addParens(s) {
-		var n = s.length;
-		var closing = n-1;
-		while (s[closing] === ')') {
-		    closing--;
-		}
-		var p1 = L.getRnd(0,closing-1);
-		var p2 = L.getRnd(closing+1,n-1);
-		// do not insert in front of a space or a dot
-		if (s[p1] === " " || s[p1] === ".") {
-		    p1++;
-		}
-		// do not insert after a lambda
-		if (p1>0 && s[p1-1] === "\u03BB" ) {
-		    p1 += 2;
-		}
-		return s.substring(0,p1) + "(" + 
-		    s.substring(p1,p2) + ")" + s.substring(p2);
-	    }
-
-	    function getSyntaxError(minDepth,maxDepth,vs) {
-		var s = L.printExp( L.getRndExp(1,minDepth,maxDepth,vs,""));
-		var rnd = L.getRnd(1,3);
-		question.answer = "True";
-		switch (rnd) {
-		case 1: 
-		    if (s.indexOf('(') !== -1) {
-			s = removeParenPair(s);
-			question.answer = "False";
-		    }
-		    //  leave s unchanged if it does not contain any parens
-		    break;
-		case 2: 
-		    if (s.indexOf('.') !== -1) {
-			s = removeDot(s);
-			question.answer = "False";
-		    }
-		    //  leave s unchanged if it does not contain any dot
-		    break;
-		case 3: 
-		    s = addParens(s);
-		    question.answer = "False";
-		    break;
-		}    
-		return s;
-	    }// getSyntaxError function
-
-	    function isNew(arr) {
-		var exp = arr[arr.length-1];
-		for(var i=0; i<arr.length-1; i++) {
-		    if (arr[i] === exp) {
-			return false;
-		    }
-		}
-		return true;
-	    }
-
-	    while (exps.length < 4) {	
-		if (L.getRnd(0,1) === 0) {
-		    // syntactically correct lambda exp
-		    exps.push( L.printExp( L.getRndExp(1,minDepth,maxDepth,vs,"")) );
-		    isCorrect = true;
-		} else {
-		    exps.push( getSyntaxError(minDepth,maxDepth,vs) );
-		    isCorrect = question.answer === "True";
-		}
-		
-		if (isNew(exps)) {
-		    numCorrect += isCorrect ? 1 : 0;
-		} else {
-		    exps.pop();
-		}
-	    }
-*/
-	    //this.expressions = exps.join("<br />");
-	    //this.answer = String(numCorrect);
-	    //options.splice(numCorrect,1);
-
-
-	} //init
-
-    };// RP20part1  
-    
     window.RP20part1 = window.RP20part1 || RP20part1;
 
 }());
