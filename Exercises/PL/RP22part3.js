596ec882254ae6fe79727009e25c935cfde5bb3b
David Furcy <furcyd@uwosh.edu>
2016-03-22 15:33:35 -0500

    Finalize RP 22 part 3

--- Exercises/PL/RP22part3.js
+++ Exercises/PL/RP22part3.js
@@ -4,23 +4,24 @@
 
     var RP22part3 = {    
 
-
 	init: function() {
 	    var SL = SLang;
 	    var A = SL.absyn;
 	    var E = SL.env;
 	    var vs = "xyz";
-	    var maxDepth = 5;
-	    var minDepth = 4;
-	    var exp, expStr;
-	    var ast, value, done;
+	    var exp, expStr,value;
 	    var globalEnv = E.update(E.createEmptyEnv(),
 				     ["x","y","z"],
 				     [E.createNum(1),
 				      E.createNum(2),
 				      E.createNum(3)]);
 	    var allVariables, selectedVar;
-
+	    var varBoundToClosure, varBoundToInt;
+	    var maxLength = 50;
+	    var rnd, params, body, env;
+	    var isClo = function (v) { return E.isClo(v.value); };
+	    var isNum = function (v) { return E.isNum(v.value); };
+	    
 	    function pickParams(variables) {
 		var n, i, result = [];
 		variables = variables.split("");
@@ -96,11 +97,14 @@
 	    }// underlinexExp function
 
 	    // store in the variable allVariables
-	    // all of the variable expressions in exp
+	    // all of the variable expressions in exp that have a value
+	    //(must be called AFTER exp has been evaluated)
 	    function collectVariables(exp) {
 		var args, i;
 		if (A.isVarExp(exp)) {
-		    allVariables.push(exp);
+		    if (exp.value) {
+			allVariables.push(exp);
+		    }
 		} else if (A.isFnExp(exp)) {
 		    collectVariables(A.getFnExpBody(exp));
 		} else if (A.isAppExp(exp)) {
@@ -239,7 +243,7 @@
 		// build args
 		for(i=0; i < p1.length; i++) {
 		    rnd = SL.absyn.getRnd(0,2);
-		    rnd = 2;
+		    //rnd = 2;
 		    switch (rnd) {
 		    case 0: 
 			args.push(SL.absyn.createVarExp(
@@ -251,9 +255,12 @@
 			break;
 		    case 2:
 			args.push(SL.absyn.createFnExp(
-			    [ variables[SL.absyn.getRnd(0,
-							variables.length-1)] ],
-			    SL.absyn.createIntExp(SL.absyn.getRnd(0,10))
+			    pickParams(vs),
+			    ( A.getRnd(0,1) === 0 ?
+			      SL.absyn.createIntExp(SL.absyn.getRnd(0,30)) :
+			      SL.absyn.createVarExp(
+				  variables[
+				      SL.absyn.getRnd(0,variables.length-1)]))
 			));
 			break;
 		    }
@@ -273,42 +280,6 @@
 		    SL.absyn.generateRandomSLang1Program(
 			0,min,max,"xyz",""));
 	    }// getRndExp function
-
-	    while (true) {
-		allVariables = [];	   
-		exp = getRndExpRP22part3();
-		expStr = SL.printExp(exp);
-		if (expStr.length > 50) { continue; }
-		collectVariables(exp);
-		// pick one variable
-		selectedVar = allVariables[A.getRnd(0,allVariables.length-1)];
-		selectedVar.selected = true;
-		try {
-		    value = evalExpRP22part3(exp,globalEnv);
-		    //value = SL.evalExp(exp,globalEnv);
-//		    console.log(JSON.stringify(value));
-		    if  (E.isClo(selectedVar.value)) {
-			done = true;
-		    } else {
-			done = false;
-		    }		    
-		} catch (e) {
-		    done = false;
-		}
-		if (done) {
-		    break;
-		}
-	    }
-		
-	    //console.log(JSON.stringify(allVariables));
-	    //console.log(JSON.stringify(value,null,2));
-	    var lines = value; // JSON.stringify(value,null,2).split('\n');
-	    var indent, largestIndent = -1;
-	    var copy = [];
-	    var longestLine = -1;
-	    var line, n = 0,tmp;
-	    var newline;
-	    var maxLength = 50;
 
 	    // to limit the number of lines used up by the JSON format, pack
 	    // as much on each line (but only up to MaxLength characters)
@@ -347,30 +318,218 @@
 		}
 		result[result.length-1] = 
 		    result[result.length-1] + "]";
-		//result.push( prefix + "]");
 		return result;
+	    }// myStringify function
 
+	    // return any integer value that is in the global environment or
+	    // in the given expression but is different from the third arg
+	    function pickAnotherInteger(exp,v,excluded)
+	    {
+		var globalValue = E.lookup(globalEnv,v);
+		if (A.getRnd(0,1) === 0) {
+		    // pick the variable's value from the global env if possible
+		    if (E.getNumValue(globalValue) !== excluded) {
+			return globalValue;
+		    }
+		}
+		var values = expStr.match(/[0-9]+/g) // from exp
+		    .map(function (s) { return Number(s); })
+		    .filter(function (v) { return v !== excluded; } );
+		if (values.length > 0) {
+		    // return a value from the expression		    
+		    return E.createNum(values[A.getRnd(0,values.length-1)]);
+		} else {
+		    return E.createNum(excluded + 1);
+		}
+	    }// pickAnotherInteger function
+	    
+	    function pickRandomParams(params) {
+		var variables = vs.split("");
+		var newVar;
+		switch (params.length) {
+		case 0:
+		    return [ variables[A.getRnd(0,variables.length-1)] ];
+		case 1:
+		    if (A.getRnd(0,1)===0) {
+			// add another var
+			while ((newVar = 
+			       variables[A.getRnd(0,variables.length-1)]) ===
+			      params[0]) { /* empty */ var x=1; }
+			if (A.getRnd(0,1)===0) {
+			    // add at beginning
+			    params.unshift(newVar);
+			} else {
+			    // add at end
+			    params.push(newVar);
+			}
+			return params;
+		    } else {
+			// delete param
+			return [];
+		    }
+		    break;
+		default:  // 2 or 3 params
+		    switch (A.getRnd(0,2)) {
+			case 0:
+			params.pop();
+			break;
+			case 1:
+			params.splice(1,0);
+			break;
+			case 2:
+			params.shift();
+			break;
+		    }
+		    return params;			    
+		}// top-level switch
+	    }// pickRandomParams function
+
+	    function pickRandomBody(body) {
+		var bodyStr = SL.printExp(body);
+		var newBody = getRndExp(0,1);
+		while (bodyStr === SL.printExp(newBody)) {
+		    newBody = getRndExp(0,1);
+		}
+		return newBody;
+	    }// pickRandomBody
+
+	    function pickRandomEnv(env) {
+		var variables = vs.split("");
+		var bindings = E.getEnvBindings(env);
+		if (! E.isEmptyEnv(env)) {
+		    if (A.getRnd(0,4)===0) {
+			return E.createEmptyEnv();
+		    } else {
+			// delete one of the bindings
+			bindings.splice(A.getRnd(0,bindings.length-1),1);
+			return E.createEnv(
+			    bindings, E.getEnvEnv(env));
+		    }
+		} else {
+		    return E.createEnv(
+			[[variables[A.getRnd(0,variables.length-1)],
+			  E.createNum(A.getRnd(0,10))]],
+			env);
+		}
+	    }// pickRandomEnv
+
+	    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	    // pick an expression
+	    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	    while (true) {
+		exp = getRndExpRP22part3();
+		expStr = SL.printExp(exp);
+		if (expStr.length > 50) { continue; }
+		try {
+		    value = evalExpRP22part3(exp,globalEnv);
+		} catch (e) {
+		    // expression does not have a value; discard it
+		    continue;
+		}
+		// expression has a value; but another condition  must hold:
+		// at least one variable must be bound to a closure
+		// and at least one variable must be bound to an integer
+		allVariables = [];	   
+		collectVariables(exp);
+		varBoundToClosure = allVariables.filter( isClo );
+		varBoundToInt = allVariables.filter( isNum );
+		if (varBoundToClosure.length !== 0  &&
+		    varBoundToInt.length !== 0) {
+		    break;
+		}
+	    }// while loop
+
+	    this.expression = expStr;
+
+	    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+	    //  	   pick correct answer
+	    // %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+	    if (A.getRnd(0,1)===0) {
+	
+		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		// correct answer is True and ...
+		this.answer = "True";
+		if (A.getRnd(0,1)===0) {
+		    // --------------------------------
+		    // ... correct  value is a closure
+		    selectedVar = 
+			varBoundToClosure[A.getRnd(
+			    0,varBoundToClosure.length-1)];
+		    value = selectedVar.value;
+		} else {
+		    // --------------------------------
+		    // ... correct  value is an integer
+		    selectedVar = 
+			varBoundToInt[A.getRnd(
+			    0,varBoundToInt.length-1)];
+		    value = selectedVar.value;
+		}
+	    } else {
+		// %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+		// correct answer is False and ...		
+		this.answer = "False";	
+		if (A.getRnd(0,1)===0) {
+		    // --------------------------------------------
+		    // ... selected variable value is a closure ...
+		    selectedVar = 
+			varBoundToClosure[A.getRnd(
+			    0,varBoundToClosure.length-1)];
+		    if (A.getRnd(0,1)===0) {
+			// ... display another closure
+			rnd = A.getRnd(0,2);
+			params = E.getCloParams(selectedVar.value);
+			body = E.getCloBody(selectedVar.value);	
+			env = E.getCloEnv(selectedVar.value);
+			switch (rnd) {
+			    case 0: // change parameters of closure
+			    value = E.createClo(
+				pickRandomParams(params),body,env);
+			    break;
+			    case 1: // change body of closure
+			    value = E.createClo(
+				params,pickRandomBody(body),env);
+			    break;
+			    case 2: // change env of closure
+			    value = E.createClo(
+				params,body,pickRandomEnv(env));
+			    break;
+			}
+		    } else {
+			// ... display an integer
+			 value = varBoundToInt[A.getRnd(
+			     0,varBoundToInt.length-1)].value;
+		    }
+		} else {
+		    // ---------------------------------------------
+		    // ... selected variable value is an integer ...
+		    selectedVar = 
+			varBoundToInt[A.getRnd(
+			    0,varBoundToInt.length-1)];
+
+		    if (A.getRnd(0,1)===0) {
+			// ... display another integer
+			value = pickAnotherInteger(
+			    exp,
+			    A.getVarExpId(selectedVar),
+			    E.getNumValue(selectedVar.value));
+
+		    } else {
+			// ... display a closure
+			value = varBoundToClosure[A.getRnd(
+			    0,varBoundToClosure.length-1)].value;
+		    }
+		}
+		
 	    }
-	    console.log(JSON.stringify(lines)); // .join("\n"));
-	    console.log("==================");
-	    var tmp = myStringify(0,lines,maxLength);
-	    for(var i=0; i<tmp.length; i++) {
-		console.log(tmp[i]);
-	    }
-	    this.value = "test";
-	    console.log(this.value);
-	    this.expression = SL.printExp(exp);
+
+	    selectedVar.selected = true;		    
 	    this.underlinedExpression = underlineExp(exp);
-	    this.answer = E.isNum(selectedVar.value) ?
-		(E.getNumValue(selectedVar.value) + "") : "closure";
-				
-	    //console.log(this.answer);
-	},// init function
-
-	validateAnswer: function (studentAnswer) {
-	    return this.answer.replace(/\s+/g,"") ===
-		studentAnswer.replace(/\s+/g,"");
-	}// validateAnswer function
+	    this.value = myStringify(0,value,maxLength)
+		.join("<br />");
+	    //console.log(JSON.stringify(selectedVar.value));
+	}// init function
 
     };
 
