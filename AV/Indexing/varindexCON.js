57617e718fb314847a69e8dcec86d0aa8a4cf843
Cliff Shaffer <shaffer@vt.edu>
2016-04-14 18:38:57 -0400

    Fixed linear indexing slideshows

--- AV/Indexing/varindexCON.js
+++ AV/Indexing/varindexCON.js
@@ -6,19 +6,20 @@ $(document).ready(function () {
     arr.css(index, {"background-color": "#FFFF00" });
   };
   var itemheight = 72;  
+  var left = 170;
 
   //Lower rectangles
-  var rect11 = jsav.g.rect(0, itemheight, 120, 20).css({"fill": "white"});
-  var rect12 = jsav.g.rect(120, itemheight, 60, 20).css({"fill": "white"});
-  var rect13 = jsav.g.rect(180, itemheight, 140, 20).css({"fill": "white"});
-  var rect14 = jsav.g.rect(320, itemheight, 30, 20).css({"fill": "white"});
-  var rect15 = jsav.g.rect(350, itemheight, 170, 20).css({"fill": "white"});
+  var rect11 = jsav.g.rect(left + 0, itemheight, 120, 20).css({"fill": "white"});
+  var rect12 = jsav.g.rect(left + 120, itemheight, 60, 20).css({"fill": "white"});
+  var rect13 = jsav.g.rect(left + 180, itemheight, 140, 20).css({"fill": "white"});
+  var rect14 = jsav.g.rect(left + 320, itemheight, 30, 20).css({"fill": "white"});
+  var rect15 = jsav.g.rect(left + 350, itemheight, 170, 20).css({"fill": "white"});
   
-  var fragLabel1 = jsav.label("73", {left : 2, top: itemheight - 15});
-  var fragLabel2 = jsav.label("52", {left : 122, top: itemheight - 15});
-  var fragLabel3 = jsav.label("98", {left : 182, top: itemheight - 15});
-  var fragLabel4 = jsav.label("37", {left : 322, top: itemheight - 15});
-  var fragLabel5 = jsav.label("42", {left : 352, top: itemheight - 15});
+  var fragLabel1 = jsav.label("73", {left: left + 2, top: itemheight - 15});
+  var fragLabel2 = jsav.label("52", {left: left + 122, top: itemheight - 15});
+  var fragLabel3 = jsav.label("98", {left: left + 182, top: itemheight - 15});
+  var fragLabel4 = jsav.label("37", {left: left + 322, top: itemheight - 15});
+  var fragLabel5 = jsav.label("42", {left: left + 352, top: itemheight - 15});
   
   // Slide 1
   jsav.umsg("Here is an array of variable length database records");
@@ -60,38 +61,38 @@ $(document).ready(function () {
   jsav.step();
   
   // Slide 9
-  var xFragArrow = jsav.g.line(160,  20,  160, 45, {'stroke-width' : 1});
-  var yFragArrow = jsav.g.line(160,  45,  325, 45, {'stroke-width' : 1});
-  var zFragArrow = jsav.g.line(325,  45,  325, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
+  var xFragArrow = jsav.g.line(left + 150,  20,  left + 150, 45, {'stroke-width' : 1});
+  var yFragArrow = jsav.g.line(left + 150,  45,  left + 325, 45, {'stroke-width' : 1});
+  var zFragArrow = jsav.g.line(left + 325,  45,  left + 325, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
   setYellow(1);
   jsav.umsg("Every key has a pointer to the beginning of the corresponding record in the database file");
   jsav.step();
   
   // Slide 10
-  var x1FragArrow = jsav.g.line(226,  20,  226, 39, {'stroke-width' : 1});
-  var y1FragArrow = jsav.g.line(226,  39,  355, 39, {'stroke-width' : 1});
-  var z1FragArrow = jsav.g.line(355,  39,  355, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
+  var x1FragArrow = jsav.g.line(left + 211,  20,  left + 211, 39, {'stroke-width' : 1});
+  var y1FragArrow = jsav.g.line(left + 211,  39,  left + 355, 39, {'stroke-width' : 1});
+  var z1FragArrow = jsav.g.line(left + 355,  39,  left + 355, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
   setYellow(3);
   jsav.step();
   
   // Slide 11
-  var x2FragArrow = jsav.g.line(289,  20,  289, 62, {'stroke-width' : 1});
-  var y2FragArrow = jsav.g.line(289,  62,  127, 62, {'stroke-width' : 1});
-  var z2FragArrow = jsav.g.line(127,  62,  127, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
+  var x2FragArrow = jsav.g.line(left + 269,  20,  left + 269, 62, {'stroke-width' : 1});
+  var y2FragArrow = jsav.g.line(left + 269,  62,  left + 127, 62, {'stroke-width' : 1});
+  var z2FragArrow = jsav.g.line(left + 127,  62, left + 127, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
   setYellow(5);
   jsav.step();
   
   // Slide 12
-  var x3FragArrow = jsav.g.line(351,  20, 351, 57, {'stroke-width' : 1});
-  var y3FragArrow = jsav.g.line(351,  57,  7, 57, {'stroke-width' : 1});
-  var z3FragArrow = jsav.g.line(7,  57,  7, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
+  var x3FragArrow = jsav.g.line(left + 331,  20, left + 331, 57, {'stroke-width' : 1});
+  var y3FragArrow = jsav.g.line(left + 331,  57,  left + 7, 57, {'stroke-width' : 1});
+  var z3FragArrow = jsav.g.line(left + 7,  57,  left + 7, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});
   setYellow(7);
   jsav.step();
 
   // Slide 13
-  var x4FragArrow = jsav.g.line(412,  20,  412, 51, {'stroke-width' : 1});
-  var y4FragArrow = jsav.g.line(412,  51,  184, 51, {'stroke-width' : 1});
-  var z4FragArrow = jsav.g.line(184,  51,  184, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});  
+  var x4FragArrow = jsav.g.line(left + 392,  20,  left + 392, 51, {'stroke-width' : 1});
+  var y4FragArrow = jsav.g.line(left + 392,  51,  left + 184, 51, {'stroke-width' : 1});
+  var z4FragArrow = jsav.g.line(left + 184,  51,  left + 184, 70, {'arrow-end': 'classic-wide-long','stroke-width' : 1});  
   setYellow(9);
   jsav.step();
   
