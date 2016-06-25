078cdda648730eb2b62e8def99efd911bc7e1aab
Souleymane Dia <sould32@vt.edu>
2016-06-25 18:21:41 -0400

    updated SkipNode script

--- AV/Development/SkipList/SkipNode.js
+++ AV/Development/SkipList/SkipNode.js
@@ -1,75 +1,142 @@
 /**
  * @author Souleymane Dia
  * @version <05/24/2016>~ summer 2016
- * see SkipList file comments before use
+ * represent the Skipnode script use in the SkipList implementation
  */
 function SkipNode(p, nodeLevel, jsav, options, pointer, num) {
 	this.jsav = jsav;
 	this.options = options;
-	this.pointer = pointer;
-	// to determine how long the arraow should be extended
 	this.nodeNum = num;
-	// ------------------------------
 	this.pair = p;
 	this.nodeLevel = nodeLevel;
-	var arr = new Array(nodeLevel + 1);
+	this.arr = new Array(nodeLevel + 1);
 	this.forward = new Array(nodeLevel + 1);
+	this.pointer = new Array(nodeLevel+1);
 	for (var i = 0; i < nodeLevel + 1; i++) {
-		arr[i] = ' \ -/';
+		this.arr[i] = ' \ -/';
 		this.forward[i] = null;
+		this.pointer[i] = null;
 	}
-	this.disArr = this.jsav.ds.array(arr, options);
-	var newOp = $.extend({}, this.options);
-	newOp.indexed = false;
-	newOp.top = -10;
-	newOp.left = (this.options.indexed === true)? this.options.left + 8: this.options.left;
-	//newOp.top = this.options.top - 100;
+	this.disArr = this.jsav.ds.array(this.arr, options);
+	this.newOp = $.extend({}, this.options);
+	this.newOp.indexed = false;
+	this.newOp.top = -15;
+	this.newOp.left = (this.options.indexed === true)? this.options.left + 8: this.options.left;
 	if (this.pair === null){
-		this.val = jsav.ds.array(['head'],newOp);
+		this.val = jsav.ds.array(['head'],this.newOp);
 	}else{
-		this.val = jsav.ds.array([ p.toString() ],newOp);
+		this.val = jsav.ds.array([p.toString() ],this.newOp);
 	}
-	//var Edge = JSAV._types.ds.Edge;
-	//var csf = new Edge(this.jsav, this.forward[i], this.forward[i], { 'arrow-end': 'classic-wide-long' });
 }
+SkipNode.prototype.updateDis = function(options, newOp) {
+	this.val.hide();
+	this.val = this.jsav.ds.array([ this.pair.toString() ], newOp);
+	this.disArr.hide();
+	this.disArr = this.jsav.ds.array(this.arr, options);
+	this.val.show();
+	this.disArr.show();
+};
+SkipNode.prototype.resetArrDis = function(i) {
+	this.arr[i] = " \ -/";
+	this.disArr.value(i, " \ -/");
+};
+SkipNode.prototype.updateArrDis = function(i) {
+	this.arr[i] = " " ;
+	this.disArr.value(i, " ");
+};
+SkipNode.prototype.equalKey = function(other) {
+	return (this.pair.getKey() === other);
+};
+SkipNode.prototype.equals = function(other) {
+	return (this.pair.getKey() === other.pair.getKey()) &&
+				(this.pair.getVal() === other.pair.getVal());
+};
+SkipNode.prototype.getNewOp = function() {
+	return this.newOp;
+};
 SkipNode.prototype.getNodeNum = function() {
 	return this.nodeNum;
+};
+SkipNode.prototype.incrNodeNum = function(i) {
+	this.nodeNum++;
 };
 SkipNode.prototype.getOptions = function() {
 	return this.options;
 };
-SkipNode.prototype.setOptions = function(op) {
-	return this.options = op;
+SkipNode.prototype.updateNextPointer = function(longer, j){
+	this.pointer[j].hide();
+	this.pointer[j] =  this.jsav.pointer(" ", this.disArr, {
+			targetIndex: j,
+			left: -(longer - 1)*100 + (-85),
+			top: 23,
+			arrowAnchor: "left center",
+			fixed: false,
+			"stroke-width": 2	
+			});
+}
+SkipNode.prototype.movePointerRight = function(lev, longer, t) {
+	var point = this.pointer;
+	for (var i = 0; (i < (this.nodeLevel + 1) && point[i] !== null); i++) {
+		var lef = this.pointer[i].options.left;
+		this.pointer[i].hide();
+		var d = -((this.nodeNum - longer) -1) * 100 - 85;
+		d = -d;
+		this.pointer[i] = this.jsav.pointer(" ", this.disArr, {
+			targetIndex: i,
+			left: (t == 0)? lef - 100: (d <= -lef)? lef - 100: lef,
+			top: 23,
+			arrowAnchor: "left center",
+			fixed: false,
+			"stroke-width": 2	
+		});
+	}
 };
-SkipNode.prototype.setPointer = function(pt) {
-	return this.pointer = pt;
+
+SkipNode.prototype.decrNodeNum = function(i) {
+	this.nodeNum--;
+};
+
+SkipNode.prototype.movePointerLeft = function(lev, longer, t) {
+	var point = this.pointer;
+	for (var i = 0; (i < (this.nodeLevel + 1) && point[i] !== null); i++) {
+		var lef = this.pointer[i].options.left;
+		this.pointer[i].hide();
+		var d = -((this.nodeNum - longer)) * 100 - 85;
+		d = -d;
+		this.pointer[i] = this.jsav.pointer(" ", this.disArr, {
+			targetIndex: i,
+			left: (d > -lef)? lef: lef + 100,
+			top: 23,
+			arrowAnchor: "left center",
+			fixed: false
+		});
+		this.pointer[i].css({"stroke-width": 2});	
+	}
+}
+SkipNode.prototype.updPter = function(i, opt) {
+	this.pointer[i].hide();
+	this.pointer[i] = this.jsav.pointer(" ", this.disArr,opt);
+	this.pointer[i].css({"stroke-width": 2});	
+};	
+SkipNode.prototype.clear = function(i, pt) {
+	for (var i = 0; i < this.nodeLevel + 1; i++) {
+		this.pointer[i].hide();
+	}
+	this.disArr.hide();
+	this.val.hide();
+};
+SkipNode.prototype.setPointer = function(i, pt) {
+	return this.pointer[i] = pt;
+	this.pointer[i].css({"stroke-width": 2});	
 };
 SkipNode.prototype.getLevel = function() {
-	return this.nodeLevel;
-};
-SkipNode.prototype.setLevel = function(lev) {
-	this.nodeLevel = lev;
 	return this.nodeLevel;
 };
 SkipNode.prototype.getForward = function() {
 	return this.forward;
 };
-SkipNode.prototype.setForward = function(forward) {
-	this.forward = forward;
-	return this.forward;
-};
 SkipNode.prototype.getDispArr = function() {
 	return this.disArr;
-};
-SkipNode.prototype.setDispArr = function(i, valu1) {
-	this.disArr[i] = valu1;
-};
-SkipNode.prototype.setForwardVal = function(i, val) {
-	this.forward[i] = val;
-	return this.forward;
-};
-SkipNode.prototype.getForwardVal = function() {
-	return this.val;
 };
 SkipNode.prototype.getPair = function(forward) {
 	return this.pair;
@@ -78,7 +145,6 @@ SkipNode.prototype.toString = function() {
 	if (this.pair === null) {
 		return 'Node has depth ' + this.nodeLevel + ', Value (null) ';
 	}
-
 	else if (this.pair.getVal() === null) {
 		return 'Node has depth ' + this.nodeLevel + ', Value (null) ';
 	} else {
