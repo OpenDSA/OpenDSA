9fae5c2579ea6f3b000750636cae5bf9e50a4a6b
Bill Yu <han.yu@duke.edu>
2016-07-01 11:48:28 -0400

    traversal on TuringMachine

--- AV/FLA/billyu/fa/TuringMachine.js
+++ AV/FLA/billyu/fa/TuringMachine.js
@@ -1,7 +1,6 @@
 // Turing Machine "class", extending FiniteAutomaton
 var TuringMachine = function(jsav, options) {
 	FiniteAutomaton.apply(this, arguments);
-	this.tape = new Tape(jsav, this, {});
 	this.transitions = [];
 }
 
@@ -12,7 +11,7 @@ JSAV.ext.ds.tm = function (options) {
 
 JSAV.utils.extend(TuringMachine, JSAV._types.ds.Graph);
 
-TuringMachine.prototype = Object.create(FA.FiniteAutomaton.prototype, {});
+TuringMachine.prototype = Object.create(FiniteAutomaton.prototype, {});
 var tm = TuringMachine.prototype;
 
 tm.addTransition = function(start, end, toRead, toWrite, direction) {
@@ -32,9 +31,9 @@ tm.play = function(inputString) {
 	for (var j = 0; j < currentStates.length; j++) {
 		configView.push(currentStates[j].toString());
 	}
-	jsav.umsg(configView.join(' | '));
-	g.initial.addClass('current');
-	jsav.displayInit();
+	this.jsav.umsg(configView.join(' | '));
+	this.initial.highlight();
+	this.jsav.displayInit();
 
 	while (true) {
 		if (counter === 500) {
@@ -43,11 +42,11 @@ tm.play = function(inputString) {
 		}
 		counter++;
 		for (var j = 0; j < currentStates.length; j++) {
-			currentStates[j].state.removeClass('current');
-			currentStates[j].state.removeClass('accepted');
+			currentStates[j].state.unhighlight();
+			this.removeAccept(currentStates[j].state);
 		}
 		// get next states
-		cur = traverse(currentStates);
+		cur = this.traverse(currentStates);
 		if (!cur || cur.length === 0) {
 			break;
 		}
@@ -55,33 +54,54 @@ tm.play = function(inputString) {
 		configView = [];
 		// add highlighting and display
 		for (var j = 0; j < currentStates.length; j++) {
-			currentStates[j].state.addClass('current');
-			if (currentStates[j].state.hasClass('final')) {
-				currentStates[j].state.addClass('accepted');
+			currentStates[j].state.highlight();
+			if (this.isFinal(currentStates[j].state)) {
+				this.showAccept(currentStates[j].state);
 			}
 			configView.push(currentStates[j].toString());
 		}
-		jsav.umsg(configView.join(' | '));
-		jsav.step();
+		this.jsav.umsg(configView.join(' | '));
+		this.jsav.step();
 	}
 	for (var k = 0; k < currentStates.length; k++) {
-		if (currentStates[k].state.hasClass('final')) {
-			currentStates[k].state.addClass('accepted');
+		if (this.isFinal(currentStates[k].state)) {
+			this.showAccept(currentStates[k].state);
 		} else {
-			currentStates[k].state.addClass('rejected');
+			this.showReject(currentStates[k].state);
 		}
 	}
-	jsav.step();
-	jsav.recorded();
+	this.jsav.step();
+	this.jsav.recorded();
 };
+
+tm.showAccept = function(state) {
+	state.addClass('accepted');
+}
+
+tm.removeAccept = function(state) {
+	state.removeClass('accepted');
+}
+
+tm.showReject = function(state) {
+	state.addClass('rejected');
+}
+
+tm.isInitial = function(state) {
+	return state == this.initial;
+}
+
+tm.isFinal = function(state) {
+	return state.hasClass('final');
+}
+
 // given a list of configurations, returns the set of next configurations
-var traverse = function(currentStates) {
+tm.traverse = function(currentStates) {
 	var nextStates = [];
 	for (var j = 0; j < currentStates.length; j++) {
 		var currentState = currentStates[j];
 		var successors = currentState.state.neighbors();
 		for (var next = successors.next(); next; next = successors.next()) {
-			var edge = g.getEdge(currentState.state, next),
+			var edge = this.getEdge(currentState.state, next),
 					weight = edge.weight().split('<br>');
 			for (var i = 0; i < weight.length; i++) {
 				var w = weight[i].split(':');
@@ -100,6 +120,7 @@ var traverse = function(currentStates) {
 	return nextStates;
 };
 
+
 var Configuration = function(state, tape) {
 	this.state = state;
 	this.tape = new Tape(tape);
@@ -116,7 +137,7 @@ var Configuration = function(state, tape) {
 
 // Transition object for Turing Machine
 var TMTransition = function(jsav, start, end, toRead, toWrite, direction, options) {
-	var weight = toRead + ", " + toWrite + ", " + direction;
+	var weight = toRead + ":" + toWrite + ":" + direction;
 	this.weight = weight;
 	this.jsav = jsav;
 	this.start = start;
@@ -133,12 +154,258 @@ tmTrans.getWeight = function() {
 }
 
 
-// Tape object for Turing Machine
-var Tape = function(jsav, tm, options) {
-	this.jsav = jsav;
-	this.tm = tm;
-	this.head = 0;
-	this.cells = ["w"];
-}
+/*
+	 Holds the tape as a linked list and keeps track of the current position
+	 as well as the beginning of the string.
+ */
+var Tape = function (str) {
+	// if the tape is initialized using a string, writes the string to the new tape
+	if (typeof str === 'string') {
+		this.head = makeTape(str);
+		this.current = this.head.right()[0];  // the current symbol
+		this.currentIndex = 0;                // the current position
+	} 
+	// else, assume that a Tape object was passed in, and create a copy of it
+	else {
+		var copy = copyTape(str);
+		this.head = copy[0];
+		this.current = copy[1];
+		this.currentIndex = str.currentIndex;
+	}
+	// Returns the string written on the tape starting from the head of the string, including empty squares
+	this.toString = function() {
+		var temp = this.head,
+				ret = "";
+		while (temp) {
+			ret += temp.value();
+			temp = temp._right;
+		}
+		return ret;
+	};
+	// Move the tape left and set the new head of the string
+	this.left = function() {
+		var next = this.current.left();
+		this.current = next[0];
+		this.currentIndex--;
+		if (next[1]) {
+			this.head = next[1];
+		}
+		return this.current;
+	};
+	// Move the tape right
+	this.right = function() {
+		var next = this.current.right();
+		this.current = next[0];
+		this.currentIndex++;
+		return this.current;
+	};
+	// Write to the current position
+	this.value = function (newVal) {
+		return this.current.value(newVal);
+	};
+	// Move the tape and read the symbol
+	this.move = function (str) {
+		if (str === "L") {
+			return this.left();
+		} else if (str === "R") {
+			return this.right();
+		} else if (str === "S") {
+			return this.current;
+		}
+	};
+};
 
-var tape = Tape.prototype;
+// Tape linked list node
+var TapeNode = function (left, right, val) {
+	this._left = left;
+	this._right = right;
+	if (typeof val === "undefined") {
+		//this._value = "";
+		this._value = String.fromCharCode(9633);
+	} else {
+		this._value = val;
+	}
+	// Return the value written at this position or write a symbol to this position
+	this.value = function (newVal) {
+		if (typeof newVal === "undefined") {
+			return this._value;
+		} else {
+			this._value = newVal;
+			return this._value;
+		}
+	};
+	/*
+		 Traverse left or right and read. 
+		 If can't, create empty tape nodes. 
+		 Returns an array containing the read symbol and the new head of the string
+	 */
+	this.left = function (n) {
+		if (this._left) {
+			return [this._left];
+		} else {
+			if (!n) { n = 10; }
+			return extendTape("left", this, n);
+		}
+	}
+	this.right = function (n) {
+		if (this._right) {
+			return [this._right];
+		} else {
+			if (!n) { n = 10; }
+			return extendTape("right", this, n);
+		}
+	}
+};
+
+// Function to initialize the linked list from an inputted string
+var makeTape = function (str) {
+	var prev = new TapeNode(null, null);
+	var head = prev;
+	for (var i = 0; i < str.length; i++) {
+		var temp = new TapeNode(prev, null, str.charAt(i));
+		prev._right = temp;
+		prev = temp;
+	}
+	return head;
+};
+
+// Function to initialize the linked list by copying the inputted tape
+var copyTape = function (t) {
+	var prev = new TapeNode(null, null, t.current.value());
+	var cur = prev;
+	var temp = t.current._right;
+	while (temp) {
+		var next = new TapeNode(prev, null, temp.value());
+		prev._right = next;
+		prev = next;
+		temp = temp._right;
+	}
+	prev = cur;
+	temp = t.current;
+	while (temp._left) {
+		var next = new TapeNode(null, prev, temp._left.value());
+		prev._left = next;
+		prev = next;
+		temp = temp._left;
+	}
+	return [temp, cur];
+};
+
+/*
+	 Creates n empty tape nodes to the beginning or end of the tape 
+	 and returns the read symbol and the leftmost/rightmost new node
+ */
+var extendTape = function (dir, node, n) {
+	if (dir === 'left') {
+		var next = new TapeNode(null, node),
+				prev = next;
+		node._left = next;
+		for (var i = 0; i < n - 1; i++) {
+			var temp = new TapeNode(null, prev);
+			prev._left = temp;
+			prev = temp;
+		}
+		return [next, prev];
+	}
+	if (dir === 'right') {
+		var next = new TapeNode(node, null),
+				prev = next;
+		node._right = next;
+		for (var i = 0; i < n - 1; i++) {
+			var temp = new TapeNode(prev, null);
+			prev._right = temp;
+			prev = temp;
+		}
+		return [next, prev];
+	}
+};
+
+/*
+	 Function to get the tape alphabet.
+ */
+var getTapeAlphabet = function (graph) {
+	var alphabet = [];
+	var edges = graph.edges();
+	var w;
+	for (var next = edges.next(); next; next = edges.next()) {
+		w = next.weight();
+		w = w.split('<br>');
+		for (var i = 0; i < w.length; i++) {
+			var t = w[i].split('|');
+			for (var k = 0; k < t.length; k++) {
+				var letter1 = t[k].split(':')[0],
+						letter2 = t[k].split(':')[1],
+						letters;
+				if (letter1 !== graph.emptystring && letter2 !== graph.emptystring) {
+					letters = letter1.split('').concat(letter2.split(''));
+				} else if (letter1 !== graph.emptystring) {
+					letters = letter1.split('');
+				} else if (letter2 !== graph.emptystring) {
+					letters = letter2.split('');
+				} else {
+					break;
+				}
+				for (var j = 0; j < letters.length; j++) {
+					if (letters[j] !== graph.emptystring && alphabet.indexOf(letters[j]) === -1){
+						alphabet.push(letters[j]);
+					}
+				}
+			}
+		}
+	}
+	return alphabet;
+};
+
+/*
+	 Function to get the tape "viewport".
+	 This returns a string showing the current position and the seven
+	 symbols to the left and right of the current position.
+	 The current position is highlighted as well.
+ */
+var viewTape = function (t) {
+	var arr = new Array(15);    // arbitrary size
+	for (var i = 0; i < 15; i++) {
+		arr[i] = String.fromCharCode(9633);;
+	}
+	i = 7;
+	var temp = t.current;
+	while (temp) {
+		if (i < 0) {break;}
+		arr[i] = temp.value();
+		i--;
+		temp = temp._left;
+	}
+	i = 7;
+	temp = t.current;
+	while (temp) {
+		if (i >= arr.length) {break;}
+		arr[i] = temp.value();
+		i++;
+		temp = temp._right;
+	}
+	var view = "|";
+	for (var i = 0; i < arr.length; i++) {
+		if (i === 7) {
+			view+="<mark>" + arr[i] + "</mark>";
+		} else {
+			view+=arr[i];
+		}
+	}
+	view+="|";
+	return view;
+};
+
+/*
+	 Function to get the tape output, the string made from the current position up to,
+	 but not including, the first empty square found.
+ */
+
+var produceOutput = function (t) {
+	var temp = t.current,
+			output = "";
+	while (temp && temp.value() !== String.fromCharCode(9633)) {
+		output += temp.value();
+		temp = temp._right;
+	}
+	return output;
+};
--- AV/FLA/billyu/ui/TMEditor.js
+++ AV/FLA/billyu/ui/TMEditor.js
@@ -1,9 +1,393 @@
+var lambda = String.fromCharCode(955),
+		epsilon = String.fromCharCode(949),
+		square = String.fromCharCode(9633),
+		emptystring;
+
 (function($) {
 	var jsav = new JSAV("av");
-	var g = new jsav.ds.tm({width: '730px', height: 440, layout: "automatic", editable: true});
-	var n1 = g.addNode({left: '150px'});
-	var n2 = g.addNode();
-	var e1 = g.addTransition(n1, n2, "a", "b", "R");
-	console.log(e1);
-	console.log(e1.getWeight());
+	jsav.displayInit();
+	var g;
+
+// initialize graph
+	var initGraph = function(opts) {
+		g = jsav.ds.tm($.extend({width: '90%', height: 440, emptystring: square}, opts));
+		emptystring = g.emptystring;
+		var gWidth = g.element.width(),
+				gHeight = g.element.height();
+  		var a = g.addNode({left: 0.10 * gWidth, top: 0.3 * gHeight}),		
+      		b = g.addNode({left: 0.35 * gWidth, top: 0.7 * gHeight}),
+      		c = g.addNode({left: 0.10 * gWidth, top: 0.7 * gHeight}),
+      		d = g.addNode({left: 0.6 * gWidth, top: 0.7 * gHeight}),
+      		e = g.addNode({left: 0.85 * gWidth, top: 0.5 * gHeight}),
+      		f = g.addNode({left: 0.35 * gWidth, top: 0.3 * gHeight});
+      	g.makeInitial(a);
+      	c.addClass('final');
+
+	    g.addEdge(a, b, {weight: 'a:#:R'});
+	   	g.addEdge(a, a, {weight: '#:#:R'});
+	    //g.addEdge(a, d); 		it's a FA, need to always provide a weight
+
+	    g.addEdge(b, b, {weight: '#:#:R'});
+	    g.addEdge(b, c, {weight: square + ':' + square + ':L'});
+	    g.addEdge(b, d, {weight: 'a:a:R'});
+
+	   	g.addEdge(d, d, {weight: '#:#:R'});
+	    g.addEdge(d, e, {weight: 'a:#:R'});
+	   	g.addEdge(d, f, {weight: square + ':' + square + ':L'});
+
+	    g.addEdge(e, e, {weight: '#:#:R'});
+	    g.addEdge(e, d, {weight: 'a:a:R'});
+
+	    g.addEdge(f, f, {weight: '#:#:L'});
+	    g.addEdge(f, f, {weight: 'a:a:L'});
+	    g.addEdge(f, a, {weight: square + ':' + square + ':R'});
+	
+    	$(".jsavgraph").click(graphClickHandler);
+    	g.click(nodeClickHandler);
+		g.click(edgeClickHandler, {edge: true});
+		$('.jsavedgelabel').click(labelClickHandler);
+		return g;
+	};
+
+	// handler for editing edges/transitions
+	var labelClickHandler = function(e) {
+		if ($(".jsavgraph").hasClass("editNodes") && !$(".jsavgraph").hasClass("working")) {
+			var jg = $(".jsavgraph");
+			jg.addClass("working");
+			var self = this;
+			var values = $(this).html().split('<br>');
+			// interface for editing individual transitions is as a dropdown menu
+			var createForm = '<form id="editedgelabel"><select class="labelmenu" id="edgelabelselect" size="' + values.length + '">'
+			for (var i = 0; i < values.length; i++) {
+				createForm += '<option>' + values[i] + '</option>';
+			}
+			createForm += '</select><br><input type="button" class="labelmenu" id="changetransitionbutton" value="Change transition"><input type="button" class="labelmenu" id="deletetransitionbutton" value="Delete transition"><input type="button" class="labelmenu" id="donelabelbutton" value="Done"></form>'
+			$(createForm).appendTo(jg);
+			// place menu where the user clicked (adjust positioning near the edges of the window)
+			var xBound = jg.offset().left + jg.width(),
+				yBound = jg.offset().top + jg.height(),
+				xOffset = e.pageX,
+				yOffset = e.pageY,
+				xWidth = $('#editedgelabel').width(),
+				yHeight = $('#editedgelabel').height();
+			if (xBound < xOffset + xWidth) {
+				xOffset -= xWidth;
+			}
+			if (yBound < yOffset + yHeight) {
+				yOffset -= yHeight;
+			}
+			$('#editedgelabel').offset({top: yOffset, left: xOffset});
+			// function for editing the selected transition
+			var changeTransition = function() {
+				var x = document.getElementById("edgelabelselect").selectedIndex;
+				if (x !== -1) {
+					var y = document.getElementById('edgelabelselect').options[x].text;
+					var n = prompt("New transition label?", y);
+					if (n) {
+						var nSplit = n.split(':');
+						for (var i = 0; i < nSplit.length; i++) {
+							if (nSplit[i] === "") {
+								nSplit[i] = emptystring;
+							}
+						}
+						n = nSplit.join(':');
+						document.getElementById('edgelabelselect').options[x].innerHTML = n;
+					}
+				}
+			};
+			// function for deleting the selected transition
+			var deleteTransition = function() {
+				var x = document.getElementById('edgelabelselect').selectedIndex;
+				if (x !== -1) {
+					document.getElementById('edgelabelselect').remove(x);
+					document.getElementById('edgelabelselect').size--;
+					// if all transitions are deleted, close the menu
+					if (document.getElementById('edgelabelselect').size === 0) {
+						$('#donelabelbutton').trigger("click");
+					}
+				}
+			};
+			// applies changes to the transitions and closes the menu
+			var finishEdgeLabel = function() {
+				var newVal = [];
+				for (var j = 0; j < $('#edgelabelselect > option').length; j++) {
+					newVal.push(document.getElementById('edgelabelselect').options[j].text);
+				}
+				newVal = newVal.join('<br>');
+				$(self).html(newVal);
+				$('#editedgelabel').remove();
+				g.layout({layout: "automatic"});
+				// console.log(_.map(g._alledges, function(x){return x.weight()}))
+				$('.jsavgraph').removeClass("working");
+				updateAlphabet();
+			};
+			$('#changetransitionbutton').click(changeTransition);
+			$('#deletetransitionbutton').click(deleteTransition);
+			$('#donelabelbutton').click(finishEdgeLabel);
+		}
+	};
+
+	// handler for the graph window
+	var graphClickHandler = function(e) {
+		if ($(".jsavgraph").hasClass("addNodes")) {
+			var newNode = g.addNode(),
+			    nodeX = newNode.element.width()/2.0,
+				nodeY = newNode.element.height()/2.0;
+			$(newNode.element).offset({top: e.pageY - nodeY, left: e.pageX - nodeX});
+		} 
+		else if ($('.jsavgraph').hasClass('moveNodes') && selectedNode != null) {
+			var nodeX = selectedNode.element.width()/2.0,
+				nodeY = selectedNode.element.height()/2.0,
+				edges = g.edges();
+			$(selectedNode.element).offset({top: e.pageY - nodeY, left: e.pageX - nodeX});
+			selectedNode.stateLabelPositionUpdate();
+			for (var next = edges.next(); next; next = edges.next()) {
+				if (next.start().equals(selectedNode) || next.end().equals(selectedNode)) {
+					next.layout();
+				}
+			}
+			selectedNode.unhighlight();
+			selectedNode = null;
+			e.stopPropagation();
+			jsav.umsg("Click a node");
+		}
+	};
+
+	// handler for the nodes of the graph
+	var nodeClickHandler = function(e) {	
+		// editing nodes should be changed to match the interface in multitapeTest.js
+		if ($(".jsavgraph").hasClass("editNodes")) {
+			this.highlight();
+			var input = prompt("Delete state, make state initial, make state final, or give state a label? d, i, f, or l");
+			if (input === null) {
+				this.unhighlight();
+				return;
+			}
+			input = input.toUpperCase();
+			if (input == 'D') {
+				g.removeNode(this);
+				updateAlphabet();
+			}
+			else if (input == 'I') {
+				var nodes = g.nodes();
+				for (var next = nodes.next(); next; next = nodes.next()) {
+					g.removeInitial(next);
+				}
+				g.makeInitial(this);
+			} else if (input == 'F') {
+				this.toggleClass('final');
+			} 
+			//adds labels to states
+			else if (input == 'L') {
+				var input2 = prompt("Label?");
+				if (input2 !== null) {
+					this.stateLabel(input2);
+					this.stateLabelPositionUpdate();
+				}
+			}
+   			this.unhighlight();
+		} else if ($(".jsavgraph").hasClass("addEdges")) {
+			this.highlight();
+			if (!$(".jsavgraph").hasClass("working")) {
+				first = this;
+				$('.jsavgraph').addClass("working");
+				jsav.umsg("Select a state to make a transition to");
+   			} else {
+   				var input2 = prompt("Character to read?");
+   				var input3 = prompt('Character to write?');
+   				var input4 = prompt('Direction to move the tape head: left, right, or stay? L, R, S?');
+   				if (input4) {
+   					input4 = input4.toUpperCase();
+   				}
+   				var newEdge;
+   				if (!input2) {
+   					input2 = emptystring;
+   				}
+   				if (!input3) {
+   					input3 = emptystring;
+   				}
+   				if (!input4 || (input4 !== 'L' && input4 !== 'R' && input4 !== 'S')) {
+   					input4 = 'S';
+   				}
+   				var w = input2 + ':' + input3 + ':' + input4;
+				if (input2 != null) {
+					newEdge = g.addEdge(first, this, {weight: w});
+					if (newEdge) {
+						$(newEdge._label.element).click(labelClickHandler);
+					}
+				} 
+				if (!(typeof newEdge === 'undefined')) {
+					newEdge.layout();
+				}
+				$('.jsavgraph').removeClass("working");
+				first.unhighlight();
+				this.unhighlight();
+				updateAlphabet();
+				jsav.umsg("Click a node");
+   			}
+		} else if ($('.jsavgraph').hasClass('moveNodes')) {
+			if (selectedNode) {
+				selectedNode.unhighlight();
+			}
+			this.highlight();
+			selectedNode = this;
+			jsav.umsg("Click to place node");
+			e.stopPropagation();
+		}
+	};
+
+	// handler for the edges of the graph
+	var edgeClickHandler = function(e) {
+		if ($('.jsavgraph').hasClass('editNodes')) {
+			this.highlight();
+			var input = confirm("Delete edge?");
+			if (input === null) {
+				this.unhighlight();
+				return;
+			}
+			if (input) {
+				g.removeEdge(this);
+			}
+			updateAlphabet();
+			this.unhighlight();
+		}
+	};
+	
+	//===============================
+	// updates the alphabets displayed above the graph
+	var updateAlphabet = function() {
+		g.updateAlphabet();
+		$("#alphabet").html("" + Object.keys(g.alphabet).sort());
+		var sa = getTapeAlphabet(g);
+		$('#stackalphabet').html(emptystring + "," + sa.sort());
+	};
+
+	//===============================
+	//editing modes
+
+	var addNodesMode = function() {
+		removeEdgeSelect();
+		removeLabelMenu();
+		var jg = $(".jsavgraph");
+		jg.removeClass("working");
+		jg.removeClass("addEdges");
+		jg.removeClass("moveNodes");
+		jg.removeClass("editNodes");
+		jg.addClass("addNodes");
+		$("#mode").html('Adding nodes');
+		jsav.umsg("Click to add nodes");
+	};
+	var addEdgesMode = function() {
+		removeEdgeSelect();
+		removeLabelMenu();
+		var jg = $(".jsavgraph");
+		jg.removeClass("working");
+		jg.removeClass("addNodes");
+		jg.removeClass("moveNodes");
+		jg.removeClass("editNodes");
+		jg.addClass("addEdges");
+		$("#mode").html('Adding edges');
+		jsav.umsg("Click a node");
+	};
+	var moveNodesMode = function() {
+		removeEdgeSelect();
+		removeLabelMenu();
+		var jg = $(".jsavgraph");
+		jg.removeClass("working");
+		jg.removeClass("addNodes");
+		jg.removeClass("addEdges");
+		jg.removeClass("editNodes");
+		jg.addClass("moveNodes");
+		$("#mode").html('Moving nodes');
+		jsav.umsg("Click a node");
+	};
+	var editNodesMode = function() {
+		var jg = $(".jsavgraph");
+		jg.removeClass("working");
+		jg.removeClass("addNodes");
+		jg.removeClass("addEdges");
+		jg.removeClass("moveNodes");
+		jg.addClass("editNodes");
+		$("#mode").html('Editing nodes and edges');
+		addEdgeSelect();
+		jsav.umsg("Click a node or edge");
+	};
+	// change between editing and not editing (traversal)
+	var changeEditingMode = function() {
+		removeLabelMenu();
+		var jg = $(".jsavgraph");
+		jg.removeClass("working");
+		jg.removeClass("addNodes");
+		jg.removeClass("addEdges");
+		jg.removeClass("moveNodes");
+		jg.removeClass('editNodes');
+		removeEdgeSelect();
+		$("#mode").html('Editing');
+		if ($(".notEditing").is(":visible")) {
+			$('#changeButton').html('Done editing');
+		} else {
+			$('#changeButton').html('Edit');
+		}
+		$('.notEditing').toggle();
+		$('.editing').toggle();
+	};
+
+	// make edges easier to click
+	var addEdgeSelect = function () {
+		var edges = g.edges();
+		for (var next = edges.next(); next; next= edges.next()) {
+			next.addClass('edgeSelect');
+			next.layout();
+		}
+	};
+	var removeEdgeSelect = function () {
+		var edges = g.edges();
+		for (var next = edges.next(); next; next = edges.next()) {
+			if (next.hasClass('edgeSelect')) {
+				next.removeClass('edgeSelect');
+				next.layout();
+			}
+		}
+	};
+	var removeLabelMenu = function() {
+		if ($('#editedgelabel')) {
+			$('#editedgelabel').remove();
+		}
+	};
+
+	var onClickTraverse = function() {
+		if (!g.initial) {
+			alert('Please define an initial state');
+			return;
+		}
+		var inputString = prompt("Input string?", "aaaa");
+		while (inputString == null) {
+			alert("Don't try to trick the program!");
+			inputString = prompt("Input string?", "aaaa");	
+		}
+		jsav.umsg("");
+		$("#functionality").hide();			//disable buttons
+		$('#alphabets').hide();
+		$("#mode").html('');
+		$('.jsavcontrols').show();
+		$('.jsavoutput').css({"text-align": "center", 'font-size': '2em', 'font-family': 'monospace'});
+
+		g.play(inputString);
+	}
+
+	$('#playButton').click(function() {onClickTraverse()});
+	//$('#multiplebutton').click(displayTraversals);
+	$('#nodeButton').click(addNodesMode);
+	$('#changeButton').click(changeEditingMode);
+	$('#edgeButton').click(addEdgesMode);
+	$('#moveButton').click(moveNodesMode);
+	$('#editButton').click(editNodesMode);
+	//$('#savebutton').click(save);
+  //$('#loadbutton').on('change', load);
+
+	g = initGraph({layout: "manual"});
+	g.layout();
+	jsav.displayInit();
+	updateAlphabet();
+
 }(jQuery));
