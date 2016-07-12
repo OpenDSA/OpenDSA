6d73d732cd3cc5fb99ef69b81b41d6c815c6922e
Zeynep Öner <zeyneponer@Zeynep-MBP.home>
2016-07-11 22:15:13 -0400

    sorting fıles update

--- SourceCode/Java/SortTime/MergeSort.java
+++ SourceCode/Java/SortTime/MergeSort.java
@@ -1,6 +1,5 @@
 
 static void sorttime(){
-// inssort
 setupint();
 
 
@@ -108,9 +107,272 @@ System.out.println("mergeSort Time for lists of size 10000 Many Duplicates: "+ t
 
 System.out.println();
 
+System.out.println();
+
+
+setupInteger();
+
+
+mergeSortInteger(size10,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer random distribution: "+ totaltime);
+
+mergeSortInteger(size10Asc,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer in ascending order: "+ totaltime);
+
+mergeSortInteger(size10Des,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer in decreasing order: "+ totaltime);
+
+mergeSortInteger(size10Ide,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer idendical keys: "+ totaltime);
+
+mergeSortInteger(size10FewDup,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer Few Duplicates: "+ totaltime);
+
+mergeSortInteger(size10SomeDup,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer Some Duplicates: "+ totaltime);
+
+mergeSortInteger(size10ManyDup,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer Many Duplicates: "+ totaltime);
+
+mergeSortInteger(size10SlightlyDis,10);
+System.out.println("mergeSortInteger Time for lists of size 10 Integer Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+mergeSortInteger(size100,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer random distribution: "+ totaltime);
+
+mergeSortInteger(size100Asc,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer in ascending order: "+ totaltime);
+
+mergeSortInteger(size100Des,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer in decreasing order: "+ totaltime);
+
+mergeSortInteger(size100Ide,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer idendical keys: "+ totaltime);
+
+mergeSortInteger(size100FewDup,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer Few Duplicates: "+ totaltime);
+
+mergeSortInteger(size100SomeDup,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer Some Duplicates: "+ totaltime);
+
+mergeSortInteger(size100ManyDup,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer Many Duplicates: "+ totaltime);
+
+mergeSortInteger(size100SlightlyDis,100);
+System.out.println("mergeSortInteger Time for lists of size 100 Integer Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+mergeSortInteger(size1000,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer random distribution: "+ totaltime);
+
+mergeSortInteger(size100Asc,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer in ascending order: "+ totaltime);
+
+mergeSortInteger(size1000Des,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer in decreasing order: "+ totaltime);
+
+mergeSortInteger(size1000Ide,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer idendical keys: "+ totaltime);
+
+mergeSortInteger(size1000FewDup,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer Few Duplicates "+ totaltime);
+
+mergeSortInteger(size1000SomeDup,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer Some Duplicates "+ totaltime);
+
+mergeSortInteger(size1000ManyDup,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer Many Duplicates "+ totaltime);
+
+mergeSortInteger(size1000SlightlyDis,1000);
+System.out.println("mergeSortInteger Time for lists of size 1000 Integer Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+mergeSortInteger(size10000,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer random distribution: "+ totaltime);
+
+mergeSortInteger(size10000Asc,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer in ascending order: "+ totaltime);
+
+mergeSortInteger(size10000Des,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer in decreasing order: "+ totaltime);
+
+mergeSortInteger(size10000Ide,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer idendical keys: "+ totaltime);
+
+mergeSortInteger(size10000FewDup,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer Few Duplicates: "+ totaltime);
+
+mergeSortInteger(size10000SomeDup,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer Some Duplicates: "+ totaltime);
+
+mergeSortInteger(size10000ManyDup,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer Many Duplicates: "+ totaltime);
+
+mergeSortInteger(size10000SlightlyDis,10000);
+System.out.println("mergeSortInteger Time for lists of size 10000 Integer Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+
+System.out.println();
+
+setupDouble();
+
+mergeSortDouble(dsize10Asc,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double in ascending order: "+ totaltime);
+
+mergeSortDouble(dsize10Des,10);
+System.out.println("mergeSortDouble Time for lists of double 10 in decreasing order: "+ totaltime);
+
+mergeSortDouble(dsize10Ide,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double idendical keys: "+ totaltime);
+
+mergeSortDouble(dsize10FewDup,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double Few Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10SomeDup,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double Some Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10ManyDup,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double Many Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10SlightlyDis,10);
+System.out.println("mergeSortDouble Time for lists of size 10 double Slightly Disordered: "+ totaltime);
+
+
+System.out.println();
+
+mergeSortDouble(dsize100Asc,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double in ascending order: "+ totaltime);
+
+mergeSortDouble(dsize100Des,100);
+System.out.println("mergeSortDouble Time for lists of double 100 in decreasing order: "+ totaltime);
+
+mergeSortDouble(dsize100Ide,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double idendical keys: "+ totaltime);
+
+mergeSortDouble(dsize100FewDup,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double Few Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize100SomeDup,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double Some Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize100ManyDup,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double Many Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize100SlightlyDis,100);
+System.out.println("mergeSortDouble Time for lists of size 100 double Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+mergeSortDouble(dsize1000Asc,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double in ascending order: "+ totaltime);
+
+mergeSortDouble(dsize1000Des,1000);
+System.out.println("mergeSortDouble Time for lists of double 1000 in decreasing order: "+ totaltime);
+
+mergeSortDouble(dsize1000Ide,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double idendical keys: "+ totaltime);
+
+mergeSortDouble(dsize1000FewDup,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double Few Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize1000SomeDup,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double Some Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize1000ManyDup,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double Many Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize1000SlightlyDis,1000);
+System.out.println("mergeSortDouble Time for lists of size 1000 double Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
+mergeSortDouble(dsize10000Asc,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double in ascending order: "+ totaltime);
+
+mergeSortDouble(dsize10000Des,10000);
+System.out.println("mergeSortDouble Time for lists of double 10000 in decreasing order: "+ totaltime);
+
+mergeSortDouble(dsize10000Ide,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double idendical keys: "+ totaltime);
+
+mergeSortDouble(dsize10000FewDup,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double Few Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10000SomeDup,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double Some Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10000ManyDup,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double Many Duplicates: "+ totaltime);
+
+mergeSortDouble(dsize10000SlightlyDis,10000);
+System.out.println("mergeSortDouble Time for lists of size 10000 double Slightly Disordered: "+ totaltime);
+
+System.out.println();
+
 
 
 }
+
+
+public static void mergeSortInteger(Integer a[ ],int length)
+{
+
+Integer [] tmp = new Integer[length];
+
+Integer[] b = new Integer[length];
+int numruns = 20;
+int runs;
+for (runs=0; runs<numruns; runs++) {
+for (int i=0; i<length; i++) b[i] = new Integer(a[i]);
+long startTime=System.nanoTime();
+MergeSortInteger(b, tmp,  0,  length - 1);
+long endTime   = System.nanoTime();
+totaltime += (endTime - startTime);
+}
+totaltime = totaltime/numruns;
+}
+
+private static void MergeSortInteger(Integer a[ ], Integer tmp[ ], int left, int right)
+{
+if( left < right )
+{
+int center = (left + right) / 2;
+MergeSortInteger(a, tmp, left, center);
+MergeSortInteger(a, tmp, center + 1, right);
+Merge(a, tmp, left, center + 1, right);
+}
+}
+
+private static void Merge(Integer a[ ],Integer tmp[ ], int left, int right, int rightEnd )
+{
+int leftEnd = right - 1;
+int k = left;
+int num = rightEnd - left + 1;
+
+while(left <= leftEnd && right <= rightEnd)
+if(a[left] <= a[right])
+tmp[k++] = a[left++];
+else
+tmp[k++] = a[right++];
+
+while(left <= leftEnd)    // Copy rest of first half
+tmp[k++] = a[left++];
+
+while(right <= rightEnd)  // Copy rest of right half
+tmp[k++] = a[right++];
+
+// Copy tmp back
+for(int i = 0; i < num; i++, rightEnd--)
+a[rightEnd] = tmp[rightEnd];
+}
+
+
 	public static void mergeSort(int a[ ],int length)
 	{
 
@@ -122,20 +384,20 @@ System.out.println();
         for (runs=0; runs<numruns; runs++) {
             for (int i=0; i<length; i++) b[i] = a[i];
         long startTime=System.nanoTime();
-		mergeSort(b, tmp,  0,  length - 1);
+		MergeSort(b, tmp,  0,  length - 1);
         long endTime   = System.nanoTime();
          totaltime += (endTime - startTime);
         }
             totaltime = totaltime/numruns;
 	}
 
-	private static void mergeSort(int a[ ], int tmp[ ], int left, int right)
+	private static void MergeSort(int a[ ], int tmp[ ], int left, int right)
 	{
 		if( left < right )
 		{
 			int center = (left + right) / 2;
-			mergeSort(a, tmp, left, center);
-			mergeSort(a, tmp, center + 1, right);
+			MergeSort(a, tmp, left, center);
+			MergeSort(a, tmp, center + 1, right);
 			merge(a, tmp, left, center + 1, right);
 		}
 	}
@@ -163,4 +425,56 @@ System.out.println();
         for(int i = 0; i < num; i++, rightEnd--)
             a[rightEnd] = tmp[rightEnd];
     }
-  
+
+
+public static void mergeSortDouble(double a[ ],int length)
+{
+
+double [] tmp = new double[length];
+
+double[] b = new double[length];
+int numruns = 20;
+int runs;
+for (runs=0; runs<numruns; runs++) {
+for (int i=0; i<length; i++) b[i] = a[i];
+long startTime=System.nanoTime();
+MergeSortDouble(b, tmp,  0,  length - 1);
+long endTime   = System.nanoTime();
+totaltime += (endTime - startTime);
+}
+totaltime = totaltime/numruns;
+}
+
+private static void MergeSortDouble(double a[ ], double tmp[ ], int left, int right)
+{
+if( left < right )
+{
+int center = (left + right) / 2;
+MergeSortDouble(a, tmp, left, center);
+MergeSortDouble(a, tmp, center + 1, right);
+MergeDouble(a, tmp, left, center + 1, right);
+}
+}
+
+private static void MergeDouble(double a[ ],double tmp[ ], int left, int right, int rightEnd )
+{
+int leftEnd = right - 1;
+int k = left;
+int num = rightEnd - left + 1;
+
+while(left <= leftEnd && right <= rightEnd)
+if(a[left] <= a[right])
+tmp[k++] = a[left++];
+else
+tmp[k++] = a[right++];
+
+while(left <= leftEnd)    // Copy rest of first half
+tmp[k++] = a[left++];
+
+while(right <= rightEnd)  // Copy rest of right half
+tmp[k++] = a[right++];
+
+// Copy tmp back
+for(int i = 0; i < num; i++, rightEnd--)
+a[rightEnd] = tmp[rightEnd];
+}
