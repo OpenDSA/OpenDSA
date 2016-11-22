60df342d64b35014117f698a002d5c64175c838f
Souleymane Dia <sould32@vt.edu>
2016-11-21 23:16:57 -0500

    Update SkipListRmvCON.js

--- AV/Development/SkipList/SkipListRmvCON.js
+++ AV/Development/SkipList/SkipListRmvCON.js
@@ -5,9 +5,9 @@ $(document).ready(function() {
 //    var interpret = ODSA.UTILS.loadConfig({av_name: av_name}).interpreter;
   
     var jsav = new JSAV(av_name);
+	isStepShown(false);
     jsav.umsg("We start with an empty SkipList");
     var ll = new SkipList(jsav);
-    jsav.displayInit();
     jsav.umsg("adding one key-value pair (0, D)");
     ll.insert(new KVPair(0, "D"));
     jsav.step();
@@ -25,6 +25,8 @@ $(document).ready(function() {
     jsav.step();
     jsav.umsg("adding another key-value pair (9, B)");
     ll.insert(new KVPair(9, "B"));
+	jsav.displayInit();
+	isStepShown(true);
     jsav.step();
     jsav.umsg("removing key 1");
     ll.removeKey(1);
