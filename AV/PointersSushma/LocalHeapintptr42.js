32338ce6266bf69604855586ae99dce07fd2cf5b
sushma16 <sushma16@vt.edu>
2017-02-18 23:40:39 -0500

    <Finished all visualizations with minor modifications to come>

---
+++ AV/PointersSushma/LocalHeapintptr42.js
@@ -0,0 +1,48 @@
+/*global JSAV, document */
+// Written by Sushma Mandava
+$(document).ready(function() {
+  "use strict";
+
+  var av_name = "LocalHeapintptr42";
+  // Load the config object with interpreter and code created by odsaUtils.js
+  var av;
+  av = new JSAV(av_name);
+  var xPositionLocalRectangles = 340;
+  var yPositionLocal1 = 60;
+  var xPositionHeapRectangles = 500;
+  var length1 = 100;
+  var width = 30;
+
+
+  //first image of the three
+  av.label("Local",  {top: 0, left: xPositionLocalRectangles + 20});
+  av.label("Heap",  {top: 0, left: xPositionLocalRectangles + 200});
+  //av.label("Heap",  {top: 0, left: xPositionLocalRectangles + length1 / 2});
+  av.g.rect(xPositionLocalRectangles, yPositionLocal1, length1, width);
+  av.label("empPtr",  {top: yPositionLocal1 - (width / 2), left: xPositionLocalRectangles - 55});
+  //creating the x's
+  var x1 = av.g.line(xPositionLocalRectangles + 16, yPositionLocal1 + 23, xPositionLocalRectangles + 27, yPositionLocal1 + 8, {"stroke-width": 2});
+  var x2 = av.g.line(xPositionLocalRectangles + 16, yPositionLocal1 + 8, xPositionLocalRectangles + 27, yPositionLocal1 + 23, {"stroke-width": 2});
+
+  var x3 = av.g.line(xPositionLocalRectangles + 41, yPositionLocal1 + 23, xPositionLocalRectangles + 53, yPositionLocal1 + 8, {"stroke-width": 2});
+  var x4 = av.g.line(xPositionLocalRectangles + 41, yPositionLocal1 + 8, xPositionLocalRectangles + 53, yPositionLocal1 + 23, {"stroke-width": 2});
+
+  var x5 = av.g.line(xPositionLocalRectangles + 66, yPositionLocal1 + 23, xPositionLocalRectangles + 77, yPositionLocal1 + 8, {"stroke-width": 2});
+  var x6 = av.g.line(xPositionLocalRectangles + 66, yPositionLocal1 + 8, xPositionLocalRectangles + 77, yPositionLocal1 + 23, {"stroke-width": 2});
+  //gray line in the middle
+  av.g.line((xPositionLocalRectangles + 130), 0, (xPositionLocalRectangles + 130),
+   140, {"stroke-width": 3, stroke: "gray"});
+  av.displayInit();
+  av.step();
+  //second image
+  x1.hide();
+  x2.hide();
+  x3.hide();
+  x4.hide();
+  x5.hide();
+  x6.hide();
+  var heapRectangle = av.g.rect(xPositionHeapRectangles, yPositionLocal1, length1, width);
+  av.step();
+  heapRectangle.hide();
+  av.recorded();
+});
---
+++ AV/PointersSushma/T1-T3CON.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+  "use strict";
+  var av_name = "T1-T3CON";
+  // Load the config object with interpreter and code created by odsaUtils.js
+  var av;
+  av = new JSAV(av_name);
+  var widthBig = 40;
+  var lengthBig = 140;
+  var widthSmall = 27;
+  var lengthSmall = 55;
+  var xPositionBigRectangles = 350;
+  var yPositionRectA = 50;
+  var yPositionRectB = 0;
+  var xPositionSmallRectangles = xPositionBigRectangles + lengthBig - 60;
+  var yPosition55Top = yPositionRectA + (widthBig / 2) + 6;
+  var yPosition55Bottom = yPositionRectB + (widthBig / 2) + 6;
+  //var yPositionP = yPositionRectY + (widthBig / 2) + 10;
+  //var yPositionQ = yPositionP + widthSmall
+
+  //creating everything in the X rectangle
+  //av.umsg(interpret("My message here"));
+  av.g.rect(xPositionBigRectangles, yPositionRectA + (widthBig / 2), lengthBig, widthBig);
+  av.g.rect(xPositionSmallRectangles, yPosition55Top, lengthSmall, widthSmall);
+  av.label("A ()",  {top: yPosition55Top - (widthBig / 2) + 3, left: xPositionBigRectangles - 30});
+  av.label("networth",  {top: yPosition55Top - (widthSmall / 2) - 1, left: xPositionBigRectangles + 14});
+  av.label("55", {top: yPosition55Top - (widthSmall / 2), left: xPositionSmallRectangles + 5});
+  av.umsg("The value of interest netWorth is local to A()");
+  av.displayInit();
+  av.step();
+  //creating the second slide
+  var bigB = av.g.rect(xPositionBigRectangles, yPositionRectB + (widthBig / 2), lengthBig, widthBig);
+  var smallB = av.g.rect(xPositionSmallRectangles, yPosition55Bottom, lengthSmall, widthSmall);
+  var labelB = av.label("B ()",  {top: yPosition55Bottom - (widthBig / 2) + 3, left: xPositionBigRectangles - 30});
+  var labelWorth = av.label("worth",  {top: yPosition55Bottom - (widthSmall / 2), left: xPositionBigRectangles + 30});
+  var label55 = av.label("55", {top: yPosition55Bottom - (widthSmall / 2), left: xPositionSmallRectangles + 5});
+  var x1 = av.g.line(xPositionSmallRectangles + 6, yPosition55Bottom - (widthSmall / 2) + 30, xPositionSmallRectangles + 23, yPosition55Bottom - (widthSmall / 2) + 20, {"stroke-width": 2});
+  var x2 = av.g.line(xPositionSmallRectangles + 6, yPosition55Bottom - (widthSmall / 2) + 20, xPositionSmallRectangles + 23, yPosition55Bottom - (widthSmall / 2) + 30, {"stroke-width": 2});
+  var label56 = av.label("56", {top: yPosition55Bottom - (widthSmall / 2), left: xPositionSmallRectangles + 30});
+  av.umsg("netWorth is copied to B()'s local worth. B() changes its local worth from 55 to 56");
+  av.step();
+
+  bigB.hide();
+  smallB.hide();
+  labelB.hide();
+  labelWorth.hide();
+  label55.hide();
+  x1.hide();
+  x2.hide();
+  label56.hide();
+  av.umsg("B() exits and its local worth is deallocated. The value of interest has not been changed");
+  av.recorded();
+});
