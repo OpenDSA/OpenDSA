546fd3d7f78c46f66939e7ef2571bdc579959497
Cliff Shaffer <shaffer@vt.edu>
2017-03-05 13:41:17 -0500

    Tweak pointers image, remove duplicates

--- AV/PointersSushma/LocalHeapintptr42.js
+++ AV/PointersSushma/LocalHeapintptr42.js
@@ -11,10 +11,11 @@ $(document).ready(function() {
   var xPositionHeapRectangles = 500;
   var length1 = 100;
   var width = 30;
-  //first image of the three
+
+  // Slide 1
   av.label("Local",  {top: 0, left: xPositionLocalRectangles + 20});
   av.label("Heap",  {top: 0, left: xPositionLocalRectangles + 200});
-  av.g.rect(xPositionLocalRectangles, yPositionLocal1, length1, width);
+  av.g.rect(xPositionLocalRectangles, yPositionLocal1, length1, width, {"stroke-width": 2});
   av.label("empPtr",  {top: yPositionLocal1 - (width / 2), left: xPositionLocalRectangles - 55});
   //creating the x's
   var x1 = av.g.line(xPositionLocalRectangles + 16, yPositionLocal1 + 23, xPositionLocalRectangles + 27, yPositionLocal1 + 8, {"stroke-width": 2});
@@ -25,24 +26,29 @@ $(document).ready(function() {
 
   var x5 = av.g.line(xPositionLocalRectangles + 66, yPositionLocal1 + 23, xPositionLocalRectangles + 77, yPositionLocal1 + 8, {"stroke-width": 2});
   var x6 = av.g.line(xPositionLocalRectangles + 66, yPositionLocal1 + 8, xPositionLocalRectangles + 77, yPositionLocal1 + 23, {"stroke-width": 2});
+
   //gray line in the middle
   av.g.line((xPositionLocalRectangles + 130), 0, (xPositionLocalRectangles + 130),
    140, {"stroke-width": 3, stroke: "gray"});
+  var ptline = av.g.line(340 + 80,  yPositionLocal1 + (width / 2), 490,
+            yPositionLocal1 + (width / 2),
+            {"arrow-end": "classic-wide-long", "stroke-width": 2});
+  ptline.hide();
   av.displayInit();
-  av.step();
-  //second image
+
+  // Slide 2
   x1.hide();
   x2.hide();
   x3.hide();
   x4.hide();
   x5.hide();
   x6.hide();
-  var heapRectangle = av.g.rect(xPositionHeapRectangles, yPositionLocal1, length1, width);
-  av.g.path(["M", 340 + 80,  yPositionLocal1 + (width / 2),
-    490, yPositionLocal1 + (width / 2)].join(","), {"arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+  ptline.show();
+  var heapRectangle = av.g.rect(xPositionHeapRectangles, yPositionLocal1, length1, width, {"stroke-width": 2});
   av.step();
+
+  // Slide 3
   heapRectangle.hide();
-  //av.g.path(["M", 340 + 80,  yPositionLocal1 + (width / 2),
-    //490, yPositionLocal1 + (width / 2)].join(","), {stroke: "gray", "arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+  ptline.css({stroke: "gray"});
   av.recorded();
 });
