da0ede2216219c8beb1514d4340d67be9444662a
Jieun Chon <g471000@G47Air.local>
2017-04-10 19:18:46 -0400

    Working on GrowthRates

--- AV/AlgAnal/GrowthRatesCON.js
+++ AV/AlgAnal/GrowthRatesCON.js
@@ -5,19 +5,24 @@ $(document).ready(function() {
   "use strict";
   var av = new JSAV("GrowthRatesCON", {animationMode: "none"});
 
-  var xStart = 100, yStart = 50;
   var width = 600, height = 300;
+
+  var xStart = 100;
+  var xEnd = xStart + width;  //end position of the x on the chart
+
+  var yEnd = 50;
+  var yStart = yEnd + height;  //end position of the y on the chart
   var xyScale = height/width;
   var xMax = 50, yMax = 1500;
 
   var xSteps = width / xMax;  //each pixels per 1 x-unit.
   var ySteps = height / yMax;  //each pixels per 1 y-unit.
 
-  var xEnd = xStart + width;  //end position of the x on the chart
-  var yEnd = yStart + height;  //end position of the y on the chart
+
+
 
   //x-axis 1
-  av.g.line(xStart, yEnd, xEnd, yEnd, {"stroke-width": 2});
+  av.g.line(xStart, yStart, xEnd, yStart, {"stroke-width": 2});
 
   //y-axis 1
   av.g.line(xStart, yStart, xStart, yEnd, {"stroke-width": 2});
@@ -26,13 +31,13 @@ $(document).ready(function() {
   var stepx1 = width/10;
   var x1 = xStart + stepx1;
   for(i = 0; i < 10; i++){
-    av.g.line(x1, yEnd - 5, x1, yEnd, {"stroke-width": 0.8});
+    av.g.line(x1, yStart - 5, x1, yStart, {"stroke-width": 0.8});
     x1 += stepx1;
   }
 
   // draw y-asix lines for graph 1:
   var stepy1 = height/15;
-  var y1 = yEnd - stepy1;
+  var y1 = yStart - stepy1;
   for(i = 0; i < 15; i++){
     av.g.line(xStart, y1, xStart + 5, y1, {"stroke-width": 0.8});
     y1 -= stepy1;
@@ -40,7 +45,7 @@ $(document).ready(function() {
 
   //plot1 x-asix labels
   var labelx1_x = xStart - 10;
-  var labelx1_y = yEnd;
+  var labelx1_y = yStart;
   for (i = 0; i <= 50; i += 10){
     av.label(i,  {left: labelx1_x, top: labelx1_y});
     labelx1_x += (xSteps * 10);
@@ -48,7 +53,7 @@ $(document).ready(function() {
 
   // plot1 y-asix labels
   var labely1_x = xStart - 40;
-  var labely1_y = yEnd - 25;
+  var labely1_y = yStart - 25;
   for (var i = 0; i <= 1400; i += 200){
       av.label(i,  {left: labely1_x, top: labely1_y}).addClass("yLabel");
       labely1_y -= (ySteps * 200);
@@ -56,49 +61,41 @@ $(document).ready(function() {
 
   //horizontal lines
   var xBoxEnd = xStart + (xSteps * 15);
-  var yBoxEnd = yEnd - (ySteps * 500);
-  var dashBoxLine1 = av.g.line(xStart, yBoxEnd, xBoxEnd, yBoxEnd);
-  var dashBoxLine2 = av.g.line(xStart, yEnd, xBoxEnd, yEnd);
+  var yBoxEnd = yStart - (ySteps * 500);
+  av.g.line(xStart, yBoxEnd, xBoxEnd, yBoxEnd).addClass("dashBoxLine");;
+  av.g.line(xStart, yStart, xBoxEnd, yStart).addClass("dashBoxLine");;
   // //vertical lines
-  var dashBoxLine3 = av.g.line(xStart,  yBoxEnd, xStart, yEnd);
-  var dashBoxLine4 = av.g.line(xBoxEnd, yBoxEnd, xBoxEnd, yEnd);
-  dashBoxLine1.addClass("dashBoxLine");
-  dashBoxLine2.addClass("dashBoxLine");
-  dashBoxLine3.addClass("dashBoxLine");
-  dashBoxLine4.addClass("dashBoxLine");
+  av.g.line(xStart,  yBoxEnd, xStart, yStart).addClass("dashBoxLine");;
+  av.g.line(xBoxEnd, yBoxEnd, xBoxEnd, yStart).addClass("dashBoxLine");;
+
 
 //--------------------------------------------------------------------
   /**
     Drawing Dash Line Function:
     drawingDashLine(func(xMax), xFrom, xTo, yFrom, xMax, yMax, height)
   */
-  //1. inputs
-  var DL_func = cal_10n(xMax);
-  var DL_xFrom = xStart;
-  var DL_xTo = xEnd;
-  var DL_yFrom = yEnd;
-  var DL_yMax = yMax;
-  var DL_xMax = xMax
-  var DL_height = height;
+  function drawDash(func, xStart, yStart, xEnd, xMax, yMax, height){
+    var points = [xStart, yStart, xEnd];
+    var y = yStart - func(xMax) * height / yMax;
+    points.push(y);
+    return points;
+  }
 
-  //2. calculate and make a line
-  var DL_result = -1;
-  //10n
-  DL_result = DL_yFrom - ((DL_func/DL_yMax) * DL_height);
-  av.g.line(DL_xFrom, DL_yFrom, DL_xTo, DL_result).addClass("dashLine");
-  av.label("10n",  {left: DL_xTo - 30, top: DL_result - 35});
+    // draw dashline and label for 10n
+    var dash1 = drawDash(_10n, xStart, yStart, xEnd, xMax, yMax, height);
+    av.g.line(dash1).addClass("dashLine");
+    av.label("$10n$",  {left: dash1[2] - 30, top: dash1[3] - 35});
 
-  // //20n
-  DL_func = cal_20n(xMax);
-  DL_result = DL_yFrom - ((DL_func/DL_yMax) * DL_height);
-  av.g.line(DL_xFrom, DL_yFrom, DL_xTo, DL_result).addClass("dashLine");
-  av.label("20n",  {left: DL_xTo - 30, top: DL_result - 35});
+    // draw dashline and label for 10n
+    var dash2 = drawDash(_20n, xStart, yStart, xEnd, xMax, yMax, height);
+    av.g.line(dash2).addClass("dashLine");
+    av.label("$20n$",  {left: dash2[2] - 30, top: dash2[3] - 35});
 
-  function cal_10n(n){
+  function _10n(n){
     return 10 * n;
   }
 
-  function cal_20n(n){
+  function _20n(n){
     return 20 * n;
   }
 
@@ -110,12 +107,12 @@ $(document).ready(function() {
   */
   //1. inputs
   var DC_func = function(n){
-    return 15 * n * (Math.log(n) / Math.LN10);
+    return 5 * n * (Math.log2(n));
   };
   var DC_xFrom = xStart;
   var DC_xTo = xEnd;
-  var DC_yFrom = yEnd;
-  var DC_yTO = yStart;
+  var DC_yFrom = yStart;
+  var DC_yTO = yEnd;
   var DC_xMax = xMax;
   var DC_yMax = yMax;
   var DC_width = width;
@@ -126,85 +123,63 @@ $(document).ready(function() {
   var DC_x;
   var DC_y;
 
-//20nlogn-----------------------------------------------------
-  for(i = 0; i < DC_xMax; i += 0.1){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - ((DC_func(i)/DC_yMax) * DC_height);
-    av.g.circle(DC_x, DC_y, 0.7, {fill: 'black'});
-    if(DC_y < DC_yTO){
+
+
+function drawCurve(func, xStart, yStart, yEnd, xMax, width, label, labelx, labely, increment, isLog){
+  var points = [];
+  var xStep = width / xMax;
+  var start = isLog ? 1 : 0;
+  var x, y;
+  for (var i = start ; i <= xMax; i += increment){
+    x = xStart + (i * xStep);
+    y = yStart - ((func(i)/yMax) * height);
+    points.push([x, y]);
+    if(y < yEnd){
       break;
     }
   }
-  av.label("15nlogn",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
+  av.label(label,  {left: x + labelx, top: y + labely});
+  return points;
+};
 
-//2 * pow(n, 2)-----------------------------------------------------
-  DC_func = function(n){
-   return 2 * Math.pow(n, 2);
-  };
-  for(i = 0; i < DC_xMax; i += 0.05){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    av.g.circle(DC_x, DC_y, 0.7, {fill: 'black'});
-    if(DC_y < DC_yTO){
-      break;
-    }
-  }
-  av.label("2pow(n,2)",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
+var curve1 = drawCurve(_5nlog2n, xStart, yStart, yEnd, xMax, width,
+  "$5n\\log_2{n}$", -40, -40, 1, true);
+av.g.polyline(curve1, {"stroke-width": 3});
+
+var curve2 = drawCurve(_2npow2, xStart, yStart, yEnd, xMax, width,
+  "$2n^2$", -40, -30, 0.1, false);
+av.g.polyline(curve2, {"stroke-width": 3});
+
+var curve3 = drawCurve(_2pown, xStart, yStart, yEnd, xMax, width,
+  "$2^n$", -30, -20, 0.01, false);
+av.g.polyline(curve3, {"stroke-width": 3});
+
+var curve4 = drawCurve(_factorial, xStart, yStart, yEnd, xMax, width,
+  "$n!$", -25, 0, 0.1, false);
+av.g.polyline(curve4, {"stroke-width": 3});
 
 
-// pow(2, n)-----------------------------------------------------
-  DC_func = function(n){
-   return Math.pow(2, n);
-  };
-  for(i = 0; i < DC_xMax; i += 0.005){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    av.g.circle(DC_x, DC_y, 0.7, {fill: 'black'});
-    if(DC_y < DC_yTO){
-      break;
-    }
-  }
-  av.label("2pow(n,2)",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
+function _5nlog2n(n){
+  return 5 * n * (Math.log(n) / Math.log(2));
+};
 
+function _2npow2(n){
+ return 2 * Math.pow(n, 2);
+};
+
+function _2pown(n){
+ return Math.pow(2, n);
+};
 
 // n! using polyline---------------------------------------------
-
-  var g = 7;
-  var C = [0.99999999999980993, 676.5203681218851, -1259.1392167224028,771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
-
-  function gamma(z) {
-      if (z < 0.5) return Math.PI / (Math.sin(Math.PI * z) * gamma(1 - z));
-      else {
-          z -= 1;
-
-          var x = C[0];
-          for (var i = 1; i < g + 2; i++)
-          x += C[i] / (z + i);
-
-          var t = z + g + 0.5;
-          return Math.sqrt(2 * Math.PI) * Math.pow(t, (z + 0.5)) * Math.exp(-t) * x;
-      }
-  };
-
-  DC_func = function(n){
-    return (gamma(n+1));
-  };
-
-  var points = [];
-  for(i = 0; i < DC_xMax; i += 0.01){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    points.push([DC_x, DC_y]);
-    if(DC_y < DC_yTO){
-      break;
-    }
+var f = [1, 1];
+function _factorial(n){
+  if (n==0 || n==1){
+    return 1;
+  } else if (n > 1){
+    return f[n] = f[n - 1] * n;
   }
-  av.g.polyline(points, {"stroke-width": 3});
-  av.label("n!",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
+};
 
   av.displayInit();
 	av.recorded();
--- AV/AlgAnal/GrowthRatesZoomCON.js
+++ AV/AlgAnal/GrowthRatesZoomCON.js
@@ -5,19 +5,19 @@ $(document).ready(function() {
   "use strict";
   var av = new JSAV("GrowthRatesZoomCON", {animationMode: "none"});
 
-  var xStart = 100, yStart = 50;
   var width = 600, height = 300;
+  var xStart = 100;
+  var xEnd = xStart + width;  //end position of the x on the chart
+  var yEnd = 50;
+  var yStart = yEnd + height;  //end position of the y on the chart
   var xyScale = height/width;
   var xMax = 15, yMax = 400;
 
   var xSteps = width / xMax;  //each pixels per 1 x-unit.
   var ySteps = height / yMax;  //each pixels per 1 y-unit.
 
-  var xEnd = xStart + width;  //end position of the x on the chart
-  var yEnd = yStart + height;  //end position of the y on the chart
-
   //x-axis 1
-  av.g.line(xStart, yEnd, xEnd, yEnd, {"stroke-width": 2});
+  av.g.line(xStart, yStart, xEnd, yStart, {"stroke-width": 2});
 
   //y-axis 1
   av.g.line(xStart, yStart, xStart, yEnd, {"stroke-width": 2});
@@ -26,13 +26,13 @@ $(document).ready(function() {
   var stepx = width/3;
   var lines_x = xStart + stepx;
   for(i = 0; i < 3; i++){
-    av.g.line(lines_x, yEnd - 5, lines_x, yEnd, {"stroke-width": 0.8});
+    av.g.line(lines_x, yStart - 5, lines_x, yStart, {"stroke-width": 0.8});
     lines_x += stepx;
   }
 
   // draw y-asix lines for graph 1:
   var stepy = height/4;
-  var lines_y = yEnd - stepy;
+  var lines_y = yStart - stepy;
   for(i = 0; i < 4; i++){
     av.g.line(xStart, lines_y, xStart + 5, lines_y, {"stroke-width": 0.8});
     lines_y -= stepy;
@@ -40,163 +40,106 @@ $(document).ready(function() {
 
   //plot1 x-asix labels
   var labelx1_x = xStart - 10;
-  var labelx1_y = yEnd;
+  var labelx1_y = yStart;
   var xlabelSize = 5;
   for (i = 0; i <= xMax; i += xlabelSize){
     av.label(i,  {left: labelx1_x, top: labelx1_y});
     labelx1_x += (xSteps * xlabelSize);
   }
-//
+
   // plot1 y-asix labels
   var labely1_x = xStart - 40;
-  var labely1_y = yEnd - 25;
+  var labely1_y = yStart - 25;
   var ylabelSize = 100;
   for (var i = 0; i <= yMax; i += ylabelSize){
       av.label(i,  {left: labely1_x, top: labely1_y}).addClass("yLabel");
       labely1_y -= (ySteps * ylabelSize);
   }
+
+
 //--------------------------------------------------------------------
   /**
     Drawing Dash Line Function:
     drawingDashLine(func(xMax), xFrom, xTo, yFrom, xMax, yMax, height)
   */
-  //1. inputs
-  var DL_func = cal_10n(xMax);
-  var DL_xFrom = xStart;
-  var DL_xTo = xEnd;
-  var DL_yFrom = yEnd;
-  var DL_yMax = yMax;
-  var DL_xMax = xMax
-  var DL_height = height;
 
-  //2. calculate and make a line
-  var DL_result = -1;
-  //10n
-  DL_result = DL_yFrom - ((DL_func/DL_yMax) * DL_height);
-  av.g.line(DL_xFrom, DL_yFrom, DL_xTo, DL_result).addClass("dashLine");
-  av.label("10n",  {left: DL_xTo - 30, top: DL_result - 35});
+  function drawDash(func, xStart, yStart, xEnd, xMax, yMax, height){
+    var points = [xStart, yStart, xEnd];
+    var y = yStart - func(xMax) * height / yMax;
+    points.push(y);
+    return points;
+  }
 
-  // //20n
-  DL_func = cal_20n(xMax);
-  DL_result = DL_yFrom - ((DL_func/DL_yMax) * DL_height);
-  av.g.line(DL_xFrom, DL_yFrom, DL_xTo, DL_result).addClass("dashLine");
-  av.label("20n",  {left: DL_xTo - 30, top: DL_result - 35});
+    // draw dashline and label for 10n
+    var p1 = drawDash(_10n, xStart, yStart, xEnd, xMax, yMax, height);
+    av.g.line(p1).addClass("dashLine");
+    av.label("$10n$",  {left: p1[2] - 30, top: p1[3] - 35});
 
-  function cal_10n(n){
+    // draw dashline and label for 10n
+    var p1 = drawDash(_20n, xStart, yStart, xEnd, xMax, yMax, height);
+    av.g.line(p1).addClass("dashLine");
+    av.label("$20n$",  {left: p1[2] - 30, top: p1[3] - 35});
+
+
+  function _10n(n){
     return 10 * n;
   }
 
-  function cal_20n(n){
+  function _20n(n){
     return 20 * n;
   }
 
 //--------------------------------------------------------------------
-  /**
-    Drawing Curve:
-    drawingCurve(func(), xFrom, xTo, yFrom, xMax, yMax, width, height)
-  */
-  //1. inputs
-  var DC_func = function(n){
-    return 15 * n * (Math.log(n) / Math.LN10);
-  };
-  var DC_xFrom = xStart;
-  var DC_xTo = xEnd;
-  var DC_yFrom = yEnd;
-  var DC_yTO = yStart;
-  var DC_xMax = xMax;
-  var DC_yMax = yMax;
-  var DC_width = width;
-  var DC_height = height;
+//   /**
+//     Drawing Curve:
+//     drawingCurve(func(), xFrom, xTo, yFrom, xMax, yMax, width, height)
+//   */
 
-  //2. calculate and make a line
-  var DC_xStep = DC_width / DC_xMax;
-  var DC_x;
-  var DC_y;
-  var DC_incSize = 0.01;
-
-//20nlogn-----------------------------------------------------
-  for(i = 1; i < DC_xMax; i += DC_incSize){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - ((DC_func(i)/DC_yMax) * DC_height);
-    av.g.circle(DC_x, DC_y, 0.5, {fill: 'black'});
-    if(DC_y < DC_yTO){
-      break;
-    }
-  }
-  av.label("15nlogn",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
-
-//2 * pow(n, 2)-----------------------------------------------------
-
-  DC_incSize = 0.001;
-  DC_func = function(n){
-   return 2 * Math.pow(n, 2);
-  };
-  for(i = 0; i < DC_xMax; i += DC_incSize){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    av.g.circle(DC_x, DC_y, 0.7, {fill: 'black'});
-    if(DC_y < DC_yTO){
-      break;
-    }
-  }
-  av.label("2pow(n,2)",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
-
-
-// pow(2, n)-----------------------------------------------------
-  DC_incSize = 0.0005;
-  DC_func = function(n){
-   return Math.pow(2, n);
-  };
-  for(i = 0; i < DC_xMax; i += 0.005){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    av.g.circle(DC_x, DC_y, 1, {fill: 'black'});
-    if(DC_y < DC_yTO){
-      break;
-    }
-  }
-  av.label("2pow(n,2)",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
-
-
-// n! using polyline---------------------------------------------
-
-  var g = 7;
-  var C = [0.99999999999980993, 676.5203681218851, -1259.1392167224028,771.32342877765313, -176.61502916214059, 12.507343278686905, -0.13857109526572012, 9.9843695780195716e-6, 1.5056327351493116e-7];
-
-  function gamma(z) {
-      if (z < 0.5) return Math.PI / (Math.sin(Math.PI * z) * gamma(1 - z));
-      else {
-          z -= 1;
-
-          var x = C[0];
-          for (var i = 1; i < g + 2; i++)
-          x += C[i] / (z + i);
-
-          var t = z + g + 0.5;
-          return Math.sqrt(2 * Math.PI) * Math.pow(t, (z + 0.5)) * Math.exp(-t) * x;
-      }
-  };
-
-  DC_func = function(n){
-    return (gamma(n+1));
-  };
-
+function drawCurve(func, xStart, yStart, yEnd, xMax, width, label, labelx, labely, increment, isLog){
   var points = [];
-  DC_incSize = 0.001;
-  for(i = 0; i < DC_xMax; i += DC_incSize){
-    DC_x = DC_xFrom + (i * DC_xStep);
-    DC_y = DC_yFrom - (DC_func(i)/DC_yMax * DC_height);
-    points.push([DC_x, DC_y]);
-    if(DC_y < DC_yTO){
+  var xStep = width / xMax;
+  var start = isLog ? 1 : 0;
+  var x, y;
+  for (var i = start ; i <= xMax; i += increment){
+    x = xStart + (i * xStep);
+    y = yStart - ((func(i)/yMax) * height);
+    if(y < yEnd){
       break;
     }
+    points.push([x, y]);
   }
-  av.g.polyline(points, {"stroke-width": 3});
-  av.label("n!",  {left: DC_x - 30, top: DC_y - 35});
-//-------------------------------------------------------------
+  av.label(label,  {left: x + labelx, top: y + labely});
+  return points;
+};
+
+var c1 = drawCurve(_5nlog2n, xStart, yStart, yEnd, xMax, width,
+  "$5n\\log_2{n}$", -40, -40, 1, true);
+av.g.polyline(c1, {"stroke-width": 3});
+
+var c2 = drawCurve(_2npow2, xStart, yStart, yEnd, xMax, width,
+  "$2n^2$", -40, -30, 0.1, false);
+av.g.polyline(c2, {"stroke-width": 3});
+
+var c3 = drawCurve(_2pown, xStart, yStart, yEnd, xMax, width,
+  "$2^n$", -30, -20, 0.01, false);
+av.g.polyline(c3, {"stroke-width": 3});
+
+// var c4 = drawCurve(factorial, xStart, yStart, yEnd, xMax, width,
+//   "$n!$", -30, -20, 1, false);
+// av.g.polyline(c3, {"stroke-width": 3});
+
+
+function _5nlog2n(n){
+  return 5 * n * Math.log(n);
+};
+
+function _2npow2(n){
+ return 2 * Math.pow(n, 2);
+};
+
+function _2pown(n){
+ return Math.pow(2, n);
+};
 
   av.displayInit();
 	av.recorded();
