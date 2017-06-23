c35cb753b1da8bb6bf667c6545d036e5ca978da4
Yanbo Fang <yf59@duke.edu>
2017-06-23 16:16:12 -0400

    added step and complete for CYK Parser

--- AV/FLA/summer2017/ui/CYKParser.js
+++ AV/FLA/summer2017/ui/CYKParser.js
@@ -6,8 +6,10 @@ $(document).ready(function () {
       fi,
       oldrow,
       oldcol,
-      jsavParseTable;
-
+      table, //table with the answer
+      userTable, //has the same value as jsavParseTable, but just for backend use
+      jsavParseTable, //table that the user could see and input
+      currentStep = 0; //for the step function
 
   var lambda = String.fromCharCode(955),
       epsilon = String.fromCharCode(949),
@@ -34,11 +36,9 @@ $(document).ready(function () {
       alert('No grammar.');
       return;
     }
-    jsav.umsg('Parsing');
+    jsav.umsg('Please input your answer');
 
     // startParse();
-
-    console.log('productions: ' + productions + ' input string: ' + inputString);
 
     var inputLength = inputString.length;
     var nonterminals = getNonTerminals();
@@ -53,18 +53,22 @@ $(document).ready(function () {
     //   }
     // }
 
-    var table = new Array(inputLength);
+    //table will be the one with the correct answer, while userTable will be used for user to input, 
+    //TODO: need to refactor this part
+    table = new Array(inputLength);
+    userTable = new Array(inputLength);
     for (var s = 0; s < inputLength; s++) {
       table[s] = new Array(inputLength-s);
+      userTable[s] = new Array(inputLength-s);
       for(var i = 0; i < inputLength-s; i++){
         table[s][i] = new Array();
-        }
+        userTable[s][i] = new Array();
+      }
     }
 
     var unitProductions = _.filter(productions, function(x) {
       return x[2].length === 1 && variables.indexOf(x[2]) === -1;
     });
-
     //the first row, unit productions
     for (var s = 0; s < inputLength; s++) {
       unitProductions.forEach(function(production) {
@@ -73,12 +77,9 @@ $(document).ready(function () {
         }
       });
     }
-
     var otherProductions = _.filter(productions, function(x) {
       return x[2].length === 2;
     });
-
-
     for (var l = 2; l <= inputString.length; l++) { //l is length of the span
       for (var s = 0; s <= inputString.length - l; s++) {  //s is the start of the span
         e = s + l - 1; //e is the end of the span
@@ -110,16 +111,34 @@ $(document).ready(function () {
     //   return t;
     // };
 
-    //initialize the parsetable
-    var initCYKParseTable = function (table) {
+    jsavParseTable = initCYKParseTable(userTable);
+
+    // var cykParseTable = new jsav.ds.matrix(table);
+
+    // layoutTable(cykParseTable);
+
+    checkAcceptance(table);
+
+  };
+
+  var checkAcceptance = function (table) {
+    if(table[inputString.length - 1][0].length > 0) {
+      alert('Input accepted');
+    }else{
+      alert('Input not accpeted');
+    }
+  }
+
+  //initialize the parsetable
+  var initCYKParseTable = function (table) {
       var t = jsav.ds.matrix(table);
       // layoutTable(t);
       t = layoutCYKTable(t);
       t.on('click', cykParseTableHandler);
       return t;
-    };
+  };
 
-    var cykParseTableHandler = function(row, col) {
+  var cykParseTableHandler = function(row, col) {
       console.log(row + '  ' + col);
 
       if (fi) {
@@ -132,8 +151,19 @@ $(document).ready(function () {
         }
         fi.remove();
         jsavParseTable.value(oldrow, oldcol, input);
-        // arr[row][col] = input;
+        userTable[oldrow][oldcol] = input;
         jsavParseTable = layoutCYKTable(jsavParseTable);
+
+        //If user input wrong answer, let them know
+        if(!checkAnswer(oldrow, oldcol)){
+          jsav.umsg('Your answer for index [' + oldrow + '][' + oldcol + '] is wrong or incomplete');
+        } else {
+          jsav.umsg('Please continue to input your answer');
+          if(jsavParseTable.value(inputString.length - 1, 0).length > 0) {
+            alert('Congratulations!');
+            jsav.umsg('Accepted!');
+          }
+        }
       }
 
       var offset = jsavParseTable._arrays[row]._indices[col].element.offset();
@@ -152,32 +182,28 @@ $(document).ready(function () {
       oldrow = row;
       oldcol = col;
 
-    };
+  };
 
-
-    jsavParseTable = initCYKParseTable(table);
-
-    // var cykParseTable = new jsav.ds.matrix(table);
-
-    // layoutTable(cykParseTable);
-
-    if(table[inputString.length - 1][0].length > 0) {
-      alert('Input accepted');
-    }else{
-      alert('Input not accpeted');
+  //checks the answer at a specific index
+  function checkAnswer(row, col) {
+    for(var i = 0; i < table[row][col].length; i++){
+      if(!jsavParseTable.value(row, col).includes(table[row][col][i])){
+        return false;
+      }
     }
+    return true;
+  };
 
-  }
 
   //a special layout function for CYK parse table, shows only half of the matrix.
   function layoutCYKTable(table) {
     table.layout();
     for (var i = 0; i < table._arrays.length; i++) {
       var arry = table._arrays[i].element;
-      arry.css({"width": "161px"}); //the width is currently hardcoded to ensure left align, need to change in the future
+      arry.css({"width": "230px"}); //the width is currently hardcoded to ensure left align, need to change in the future
     }
     return table;
-  }
+  };
 
   //return a set of unique terminals on the right side
   function getTerminals() {
@@ -220,15 +246,41 @@ $(document).ready(function () {
     console.log(fi);
     fi.remove();
     jsavParseTable.value(oldrow, oldcol, input);
-    // arr[row][col] = input;
+    userTable[oldrow][oldcol] = input;
     jsavParseTable = layoutCYKTable(jsavParseTable);
+
+    //If user input wrong answer, let them know
+    if(!checkAnswer(oldrow, oldcol)){
+          jsav.umsg('Your answer for index [' + oldrow + '][' + oldcol + '] is wrong or incomplete');
+    } else {
+          jsav.umsg('Please continue to input your answer');
+          if(jsavParseTable.value(inputString.length - 1, 0).length > 0) {
+            alert('Congratulations!');
+            jsav.umsg('Accepted!');
+          }
+    }
   }
+
+  function completeCYKTable(){
+    initCYKParseTable(table);
+  };
+
+  function step(){
+    if(currentStep < table.length){
+      console.log(table);
+      userTable[currentStep] = table[currentStep];
+      initCYKParseTable(userTable);
+      currentStep++;
+    }
+  };
 
 
   $('#inputbutton').click(enterInput);
+  $('#completebutton').click(completeCYKTable);
+  $('#stepbutton').click(step);
+  $('#animatebutton').click(animate);
 
   $(document).click(defocus);
-
 
 
 
