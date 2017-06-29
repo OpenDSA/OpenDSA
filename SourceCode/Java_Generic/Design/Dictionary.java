b30840e60a5706a89a239eec050ba55524c56d0a
Steven Huss-Lederman <huss@beloit.edu>
2017-06-29 16:18:37 -0400

    remove copyright

--- SourceCode/Java_Generic/Design/Dictionary.java
+++ SourceCode/Java_Generic/Design/Dictionary.java
@@ -1,9 +1,3 @@
-/** Source code example for "A Practical Introduction to Data
-    Structures and Algorithm Analysis, 3rd Edition (Java)" 
-    by Clifford A. Shaffer
-    Copyright 2008-2011 by Clifford A. Shaffer
-*/
-
 /* *** ODSATag: DictionaryADT *** */
 /** The Dictionary abstract class. */
 public interface Dictionary<K, E> {
