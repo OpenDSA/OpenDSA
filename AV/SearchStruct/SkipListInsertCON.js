4f0b9596ebd3df4aa938294d79caec86f5d95001
Steven Huss-Lederman <huss@beloit.edu>
2017-06-29 10:49:31 -0400

    insert & remove examples changed
    The examples for skip lists were changed for several reasons.
    First, they were made a little shorter. Second, the remove not
    reverses the inserts so you see the same lists in reverse.
    Third, the adjustHead is not done on the first insert.
    Fourth, more cases are shown for nodes before/after being
    larger or smaller.

--- AV/SearchStruct/SkipListInsertCON.js
+++ AV/SearchStruct/SkipListInsertCON.js
@@ -16,34 +16,28 @@ var jsav = new JSAV(av_name, {left: 250});
   jsav.step();
 
   // Slide 3
-  jsav.umsg("Now we will insert initial value 10, assuming randomLevel returns 1.");
+  jsav.umsg("Now we will insert initial value 10, assuming randomLevel returns 0.");
   jsav.step();
 
-  ll.insert(new KVPair(10, "A"), 1);
+  ll.insert(new KVPair(10, "A"), 0);
   jsav.step();
 
-  jsav.umsg("Insert value 20, assuming randomLevel returns 0.");
+  jsav.umsg("Insert value 30, assuming randomLevel returns 1.");
   jsav.step();
 
-  ll.insert(new KVPair(20, "B"), 0);
+  ll.insert(new KVPair(30, "B"), 1);
   jsav.step();
 
-  jsav.umsg("Insert value 5, assuming randomLevel returns 0.");
+  jsav.umsg("Insert value 20, assuming randomLevel returns 2.");
   jsav.step();
 
-  ll.insert(new KVPair(5, "C"), 0);
+  ll.insert(new KVPair(20, "C"), 2);
   jsav.step();
 
-  jsav.umsg("Insert value 2, assuming randomLevel returns 3.");
+  jsav.umsg("Insert value 25, assuming randomLevel returns 0.");
   jsav.step();
 
-  ll.insert(new KVPair(2, "D"), 3);
-  jsav.step();
-
-  jsav.umsg("Finally, we will insert value 30, assuming randomLevel returns 2.");
-  jsav.step();
-
-  ll.insert(new KVPair(30, "D"), 2);
+  ll.insert(new KVPair(25, "D"), 0);
   jsav.step();
 
   jsav.recorded();
