4259ad607e23363e89b4cc734a73050db9335f48
Jieun Chon <g471000@vt.edu>
2017-10-10 19:42:51 -0400

    fixed for loop start index

--- SourceCode/Java_Generic/Searching/Sequential.java
+++ SourceCode/Java_Generic/Searching/Sequential.java
@@ -9,7 +9,7 @@ static boolean SUCCESS = true;
 // Return the position of an element in array A with value K.
 // If K is not in A, return A.length.
 static int sequential(int[] A, int K) {
-  for (int i=1; i<A.length; i++) // For each element
+  for (int i=0; i<A.length; i++) // For each element
     if (A[i] == K)               // if we found it
        return i;                 //   return this position
   return A.length;               // Otherwise, return the array length
