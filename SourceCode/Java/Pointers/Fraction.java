73a65e2cdd2cf89cccda16d7210db0313fb9baf1
Cliff Shaffer <shaffer@vt.edu>
2017-10-14 18:12:39 -0400

    Fixed some problems that Jieun pointed out

--- SourceCode/Java/Pointers/Fraction.java
+++ SourceCode/Java/Pointers/Fraction.java
@@ -1,36 +1,29 @@
-/**
- * Fraction class
- */
-class Fraction
-{
-    int numerator, denominator;
+// Fraction class
+class Fraction 
+  int numerator, denominator;
 
-    /**
-     * Fraction constructor
-     * @param num numerator value
-     * @param den denominator value
-     */
-    public Fraction(int num, int den)
-    {
-        numerator = num;
-        denominator = den;
-    }
+  // Fraction constructor
+  public Fraction(int num, int den) {
+    numerator = num;
+    denominator = den;
+  }
 }
 
-/**
- * tester class
- */
+// Test class
 class TestHeapArray {
-    static void main() {
-        Fraction[] fracts;
-        int i;
-        // allocate the array
-        fracts = new Fraction[100];
-        // use it like an array -- in this case set them all to 22/7
-        for (i = 0; i < 99; i++) {
-            fracts[i] = new Fraction(22, 7);
-        }
-        // Deallocate the whole array
-        fracts = null;
+  static void main() {
+    Fraction[] fracts;
+    int i;
+    int length = 100;
+    
+    // allocate the array
+    fracts = new Fraction[length];
+    // use it like an array -- in this case set them all to 22/7
+    for (i = 0; i < length; i++) {
+      fracts[i] = new Fraction(22, 7);
     }
+    // Remove access to the array, making it garbage.
+    // If the Fraction objects that were created are also garbage now.
+    fracts = null;
+  }
 }
