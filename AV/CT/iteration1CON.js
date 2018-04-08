3d1a3f2de6d31a77c5cb71792599886e9341f766
Jieun Chon <g471000@vt.edu>
2018-04-08 01:08:33 -0400

    adding first slide's style

--- AV/CT/iteration1CON.js
+++ AV/CT/iteration1CON.js
@@ -5,8 +5,7 @@
 
 var it1_midblue1,
     it1_midblue2,
-    it1_midblue3,
-    it1_arr;
+    it1_midblue3;
 
 $(document).ready(function() {
   "use strict";
@@ -58,60 +57,82 @@ $(document).ready(function() {
       // BlueStepAnim END -----------------------------------------------
 
   var av = new JSAV(av_name);
-  var leftMargin = 450,
-      rect_left = 300,
-      rect0_top = 0,
-      rect_top = 40,
-      topMargin = rect_top + 20;
+  var left = 300,
+      top = 20,
+      topMargin = top + 20;
 
   var nodegap = 40;
 
-
-  // blue boxes, floor 1, last floor
-  var topblue = av.g.rect(rect_left, rect0_top, 280, 35, 10).addClass("bluebox");
-  var botblue = av.g.rect(rect_left, rect0_top + 295, 280, 35, 10).addClass("bluebox");
+  // top blue
+  var top1 = top;
+  var box1 = av.g.rect(left, top, 280, 35, 10).addClass("bluebox");
 
   // floor 2
-  av.g.rect(rect_left, rect_top, 250, 35, 10).addClass("purplebox")
-  av.g.rect(rect_left, rect_top + 20, 50, 15).addClass("purplebox"); // for no-roung on the corner
+  var top2 = top + 40;
+  av.g.rect(left, top2, 250, 35, 10).addClass("purplebox")
+  av.g.rect(left, top2 + 20, 50, 15).addClass("purplebox"); // for no-roung on the corner
 
   //floor 3
-  av.g.rect(rect_left, rect_top + 25, 30, 60).addClass("purplebox").css({opacity: 0.7});
-  av.g.rect(rect_left + 73, rect_top + 25, 30, 60).addClass("purplebox").css({opacity: 0.9});
+  var top3 = top2 + 25;
+  av.g.rect(left, top3, 30, 60).addClass("purplebox").css({opacity: 0.7});
+  av.g.rect(left + 73, top3, 30, 60).addClass("purplebox").css({opacity: 0.9});
+
   //create array contains 5 values.
-  it1_arr = av.ds.array(arrValues, {indexed: false, left: leftMargin, top: topMargin, position: "absolute"});
+  var top4 = top3 - 5;
+  var arrayLeft = left + 160;
+  var array = av.ds.array(arrValues, {indexed: false, left: arrayLeft, top: top4, position: "absolute"});
 
-  //floor 4, long purple
-  av.g.rect(rect_left, rect_top + 76, 300, 30, 10).addClass("purplebox");
-  av.g.rect(rect_left, rect_top + 76, 50, 15).addClass("purplebox"); // for no-roung on the corner
+  //floor 4, long purple under the green array
+  var top5 = top3 + 50;
+  av.g.rect(left, top5, 300, 30, 10).addClass("purplebox");
+  av.g.rect(left, top5, 50, 15).addClass("purplebox"); // for no-roung on the corner
 
-  //floor 5, left big purple box and 3 blue boxes
-  av.g.rect(rect_left, rect_top + 80, 110, 170, 10).addClass("purplebox");
+  // //floor 5, left big purple box and 3 blue boxes
+  // var top5 = top4 + 4;
+  // av.g.rect(rect_left, top5, 110, 170, 10).addClass("purplebox");
+  //
+  // var top6 = top + 110;
+  // var top6gap = 30;
+  // it1_midblue1 = av.g.rect(rect_left + 130, top6, 180, 25, 10).addClass("bluebox");
+  //
+  // var top7 = top6 + top6gap;
+  // it1_midblue2 = av.g.rect(rect_left + 130, top6 + top6gap, 180, 25, 10).addClass("bluebox");
+  //
+  // var top8 = top7 + top7gap;
+  // it1_midblue3 = av.g.rect(rect_left + 130, top6 + top6gap + 170, 180, 25, 10).addClass("bluebox");
+  //
+  // //floor 5, right big putple box below blue boxes
+  // var top9 = top5 + 120;
+  // av.g.rect(rect_left + 90, top9, 230, 50, 10).addClass("purplebox");
+  //
+  // // bot blue
+  // var top10 = top + 345;
+  // var botblue = av.g.rect(rect_left, top10rect_top, 280, 35, 10).addClass("bluebox");
 
-  //floor 5, right big putple box below blue boxes
-  av.g.rect(rect_left + 90, rect_top + 200, 230, 50, 10).addClass("purplebox");
-
-  it1_midblue1 = av.g.rect(rect_left + 130, rect_top + 110, 180, 25, 10).addClass("bluebox");
-  it1_midblue2 = av.g.rect(rect_left + 130, rect_top + 140, 180, 25, 10).addClass("bluebox");
-  it1_midblue3 = av.g.rect(rect_left + 130, rect_top + 170, 180, 25, 10).addClass("bluebox");
-
-// create labels
-  var label1 = av.label("for each item", {left: rect_left + 10, top: rect_top - 30});
-  label1.addClass("loopLabels");
-
-  var label2 = av.label("price", {left: rect_left + 22, top: rect_top + 45});
-  label2.addClass("loopLabels");
-
-  var label3 = av.label("do", {left: rect_left + 35, top: rect_top + 100});
-  label3.addClass("loopLabels");
+// ------------------- Labels -------------------------
+  // var labeltop =
+  // av.label("for each item", {left: rect_left + 10, top: rect_top - 27});
+  // label1.addClass("loopLabels");
+  //
+  // av.label("price", {left: rect_left + 22, top: rect_top + 45});
+  // label2.addClass("loopLabels");
+  //
+  // av.label("do", {left: rect_left + 35, top: rect_top + 100});
+  // label3.addClass("loopLabels");
 
 
-// Create Iteration Property box and text
-  var iplabel = av.label("Iteration<br>Variable", {left: rect_left - 120, top: rect_top + 120}).css({'font-weight': '600'});
+// ------------------- Create Iteration Property box (ipb) and text -------------------------
+
+  var ipb_left = left - 180;
+  var ipb_top = 120;
+  var iplabel = av.label("Iteration<br>Variable", {left: ipb_left, top: ipb_top}).css({'font-weight': '600'});
+  // var iplabel = av.label("Iteration<br>Variable", {left: rect_left - 120, top: rect_top + 120}).css({'font-weight': '600'});
   iplabel.addClass("hiding");
-  var iprec = av.g.rect(rect_left - 140, rect_top + 125, 100, 70).addClass("fourRoundBox");
+  var iprec = av.g.rect(ipb_left - 20, ipb_top + 5, 100, 70).addClass("fourRoundBox");
+  // var iprec = av.g.rect(rect_left - 140, rect_top + 125, 100, 70).addClass("fourRoundBox");
   iprec.addClass("hiding");
-  var ipline = av.g.line(rect_left - 40, rect_top + 150, rect_left + 5, rect_top + 110, {'arrow-end': 'classic-wide-long', 'stroke-width': 3});
+  var ipline = av.g.line(ipb_left + 80, ipb_top + 30, ipb_left + 125, ipb_top - 10, {'arrow-end': 'classic-wide-long', 'stroke-width': 3});
+  // var ipline = av.g.line(rect_left - 40, rect_top + 150, rect_left + 5, rect_top + 110, {'arrow-end': 'classic-wide-long', 'stroke-width': 3});
   ipline.addClass("hiding");
 
 // ------------------- slide show start -------------------------
@@ -125,65 +146,65 @@ $(document).ready(function() {
   iplabel.removeClass("hiding");
   iprec.removeClass("hiding");
   ipline.removeClass("hiding");
-  var nextleft = leftMargin - 120;
+  var nextleft = arrayLeft - 120;
   av.step();
-
-  // Slide 3
-  av.umsg(interpret("sc3"));
-  av.bluehigh(topblue, 150);
-  av.step();
-
-  // Slide 4
-  av.umsg(interpret("sc4"));
-  it1_arr.css({left: nextleft});
-  nextleft -= nodegap;
-  av.step();
-
-  // Slide 5
-  av.umsg(interpret("sc5"));
-  av.blueStepAnim(0, 200);
-  av.step();
-
-  // Slide 6
-  av.umsg(interpret("sc6"));
-  it1_arr.css({left: nextleft});
-  av.blueStepAnim(400, 200);
-  nextleft -= nodegap;
-  av.step();
-
-
-  // Slide 7
-  av.umsg(interpret("sc7"));
-  it1_arr.css({left: nextleft});
-  nextleft -= nodegap;
-  av.blueStepAnim(400, 200);
-  av.step();
-
-
-  // Slide 8
-  av.umsg(interpret("sc8"));
-  it1_arr.css({left: nextleft});
-  nextleft -= nodegap;
-  av.blueStepAnim(400, 200);
-  av.step();
-
-
-  // Slide 9
-  av.umsg(interpret("sc9"));
-  it1_arr.css({left: nextleft});
-  nextleft -= (nodegap + 50);
-  av.blueStepAnim(400, 200);
-  av.step();
-
-
-  // Slide 10
-  av.umsg(interpret("sc10"));
-  it1_arr.css({left: nextleft});
-  nextleft -= (nodegap + 100);
-  av.step();
+  //
+  // // Slide 3
+  // av.umsg(interpret("sc3"));
+  // av.bluehigh(box1, 150);
+  // av.step();
+  //
+  // // Slide 4
+  // av.umsg(interpret("sc4"));
+  // array.css({left: nextleft});
+  // nextleft -= nodegap;
+  // av.step();
+  //
+  // // Slide 5
+  // av.umsg(interpret("sc5"));
+  // av.blueStepAnim(0, 200);
+  // av.step();
+  //
+  // // Slide 6
+  // av.umsg(interpret("sc6"));
+  // array.css({left: nextleft});
+  // av.blueStepAnim(400, 200);
+  // nextleft -= nodegap;
+  // av.step();
+  //
+  //
+  // // Slide 7
+  // av.umsg(interpret("sc7"));
+  // array.css({left: nextleft});
+  // nextleft -= nodegap;
+  // av.blueStepAnim(400, 200);
+  // av.step();
+  //
+  //
+  // // Slide 8
+  // av.umsg(interpret("sc8"));
+  // array.css({left: nextleft});
+  // nextleft -= nodegap;
+  // av.blueStepAnim(400, 200);
+  // av.step();
+  //
+  //
+  // // Slide 9
+  // av.umsg(interpret("sc9"));
+  // array.css({left: nextleft});
+  // nextleft -= (nodegap + 50);
+  // av.blueStepAnim(400, 200);
+  // av.step();
+  //
+  //
+  // // Slide 10
+  // av.umsg(interpret("sc10"));
+  // array.css({left: nextleft});
+  // nextleft -= (nodegap + 100);
+  // av.step();
 
   // Slide 15
   av.umsg(interpret("sc11"));
-  av.bluehigh(botblue, 150);
+  // av.bluehigh(botblue, 150);
   av.recorded();
 });
