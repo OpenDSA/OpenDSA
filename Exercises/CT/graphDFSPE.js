53322589547383e45a9736466c2396f128cfe44b
Jieun Chon <g471000@vt.edu>
2018-05-09 16:29:15 -0400

    working on JSAV exercise. added init()

--- Exercises/CT/graphDFSPE.js
+++ Exercises/CT/graphDFSPE.js
@@ -68,28 +68,46 @@
     nodegap = 40;
 
     //floor 4, long purple
-jsav.g.rect(rect_left, purple_top + 76, 300, 30, 10).addClass("purplebox");
+    jsav.g.rect(rect_left, purple_top + 76, 300, 30, 10).addClass("purplebox");
 
-//floor 5, left big purple box and 3 blue boxes
-jsav.g.rect(rect_left, purple_top + 80, 110, 170, 10).addClass("purplebox");
-jsav.g.rect(rect_left, purple_top + 76, 50, 15).addClass("purplebox"); // for no-roung on the corner
+    //floor 5, left big purple box and 3 blue boxes
+    jsav.g.rect(rect_left, purple_top + 80, 110, 170, 10).addClass("purplebox");
+    jsav.g.rect(rect_left, purple_top + 76, 50, 15).addClass("purplebox"); // for no-roung on the corner
 
-//blue boxes and the the sets of it for the iterations later
-var midblue1 = jsav.g.rect(rect_left + 130, purple_top + 120, 230, 66, 10).addClass("bluebox");
+    //blue boxes and the the sets of it for the iterations later
+    var midblue1 = jsav.g.rect(rect_left + 130, purple_top + 120, 230, 66, 10).addClass("bluebox");
 
-// last purple box.
-jsav.g.rect(rect_left + 90, purple_top + 200, 240, 50, 10).addClass("purplebox");
+    // last purple box.
+    jsav.g.rect(rect_left + 90, purple_top + 200, 240, 50, 10).addClass("purplebox");
 
 
-// ---------------clickerbale boxes-----------------------
-box1 = jsav.g.rect(rect_left, top, 280, 35, 10).addClass("box");
-// box1.click(clickHandler1);
-box2 = jsav.g.rect(rect_left, purple_top, 350, 106, 10).addClass("box");
-// box2.click(clickHandler2);
-box3 = jsav.g.rect(rect_left, purple_top + 105, 360, 145, 10).addClass("box");
-// box3.click(clickHandler3);
-box4 = jsav.g.rect(rect_left, top + 295, 280, 35, 10).addClass("box");
-// box4.click(clickHandler4);
+    // ---------------clickerbale boxes-----------------------
+    box1 = jsav.g.rect(rect_left, top, 280, 35, 10).addClass("box");
+    // box1.click(clickHandler1);
+    box2 = jsav.g.rect(rect_left, purple_top, 350, 106, 10).addClass("box");
+    // box2.click(clickHandler2);
+    box3 = jsav.g.rect(rect_left, purple_top + 105, 360, 145, 10).addClass("box");
+    // box3.click(clickHandler3);
+    box4 = jsav.g.rect(rect_left, top + 295, 280, 35, 10).addClass("box");
+    // box4.click(clickHandler4);
+
+    // ---------------loop -labels-----------------------
+    var label1 = jsav.label("for each item", {
+      left: rect_left + 10,
+      top: purple_top - 30
+    }).addClass("loopLabels");
+    label1.onmouseover = function() {
+      box1.addClass("hover")
+    };
+
+    jsav.label("price", {
+      left: rect_left + 20,
+      top: purple_top + 48
+    }).addClass("loopLabels");
+    jsav.label("do", {
+      left: rect_left + 35,
+      top: purple_top + 110
+    }).addClass("loopLabels");
 
     return jsavArray;
   }
@@ -153,10 +171,9 @@ box4 = jsav.g.rect(rect_left, top + 295, 280, 35, 10).addClass("box");
 
   exercise = jsav.exercise(model, init, {
     compare: {
-      class: "marked"
+      class: "jsavhighlight"
     },
     controls: $('.jsavexercisecontrols'),
-    fix: fixState
   });
   exercise.reset();
 
