3b3dc7fa20e7b4369a371cbcf06a710a63698ade
Cliff <shaffer@vt.edu>
2018-05-29 17:29:28 -0400

    Delint ptrSwapCON, update eslintrc

--- AV/Liling/ptrSwapCON.js
+++ AV/Liling/ptrSwapCON.js
@@ -7,20 +7,20 @@ $(document).ready(function() {
 
   // Boxes and arrows
   var left = 50;
-  for(left; left < 150; left += 30){
+  for (left; left < 150; left += 30) {
     av.g.rect(250, left, 30, 30);
     av.g.rect(470, left, 30, 30);
     av.g.rect(315, left + 5, 80, 20);
     av.g.rect(535, left + 5, 80, 20);
     av.g.line(265, left + 15, 315, left + 15,
-              {"stroke-width": 2, "arrow-end":"classic-wide-long"});
+              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
   }
 
   // Arrows on the right
-  av.g.line(485, 65, 535, 95, {"stroke-width": 2, "arrow-end":"classic-wide-long"});
-  av.g.line(485, 95, 535, 65, {"stroke-width": 2, "arrow-end":"classic-wide-long"});
-  av.g.line(485, 125, 535, 125, {"stroke-width": 2, "arrow-end":"classic-wide-long"});
-  av.g.line(485, 155, 535, 155, {"stroke-width": 2, "arrow-end":"classic-wide-long"});
+  av.g.line(485, 65, 535, 95, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, 95, 535, 65, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, 125, 535, 125, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, 155, 535, 155, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
 
   // Text labels
   av.label("Key = 42", {left: 325, top: 40});
