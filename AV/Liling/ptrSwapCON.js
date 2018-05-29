0a79af9d8dc2038e2415728b891bb1036bbea119
Cliff Shaffer <shaffer@vt.edu>
2018-05-29 17:42:20 -0400

    Tune ptrSwapCON positioning

--- AV/Liling/ptrSwapCON.js
+++ AV/Liling/ptrSwapCON.js
@@ -6,35 +6,35 @@ $(document).ready(function() {
   var av = new JSAV("ptrSwapCON", {animationMode: "none"});
 
   // Boxes and arrows
-  var left = 50;
-  for (left; left < 150; left += 30) {
-    av.g.rect(250, left, 30, 30);
-    av.g.rect(470, left, 30, 30);
-    av.g.rect(315, left + 5, 80, 20);
-    av.g.rect(535, left + 5, 80, 20);
-    av.g.line(265, left + 15, 315, left + 15,
+  var top = 20;
+  for (var pos = top; pos < 120; pos += 30) {
+    av.g.rect(250, pos, 30, 30);
+    av.g.rect(470, pos, 30, 30);
+    av.g.rect(315, pos + 5, 80, 20);
+    av.g.rect(535, pos + 5, 80, 20);
+    av.g.line(265, pos + 15, 315, pos + 15,
               {"stroke-width": 2, "arrow-end": "classic-wide-long"});
   }
 
   // Arrows on the right
-  av.g.line(485, 65, 535, 95, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, 95, 535, 65, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, 125, 535, 125, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, 155, 535, 155, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, top + 15, 535, top + 45, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, top + 45, 535, top + 15, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, top + 75, 535, top + 75, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(485, top + 105, 535, top + 105, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
 
   // Text labels
-  av.label("Key = 42", {left: 325, top: 40});
-  av.label("Key = 5", {left: 325, top: 70});
-  av.label("Key = 23", {left: 325, top: 100});
-  av.label("Key = 10", {left: 325, top: 130});
+  av.label("Key = 42", {left: 325, top: top - 10});
+  av.label("Key = 5", {left: 325, top: top + 20});
+  av.label("Key = 23", {left: 325, top: top + 50});
+  av.label("Key = 10", {left: 325, top: top + 80});
 
-  av.label("Key = 42", {left: 545, top: 40});
-  av.label("Key = 5", {left: 545, top: 70});
-  av.label("Key = 23", {left: 545, top: 100});
-  av.label("Key = 10", {left: 545, top: 130});
+  av.label("Key = 42", {left: 545, top: top - 10});
+  av.label("Key = 5", {left: 545, top: top + 20});
+  av.label("Key = 23", {left: 545, top: top + 50});
+  av.label("Key = 10", {left: 545, top: top + 80});
 
-  av.label("(a)", {left: 325, top: 160});
-  av.label("(b)", {left: 545, top: 160});
+  av.label("(a)", {left: 325, top: top + 115});
+  av.label("(b)", {left: 545, top: top + 115});
   av.displayInit();
   av.recorded();
 });
