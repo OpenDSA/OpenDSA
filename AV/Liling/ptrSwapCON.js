54f70ba09f94c33651c6455c31e2d8c5dfc49724
yliling <35701243+yliling@users.noreply.github.com>
2018-06-07 14:50:59 -0400

    modify the height of the slideshow

--- AV/Liling/ptrSwapCON.js
+++ AV/Liling/ptrSwapCON.js
@@ -5,21 +5,21 @@ $(document).ready(function() {
   "use strict";
   var av_name = "ptrSwapCON";
   var av = new JSAV(av_name);
-  var ytop = 50;
+  var ytop = 1;
 
   // Slide 1
   av.umsg("Here we see an array with references to four records.");
-  for (var pos = ytop; pos < 145; pos += 30) {
+  for (var pos = ytop; pos < 115; pos += 30) {
     av.g.rect(360, pos, 30, 30);
     av.g.rect(425, pos + 5, 80, 20);
   }
-  var arrow42 = av.g.line(375, 65, 425, 65,
+  var arrow42 = av.g.line(375, 16, 425, 16,
                           {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  var arrow5 = av.g.line(375, 95, 425, 95,
+  var arrow5 = av.g.line(375, 46, 425, 46,
                          {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(375, 125, 425, 125,
+  av.g.line(375, 76, 425, 76,
             {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(375, 155, 425, 155,
+  av.g.line(375, 106, 425, 106,
             {"stroke-width": 2, "arrow-end": "classic-wide-long"});
   var label42 = av.label("Key = 42", {left: 435, top: ytop - 10});
   var label5 = av.label("Key = 5", {left: 435, top: ytop + 20});
@@ -29,9 +29,9 @@ $(document).ready(function() {
 
   // Slide 2
   av.umsg("To start with, the record with key value 42 comes before the record with key value 5. So we want to swap them.");
-  var swap1 = av.g.path("M 505 65 A 10 10 0 1 1 505 95",
+  var swap1 = av.g.path("M 505 16 A 10 10 0 1 1 505 46",
                         {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  var swap2 = av.g.path("M 505 95 A 10 10 0 1 0 505 65",
+  var swap2 = av.g.path("M 505 46 A 10 10 0 1 0 505 16",
                         {"stroke-width": 2, "arrow-end": "classic-wide-long"});
   av.step();
 
