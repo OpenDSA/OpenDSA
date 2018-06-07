75cb0398c777c845cd3dce7167ec729830994fc5
yliling <35701243+yliling@users.noreply.github.com>
2018-06-06 22:05:10 -0400

    making SlideShow

--- AV/Liling/ptrSwapCON.js
+++ AV/Liling/ptrSwapCON.js
@@ -5,45 +5,49 @@ $(document).ready(function() {
   "use strict";
   var av_name = "ptrSwapCON";
   var av = new JSAV(av_name);
-  // Load the config object with interpreter and code created by odsaUtils.js
-  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
-      interpret = config.interpreter;       // get the interpreter
-  var ytop = 0;
+  var ytop = 50;
 
   //first slide
-//  av.label("A series of four records. The record with key value 42 comes before the record with key value 5.", {bottom: 183});
-  av.umsg(interpret("description1"));
+  av.umsg("A series of four records. The record with key value 42 comes before the record with key value 5.");
   for (var pos = ytop; pos < 145; pos += 30) {
-    av.g.rect(250, pos, 30, 30);
-    av.g.rect(315, pos + 5, 80, 20);
-    av.g.line(265, pos + 15, 315, pos + 15,
-              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+    av.g.rect(360, pos, 30, 30);
+    av.g.rect(425, pos + 5, 80, 20);
   }
-  av.label("Key = 42", {left: 325, top: ytop - 10});
-  av.label("Key = 5", {left: 325, top: ytop + 20});
-  av.label("Key = 23", {left: 325, top: ytop + 50});
-  av.label("Key = 10", {left: 325, top: ytop + 80});
+  var arrow42 = av.g.line(375, 65, 425, 65,
+              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  var arrow5 = av.g.line(375, 95, 425, 95,
+              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(375, 125, 425, 125,
+              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(375, 155, 425, 155,
+              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  var label42 = av.label("Key = 42", {left: 435, top: ytop - 10});
+  var label5 = av.label("Key = 5", {left: 435, top: ytop + 20});
+  av.label("Key = 23", {left: 435, top: ytop + 50});
+  av.label("Key = 10", {left: 435, top: ytop + 80});
   av.displayInit();
 
   //second slide
-  av.umsg(interpret("description2"));
+  av.umsg("The four records after the top two pointers have been swapped.");
+  var swap1 = av.g.path("M 505 65 A 10 10 0 1 1 505 95",
+                              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  var swap2 = av.g.path("M 505 95 A 10 10 0 1 0 505 65",
+                              {"stroke-width": 2, "arrow-end": "classic-wide-long"});
   av.step();
 
 
   //third slide
-  av.umsg(interpret("description3"));
-  for (var pos = ytop; pos < 145; pos += 30) {
-    av.g.rect(470, pos, 30, 30);
-    av.g.rect(535, pos + 5, 80, 20);
-  }
-  av.g.line(485, ytop + 15, 535, ytop + 45, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, ytop + 45, 535, ytop + 15, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, ytop + 75, 535, ytop + 75, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.g.line(485, ytop + 105, 535, ytop + 105, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
-  av.label("Key = 42", {left: 545, top: ytop - 10});
-  av.label("Key = 5", {left: 545, top: ytop + 20});
-  av.label("Key = 23", {left: 545, top: ytop + 50});
-  av.label("Key = 10", {left: 545, top: ytop + 80});
+  av.umsg("Now the record with key value 5 comes before the record with key value 42.");
+  label42.hide();
+  label5.hide();
+  arrow42.hide();
+  arrow5.hide();
+  swap1.hide();
+  swap2.hide();
+  av.g.line(375, ytop + 15, 425, ytop + 45, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.g.line(375, ytop + 45, 425, ytop + 15, {"stroke-width": 2, "arrow-end": "classic-wide-long"});
+  av.label("Key = 5", {left: 435, top: ytop - 10});
+  av.label("Key = 42", {left: 435, top: ytop + 20});
 
   av.recorded();
 
