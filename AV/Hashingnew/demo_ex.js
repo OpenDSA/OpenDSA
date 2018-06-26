872bca3c14c78bfdffb44575520c9287cef9c1f8
Yuhui Lyu <yuhuilu@Yuhuis-MacBook-Pro.local>
2018-06-25 21:13:26 -0400

    Support js file for demo_ex.html

--- AV/Hashingnew/demo_ex.js
+++ AV/Hashingnew/demo_ex.js
@@ -1,4 +1,21 @@
 /*global window */
 (function() {
   "use strict";
+    var demo_ex = {  		
+		recordNumber: function(tableSize, percentage) {
+			var fact;
+			var prob;
+			for (var i = 1; i < tableSize; i++) {
+				fact = 1.0;
+				for (var j = tableSize - i + 1; j < tableSize; j++) {
+					fact = fact * j / tableSize;
+				}
+				prob = 1.0 - fact;
+				if (prob * 100 >= percentage) {
+					return i;
+				}
+			}
+		}
+  };
+  window.demo_ex = window.demo_ex || demo_ex;
 }());
