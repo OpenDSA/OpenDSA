142f1c6181c69783dd7656ff10ab89d5cb84d569
MKOMohammed <mostafakamelosman@gmail.com>
2018-08-24 18:52:18 +0000

    modify the PIFrame JS code

--- AV/PIExample/Frames.js
+++ AV/PIExample/Frames.js
@@ -7,6 +7,47 @@ $(document).ready(function() {
         interpret = config.interpreter, // get the interpreter
         code = config.code;             // get the code object
     var goNext = false;
+    av.umsg("First Information");
+    var q = av.question("TF", "Understand the given information", {correct: true, falseLabel: "No", trueLabel: "Yes"});
+    var answer = q.show();
+    alert(answer);
+    av.step();
+    av.umsg("Second Information");
+    av.step();
+    av.umsg("Third Information");
+    av.step();
+    av.umsg("Fourth Information");
+
+
+
     av.recorded();
-    $(".jsavforward").css("pointer-events", "none");  });
+    $(".jsavforward").css("pointer-events", "none");  
+    $(".jsavend").css("pointer-events", "none");  
+    var currnet = 0;
+    var index = 0;
+    $('.jsavforward').click(function() {
+        if(index === currnet)
+        $(".jsavforward").css("pointer-events", "none");  
+        else{
+        $(".jsavforward").css("pointer-events", "auto");  
+        index++;
+        }
+        });
+
+    $('.jsavbackward').click(function() {
+          
+        if(index>0){
+        index--;
+        $(".jsavforward").css("pointer-events", "auto");
+        }
+        });
+    $('.jsavbegin').click(function() {
+        $(".jsavforward").css("pointer-events", "auto");  
+        index=0;
+        });
+    $('.jsavend').click(function() {
+        $(".jsavforward").css("pointer-events", "none");  
+        });
+});
+    
   
