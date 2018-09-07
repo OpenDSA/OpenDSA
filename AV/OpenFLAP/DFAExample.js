2f9873912a6480aa6c485cee8a87d6f37af34263
Galina Belolipetski <gbelolipetski@localhost.localdomain>
2018-09-07 14:58:07 -0400

    Galina final solution

--- AV/OpenFLAP/DFAExample.js
+++ AV/OpenFLAP/DFAExample.js
@@ -6,28 +6,30 @@ $(document).ready(function() {
   var yStart = 0;
   av.label("input tape", {left: 0 + xStart, top: 0 + yStart});
   var rectangleHeight = 30;
-  var rectangleWidth = 150;
-  av.g.rect(xStart, yStart + 20, rectangleWidth, rectangleHeight);
-  /*
-  var x = 15;
+  var rectangleWidth = 300;
+  av.g.rect(xStart, yStart + 50, rectangleWidth, rectangleHeight);
+
+  var difference = 30;
+  var x = 30;
   var strings = ["a", "a", "b", "b", "a", "b"];
-  while (x !== 150){
-    av.g.line(xStart + x, yStart + 20, xStart + x, yStart + 20 + rectangleHeight);
-    if (x % 15 < 6){
-      av.label(strings[(x%15) - 1], {left: xStart + x, top: yStart + 20 + (rectangleHeight/2)});
+  while (x !== 300){
+    av.g.line(xStart + x, yStart + 50, xStart + x, yStart + 50 + rectangleHeight);
+    if (x / difference < 6){
+      av.label(strings[(x/difference) - 1], {left: xStart + x - (difference/2), top: yStart + 50});
     }
-    if (x % 15 == 7){
-      av.label("head moves", {left: xStart + x, top: yStart + 20 + (rectangleHeight/2)} + 20);
+    if (x / difference == 7){
+      av.label("head moves", {left: xStart + x, top: yStart + 50 + (rectangleHeight/2)+20});
     }
-    x = x + 15;
+    x = x + difference;
   }
-  var squareHeight = 60;
-  var squareWidth = 50;
+
+  var squareHeight = 90;
+  var squareWidth = 90;
   av.label("current state", {left: xStart + 40 + 5, top: yStart + + 20 + rectangleHeight + 40 + 5});
-  av.g.rect(xStart + 40, yStart + 20 + rectangleHeight + 40, squareWidth, squareHeight);
-  avg.g.circle(xStart + 40 + squareWidth/2, yStart + 20 + rectangleHeight + 40 + ((squareHeight-10)/2));
-  av.g.line()*/
+  av.g.rect(xStart + 40, yStart + 40 + rectangleHeight + 40, squareWidth, squareHeight);
+  av.g.circle(xStart + 40 + (squareWidth/2), yStart + 50 + rectangleHeight + 40 + ((squareHeight-10)/2), 30);
+
   av.displayInit();
   av.recorded();
 
-}
+});
