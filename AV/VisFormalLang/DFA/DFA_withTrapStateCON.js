a48573a1b11b6bcee2654069c222b612d73b3806
Mostafa Mohammed <mostafakamelosman@gmail.com>
2018-10-18 15:12:26 -0400

    Finish NFA, and DFA sections in VisFormalLang book

--- AV/VisFormalLang/DFA/DFA_withTrapStateCON.js
+++ AV/VisFormalLang/DFA/DFA_withTrapStateCON.js
@@ -4,28 +4,13 @@ $(document).ready(function() {
 
   var av_name = "DFA_withTrapStateCON";
   var av = new JSAV(av_name, {animationMode: "none"});
-  var xPosition = 375;
-  var yPosition = 10;
-  var length1 = 125;
-  var width = 30;
+  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
+  interpret = config.interpreter;
+  var url = interpret("fa1");
+
   var BinaryDFA = new av.ds.fa();
-  var q0 = BinaryDFA.addNode({left: 50, top: 50});
-  var q1 = BinaryDFA.addNode({left: 150, top: 50});
-  var q2 = BinaryDFA.addNode({left: 250, top: 50});
-  var trap = BinaryDFA.addNode({left: 150, top: 150});
-  trap.value("trap");
+  FiniteAutomaton.prototype.loadFAFromJFLAPFile.call(BinaryDFA,url);
   BinaryDFA.disableDragging();
-  toggleInitial(BinaryDFA, q0);
-  toggleFinal(BinaryDFA, q2);
-  BinaryDFA.addEdge(q0, q1, {weight: "b"});
-  BinaryDFA.addEdge(q1, q1, {weight: "b"});
-  BinaryDFA.addEdge(q1, q2, {weight: "a"});
-  BinaryDFA.addEdge(q0, trap, {weight: "a"});
-  BinaryDFA.addEdge(q2, trap, {weight: "a, b"});
-  //BinaryDFA.addEdge(q2, trap, {weight: "b"});
-  BinaryDFA.addEdge(trap, trap, {weight: "a, b"});
-  //BinaryDFA.addEdge(trap, trap, {weight: "b"});
-  BinaryDFA.layout();
   av.displayInit();
   av.recorded();
 });
--- AV/VisFormalLang/DFA/EvenBinaryDFACON.js
+++ AV/VisFormalLang/DFA/EvenBinaryDFACON.js
@@ -1,25 +1,17 @@
+document.write('<script src="../../../AV/Development/formal_language/fa/Automaton.js"></script>');
 document.write('<script src="../../../AV/Development/formal_language/fa/FA.js"></script>');
 document.write("<link rel=\"stylesheet\" type=\"text/css\" href=\"../../../AV/Development/formal_language/css/FA.css\" />");
 $(document).ready(function() {
   "use strict";
-
   var av_name = "EvenBinaryDFACON";
   var av = new JSAV(av_name, {animationMode: "none"});
-  var xPosition = 375;
-  var yPosition = 10;
-  var length1 = 125;
-  var width = 30;
-  var BinaryDFA = new av.ds.fa();
-  var q0 = BinaryDFA.addNode({left: 50, top: 100});
-  var q1 = BinaryDFA.addNode({left: 250, top: 100});
+  var config = ODSA.UTILS.loadConfig({av_name: av_name}),
+      interpret = config.interpreter;
+
+  var url = interpret("fa1");
+  var BinaryDFA = new av.ds.fa({center: true});
+  FiniteAutomaton.prototype.loadFAFromJFLAPFile.call(BinaryDFA,url);
   BinaryDFA.disableDragging();
-  toggleInitial(BinaryDFA, q0);
-  toggleFinal(BinaryDFA, q1);
-  BinaryDFA.addEdge(q0, q1, {weight: "0"});
-  BinaryDFA.addEdge(q1, q0, {weight: "1"});
-  BinaryDFA.addEdge(q0, q0, {weight: "1"});
-  BinaryDFA.addEdge(q1, q1, {weight: "0"});
-  BinaryDFA.layout();
   av.displayInit();
   av.recorded();
 });
