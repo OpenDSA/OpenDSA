649fb62651175ddff8cc8a9231418f51c4922172
Tom Naps <naps@uwosh.edu>
2018-11-15 16:53:24 -0600

    Enhanced AVs for Section 5.3 on scope

--- AV/PL/FP/FP5Code1CON.js
+++ AV/PL/FP/FP5Code1CON.js
@@ -5,12 +5,16 @@ $(document).ready(function() {
     var av_name = "FP5Code1CON";
     var av = new JSAV(av_name);
     var leftMargin = 10;
-    var glx = av.ds.array(['"outside"'], {indexed: false, left: leftMargin + 50, top: 20}).hide();
+    var offset_for_each_var = 50;
+    var offset_between_var_label_and_cell = 20;
+    var glx = av.ds.array(['"outside"'], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell}).hide();
     var label_glx = av.label("Global x", {left: leftMargin, top: 0}).hide();
-    var f1 = av.ds.array(["Function definition"], {indexed: false, left: leftMargin + 50, top: 70}).hide();
-    var label_f1 = av.label("f1", {left: leftMargin, top: 50}).hide();
-    var f1x = av.ds.array(["inside f1"], {indexed: false, left: leftMargin + 50, top: 120}).hide();
-    var label_f1x = av.label("f1's x", {left: leftMargin, top: 100}).hide();
+    var f1 = av.ds.array(["Function definition"], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + offset_for_each_var}).hide();
+    var label_f1 = av.label("f1", {left: leftMargin, top: offset_for_each_var}).hide();
+    var f1x = av.ds.array(['"inside f1"'], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + 2 * offset_for_each_var}).hide();
+    var label_f1x = av.label("f1's x", {left: leftMargin, top: 2 * offset_for_each_var}).hide();
+    var f2 = av.ds.array(["Function definition"], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + 3 * offset_for_each_var}).hide();
+    var label_f2 = av.label("f2", {left: leftMargin, top: 3 * offset_for_each_var}).hide();
 
     var pseudo = av.code(
         [
@@ -56,19 +60,44 @@ $(document).ready(function() {
     av.step();
     
     //Slide 3
-    av.umsg('When the definition of f1 in lines 2-5 is read into the interpreter, a variable f1 is established in the global scope and a value, namely the definition of the function, is associated with it.  Note that in functional languages the definition of a function is itself a "value".   This is one aspect of functional languages that sets them apart.');
+    av.umsg('When the definition of f1 in lines 2-5 is read into the interpreter, a variable f1 is established in the global scope and a value, namely the definition of the function, is associated with it.  Note that in functional languages the definition of a function is itself a "value".  This is one aspect of functional languages that sets them apart.  In the definition of the function, a variable x is declared in the local scope of the function, making the x declared in the global scope inaccessible within the function.');
     pseudo.unhighlight(1);
     label_f1.show();
     f1.show();
+    glx.addClass(0,"inaccessible");
     av.step();
     
     // Slide 4
-    av.umsg('When function f1 is called in line 6, the x declared in f1 becomes visible in the local scope of f1.   Hence the value "inside f1" is returned from the function call.   Note that, during the execution of the code in the definition, the global x is not accessible within the code of the function.');
+    av.umsg('The function f1, which was defined in lines 2-5, is now called in line 6.  The variable x declared in f1 becomes visible in the local scope of f1.   Hence the value "inside f1" is returned from the function call.   Note that, during the execution of the code in the definition, the global x is not accessible within the code of the function.');
     f1x.show();
     label_f1x.show();
 //    glx.hide();
     //    label_glx.hide();
     pseudo.highlight(6);
     av.step();
+
+    //Slide 5
+    av.umsg('When the definition of f2 in lines 7-10 is read into the interpreter, a variable f2 is established in the global scope and the definition of the function is associated with it.  In the definition of the function, the absence of the "var" in front of x means that variable x is *not* being declared in the local scope of the function.  Instead this is a reference to the x in the global scope.');
+    pseudo.unhighlight(6);
+    label_f2.show();
+    f2.addClass(0,"wider");
+    f2.show();
+    f1x.addClass(0,"inaccessible");
+    glx.removeClass(0,"inaccessible");
+    av.step();
+
+    // Slide 6
+    av.umsg('When f2 is called in line 11, the code in the function definition is executed, affecting the global x and returning the value "inside f2"');
+    pseudo.highlight(11);
+    glx.value(0,'"inside f2"');
+    av.step();
+
+    // Slide 7
+    av.umsg('When x is evaluated in line 12, the value "inside f2" is returned, reflecting that the content of the global x was changed during execution of f2');
+    pseudo.unhighlight(11);
+    pseudo.highlight(12);
+    av.step();
+
+    
     av.recorded();
 });
