133c2c5c7fe5f56a8365f2bf8d14ff815d0bbba4
Tom Naps <naps@uwosh.edu>
2018-11-23 13:24:33 -0600

    Completed new section on mapping pattern.

--- AV/PL/FP/FP6Code1CON.js
+++ AV/PL/FP/FP6Code1CON.js
@@ -7,22 +7,17 @@ $(document).ready(function() {
     var leftMargin = 10;
 //     var offset_for_each_var = 50;
 //     var offset_between_var_label_and_cell = 20;
-//     var glx = av.ds.array(['"outside"'], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell}).hide();
-//     var label_glx = av.label("Global x", {left: leftMargin, top: 0}).hide();
-//     var f1 = av.ds.array(["Function definition"], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + offset_for_each_var}).hide();
-//     var label_f1 = av.label("f1", {left: leftMargin, top: offset_for_each_var}).hide();
-//     var f1x = av.ds.array(['"inside f1"'], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + 2 * offset_for_each_var}).hide();
-//     var label_f1x = av.label("f1's x", {left: leftMargin, top: 2 * offset_for_each_var}).hide();
-//     var f2 = av.ds.array(["Function definition"], {indexed: false, left: leftMargin + offset_for_each_var, top: offset_between_var_label_and_cell + 3 * offset_for_each_var}).hide();
-//     var label_f2 = av.label("f2", {left: leftMargin, top: 3 * offset_for_each_var}).hide();
 
     var arrValues = [3, 2, 5, 8];
     var arr = av.ds.array(arrValues, {indexed: false, left: leftMargin, top: 20});
     var arr_label = av.label("ns", {left: leftMargin, top: 0});
-    var arr_ret = av.ds.array([4,3,6,9], {indexed: false, left: leftMargin, top: 90});
-    var arr_ret_label = av.label("addBonusPoint:", {left: leftMargin, top: 70});
-    var arr_ret1 = av.ds.array([6,4,10,16], {indexed: false, left: leftMargin, top: 160});
-    var arr_ret_label1 = av.label("doubleAll:", {left: leftMargin, top: 140});
+    var arr_ret = av.ds.array([4,3,6,9], {indexed: false, left: leftMargin, top: 110});
+    var arr_ret_label = av.label("addBonusPoint", {left: leftMargin, top: 70});
+    var arr_ret_labela = av.label("returns:", {left: leftMargin, top: 90});
+    var arr_ret1 = av.ds.array([6,4,10,16], {indexed: false, left: leftMargin, top: 220});
+    var arr_ret_label1 = av.label("doubleAll", {left: leftMargin, top: 180});
+    var arr_ret_label1a = av.label("returns:", {left: leftMargin, top: 200});
+    var map_label = av.label("The mapping abstraction", {left:leftMargin + 280, top:260}).hide();
     var code1 =         [
 	    "var add1 = function (x) { ",
 	    "                 return fp.add(x,1); };",
@@ -47,9 +42,6 @@ $(document).ready(function() {
 	    left: leftMargin,
 	    top: 0,
             lineNumbers: false
-//             left: leftMargin,
-// 	    right: leftMargin,
-// 	    top: 0
         }
     );
     
@@ -80,7 +72,6 @@ $(document).ready(function() {
     );
     
     var code3 = [
-	"var doubleIt = function (x) { return fp.add(x,x); };",
 	"var map = function (f,ns) {",
 	"  if (fp.isNull(ns))",
 	"      return [ ];",
@@ -89,15 +80,16 @@ $(document).ready(function() {
 	"                 f(fp.hd(ns)), ",
 	"                 map(f, fp.tl(ns)));",
 	"}",
-	"map( doubleIt, [1,2,3,4,5] );",
-	"map( function (x) { return x+1; }, [1,2,3,4,5] );"
+	"var add1 = function (x) { return fp.add(x,1); };",
+	"map( add1, [3,2,5,8] );",
+	"map( function (x) { return fp.add(x,x); }, [3,2,5,8] );"
 	];
 
     var pseudo3 = av.code(
 	code3,
         {
  	    left: 250,
-	    top: 250,
+	    top: 285,
 	    lineNumbers: false
         }
     );
@@ -106,23 +98,27 @@ $(document).ready(function() {
 
 
     // Slide 1
-    av.umsg("Both addBonusPoint and doubleAll use very similar patterns of computation. Given a list ns, they return a new list by applying a function -- add1 for addBonusPoint and doubleIt for doubleAll -- to every element of the given list.  This is done by calling the function with the head of the list and then using recursion to process the remainder of the list.");
-    pseudo1.addClass([8,9], "red");
-    pseudo2.addClass([8,9], "red");
+    av.umsg("Both addBonusPoint and doubleAll use very similar patterns of computation. Given a list ns, they return a new list by applying a function -- add1 for addBonusPoint and doubleIt for doubleAll -- to every element of the given list.  This is done by calling the function with the head of the list and then consing that result onto what recursion returns from the tail of the list.  This pattern is highlighted in red below.");
+    pseudo1.addClass([7,8,9], "red");
+    pseudo2.addClass([7,8,9], "red");
     pseudo1.hide(11);
     pseudo2.hide(11);
     arr_ret_label.hide();
+    arr_ret_labela.hide();
     arr_ret.hide();
     arr_ret_label1.hide();
+    arr_ret_label1a.hide();
     arr_ret1.hide();
     av.displayInit();
 
     // Slide 2
-    av.umsg("The call, highlighted in blue, to addBonusPoint in the code on the left will return the list also highlighted in blue.   The call, highlighted in green, to doubleAll in the code on the right will return the list also highlighted in blue.");
+    av.umsg("Highlighted in blue, the call to addBonusPoint in the code on the left will return the list also highlighted in blue.   Highlighted in green, the call to doubleAll in the code on the right will return the list also highlighted in green.");
     arr_ret_label.show();
+    arr_ret_labela.show();
     arr_ret.show();
     arr_ret.addClass([0,1,2,3], "addBonusPoint");
     arr_ret_label1.show();
+    arr_ret_label1a.show();
     arr_ret1.show();
     arr_ret1.addClass([0,1,2,3], "doubleAll");
     pseudo1.show(11);
@@ -130,17 +126,28 @@ $(document).ready(function() {
     pseudo2.show(11);
     pseudo2.addClass(11,"green");
     av.step();
-    
-//     // Slide 3
-//     av.umsg("The call, highlighted in green, to doubleAll in the code on the right will return the indicated list.");
-//     arr_ret.value(0,6);
-//     arr_ret.value(1,4);
-//     arr_ret.value(2,10);
-//     arr_ret.value(3,16);
-//     pseudo1.removeClass(11,"blue");
-//     pseudo2.show(11);
-//     pseudo2.addClass(11,"green");
-//     av.step();
+
+    // Slide 3
+    av.umsg('The pattern of computation in each of these two functions is highlighted by the three red lines.    That pattern is: apply an operation (add1 or doubleIt) to the head and then recur on the tail.   How to capture this pattern in our abstraction?  With functions as first-class values, we can pass in the operation to apply as an additional parameter f.  This is done in the function that now appears below addBonusPoint and doubleAll.   Because the pattern is called the "mapping pattern", we call the function map.');
+    map_label.addClass("emphasize");
+    map_label.show();
+    pseudo3.show();
+    pseudo3.hide([9, 10, 11]);
+    pseudo3.addClass([5,6,7],"red");
+    av.step();
+
+    // Slide 4
+    av.umsg("To achieve what addBonusPoint does, we can pass in the add1 function to map as indicated in blue below.");
+    pseudo3.show([9,10]);
+    pseudo3.addClass([9,10],"blue");
+    av.step();
+
+    // Slide 5
+    av.umsg("To achieve what doubleAll does, we use a different strategy highlighted in green below.   This strategy passes in an anonymous function instead of the named function doubleIt.  Passing in a function parameter as an anonymous function is a frequently used technique in functional programming.");
+    pseudo3.show([11]);
+    pseudo3.addClass([11],"green");
+    av.step();
+
     
     av.recorded();
 });
--- RST/en/PL/FP6.rst
+++ RST/en/PL/FP6.rst
@@ -15,58 +15,59 @@ The Mapping Pattern
 
 **Abstraction** is one of the most powerful tools in programming. But
 what is it?  Let's begin getting our head around this notion by
-considering the following two examples.
+considering the following two examples called *addBonusPoint* and *doubleAll* .
 
-::
-
-    var add1 = function (x) { 
-                     return fp.add(x,1); };
-    var addBonusPoint = function (ns) {
-      if (fp.isNull(ns))
-          return [ ];
-      else
-          return fp.cons( 
-                   add1(fp.hd(ns)),
-                   addBonusPoint(fp.tl(ns)));
-    }
-    addBonusPoint( [1,2,3,4,5] );
-
-::
-
-    var doubleIt = function (x) { 
-                     return fp.add(x,x); };
-    var doubleAll = function (ns) {
-      if (fp.isNull(ns))
-          return [ ];
-      else
-          return fp.cons( 
-                   doubleIt(fp.hd(ns)), 
-                   doubleAll(fp.tl(ns)));
-    }
-    doubleAll( [1,2,3,4,5] );
-
-
-
-    
-Both *addBonusPoint* and *doubleAll* use very similar patterns of
-computation.  Given a list, they return a new list by applying a
-function to every element of the given list.  How can we lift this
-pattern of computation to a level where we can write it once and be
-done with it?
-
-::
-
-    var doubleIt = function (x) { return fp.add(x,x); };
-    var map = function (f,ns) {
-      if (fp.isNull(ns))
-          return [ ];
-      else
-          return fp.cons(
-                     f(fp.hd(ns)), 
-                     map(f, fp.tl(ns)));
-    }
-    map( doubleIt, [1,2,3,4,5] );
-    map( function (x) { return x+1; }, [1,2,3,4,5] );
+.. 
+.. ::
+.. 
+..     var add1 = function (x) { 
+..                      return fp.add(x,1); };
+..     var addBonusPoint = function (ns) {
+..       if (fp.isNull(ns))
+..           return [ ];
+..       else
+..           return fp.cons( 
+..                    add1(fp.hd(ns)),
+..                    addBonusPoint(fp.tl(ns)));
+..     }
+..     addBonusPoint( [1,2,3,4,5] );
+.. 
+.. ::
+.. 
+..     var doubleIt = function (x) { 
+..                      return fp.add(x,x); };
+..     var doubleAll = function (ns) {
+..       if (fp.isNull(ns))
+..           return [ ];
+..       else
+..           return fp.cons( 
+..                    doubleIt(fp.hd(ns)), 
+..                    doubleAll(fp.tl(ns)));
+..     }
+..     doubleAll( [1,2,3,4,5] );
+.. 
+.. 
+.. 
+..     
+.. Both *addBonusPoint* and *doubleAll* use very similar patterns of
+.. computation.  Given a list, they return a new list by applying a
+.. function to every element of the given list.  How can we lift this
+.. pattern of computation to a level where we can write it once and be
+.. done with it?
+.. 
+.. ::
+.. 
+..     var doubleIt = function (x) { return fp.add(x,x); };
+..     var map = function (f,ns) {
+..       if (fp.isNull(ns))
+..           return [ ];
+..       else
+..           return fp.cons(
+..                      f(fp.hd(ns)), 
+..                      map(f, fp.tl(ns)));
+..     }
+..     map( doubleIt, [1,2,3,4,5] );
+..     map( function (x) { return x+1; }, [1,2,3,4,5] );
 
 
 .. inlineav:: FP6Code1CON ss
@@ -74,4 +75,46 @@ done with it?
    :links: AV/PL/FP/FP6CON.css
    :scripts: AV/PL/FP/FP6Code1CON.js
    :output: show
+
+This pattern of computation in the preceding example is called the
+**mapping pattern**: it takes a function and a list and returns the
+list obtained by applying the function to each element in the input
+list.  This problem is about the mapping pattern.
+
+.. avembed:: Exercises/PL/Map.html ka
+   :long_name: Mapping Pattern
+
+   
+Function Composition
+--------------------
+
+The next example of abstraction we will consider is called function
+composition.  We want a function called *compose* that takes in two
+functions *f* and *g* and returns the function that first applies *g* to its
+argument and then applies *f* to that result. In other words:
+
+.. math::
+
+   h(x) = (f \circ g)(x) = f( g(x) )  
+
+::
+
+    var compose = function (f,g) {
+
+           return ?????
+    }
+
+Once you've completed the *compose* function, use it to compose the
+*doubleIt* function with the *add1* function and apply the resulting
+function *h* to the value 2.
+
+::
+
+   > var h = ???
+   > h(2)  // returns ???
+
+This next problem is about function composition.
+
+.. avembed:: Exercises/PL/Compose.html ka
+   :long_name: Function Composition
 
