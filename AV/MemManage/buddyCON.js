4461f4d81332c0061b62c618cd49404296883e5d
Cliff Shaffer <shaffer@vt.edu>
2019-01-06 12:27:24 -0500

    Buddy menthod diagram update

--- AV/MemManage/buddyCON.js
+++ AV/MemManage/buddyCON.js
@@ -1,32 +1,30 @@
 /*global JSAV, document */
 // Written by Irena Shaffer
-
 $(document).ready(function() {
   "use strict";
   var av = new JSAV("buddyCON", {animationMode: "none"});
 
-  var rectX = 170;
-  var rectY = 50;
+  var currX = 170;
+  var posY = 30;
   var rectHeight = 30;
-  var currX = rectX;
 
-  av.g.rect(rectX, rectY, 16, rectHeight).css({fill: "coral"});
+  av.g.rect(currX, posY, 16, rectHeight).css({fill: "coral"});
   currX = currX + 16;
-  av.g.rect(currX, rectY, 16, rectHeight).css({fill: "cornflowerblue"});
+  av.g.rect(currX, posY, 16, rectHeight).css({fill: "cornflowerblue"});
   currX = currX + 16;
-  av.g.rect(currX, rectY, 32, rectHeight).css({fill: "cornflowerblue"});
+  av.g.rect(currX, posY, 32, rectHeight).css({fill: "cornflowerblue"});
   currX = currX + 32;
-  av.g.rect(currX, rectY, 64, rectHeight).css({fill: "coral"});
+  av.g.rect(currX, posY, 64, rectHeight).css({fill: "coral"});
   currX = currX + 64;
-  av.g.rect(currX, rectY, 128, rectHeight).css({fill: "cornflowerblue"});
+  av.g.rect(currX, posY, 128, rectHeight).css({fill: "cornflowerblue"});
   currX = currX + 128;
-  av.g.rect(currX, rectY, 128, rectHeight).css({fill: "coral"});
+  av.g.rect(currX, posY, 128, rectHeight).css({fill: "coral"});
   currX = currX + 128;
-  av.g.rect(currX, rectY, 128, rectHeight).css({fill: "cornflowerblue"});
+  av.g.rect(currX, posY, 128, rectHeight).css({fill: "cornflowerblue"});
 
   //2 rectangles at top of screen that show how much memory is used/remains
-  av.g.rect(550, 120, 40, 30).css({fill: "coral"});
-  av.g.rect(650, 120, 40, 30).css({fill: "cornflowerblue"});
+  av.g.rect(550, posY + 60, 40, 30).css({fill: "coral"});
+  av.g.rect(650, posY + 60, 40, 30).css({fill: "cornflowerblue"});
   //labels the blocks at the top of the screen
   av.label("Used Space", {left: 530, top: 135});
   av.label("Free Space", {left: 630, top: 135});
@@ -36,11 +34,11 @@ $(document).ready(function() {
   var freeAmountLabel = av.label(152, {left: 657, top: 110});
   freeAmountLabel.css({"z-index": 500});
 
-  av.label("Block Size", {left: 160, top: 100});
-  av.ds.array([2, 4, 8, 16, 32, 64, 128, 256], {layout: "vertical", left: 180, top: 140});
+  av.label("Block Size", {left: 160, top: posY + 40});
+  av.ds.array([1, 2, 4, 8, 16, 32, 64, 128, 256], {layout: "vertical", left: 180, top: posY + 70});
  
   //labels the free lists
-  var freeListTop = 144;
+  var freeListTop = posY + 110;
 
   var spacing = 34;
 
@@ -66,13 +64,13 @@ $(document).ready(function() {
             {"arrow-end": "classic-wide-long", "stroke-width": 2});
 
 
-  av.label("0", {left: 173, top: 10});
-  av.label("1", {left: 189, top: 10});
-  av.label("2", {left: 213, top: 10});
-  av.label("3", {left: 260, top: 10});
-  av.label("4", {left: 355, top: 10});
-  av.label("5", {left: 480, top: 10});
-  av.label("6", {left: 613, top: 10});
+  av.label("0", {left: 173, top: posY - 35});
+  av.label("1", {left: 189, top: posY - 35});
+  av.label("2", {left: 213, top: posY - 35});
+  av.label("3", {left: 260, top: posY - 35});
+  av.label("4", {left: 355, top: posY - 35});
+  av.label("5", {left: 480, top: posY - 35});
+  av.label("6", {left: 613, top: posY - 35});
 
   av.displayInit();
   av.recorded();
