2a5664ebd36c46f57f98a4a87bbc2fed545243f1
Yuhui Lyu <yuhuilu@Yuhuis-MacBook-Pro.local>
2019-01-10 03:48:38 -0500

    fix several problems

--- AV/Yuhui/PumpingLemmaCON.js
+++ AV/Yuhui/PumpingLemmaCON.js
@@ -18,25 +18,36 @@ $(document).ready(function () {
     av.umsg("There exists a constant $m>0$ such that any $w \\in L$ with $|w| \\geq m$ can be decomposed into three parts as $w=xyz$ with:");
     var arrValues = ["x", "y", "z"];
     var arr = av.ds.array(arrValues, {"left": xoffset, "top": yoffset + 50});
-    var labelm = av.label("$|-- \\geq m--|$", {"left": xoffset, "top": yoffset + 25});
+    var labelm = av.label("$\\geq m$", {"left": xoffset + 35, "top": yoffset + 25});
     var labelw = av.label("$w$: ", {"left": xoffset - 50, "top": yoffset + 50});
+    var arrow1 = av.g.line(xoffset + 30, yoffset + 52, xoffset + 2, yoffset + 52, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
+    var arrow2 = av.g.line(xoffset + 65, yoffset + 52, xoffset + 93, yoffset + 52, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
     av.step();  
     
     //Slidw 3
-    av.umsg("1. $|xy| \\leq m$");
-    var labelxy = av.label("$xy \\leq m$",{"left": xoffset + 10, "top": yoffset + 90});
+    av.umsg("There exists a constant $m>0$ such that any $w \\in L$ with $|w| \\geq m$ can be decomposed into three parts as $w=xyz$ with:");
+    av.umsg("<br> 1. $|xy| \\leq m$", {"preserve": true});
+    var labelxy = av.label("$\\leq m$",{"left": xoffset + 20, "top": yoffset + 90});
+    var arrow3 = av.g.line(xoffset + 18, yoffset + 117, xoffset + 2, yoffset + 117, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
+    var arrow4 = av.g.line(xoffset + 48, yoffset + 117, xoffset + 61, yoffset + 117, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
     av.step();
 
     //Slide 4 
-    av.umsg("2. $|y| \\geq 1$");
+    av.umsg("There exists a constant $m>0$ such that any $w \\in L$ with $|w| \\geq m$ can be decomposed into three parts as $w=xyz$ with:");
+    arrow3.hide();
+    arrow4.hide();  
+    av.umsg("<br> 1. $|xy| \\leq m$", {"preserve": true});
+    av.umsg("<br> 2. $|y| \\geq 1$", {"preserve": true});
     labelxy.hide();
-    var labely = av.label("$|y| \\geq 1$",{"left": xoffset + 25, "top": yoffset + 90});
+    var labely = av.label("$\\geq 1$",{"left": xoffset + 35, "top": yoffset + 90});
     av.step();
 
     //Slide 5
-    av.umsg("3. $xy^iz \\in L$  for all $i \\geq 0$");
+    av.umsg("There exists a constant $m>0$ such that any $w \\in L$ with $|w| \\geq m$ can be decomposed into three parts as $w=xyz$ with:");
+    av.umsg("<br> 1. $|xy| \\leq m$", {"preserve": true});
+    av.umsg("<br> 2. $|y| \\geq 1$", {"preserve": true});
+    av.umsg("<br> 3. $xy^iz \\in L$  for all $i \\geq 0$", {"preserve": true});
     labely.hide();
-    labelm.hide();
     labelw.hide();
     labelw = av.label("$w$: ", {"left": xoffset - 150, "top": yoffset + 50});
     var arrValues1 = ["x", "y", "y", "z"];
@@ -49,6 +60,11 @@ $(document).ready(function () {
     var arrValues2 = ["x", "y","...", "y", "z"];
     var arr2 = av.ds.array(arrValues2, {"left": xoffset, "top": yoffset + 250});
     var labelxyz2 = av.label("$xy^iz \\in L$($i \\geq 0$): ",{"left": xoffset - 150, "top": yoffset + 250});
+
+    var labeliy = av.label("$i$ y's", {"top": yoffset + 290, "left": xoffset + 63});
+    var arrow5 = av.g.line(xoffset + 58, yoffset + 317, xoffset + 30, yoffset + 317, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
+    var arrow6 = av.g.line(xoffset + 95, yoffset + 317, xoffset + 120, yoffset + 317, {"stroke-width": 1, "arrow-end": "classic-wide-long"});
+    
     av.step();
 
     //Slide 6
