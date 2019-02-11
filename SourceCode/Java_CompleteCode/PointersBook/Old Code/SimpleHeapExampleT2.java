ef956ed301817f1c859226720e6bc66779c223da
Cliff Shaffer <shaffer@vt.edu>
2019-02-11 16:45:43 -0500

    Deleting obsolete SourceCode/Java_CompleteCode

--- SourceCode/Java_CompleteCode/PointersBook/Old Code/SimpleHeapExampleT2.java
+++
@@ -1,5 +0,0 @@
-// Allocates heap block and stores its pointer in local variable.
-    // Dereferences the pointer to set the the name to Sam
-    empPtr = new Employee();
-    empPrt.setName("Sam");
-    // T2
