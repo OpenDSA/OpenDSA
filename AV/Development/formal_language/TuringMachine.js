4c866f8d01e145b1f60dc354ff6ece116969d230
galinabee <galinab@vt.edu>
2019-05-02 23:14:43 -0400

    cleaning up

--- AV/Development/formal_language/TuringMachine.js
+++ AV/Development/formal_language/TuringMachine.js
@@ -1,7 +1,5 @@
 // Turing Machine "class", extending FiniteAutomaton
 //Galina Belolipetski
-//To do: make a visualization of this class and the second class
-// PDA has a similar stack, concept to a tape, that is fully implemented
 var TuringMachine = function(jsav, options) {
 	Automaton.apply(this, arguments);
 	this.transitions = [];
@@ -33,8 +31,6 @@ tm.play = function(inputString) {
 			cur,
 			counter = 0,
 			configView = [];		// configurations to display in the message box
-	//TODO: issue here, displaying multiple configurations in the message box
-	//This is where the visual tape generation happens
 	for (var j = 0; j < currentStates.length; j++) {
 		configView.push(currentStates[j].toString());
 	}
@@ -117,9 +113,6 @@ tm.traverse = function(currentStates) {
 			for (var i = 0; i < weight.length; i++) {
 				weight[i] = toColonForm(weight[i]);
 				var w = weight[i].split(':');
-				//TODO: the issue is in here, there's not a proper way of handling
-				// whether we go back to the proper location
-				//how should this be handled?
 				if (tapeValue === w[0]) {
 					var nextConfig = new Configuration(next, currentState.tape, this.jsav);
 					if (w[1] !== square){
@@ -141,7 +134,6 @@ tm.traverse = function(currentStates) {
 			}
 		}
 	}
-	// remove duplicate configurations
 	nextStates = _.uniq(nextStates, function(x) {return x.toID();});
 	return nextStates;
 };
@@ -278,20 +270,13 @@ tm.initFromXML = function(text) {
 // Configuration class
 var Configuration = function(state, tape, av) {
 	this.state = state;
-	console.log("My state: " + state.value());
-	console.log("My tape: " + tape); //tape is undefined
-	// this.tape = new Tape(tape);
 	this.tape = new Tape(tape);
 	// toString returns the state value + the 'viewport' of the tape, to be displayed to the user
 	this.toString = function() {
 		this.tape.viewTape(this.tape.getArr(), av);
-		//return this.tape.toString();
-		//return this.state.value() + ' ' + viewTape(this.tape);
 	}
 	this.toID = function() {
-		// console.log(this.tape.currentIndex);
-		return this.state.value() + this.tape;// + this.tape.current;
-		// return this.state.value() + ' ' + this.tape + this.tape.current;
+		return this.state.value() + this.tape;
 	}
 };
 
@@ -323,30 +308,20 @@ tmTrans.getWeight = function() {
  */
 var Tape = function(str) {
 	"use strict";
-	// this.arr = [];
-	// this.current = 0;
+	this.arr = [];
+	this.current = 0;
 	this.currentIndex = 0;
-	// console.log("I'm getting it" + str);
 
 	if (typeof str === 'string') {
 		this.arr = str.split("");
-		console.log("My array is:" + this.arr);
-		// for (var i = 0; i < this.arr.length; i++){
-		// 	console.log(i + this.arr.get[i]);
-		// }
 		this.current = this.arr[0];  // the current symbol
 		this.currentIndex = 0;                // the current position
 	}
 	// else, assume that a Tape object was passed in, and create a copy of it
 	else {
-		console.log("str's index"+str.currentIndex);
-		console.log("str's array"+str.getArr());
 		this.currentIndex = str.currentIndex;
-		//this.arr = this.copy(str.getArr());
 		this.arr = str.getArr();
-		this.current = this.arr[this.currentIndex];  // the current symbol
-
-		               // the current position
+		this.current = this.arr[this.currentIndex];
 	}
 
 	this.copy = function(value){
@@ -356,13 +331,10 @@ var Tape = function(str) {
 		}
 		return newarr;
 	}
-	// this.jsav;
 
 	this.toString = function(){
 		return this.arr.toString();
 	}
-
-	// this.object = this.jsav.av.ds.array(this.arr);
 
 	this.write = function(value, location){
 		this.arr[location] = value;
@@ -374,51 +346,31 @@ var Tape = function(str) {
 		return this.arr;
 	}
 
-	this.currentValue = function(){
+	this.currentValue = function() {
 		return this.arr[this.currentIndex];
 	}
 
-	this.value = function(newValue){
-		//this.object.highlight(this.current);
-		console.log("myValue is :"+newValue);
-		if (typeof newValue === "undefined"){
-			//console.log("I'm undefined");
+	this.value = function(newValue) {
+		if (typeof newValue === "undefined") {
 			return undefined;
-			// return this.arr[this.currentIndex];
 		}
-		console.log("before value: "+this.arr[this.currentIndex]);
 		this.arr[this.currentIndex] = newValue;
-		console.log("after value: "+this.arr[this.currentIndex]);
 		return this.arr[this.currentIndex];
 	}
 
-	this.goRight = function(){
-		// if (this.currentIndex += 1 > this.arr.length){
-		// 	console.log("Too far right");
-		// }
-		//else{
+	this.goRight = function() {
 			this.currentIndex+=1;
 			this.current = this.arr[this.currentIndex];
 			return this.current;
-			//TODO: highlight?
-			// this.object.highlight(current);
-		//}
-
 	}
 
-	this.goLeft = function(){
-		// if (this.current -= 1 < 0){
-		// 	console.log("Too far left");
-		// }
-		//else{
+	this.goLeft = function() {
 			this.currentIndex-=1;
 			this.current = this.arr[this.currentIndex];
 			return this.current;
-			// this.object.highlight(current);
-		//}
 	}
 
-	this.removeValue = function(location){
+	this.removeValue = function(location) {
 		this.arr[location] = null;
 		for (var i = 0; i < arr.length; i++)	{
 			arr[i] = arr[i+1];
@@ -432,277 +384,12 @@ var Tape = function(str) {
 		} else if (str === "R") {
 			return this.goRight();
 		} else if (str === "S") {
-			return this.current;
+			return this.curren;
 		}
 	}
 
 	this.viewTape = function (t, av) {
 		var arr = av.ds.tape(t, 325, 30, "both", this.currentIndex);
 		return arr;
-	}
+	};
 };
-
-
-
-
-	// if the tape is initialized using a string, writes the string to the new tape
-	// if (typeof str === 'string') {
-	// 	this.head = makeTape(str);
-	// 	this.current = this.head.right()[0];  // the current symbol
-	// 	this.currentIndex = 0;                // the current position
-	// }
-	// // else, assume that a Tape object was passed in, and create a copy of it
-	// else {
-	// 	var copy = copyTape(str);
-	// 	this.head = copy[0];
-	// 	this.current = copy[1];
-	// 	this.currentIndex = str.currentIndex;
-	// }
-	// Returns the string written on the tape starting from the head of the string, including empty squares
-	// this.toString = function() {
-	// 	var temp = this.head,
-	// 			ret = "";
-	// 	while (temp) {
-	// 		ret += temp.value();
-	// 		temp = temp._right;
-	// 	}
-	// 	return ret;
-	// };
-	// // Move the tape left and set the new head of the string
-	// this.left = function() {
-	// 	var next = this.current.left();
-	// 	this.current = next[0];
-	// 	this.currentIndex--;
-	// 	if (next[1]) {
-	// 		this.head = next[1];
-	// 	}
-	// 	return this.current;
-	// };
-	// // Move the tape right
-	// this.right = function() {
-	// 	var next = this.current.right();
-	// 	this.current = next[0];
-	// 	this.currentIndex++;
-	// 	return this.current;
-	// };
-	// // Write to the current position
-	// this.value = function (newVal) {
-	// 	return this.current.value(newVal);
-	// };
-	// // Move the tape and read the symbol
-	// this.move = function (str) {
-	// 	if (str === "L") {
-	// 		return this.left();
-	// 	} else if (str === "R") {
-	// 		return this.right();
-	// 	} else if (str === "S") {
-	// 		return this.current;
-	// 	}
-	// };
-
-
-// Tape linked list node
-// var TapeNode = function (left, right, val) {
-// 	this._left = left;
-// 	this._right = right;
-// 	if (typeof val === "undefined") {
-// 		//this._value = "";
-// 		this._value = String.fromCharCode(9633);
-// 	} else {
-// 		this._value = val;
-// 	}
-// 	// Return the value written at this position or write a symbol to this position
-// 	this.value = function (newVal) {
-// 		if (typeof newVal === "undefined") {
-// 			return this._value;
-// 		} else {
-// 			this._value = newVal;
-// 			return this._value;
-// 		}
-// 	};
-// 	/*
-// 		 Traverse left or right and read.
-// 		 If can't, create empty tape nodes.
-// 		 Returns an array containing the read symbol and the new head of the string
-// 	 */
-// 	this.left = function (n) {
-// 		if (this._left) {
-// 			return [this._left];
-// 		} else {
-// 			if (!n) { n = 10; }
-// 			return extendTape("left", this, n);
-// 		}
-// 	}
-// 	this.right = function (n) {
-// 		if (this._right) {
-// 			return [this._right];
-// 		} else {
-// 			if (!n) { n = 10; }
-// 			return extendTape("right", this, n);
-// 		}
-// 	}
-// };
-//
-// // Function to initialize the linked list from an inputted string
-// var makeTape = function (str) {
-// 	var prev = new TapeNode(null, null);
-// 	var head = prev;
-// 	for (var i = 0; i < str.length; i++) {
-// 		var temp = new TapeNode(prev, null, str.charAt(i));
-// 		prev._right = temp;
-// 		prev = temp;
-// 	}
-// 	return head;
-// };
-//
-// // Function to initialize the linked list by copying the inputted tape
-// var copyTape = function (t) {
-// 	var prev = new TapeNode(null, null, t.current.value());
-// 	var cur = prev;
-// 	var temp = t.current._right;
-// 	while (temp) {
-// 		var next = new TapeNode(prev, null, temp.value());
-// 		prev._right = next;
-// 		prev = next;
-// 		temp = temp._right;
-// 	}
-// 	prev = cur;
-// 	temp = t.current;
-// 	while (temp._left) {
-// 		var next = new TapeNode(null, prev, temp._left.value());
-// 		prev._left = next;
-// 		prev = next;
-// 		temp = temp._left;
-// 	}
-// 	return [temp, cur];
-// };
-
-//Do we need to extend the tape currently?
-// /*
-// 	 Creates n empty tape nodes to the beginning or end of the tape
-// 	 and returns the read symbol and the leftmost/rightmost new node
-//  */
-// var extendTape = function (dir, node, n) {
-// 	if (dir === 'left') {
-// 		var next = new TapeNode(null, node),
-// 				prev = next;
-// 		node._left = next;
-// 		for (var i = 0; i < n - 1; i++) {
-// 			var temp = new TapeNode(null, prev);
-// 			prev._left = temp;
-// 			prev = temp;
-// 		}
-// 		return [next, prev];
-// 	}
-// 	if (dir === 'right') {
-// 		var next = new TapeNode(node, null),
-// 				prev = next;
-// 		node._right = next;
-// 		for (var i = 0; i < n - 1; i++) {
-// 			var temp = new TapeNode(prev, null);
-// 			prev._right = temp;
-// 			prev = temp;
-// 		}
-// 		return [next, prev];
-// 	}
-// };
-
-// /*
-// 	 Function to get the tape alphabet.
-//  */
-// tm.getTapeAlphabet = function () {
-// 	var alphabet = [];
-// 	var edges = this.edges();
-// 	var w;
-// 	for (var next = edges.next(); next; next = edges.next()) {
-// 		w = next.weight();
-// 		w = toColonForm(w);
-// 		w = w.split('<br>');
-// 		for (var i = 0; i < w.length; i++) {
-// 			var t = w[i].split('|');
-// 			for (var k = 0; k < t.length; k++) {
-// 				var letter1 = t[k].split(':')[0],
-// 						letter2 = t[k].split(':')[1],
-// 						letters;
-// 				if (letter1 !== this.emptystring && letter2 !== this.emptystring) {
-// 					letters = letter1.split('').concat(letter2.split(''));
-// 				} else if (letter1 !== this.emptystring) {
-// 					letters = letter1.split('');
-// 				} else if (letter2 !== this.emptystring) {
-// 					letters = letter2.split('');
-// 				} else {
-// 					break;
-// 				}
-// 				for (var j = 0; j < letters.length; j++) {
-// 					if (letters[j] !== this.emptystring && alphabet.indexOf(letters[j]) === -1){
-// 						alphabet.push(letters[j]);
-// 					}
-// 				}
-// 			}
-// 		}
-// 	}
-// 	return alphabet;
-// };
-
-//This is for tape extending infinitely in both directions
-// /*
-// 	 Function to get the tape "viewport".
-// 	 This returns a string showing the current position and the seven
-// 	 symbols to the left and right of the current position.
-// 	 The current position is highlighted as well.
-//  */
-// var viewTape = function (t) {
-// 	var arr = new Array(15);    // arbitrary size
-// 	for (var i = 0; i < 15; i++) {
-// 		arr[i] = String.fromCharCode(9633);;
-// 	}
-// 	i = 7;
-// 	var temp = t.current;
-// 	while (temp) {
-// 		if (i < 0) {break;}
-// 		arr[i] = temp.value();
-// 		i--;
-// 		temp = temp._left;
-// 	}
-// 	i = 7;
-// 	temp = t.current;
-// 	while (temp) {
-// 		if (i >= arr.length) {break;}
-// 		arr[i] = temp.value();
-// 		i++;
-// 		temp = temp._right;
-// 	}
-// 	var view = "|";
-// 	for (var i = 0; i < arr.length; i++) {
-// 		if (i === 7) {
-// 			view+="<mark>" + arr[i] + "</mark>";
-// 		} else {
-// 			view+=arr[i];
-// 		}
-// 	}
-// 	view+="|";
-// 	return view;
-// };
-//
-// tm.updateAlphabetFunction = tm.updateAlphabet;
-// tm.updateAlphabet = function() {
-// 	this.updateAlphabetFunction();
-// 	$("#alphabet").html("" + Object.keys(this.alphabet).sort());
-// 	var sa = this.getTapeAlphabet();
-// 	$('#stackalphabet').html(emptystring + "," + sa.sort());
-// }
-//
-// /*
-// 	 Function to get the tape output, the string made from the current position up to,
-// 	 but not including, the first empty square found.
-//  */
-//
-// var produceOutput = function (t) {
-// 	var temp = t.current,
-// 			output = "";
-// 	while (temp && temp.value() !== String.fromCharCode(9633)) {
-// 		output += temp.value();
-// 		temp = temp._right;
-// 	}
-// 	return output;
-// };
