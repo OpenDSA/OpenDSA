f1857775eed5348af6341a4c4913363569a1ea64
Cliff Shaffer <shaffer@vt.edu>
2019-07-10 20:33:55 -0400

    Finish cleanup of automata example

--- AV/VisFormalLang/Introduction/AutomataCON.js
+++ AV/VisFormalLang/Introduction/AutomataCON.js
@@ -7,27 +7,6 @@ $(document).ready(function() {
 
   var left = 200;
   var top = 20;
-  /*av.g.line(left, top, left + 400, top, {"stroke-width": 1}); // Top line of input tape
-  av.g.line(left, top + 35, left + 410, top + 35, {"stroke-width": 1}); // Bottom line of input tape
-  // Left jagged edge of input tape
-  av.g.polyline([[left, top], [left, top + 5], [left - 5, top + 10], [left, top + 15],
-    [left, top + 20], [left + 5, top + 25], [left, top + 30], [left - 5, top + 35], [left, top + 35]], {"stroke-width": 1});
-  // Right jegged edge of input tape
-  av.g.polyline([[left + 400, top], [left + 395, top + 4], [left + 400, top + 4], [left + 395, top + 10], [left + 404, top + 15],
-   [left + 395, top + 15], [left + 415, top + 23], [left + 400, top + 27], [left + 410, top + 30],
-   [left + 410, top + 35]], {"stroke-width": 1})
-
-  // Left most straight vertical line of input tape
-  av.g.line(left + 15, top, left + 15, top + 35, {"stroke-width": 1});
-  // inner line of input tape
-  var i = 1;
-  for (i; i <= 9; i++) {
-    var temp = av.g.line(left + 15 + 37*i, top, left + 15 + 37*i, top + 35, {"stroke-width": 1});
-  }
-  // Right most  straight vertical line of input tape
-  av.g.line(left + 385, top, left + 385, top + 35, {"stroke-width": 1});*/
-
-  //var arr = av.ds.array(['a', 'a', 'b', 'b', 'a', 'b', '', '', '', '' ], {"left": left + 30, "top": top - 10});
   var elements = ['a', 'a', 'b', 'b', 'a', 'b', '', '', '', '' ];
   var tape = av.ds.tape(elements, left + 30, top, "right");
   //Control unit
@@ -58,19 +37,12 @@ $(document).ready(function() {
 
   av.label('input tape', {"left": left + 90, "top": top - 35});
   // Inner text of input tape
-  /*av.label('a', {"left": left + 28, "top": top - 7})
-  av.label('a', {"left": left + 65, "top": top - 7})
-  av.label('b', {"left": left + 102, "top": top - 7})
-  av.label('b', {"left": left + 139, "top": top - 7})
-  av.label('a', {"left": left + 176, "top": top - 7})
-  av.label('b', {"left": left + 213, "top": top - 7})*/
   av.label('tape head', {"left": left + 126, "top": top + 25})
   av.label('control unit', {"left": left + 85, "top": top + 100})
   av.label('0', {"left": left + 105, "top": top + 145})
   av.label('1', {"left": left + 140, "top": top + 145})
   av.label('storage', {"left": left + 290, "top": top + 170})
   av.label('(optional)', {"left": left + 290, "top": top + 190})
-
 
   av.displayInit();
   av.recorded();
