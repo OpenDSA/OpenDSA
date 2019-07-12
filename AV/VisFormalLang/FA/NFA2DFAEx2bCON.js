9b25b43f212cb2c34f0a132f0939a9d90b0e35ea
Cliff Shaffer <shaffer@vt.edu>
2019-07-12 12:55:40 -0400

    Fix display bug in NFA2DFA example

--- AV/VisFormalLang/FA/NFA2DFAEx2bCON.js
+++ AV/VisFormalLang/FA/NFA2DFAEx2bCON.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   var av_name = "NFA2DFAEx2bCON";
   var av = new JSAV(av_name, {animationMode: "none"});
   var url = "../../../AV/VisFormalLang/FA/Machines/NFA2DFA2b.jff";
-  var dfa = new av.ds.fa({center: true});
+  var dfa = new av.ds.fa({width: 600, height: 200});
   FiniteAutomaton.prototype.loadFAFromJFLAPFile.call(dfa, url);
   dfa.disableDragging();
   av.displayInit();
