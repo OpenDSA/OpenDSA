76c5ba7232a379a760dc075564d60da9c3595607
Jackson Wonderly <jacksonwonderly@gmail.com>
2019-07-17 16:41:35 +0000

    Revert "Jeffrey Peng Updated Tutorial Finish"
    This reverts commit 19b3d24ac46e40ff4146b55d1b962256a08a75e7.

--- RST/en/Jeffrey_Peng_Test/CFLnew2.js
+++ RST/en/Jeffrey_Peng_Test/CFLnew2.js
@@ -2,6 +2,8 @@ $(document).ready(function() {
   "use strict";
   var av = new JSAV("mineAssigned2", {animationMode: "none"});
   // Setup the tree
+  var btTop = -50;
+  var btLeft = 305;
   var bt = av.ds.tree({nodegap: 15});
   bt.root("S");
   var rt = bt.root();
@@ -10,7 +12,7 @@ $(document).ready(function() {
   var lt3 = bt.newNode("B");
   var lt4 = bt.newNode("B");
   rt.addChild(lt);
-  rt.addChild("c");
+  rt.addChild("C");
   rt.addChild(lt3);
   lt.addChild("a");
   lt.addChild(lt2);
