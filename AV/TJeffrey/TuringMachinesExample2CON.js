6c9cb9b2686f641aa9a423be739e335e36e878e0
jeffjpeng <jeffp123@vt.edu>
2019-09-27 19:21:19 -0400

    Example done for Turing Machine

---
+++ AV/TJeffrey/TuringMachinesExample2CON.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    "use strict";
+
+    var av_name = "TuringMachinesExample2CON";
+    var av = new JSAV(av_name, {animationMode: "none"});
+    var url = "../../../AV/VisFormalLang/TM/Machines/TMexample2.jff";
+    var tm = new av.ds.TM({width: 600, height: 325, url:url});
+    av.displayInit();
+    av.recorded();
+  });
