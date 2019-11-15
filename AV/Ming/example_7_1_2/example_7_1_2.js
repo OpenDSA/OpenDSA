7eff3bb574996d15b2c14ee2a91eb834278bc143
ming98 <ming98@vt.edu>
2019-11-14 23:47:53 -0500

    Copy of CYKParser and fixed links to css

--- AV/Ming/example_7_1_2/example_7_1_2.js
+++ AV/Ming/example_7_1_2/example_7_1_2.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 
   var av_name = "example_7_1_2";
   var av = new JSAV(av_name, {animationMode: "none"});
-  var url = "../../../AV/Ming/example_7_1_2_machine.jff";
+  var url = "../../../AV/Ming/example_7_1_2/example_7_1_2_machine.jff";
   var pda = new av.ds.PDA({width: 600, height: 200, url: url});
   PDA.prototype.loadFAFromJFLAPFile.call(pda,url);
   pda.disableDragging();
