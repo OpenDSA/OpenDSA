3ee2a34afd01042c99803ac614f67d9e32ac90d2
David Furcy <furcyd@uwosh.edu>
2020-01-15 12:26:31 -0600

    Implemented exercise 3.5.2

--- Exercises/PL/SubstitutionCase1.js
+++ Exercises/PL/SubstitutionCase1.js
@@ -5,17 +5,17 @@
     var question = {};
     var arr;
 
-
-    function pickExpression(minDepth,maxDepth,vars) {
+    function pickExpressionSubcase(minDepth,maxDepth,vars,
+				    prob1a,prob1b,prob3) {
 	/* pick a, p, abd B in subst(a,p,B) */
 	var substCase,a,p,B,tmp,tmp2, x;
-	var rnd = Math.random();    
+	var rnd = Math.random();
 	a = L.getRndExp(1,1,3,vars,"");
-	if (rnd<0.15) {
+	if (rnd < prob1a) {
 	    substCase = "1a";
 	    p = L.absyn.createVarExp( vars.substr(L.getRnd(0,vars.length-1),1));
 	    B = p;
-	} else if (rnd<0.30) {
+	} else if (rnd < prob1b) {
 	    substCase = "1b";
 	    tmp = L.getRnd(0,vars.length-1);
 	    while (true) {
@@ -24,7 +24,7 @@
 	    }
 	    p = L.absyn.createVarExp( vars.substr(tmp,1));
 	    B = L.absyn.createVarExp( vars.substr(tmp2,1));
-	} else if (rnd<0.45) {
+	} else if (rnd < prob3) {
 	    substCase = "3";
 	    p = L.absyn.createVarExp( vars.substr(L.getRnd(0,vars.length-1),1));
 	    while (true) {
@@ -94,30 +94,24 @@
     var Substitution1 = {    
 
 	init: function () {
-	    //var jsav = new JSAV("jsav", {"animationMode": "none"});
 	    var vs = "xyz";
 	    var minDepth = 4;
 	    var maxDepth = 6;
-	    var subst = pickExpression(2,4,vs);
-	    var substCase = subst[0];
+	    var subst = pickExpressionSubcase(2, 4, vs, 0.4, 0.8, 0.9);
+	    var substCase = (subst[0] !== "1a" && subst[0] !== "1b") ?
+		"This is not an instance of case 1" : subst[0];
 	    var a = subst[1];
 	    var p = subst[2];
 	    var B = subst[3];
-	    var options = ["1a","1b","2a","2b","2c","3"];
+	    var options = ["1a","1b","This is not an instance of case 1"];
 	    this.substExpression = "subst( " + L.printExp(a) + ", " + 
 		L.printExp(p) + ", " + 	L.printExp(B) + " )";
-	    //arr = jsav.ds.array([subst]);
-	    //arr.addClass([0],"noBoxShadow");
 	    this.answer = substCase;
 	    
-	    //console.log(this.answer);
 	    options.splice(options.indexOf(substCase),1);
 
 	    this.option1 = options[0];
 	    this.option2 = options[1];
-	    this.option3 = options[2];
-	    this.option4 = options[3];
-	    this.option5 = options[4];
 	} // init function
 
 
