fd1a4657b52ffa0b3b320a4c9c8c2c16aadeb276
adityat <adityat@vt.edu>
2020-01-26 22:54:07 -0500

    made some updates to the inverted list js

--- AV/Background/InvListCON.js
+++ AV/Background/InvListCON.js
@@ -20,29 +20,28 @@ $(document).ready(function() {
   var xPos = 180;
 
   //label on top for primary & seconday 
-  av.label("Secondary ", {left: xPos + 10, top: yPos});
-  av.label("Primary ", {left: xPos + 350, top: yPos});
+  av.label("Secondary ", {left: xPos + 10, top: yPos - 5});
+  av.label("Primary ", {left: xPos + 325, top: yPos - 5});
 
   //height and width variables for left table first column
   var leftBoxWidth = 100;
   var leftBoxHeight = 30;
 
   //left box first column
-  av.label("Key", {left: xPos + 30, top: yPos + 50});
+  av.label("Key", {left: xPos + 30, top: yPos + 15});
   //av.label("Jones", {left: xPos - 160, top: yPos + 100});
-  av.g.rect(xPos, yPos + 100, leftBoxWidth, leftBoxHeight).css({fill: "white"});
-  av.g.rect(xPos, yPos + 100 + leftBoxHeight, leftBoxWidth, leftBoxHeight).css({fill: "white"});
-  av.g.rect(xPos, yPos + 100 + leftBoxHeight*2, leftBoxWidth, leftBoxHeight).css({fill: "white"});
+  av.g.rect(xPos, yPos + 50, leftBoxWidth, leftBoxHeight).css({fill: "white"});
+  av.g.rect(xPos, yPos + 50 + leftBoxHeight, leftBoxWidth, leftBoxHeight).css({fill: "white"});
+  av.g.rect(xPos, yPos + 50 + leftBoxHeight*2, leftBoxWidth, leftBoxHeight).css({fill: "white"});
   
 
   //height and width variable for left table second and fourth column
   var boxHeightAndWidth = 30;
 
   //left box second column
-  av.label("Index", {left: xPos + leftBoxWidth, top: yPos + 50});
-  av.g.rect(xPos + leftBoxWidth, yPos + 100, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + leftBoxWidth, yPos + 100 + leftBoxHeight, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + leftBoxWidth, yPos + 100 + leftBoxHeight*2, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
+  av.g.rect(xPos + leftBoxWidth, yPos + 50, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
+  av.g.rect(xPos + leftBoxWidth, yPos + 50 + leftBoxHeight, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
+  av.g.rect(xPos + leftBoxWidth, yPos + 50 + leftBoxHeight*2, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
 
 
   //right table left column variables
@@ -50,41 +49,25 @@ $(document).ready(function() {
   var rightBoxHeight = 30;
 
   //right box first column
-  av.label("Key", {left: xPos + 330, top: yPos + 50});
-  av.g.rect(xPos + 300, yPos + 100, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("0", {left: xPos + 278, top: yPos + 93});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("1", {left: xPos + 278, top: yPos + 93 + rightBoxHeight});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*2, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("2", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*2});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*3, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("3", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*3});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*4, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("4", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*4});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*5, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("5", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*5});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*6, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("6", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*6});
-  av.g.rect(xPos + 300, yPos + 100 + rightBoxHeight*7, rightBoxWidth, rightBoxHeight).css({fill: "white"});
-  av.label("7", {left: xPos + 278, top: yPos + 93 + rightBoxHeight*7});
+  av.label("Key", {left: xPos + 330, top: yPos + 15});
+  av.g.rect(xPos + 300, yPos + 50, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*2, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*3, rightBoxWidth, rightBoxHeight).css({fill: "white"});
 
-  //right box second column
-  av.label("Index", {left: xPos + 383, top: yPos + 50});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*2, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*3, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*4, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*5, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*6, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
-  av.g.rect(xPos + rightBoxWidth + 300, yPos + 100 + rightBoxHeight*7, boxHeightAndWidth, boxHeightAndWidth).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*5, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*6, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*7, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+
+  av.g.rect(xPos + 300, yPos + 50 + rightBoxHeight*9, rightBoxWidth, rightBoxHeight).css({fill: "white"});
+
 
   //arrows
-  av.g.line(325,  137, 436, 137,
+  av.g.line(325,  80, 469, 80,
             {"arrow-end": "classic-wide-long", "stroke-width": 2});
-  av.g.line(325,  165, 436, 165,
+  av.g.line(325,  115, 473, 208,
             {"arrow-end": "classic-wide-long", "stroke-width": 2});
-  av.g.line(325,  191, 436, 225,
+  av.g.line(325,  150, 471, 330,
             {"arrow-end": "classic-wide-long", "stroke-width": 2});
 
   av.displayInit();
