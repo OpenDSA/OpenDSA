1ce55f458f7f6b6aefcdaaa6547b10956725e9c3
mning1598 <mning1598@gmail.com>
2020-01-28 16:10:56 -0500

    finished Getting Started Task

--- AV/Ning/Ning.js
+++ AV/Ning/Ning.js
@@ -2,32 +2,41 @@ $(document).ready(function() {
     "use strict";
     var av_name = "ISAM";
     var av = new JSAV(av_name, { animationMode: "none" });
-    var rect = av.g.rect(360, 10, 120, 70);
-    // rect.show();
+    var left = 310;
 
-    var leftTop = av.g.rect(310, 120, 80, 50);
-    var leftMid = av.g.rect(310, 170, 80, 80);
-    var leftBot = av.g.rect(310, 250, 80, 50);
+    av.g.rect(left + 50, 10, 120, 70);
+    av.label("In-memory",  {left: left + 65, top: -10}).css({"font-size": "18px"});
+    av.label("Table of",  {left: left + 75, top: 10}).css({"font-size": "18px"});
+    av.label("Cylinder Keys",  {left: left + 55, top: 30}).css({"font-size": "18px"});
 
-    var rightTop = av.g.rect(450, 120, 80, 50);
-    var rightMid = av.g.rect(450, 170, 80, 80);
-    var rightBot = av.g.rect(450, 250, 80, 50);
+    av.label("Cylinder 1",  {left: left, top: 80}).css({"font-size": "18px"});
+    av.g.rect(left, 120, 80, 50);
+    av.label("Cylinder",  {left: left+5, top: 105}).css({"font-size": "18px"});
+    av.label("Index",  {left: left+15, top: 125}).css({"font-size": "18px"});
+    av.g.rect(left, 170, 80, 80);
+    av.label("Records",  {left: left+5, top: 180}).css({"font-size": "18px"});
+    av.g.rect(left, 250, 80, 50);
+    av.label("Cylinder",  {left: left+7, top: 230}).css({"font-size": "18px"});
+    av.label("Overflow",  {left: left+5, top: 250}).css({"font-size": "18px"});
 
-    var botBot = av.g.rect(370, 340, 100, 60);
-    // av.pointer(leftBot, botBot);
-    //in-memory table of cylinder
-    var lab = av.label("hello", {before: rect , visible: true});
-    av.umsg("We start by creating an array with 6 elements.");
+
+    av.label("Cylinder 2",  {left: left+140, top: 80}).css({"font-size": "18px"});
+    av.g.rect(left+140, 120, 80, 50);
+    av.label("Cylinder",  {left: left+145, top: 105}).css({"font-size": "18px"});
+    av.label("Index",  {left: left+155, top: 125}).css({"font-size": "18px"});
+    av.g.rect(left+140, 170, 80, 80);
+    av.label("Records",  {left: left+145, top: 180}).css({"font-size": "18px"});
+    av.g.rect(left+140, 250, 80, 50);
+    av.label("Cylinder",  {left: left+147, top: 230}).css({"font-size": "18px"});
+    av.label("Overflow",  {left: left+145, top: 250}).css({"font-size": "18px"});
+
+    av.g.line(left + 40, 302, left + 95, 338, {"stroke-width": "3", "arrow-end": "classic"});
+    av.g.line(left + 180, 302, left + 125, 338, {"stroke-width": "3", "arrow-end": "classic"});
+
+
+    av.g.rect(370, 340, 100, 60);
+    av.label("System",  {left: left + 80, top: 325}).css({"font-size": "18px"});
+    av.label("Overflow",  {left: left + 75, top: 345}).css({"font-size": "18px"});
     av.displayInit();
     av.recorded();
-
-    // var bt = av.ds.tree({ nodegap: 15});
-    // bt.root("S");
-    // var rt = bt.root();
-    // var lt = bt.newNode("A");
-    // rt.addChild(lt);
-    // rt.addChild("c");
-    // bt.layout();
-    // av.displayInit();
-    // av.recorded();
 });
