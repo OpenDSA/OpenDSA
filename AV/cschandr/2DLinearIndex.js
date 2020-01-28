8ad83eb118f86742fc6aeee77c62d74799cd2dc3
cschandr <cschandr@vt.edu>
2020-01-28 00:28:53 -0500

    Created js and css file for figure 12.2.1 - Linear Indexing

---
+++ AV/cschandr/2DLinearIndex.js
@@ -0,0 +1,62 @@
+/*document for JSAv*/
+// written by Chinmayee Schandra
+
+$(document).ready(function()) {
+        "use strict";
+        var table = "2DLinearIndex";
+        var config = ODSA.UTILS.loadConfig ({table:table});
+	    interpret = config.interpreter;
+	
+
+	var av;
+	var leftAlign = 200;
+	var topAlign = 30;
+
+	av = new JSAV(table);
+
+
+	//positions for table
+	var x = 100;
+	var y = 20;
+
+	//dimensions for name rectangle
+	var nHeight = 30;
+	var nWidth = 80;
+
+	//creates name rectangles
+	av.g.rect(x, y, nWidth, nHeight).css({fill: "white"})
+	av.g.rect(x, y + nHeight, nWidth, nHeight).css({fill: "white"});
+	av.g.rect(x, y + nHeight*2, nWidth, nHeight).css({fill: "white"});
+
+	//rest of the rectangles
+	var xPos = x + nWidth;
+	var yPos = y;
+
+	//dimensions of rectanlge
+	var height = 30;
+	var width = 50;
+
+	//first rwo rectangle
+	av.g.rect(xPos, yPos, width, height).css({fill: "white"});
+	av.g.rect(xPos + width, yPos, width, height).css({fill : "white"});
+	av.g.rect(xpos + 2*width. yPos, width, height).css({fill: "white"});
+	
+	//second row rectangles
+	yPos = y + 30;
+
+	av.g.rect(xPos, yPos, width, height).css({fill: "white"});
+        av.g.rect(xPos + width, yPos, width, height).css({fill : "white"});
+        av.g.rect(xpos + 2*width. yPos, width, height).css({fill: "white"});
+
+	//third row rectangles
+	 yPos += 30;
+
+	av.g.rect(xPos, yPos, width, height).css({fill: "white"});
+        av.g.rect(xPos + width, yPos, width, height).css({fill : "white"});
+        av.g.rect(xpos + 2*width. yPos, width, height).css({fill: "white"});
+
+	av.displayInit();
+	av.recorded();
+});
+
+
