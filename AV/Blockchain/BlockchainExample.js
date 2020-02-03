610b738ebc79133137a4ee01d77c9245f0adcdef
baileyg2016 <bspell20@vt.edu>
2020-02-03 17:06:02 -0500

    The hash is dynamic and works with key presses now

--- AV/Blockchain/BlockchainExample.js
+++ AV/Blockchain/BlockchainExample.js
@@ -48,6 +48,48 @@ $(document).ready(function() {
         $("#block3PrevHash").val(block2Hash);
     }
 
+    async function testKeyDown(e) {
+        var one = 1, two = 2, three = 3;
+        var block1Data = $("#data1").val();
+        var block2Data = $("#data2").val();
+        var block3Data = $("#data3").val();
+        var block1Hash, block2Hash, block3Hash;
+
+        console.log("data1: ")
+        console.log($("#data1"))
+
+        console.log(block1Data)
+        console.log(block2Data)
+        console.log(block3Data)
+
+        await sha256(one, block1Data).then(res => {
+            console.log("first sha")
+            block1Hash = res;
+        });
+
+        await sha256(two, block2Data, block1Hash).then(res => {
+            block2Hash = res;
+        });
+
+        await sha256(three, block3Data, block2Hash).then(res => {
+            block3Hash = res;
+        });
+
+        if (e.target.id === "data1") {
+                $("#block1PrevHash").val("0000000000000000000000000000000000000000000000000000000000000000");
+                $("#block1Hash").val(block1Hash);
+                $("#block2Hash").val(block2Hash);
+                $("#block2PrevHash").val(block1Hash);
+                $("#block3PrevHash").val(block2Hash);
+            }
+        else if (e.target.id === "data2") {
+            $("#block2Hash").val(block2Hash);
+            $("#block3PrevHash").val(block2Hash);
+        }
+        $("#block3Hash").val(block3Hash);
+    }
+
     // Connect action callbacks to the HTML entities
     $("#compute").click(ComputeHashes);
+    $(".data").keyup(testKeyDown);
 });
