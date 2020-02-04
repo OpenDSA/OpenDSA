8cbdfa0e3b964aed163e3f56006facff1da0c4e4
baileyg2016 <bspell20@vt.edu>
2020-02-04 17:30:20 -0500

    I have put arrows showing that each block points to its previous block

--- AV/Blockchain/BlockchainExample.js
+++ AV/Blockchain/BlockchainExample.js
@@ -20,34 +20,6 @@ $(document).ready(function() {
         return hashHex;
     }
 
-    // Computes the hash for all three blocks
-    async function ComputeHashes() {
-        var one = 1, two = 2, three = 3;
-        var block1Data = $("#data1").val();
-        var block2Data = $("#data2").val();
-        var block3Data = $("#data3").val();
-        var block1Hash, block2Hash, block3Hash;
-
-        await sha256(one, block1Data).then(res => {
-            block1Hash = res;
-        });
-
-        await sha256(two, block2Data, block1Hash).then(res => {
-            block2Hash = res;
-        });
-
-        await sha256(three, block3Data, block2Hash).then(res => {
-            block3Hash = res;
-        });
-
-        $("#block1PrevHash").val("0000000000000000000000000000000000000000000000000000000000000000");
-        $("#block1Hash").val(block1Hash);
-        $("#block2Hash").val(block2Hash);
-        $("#block3Hash").val(block3Hash);
-        $("#block2PrevHash").val(block1Hash);
-        $("#block3PrevHash").val(block2Hash);
-    }
-
     async function testKeyDown(e) {
         var one = 1, two = 2, three = 3;
         var block1Data = $("#data1").val();
@@ -55,15 +27,7 @@ $(document).ready(function() {
         var block3Data = $("#data3").val();
         var block1Hash, block2Hash, block3Hash;
 
-        console.log("data1: ")
-        console.log($("#data1"))
-
-        console.log(block1Data)
-        console.log(block2Data)
-        console.log(block3Data)
-
         await sha256(one, block1Data).then(res => {
-            console.log("first sha")
             block1Hash = res;
         });
 
@@ -76,12 +40,12 @@ $(document).ready(function() {
         });
 
         if (e.target.id === "data1") {
-                $("#block1PrevHash").val("0000000000000000000000000000000000000000000000000000000000000000");
-                $("#block1Hash").val(block1Hash);
-                $("#block2Hash").val(block2Hash);
-                $("#block2PrevHash").val(block1Hash);
-                $("#block3PrevHash").val(block2Hash);
-            }
+            $("#block1PrevHash").val("0000000000000000000000000000000000000000000000000000000000000000");
+            $("#block1Hash").val(block1Hash);
+            $("#block2Hash").val(block2Hash);
+            $("#block2PrevHash").val(block1Hash);
+            $("#block3PrevHash").val(block2Hash);
+        }
         else if (e.target.id === "data2") {
             $("#block2Hash").val(block2Hash);
             $("#block3PrevHash").val(block2Hash);
@@ -90,6 +54,9 @@ $(document).ready(function() {
     }
 
     // Connect action callbacks to the HTML entities
-    $("#compute").click(ComputeHashes);
     $(".data").keyup(testKeyDown);
+    $(".arrow").click((e) => {
+        console.log("offsetY: " + e.offsetY)
+        console.log("offsetX: " + e.offsetX)
+    });
 });
