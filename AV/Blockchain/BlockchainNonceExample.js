7b5168c87d5a66b7a186954d56df25708a5af1ae
baileyg2016 <bspell20@vt.edu>
2020-02-05 21:51:08 -0500

    Added a loading icon when the user mines.

--- AV/Blockchain/BlockchainNonceExample.js
+++ AV/Blockchain/BlockchainNonceExample.js
@@ -19,35 +19,35 @@ $(document).ready(function() {
 
         // convert bytes to hex string                  
         const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-        console.log("hashHex: " + hashHex);
+        
         return hashHex;
     }
 
     function checkHash() {
         if ($("#block1Hash").val().substring(0,4) === "0000") {
-            $("#cell1").css("background-color", "#E2EEDD");
+            // $("#cell1").css("background-color", "#E2EEDD");
             $("#arrow1").css("display", "block");
         }
         else {
-            $("#cell1").css("background-color", "#FA8072");
+            // $("#cell1").css("background-color", "#FA8072");
             $("#arrow1").css("display", "none");
         }
 
         if ($("#block2Hash").val().substring(0,4) === "0000") {
-            $("#cell2").css("background-color", "#E2EEDD");
+            // $("#cell2").css("background-color", "#E2EEDD");
             $("#arrow2").css("display", "block");
         }
         else {
-            $("#cell2").css("background-color", "#FA8072");
+            // $("#cell2").css("background-color", "#FA8072");
             $("#arrow2").css("display", "none");
         }
 
-        if ($("#block3Hash").val().substring(0,4) === "0000") {
-            $("#cell3").css("background-color", "#E2EEDD");
-        }
-        else {
-            $("#cell3").css("background-color", "#FA8072");
-        }
+        // if ($("#block3Hash").val().substring(0,4) === "0000") {
+        //     $("#cell3").css("background-color", "#E2EEDD");
+        // }
+        // else {
+        //     $("#cell3").css("background-color", "#FA8072");
+        // }
     }
 
     async function testKeyDown() {
@@ -61,11 +61,11 @@ $(document).ready(function() {
         var block1Hash, block2Hash, block3Hash;
 
         await sha256(one, block1Data, block1Nonce).then(res => {
-            console.log("one: " + one);
-            console.log("data: " + block1Data);
-            console.log("nonce: " + block1Nonce);
-            console.log("Prev: " + block1Hash)
-            console.log("Hash: " + res);
+            // console.log("one: " + one);
+            // console.log("data: " + block1Data);
+            // console.log("nonce: " + block1Nonce);
+            // console.log("Prev: " + block1Hash)
+            // console.log("Hash: " + res);
             block1Hash = res;
         });
 
@@ -90,6 +90,10 @@ $(document).ready(function() {
     }
 
     async function mine() {
+        // hide button and show spinner
+        $("button").css("visibility", "hidden");
+        $("div:last").addClass("loader");
+
         // Setting up a max nonce
         var maxNonce = 500000;
         var one = 1, two = 2, three = 3;
@@ -119,7 +123,7 @@ $(document).ready(function() {
 
             if (block2Hash.substring(0,4) === "0000") {
                 $("#block2Hash").val(block2Hash);
-                $("#block3Hash").val(block2Hash);
+                $("#block3PrevHash").val(block2Hash);
                 $("#nonce2").val(i);
                 checkHash();
                 break;
@@ -134,6 +138,8 @@ $(document).ready(function() {
             if (block3Hash.substring(0,4) === "0000") {
                 $("#block3Hash").val(block3Hash);
                 $("#nonce3").val(i);
+                $("div:last").removeClass("loader");
+                $("button").css("visibility", "visible");
                 break;
             }
         }
