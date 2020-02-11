4890bed94e8469c465d787821315d863b240dfdc
eunzle95 <eunzle95@vt.edu>
2020-02-11 02:14:49 -0500

    editing the second frame of chapter 5 regular language

--- AV/PIExample/RegularExpressionThird.js
+++ AV/PIExample/RegularExpressionThird.js
@@ -38,57 +38,6 @@ $(document).ready(function() {
 
     av.step();
 
-    //frame 3
-    av.umsg("Here is an example of NFA that accepts an empty string ($\\lambda$)");
-    FA.addEdge(q0, q1, {weight: "$\\lambda$"});
-    av.step();
-
-    //frame 4
-    av.umsg("Here is an example of NFA that accepts $a$ $\\in$ $\\Sigma$");
-    FA.removeEdge(q0, q1, {weight: "$\\lambda$"});
-    FA.addEdge(q0, q1, {weight: "a"});
-    av.step();
-
-    //frame 5
-    av.umsg("But what about the 'more interesting' regular expressions that are built from AND, OR, and concatenation? Do these all have maching NFAs? If we could find a way to 'simulate' each of these operations with an NFA, then we know that we can construct a machine for any R.E. This idea of 'simulation' is a standard approach to proving such things!");
-    FA.removeNode(q0);
-    FA.removeNode(q1);
-    av.step();
-
-    //frame 6
-    av.umsg("Suppose that r and s are R.E. (By induction...) That means that there is an NFA for r and an NFA for s. To help us visualize such things, it helps if we can have a standard way to draw the idea of an arbitrary NFA. And since we want to combine machines together, it will be much easier if we know that the arbitrary machine has one start state and one final state. Well, we already know that all NFAs have a single start state. But not all NFAs have a single final state.");
-    av.step();
-
-    //frame 7
-    av.umsg("Let's convert an NFA with multiple final states to one with a single final state");
-    av.step();
-
-    //frame 8
-    av.umsg("Let's suppose that we have a regular expression $r$, we need to find a simple representation for NFA that accepts $r$");
-    var s = FA.addNode({left: 10, top: 70});
-    var ex1 = FA.addNode({left: 160, top: 70});
-    var ex2 = FA.addNode({left: 160, top: 130});
-    var ex3 = FA.addNode({left: 160, top: 190});
-    var m = FA.addNode({left: 310, top: 70});
-    FA.disableDragging();
-    toggleInitial(FA, s);
-    toggleFinal(FA, m);
-    FA.addEdge(ex1, m, {weight: "$\\lambda$"});
-    FA.addEdge(ex2, m, {weight: "$\\lambda$"});
-    FA.addEdge(ex3, m, {weight: "$\\lambda$"});
-
-    av.step();
-
-    //frame 9
-    av.umsg(Frames.addQuestion("q0"));
-    av.step();
-
-    //frame 9
-    av.umsg("To accept any regular expression $r$, the NFA should start from $s$ and go to $\\textbf {intermediate}$ states until it reaches the final state $f$")
-    FA.addEdge(s, ex1, {weight: "$\\lambda$"});
-    FA.addEdge(s, ex2, {weight: "$\\lambda$"});
-    FA.addEdge(s, ex3, {weight: "$\\lambda$"});
-    av.step();
 
     av.recorded();
 });
