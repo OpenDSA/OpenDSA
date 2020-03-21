910e351e65716f5d5aef28dd9cc7477f454f79c4
Patrick Sullivan <sublime@vt.edu>
2020-03-21 13:43:46 -0400

    Help and defaults for server.py args.
    Using with keyword also helps fully close httpd on exit.

--- server.py
+++ server.py
@@ -1,34 +1,25 @@
 import sys
-from http.server import SimpleHTTPRequestHandler
-import http.server
+import argparse
+from http.server import SimpleHTTPRequestHandler, HTTPServer
 
+def test(port=8080, bind=""):
+    server_address = (bind, port)
+    HandlerClass = SimpleHTTPRequestHandler
+    HandlerClass.protocol_version = "HTTP/1.0"
+    with HTTPServer(server_address, HandlerClass) as httpd:
+        host, port = httpd.socket.getsockname()
+        print("Server now live at http://{0}:{1}/ ...".format(host, port))
+        try:
+            httpd.serve_forever()
+        except KeyboardInterrupt:
+            print("\nKeyboard interrupt received, exiting.")
+            sys.exit(0)
 
-def test(HandlerClass=SimpleHTTPRequestHandler,
-         ServerClass=http.server.HTTPServer):
-
-    protocol = "HTTP/1.0"
-    host = 'lti.cs.vt.edu'
-    port = 8000
-    if len(sys.argv) > 1:
-        arg = sys.argv[1]
-        if ':' in arg:
-            host, port = arg.split(':')
-            port = int(port)
-        else:
-            try:
-                port = int(sys.argv[1])
-            except:
-                host = sys.argv[1]
-
-    server_address = (host, port)
-
-    HandlerClass.protocol_version = protocol
-    httpd = ServerClass(server_address, HandlerClass)
-
-    sa = httpd.socket.getsockname()
-    print(("Serving HTTP on", sa[0], "port", sa[1], "..."))
-    httpd.serve_forever()
-
-
-if __name__ == "__main__":
-    test()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('port', default=8080, type=int, nargs='?',
+                        help='Specify alternate port [default: 8080]')
+    parser.add_argument('--bind', '-b', default='', metavar='ADDRESS',
+                        help='Specify alternate bind address [default: all interfaces]')
+    args = parser.parse_args()
+    test(port=args.port, bind=args.bind)
