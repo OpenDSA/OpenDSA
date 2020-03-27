f809226e3185d4192c42e018d9c9da86a6db4b99
adityat <adityat@vt.edu>
2020-03-27 14:55:06 -0400

    testing folder

--- AV/Pointers/shallowdeepExampleCON.js
+++ AV/Pointers/shallowdeepExampleCON.js
@@ -11,14 +11,39 @@ $(document).ready(function() {
   var av = new JSAV(av_name);
   var pseudo = av.code(code[0]);
 
+  //variables for making rectangles
+  var width = 40;
+  var length = 80;
+  var xPosRect = 30;
+  var yPosRect = 30;
+
+  //arrow variables
+  var shallowCopyEmployeeArrow = av.g.line(125, 175, xPosRect + 60, yPosRect + 48,
+            {"arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+  var secondEmployeeArrow = av.g.line(xPosRect + 670, yPosRect + 45, xPosRect + 692, yPosRect,
+            {"arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+  var deepCopyEmployee = av.g.line(xPosRect + 670, yPosRect + 45 + 107, xPosRect + 692, yPosRect + 107,
+           {"arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+
   // Slide 1
   av.umsg(interpret("sc1"));
+  var sam = av.label("Sam", {top: 25, left: 50});
+  sam.show();
+  av.g.rect(xPosRect, yPosRect, length, width);
+  av.label("firstEmployee", {top: 100, left: 10});
+  av.g.line(50, 110, xPosRect + 25, yPosRect + 48,
+            {"arrow-end": "classic-wide-long", opacity: 100, "stroke-width": 2});
+  shallowCopyEmployeeArrow.hide();
+  secondEmployeeArrow.hide();
+  deepCopyEmployee.hide();
   av.displayInit();
 
   // Slide 2
   av.umsg(interpret("sc2"));
   pseudo.highlight("line1");
   pseudo.highlight("line2");
+  av.label("shallowCopyEmployee", {top: 165, left: 21});
+  shallowCopyEmployeeArrow.show();
   av.step();
 
   // Slide 3
@@ -26,6 +51,9 @@ $(document).ready(function() {
   pseudo.unhighlight("line1");
   pseudo.unhighlight("line2");
   pseudo.highlight("line3");
+  sam.hide();
+  var john = av.label("John", {top: 25, left: 50});
+  john.show();
   av.step();
 
   // Slide 4
@@ -39,6 +67,14 @@ $(document).ready(function() {
   pseudo.unhighlight("line4");
   pseudo.highlight("line5");
   pseudo.highlight("line6");
+  av.g.rect(xPosRect + 700, yPosRect - 27, length, width);
+  secondEmployeeArrow.show();
+  av.label("Patrice", {top: 0, left: 746});
+  av.label("secondEmployee", {top: 57, left: 700});
+  av.g.rect(xPosRect + 700, yPosRect + 80, length, width);
+  deepCopyEmployee.show();
+  av.label("deepCopyEmployee", {top: 58 + 107, left: 695});
+
   av.step();
 
   // Slide 6
@@ -46,12 +82,16 @@ $(document).ready(function() {
   pseudo.unhighlight("line5");
   pseudo.unhighlight("line6");
   pseudo.highlight("line7");
+  var pat = av.label("Patrice", {top: 107, left: 746});
+  pat.show();
   av.step();
 
   // Slide 7
   av.umsg(interpret("sc7"));
   pseudo.unhighlight("line7");
   pseudo.highlight("line8");
+  pat.hide();
+  av.label("John", {top: 107, left: 746});
   av.step();
 
   // Slide 8
@@ -59,4 +99,5 @@ $(document).ready(function() {
   pseudo.unhighlight("line8");
   pseudo.highlight("line9");
   av.recorded();
+  //av.label("Prints: \"Patrice\"", {top: 185, left: 700});
 });
