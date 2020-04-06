bae95fe369de79824c714e846f973b89aba5c36d
baileyg2016 <bspell20@vt.edu>
2020-04-06 16:40:27 -0400

    the hashing should now work on the server

--- AV/Blockchain/SmallerHashExample.js
+++ AV/Blockchain/SmallerHashExample.js
@@ -1,43 +1,25 @@
 /*global alert: true, console: true, ODSA */
 // Written by Bailey Spell and Jesse Terrazas
 $(document).ready(function() {
-    "use strict";
-    // Declare and initialize state variables
-    var config = ODSA.UTILS.loadConfig(),
-        interpret = config.interpreter;       // get the interpreter
-  
-    // Convenience function for writing output messages
-    function tell(msg) {
-      $(".output").val(msg);
-    }
-  
-    async function sha256(input) {
-      // encode as UTF-8
-      const msgBuffer = new TextEncoder('utf-8').encode(input);                    
-  
-      // hash the message
-      const hashBuffer = await crypto.subtle.digest('SHA-256', msgBuffer);
-  
-      // convert ArrayBuffer to Array
-      const hashArray = Array.from(new Uint8Array(hashBuffer));
-  
-      // convert bytes to hex string                  
-      const hashHex = hashArray.map(b => ('00' + b.toString(16)).slice(-2)).join('');
-      
-      return "0000" + hashHex.substring(0, 12);
+  "use strict";
+  // Declare and initialize state variables
+  var config = ODSA.UTILS.loadConfig(),
+      interpret = config.interpreter;       // get the interpreter
+
+  function sha256(input) {
+    var hash = CryptoJS.SHA256(input);
+    return "0000" + hash.toString().substring(4, 12);
+}
+
+  // Main action: Result of clicking "Calculate" button
+  // Most of this behavior relates to checking against the expected
+  // answer for giving credit to the obsolete exercise.
+  function CreateHash() {
+    var input = $("#inputArea").val();
+    var hash = sha256(input);
+    $(".output").val(hash);
   }
-  
-    // Main action: Result of clicking "Calculate" button
-    // Most of this behavior relates to checking against the expected
-    // answer for giving credit to the obsolete exercise.
-    function CreateHash() {
-      var input = $("#inputArea").val();
-      sha256(input).then(res => {
-          tell(res);
-      });
-    }
-  
-    // Action callbacks for form entities
-    $("#inputArea").keyup(CreateHash);
-  });
-  
+
+  // Action callbacks for form entities
+  $("#inputArea").keyup(CreateHash);
+});
