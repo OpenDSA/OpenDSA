ba979e22ecf7af15d3dada7bd1ac1534e0ce38ff
Jesse Terrazas <jesseterrazas@Jesses-MacBook-Pro.local>
2020-04-30 19:36:16 -0400

    Added back in some skeletal code

--- AV/Blockchain/ProofofWork.js
+++ AV/Blockchain/ProofofWork.js
@@ -6,41 +6,44 @@ $(document).ready(function() {
     var config = ODSA.UTILS.loadConfig({av_name: av_name}),
         interpret = config.interpreter;                   // get the code object
     var av = new JSAV(av_name);
-  
-    // var graph = av.ds.graph();
-    // //graph.css({"font-size": "12px"});
-    // //graph.addNode('hello');
-    // graph.layout();
-  
-    // // Slide 1
-    // av.umsg(interpret("sc1"));
+    
+    
+    var graph = av.ds.graph();
 
-    // //enter  code here
-
-    // av.displayInit();
+    // this code is the starting state of the graph
+    graph.css({"font-size": "12px"});
+    graph.addNode('hello');
+    graph.layout();
   
-    // // Slide 2
-    // av.umsg(interpret("sc2"));
+    // Slide 1
+    av.umsg(interpret("sc1"));
+
+    //enter  code here
+
+    av.displayInit();
+  
+    // Slide 2
+    av.umsg(interpret("sc2"));
     
 
-    // av.step();
+    av.step();
 
-    // // Slide 3
-    // av.umsg(interpret("sc3"));
+    // Slide 3
+    av.umsg(interpret("sc3"));
    
 
-    // av.step();
+    av.step();
 
-    // // Slide 4
-    // av.umsg(interpret("sc4"));
+    // Slide 4
+    av.umsg(interpret("sc4"));
     
-    // av.step();
+    av.step();
     
-    // // Slide 4
-    // av.umsg(interpret("sc5"));
+    // Slide 4
+    av.umsg(interpret("sc5"));
     
-    // av.step();
+    av.step();
 
-    // av.recorded();
+    av.recorded();
   });
   
