51ccd081c839c926ef2ab6deef2ac2a383a6fa9c
baileyg2016 <bspell20@vt.edu>
2020-04-30 23:16:24 -0400

    added proofofwork slideshow

--- AV/Blockchain/ProofofWork.js
+++ AV/Blockchain/ProofofWork.js
@@ -8,7 +8,7 @@ $(document).ready(function() {
     var av = new JSAV(av_name);
     
     var topMargin = 50;
-    var leftMargin = 235;
+    var leftMargin = 155;
     var blockchain = av.ds.list({top: topMargin, left: leftMargin});
     var graph = av.ds.graph({visible: true});
 
@@ -71,6 +71,7 @@ $(document).ready(function() {
     dBlock.hide();
     aEdge.hide();
     dEdge.hide();
+    graph.hide();
 
     // start blockchain
     blockchain.addFirst("Blk 2").addFirst("Blk 1");
@@ -81,10 +82,21 @@ $(document).ready(function() {
                                 leftMargin + 168, topMargin - 5,
                                 {"arrow-end": "classic-wide-long",
                                 opacity: 0, "stroke-width": 2});
+    
+    let node1NextArrow = av.g.line(leftMargin + 122 + 84, topMargin - 17,
+                                leftMargin + 168 + 84, topMargin - 17,
+                                {"arrow-end": "classic-wide-long",
+                                opacity: 0, "stroke-width": 2});
+
+    let node1NextNextArrow = av.g.line(leftMargin + 122 + 84*2, topMargin - 17,
+                                    leftMargin + 168 + 84*2, topMargin - 17,
+                                    {"arrow-end": "classic-wide-long",
+                                    opacity: 0, "stroke-width": 2});
 
     let forkMargin = 168;
     node1Block.css({top: topMargin-100, left: forkMargin});
     node4Block.css({top: topMargin, left: forkMargin});
+
     // let node1 = blockchain.get(1).next();
     blockchain.get(1).next(node4Block);
     // node4Block.next(node1);
@@ -93,13 +105,62 @@ $(document).ready(function() {
 
     av.step();
     
-    // Slide 4
+    // Slide 5
     av.umsg(interpret("sc5"));
+    node1NextArrow.show();
+    let node4aBlock = blockchain.newNode("Node 4\'");
+    let node1aBlock = blockchain.newNode("Node 1\'");
+    node4aBlock.css({top: topMargin, left: forkMargin + 84});
+    node1aBlock.css({top: topMargin-100, left: forkMargin + 84});
+    node4Block.next(node4aBlock);
+    node1Block.next(node1aBlock);
+    blockchain.layout({updateTop: false});
+
+    av.step();
+    // Slide 6
+    av.umsg(interpret("sc6"));
+    node1NextNextArrow.show();
+    let node4bBlock = blockchain.newNode("Node 4\"");
+    let node1bBlock = blockchain.newNode("Node 1\"");
+    node4bBlock.css({top: topMargin, left: forkMargin + 84 * 2});
+    node1bBlock.css({top: topMargin-100, left: forkMargin + 84 * 2});
+    node4aBlock.next(node4bBlock);
+    node1aBlock.next(node1bBlock);
+    blockchain.layout({updateTop: false});
     
+
+    av.step();
+    // Slide 7
+    av.umsg(interpret("sc7"));
+    let node4cBlock = blockchain.newNode("Node 4\"\'");
+    node4cBlock.css({top: topMargin, left: forkMargin + 84 * 3});
+    node4bBlock.next(node4cBlock);
+    node1bBlock.next(null);
+    blockchain.layout({updateTop: false});
+
     av.step();
 
-    av.umsg(interpret("sc6"));
-
+    av.umsg(interpret("sc8"));
+    node1Arrow.hide();
+    node1NextArrow.hide();
+    node1NextNextArrow.hide();
+    node1Block.hide();
+    node1aBlock.hide();
+    node1bBlock.hide();
+    node4Block./*hide()*/css({top: topMargin});
+    node4aBlock./*hide()*/css({top: topMargin});
+    node4bBlock./*hide()*/css({top: topMargin});
+    node4cBlock./*hide()*/css({top: topMargin});
+    // create final nodes
+    // let block3 = blockchain.newNode("Blk 3");
+    // let block4 = blockchain.newNode("Blk 4");
+    // let block5 = blockchain.newNode("Blk 5");
+    // let block6 = blockchain.newNode("Blk 6");
+    // blockchain.get(1).next(block3);
+    // block3.next(block4);
+    // block4.next(block5);
+    // block5.next(block6);
+    blockchain.layout();
     av.step();
 
     av.recorded();
