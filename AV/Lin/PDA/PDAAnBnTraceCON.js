5d953cb4670b2aaeebd9bfa7e1a4c0190cb11128
LIN251 <lz2304@vt.edu>
2020-05-18 20:42:17 -0400

    update PDA chapter

--- AV/Lin/PDA/PDAAnBnTraceCON.js
+++ AV/Lin/PDA/PDAAnBnTraceCON.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   
     var av_name = "PDAAnBnTraceCON";
     var av = new JSAV(av_name);
-    var url = "../../../AV/Lin/PDA/Machines/PDADeterministic.jff";
+    var url = "../../../AV/Lin/PDA/Machines/PDAAnBnTraceCON.jff";
     av.umsg("Let us see the tace for accepting the string $aaabbb$.");
     var inputString = "aaabbb";
     var pda = new av.ds.PDA({width: 600, height: 200, url: url});
