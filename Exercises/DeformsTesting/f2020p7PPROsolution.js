40a3f077891bf441d3c8ecd46d3b4b28c4594566
Arinjoy-Basak <basakarinjoy@gmail.com>
2021-02-14 21:31:43 -0500

    adding changes for f2020p1,p7,p9,p10

---
+++ Exercises/DeformsTesting/f2020p7PPROsolution.js
@@ -0,0 +1,40 @@
+const solution = {
+    0 : {
+        type: "number",
+        equations : [],
+        variables : {
+            '':"",
+        },
+        solution : 12.73,
+        unit: "MPa",
+    },
+    1 : {
+        type: "number",
+        equations : [],
+        variables : {
+            '':"",
+        },
+        solution : 1.178,
+        unit: "",
+    },
+    2 : {
+        type: "number",
+        equations : [],
+        variables : {
+            '':"",
+        },
+        solution : 5,
+        unit: "MPa",
+    },
+    3 : {
+        type: "number",
+        equations : [],
+        variables : {
+            '':"",
+        },
+        solution : 5,
+        unit: "MPa",
+    }
+}
+// Find a way to specify sets of equations being solved at any stage,
+// and the values that are assigned to variables in them at that time.
