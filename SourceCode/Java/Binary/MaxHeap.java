c9ee26c8420827d0868ceadd81fbac6cfa25f981
sublime <sublime@vt.edu>
2021-11-25 12:48:40 -0500

    Update maxHeap for siftUp, public/private, readability
    minHeap will be later, after changes accepted.

--- SourceCode/Java/Binary/MaxHeap.java
+++ SourceCode/Java/Binary/MaxHeap.java
@@ -1,108 +1,124 @@
-// TODO: In Java, some of these are private methods (including update)
 // TODO: Need to test changes to update and modify
 /* *** ODSATag: Maxheap *** */
 // Max-heap implementation
+// use `java -ea` to enable assertions that check valid heap positions
 class MaxHeap {
-  private Comparable[] Heap; // Pointer to the heap array
-  private int size;          // Maximum size of the heap
-  private int n;             // Number of things now in heap
+  private Comparable[] heap; // Pointer to the heap array
+  private int maxSize; // Maximum size of the heap
+  private int n; // Number of things now in heap
 
   // Constructor supporting preloading of heap contents
-  MaxHeap(Comparable[] h, int num, int max)
-  { Heap = h;  n = num;  size = max;  buildheap(); }
+  MaxHeap(Comparable[] h, int heapSize, int max) {
+    heap = h;
+    n = heapSize;
+    maxSize = max;
+    buildHeap();
+  }
 
   // Return current size of the heap
-  int heapsize() { return n; }
+  public int heapSize() { return n; }
 
   // Return true if pos a leaf position, false otherwise
-  boolean isLeaf(int pos)
-  { return (pos >= n/2) && (pos < n); }
+  public boolean isLeaf(int pos) 
+  { return (n / 2 <= pos ) && (pos < n); }
 
   // Return position for left child of pos
-  int leftchild(int pos) {
-    if (pos >= n/2) { return -1; }
-    return 2*pos + 1;
-  }
+  public static int leftChild(int pos) 
+  { return 2 * pos + 1; }
 
   // Return position for right child of pos
-  int rightchild(int pos) {
-    if (pos >= (n-1)/2) { return -1; }
-    return 2*pos + 2;
-  }
+  public static int rightChild(int pos) 
+  { return 2 * pos + 2; }
 
   // Return position for parent
-  int parent(int pos) {
-    if (pos <= 0) { return -1; }
-    return (pos-1)/2;
-  }
+  public static int parent(int pos) 
+  { return (pos - 1) / 2; }
 
   // Insert val into heap
-  void insert(int key) {
-    if (n >= size) {
-      System.out.println("Heap is full");
-      return;
-    }
-    int curr = n++;
-    Heap[curr] = key;  // Start at end of heap
-    // Now sift up until curr's parent's key > curr's key
-    while ((curr != 0) && (Heap[curr].compareTo(Heap[parent(curr)]) > 0)) {
-      Swap.swap(Heap, curr, parent(curr));
-      curr = parent(curr);
-    }
+  public void insert(Comparable key) {
+    assert n < maxSize : "Heap is full; cannot insert";
+    heap[n] = key;
+    n++;
+    siftUp(n - 1);
   }
 
   // Heapify contents of Heap
-  void buildheap()
-    { for (int i=n/2-1; i>=0; i--) { siftdown(i); } }
+  private void buildHeap() {
+    for (int i = parent(n - 1); i >= 0; i--) {
+      siftDown(i);
+    }
+  }
 
-  // Put element in its correct place
-  void siftdown(int pos) {
-    if ((pos < 0) || (pos >= n)) { return; } // Illegal position
+  // Moves an element down to its correct place
+  private void siftDown(int pos) {
+    assert (0 <= pos && pos < n) : "Invalid heap position";
     while (!isLeaf(pos)) {
-      int j = leftchild(pos);
-      if ((j<(n-1)) && (Heap[j].compareTo(Heap[j+1]) < 0)) {
-        j++; // j is now index of child with greater value
+      int child = leftChild(pos);
+      if ((child + 1 < n) && isGreaterThan(child + 1, child)) {
+        child = child + 1; // child is now index with the greater value
       }
-      if (Heap[pos].compareTo(Heap[j]) >= 0) { return; }
-      Swap.swap(Heap, pos, j);
-      pos = j;  // Move down
+      if (!isGreaterThan(child, pos)) {
+        return; // stop early
+      }
+      swap(pos, child);
+      pos = child; // keep sifting down
+    }
+  }
+
+  // Moves an element up to its correct place
+  private void siftUp(int pos) {
+    assert (0 <= pos && pos < n) : "Invalid heap position";
+    while (pos > 0) {
+      int parent = parent(pos);
+      if (isGreaterThan(parent, pos)) {
+        return; // stop early
+      }
+      swap(pos, parent);
+      pos = parent; // keep sifting up
     }
   }
 
   // Remove and return maximum value
-  Comparable removemax() {
-    if (n == 0) { return -1; }  // Removing from empty heap
-    Swap.swap(Heap, 0, --n); // Swap maximum with last value
-    siftdown(0);   // Put new heap root val in correct place
-    return Heap[n];
+  public Comparable removeMax() {
+    assert n > 0 : "Heap is empty; cannot remove";
+    n--;
+    swap(0, n);  // Swap maximum with last value
+    siftDown(0); // Put new heap root val in correct place
+    return heap[n];
   }
 
   // Remove and return element at specified position
-  Comparable remove(int pos) {
-    if ((pos < 0) || (pos >= n)) { return -1; } // Illegal heap position
-    if (pos == (n-1)) { n--; } // Last element, no work to be done
-    else {
-      Swap.swap(Heap, pos, --n); // Swap with last value
-      update(pos);
-    }
-    return Heap[n];
+  public Comparable remove(int pos) {
+    assert (0 <= pos && pos < n) : "Invalid heap position";
+    n--;
+    swap(pos, n); // Swap with last value
+    update(pos);  // Move other value to correct position
+    return heap[n];
   }
 
   // Modify the value at the given position
-  void modify(int pos, Comparable newVal) {
-    if ((pos < 0) || (pos >= n)) { return; } // Illegal heap position
-    Heap[pos] = newVal;
+  public void modify(int pos, Comparable newVal) {
+    assert (0 <= pos && pos < n) : "Invalid heap position";
+    heap[pos] = newVal;
     update(pos);
   }
 
   // The value at pos has been changed, restore the heap property
-  void update(int pos) {
-    // If it is a big value, push it up
-    while ((pos > 0) && (Heap[pos].compareTo(Heap[parent(pos)]) > 0)) {
-      Swap.swap(Heap, pos, parent(pos));
-      pos = parent(pos);
-    }
-    siftdown(pos); // If it is little, push down
+  private void update(int pos) {
+    siftUp(pos);   // priority goes up
+    siftDown(pos); // unimportant goes down
+  }
+
+  // swaps the elements at two positions
+  private void swap(int pos1, int pos2) {
+    Comparable temp = heap[pos1];
+    heap[pos1] = heap[pos2];
+    heap[pos2] = temp;
+  }
+
+  // does comparison used for checking heap validity
+  private boolean isGreaterThan(int pos1, int pos2) {
+    return heap[pos1].compareTo(heap[pos2]) > 0;
   }
 }
 /* *** ODSAendTag: Maxheap *** */
