44f0acea98bdae339d9bbd4045327171b98ee50c
Cliff Shaffer <shaffer@vt.edu>
2022-01-01 21:24:32 -0500

    Finish Regular Language Closure module

--- AV/PIFLA/Regular/RLClosQuotientFS.js
+++ AV/PIFLA/Regular/RLClosQuotientFS.js
@@ -4,68 +4,56 @@ $(document).ready(function () {
   var av = new JSAV(av_name);
   var Frames = PIFRAMES.init(av_name);
 
-  //frame 1
-  av.umsg("The right quotient of $L_1$ with $L_2$ is the set of all strings $x$ where you can pick some element $y$ from $L_2$ and append it to $x$ to get something from $L_1$.");
+  // Frame 1
+  av.umsg("The right quotient of $L_1$ with $L_2$ (written $L_1\\backslash L_2$) is the set of all strings $x$ where you can pick some element $y$ from $L_2$ and append it to $x$ to get something from $L_1$.<br/><br/>That is, $x$ is in the quotient set if there is $y$ in $L_2$ for which $xy$ is in $L_1$");
   av.displayInit();
-  
-  //frame 2
-  av.umsg("That is, $x$ is in the quotient if there is $y$ in $L_2$ for which $xy$ is in $L_1$");
+
+  // Frame 2
+  av.umsg(Frames.addQuestion("ex1"));
   av.step();
 
-  //frame 3
-  av.umsg("The quotient of $L_1$ by $L_2$ is dentoed by $L_1/L_2$.")
+  // Frame 3
+  av.umsg(Frames.addQuestion("ex1b"));
   av.step();
 
-  //frame 4
-  av.umsg(Frames.addQuestion("q4"));
+  // Frame 4
+  av.umsg(Frames.addQuestion("ex2"));
   av.step();
 
-  //frame 5
-  av.umsg(Frames.addQuestion("q5"));
+  // Frame 5
+  av.umsg(Frames.addQuestion("ex2b"));
   av.step();
 
-  //frame 6
-  av.umsg("Exactly, car is the only string for which you can append something from $L_2$ to get something from $L_1$");
+  // Frame 6
+  av.umsg(Frames.addQuestion("ex3"));
   av.step();
 
-  //frame 7
-  av.umsg(Frames.addQuestion("q7"));
+  // Frame 7
+  av.umsg(Frames.addQuestion("ex4"));
   av.step();
 
-  //frame 8
-  av.umsg(Frames.addQuestion("q8"));
+  // Frame 8
+  av.umsg(Frames.addQuestion("lambda"));
   av.step();
 
-  //frame 9
-  av.umsg(Frames.addQuestion("q9"));
+  // Frame 9
+  av.umsg(Frames.addQuestion("complexex"));
   av.step();
 
-  //frame 10
-  av.umsg(Frames.addQuestion("q10"));
+  // Frame 10
+  av.umsg("OK, now we hope that you have a good understanding of how right quotient works. So now we are ready to tackle what we are here for.<br/><br/>Theorem: If $L_1$ and $L_2$ are regular, then $L_1 \\backslash L_2$ is regular.");
   av.step();
 
-  //frame 11
-  av.umsg(Frames.addQuestion("q11"));
+  // Frame 11
+  av.umsg(Frames.addQuestion("different"));
   av.step();
 
-  //frame 12
-  av.umsg(Frames.addQuestion("q12"));
+  // Frame 12
+  av.umsg("We want to construct $M'$ using the following idea: For each state $q_i$, if there is a walk labeled with strings $\\in L_2$ to a final state in $M$, we mark $q_i$ as final state.");
   av.step();
 
-  //frame 13
-  av.umsg("Theorem: If $L_1$ and $L_2$ are regular, then $L_1 \\backslash L_2$ is regular.");
-  av.step();
-
-  //frame 14
-  av.umsg(Frames.addQuestion("q14"));
-  av.step();
-
-  //frame 15
-  av.umsg("The algorithm is: for each state $q_i$, if there is a walk labeled with strings $\\in L_2$ to a final state in $M$, we mark $q_i$ as final state.");
-  av.step();
-
-  //frame 16
-  av.umsg("Let us see an example. Suppose the following DFA for $L_1 = \\{a^nb^m \\mid n\\ge 1,\\ m \\ge 0\\} \\cup \\{ba\\}$. We need to find the DFA for $L_1 \\backslash L_2$ where $L_2 = \\{b^m\\ |\\ m \\ge 1 \\}$.");
+  // Frame 13
+  av.umsg("Let us see an example. Suppose the following DFA for $L_1 = \\{a^nb^m \\mid n\\ge 1,\\ m \\ge 0\\} \\cup \\{ba\\}$. We need to find the DFA for $L_1 \\backslash L_2$ where $L_2 = \\{b^m\\ |\\ m \\ge 1 \\}$. Note that in this machine, $q4$ is the trap state, and does not strictly need to be shown (nor will it help the algorithm).");
   var DFA1 = new av.ds.FA({left: 50, height: 500, url: "../../../AV/OpenFLAP/machines/Regular/RightQuotient1.jff"});
   var q0 = DFA1.getNodeWithValue("q0");
   var q1 = DFA1.getNodeWithValue("q1");
@@ -75,82 +63,82 @@ $(document).ready(function () {
   var q5 = DFA1.getNodeWithValue("q5");
   av.step();
 
-  //frame 17
-  av.umsg(Frames.addQuestion("q17"));
+  // Frame 14
+  av.umsg(Frames.addQuestion("q0"));
   q0.highlight();
   av.step();
 
-  //frame 18
-  av.umsg(Frames.addQuestion("q18"));
+  // Frame 15
+  av.umsg(Frames.addQuestion("q0final"));
   av.step();
 
-  //frame 19
+  // Frame 16
   q0.unhighlight();
-  av.umsg(Frames.addQuestion("q19"));
+  av.umsg(Frames.addQuestion("q1"));
   q1.highlight();
   av.step();
 
-  //frame 20
-  av.umsg(Frames.addQuestion("q20"));
+  // Frame 17
+  av.umsg(Frames.addQuestion("q1final"));
   av.step();
 
-  //frame 21
+  // Frame 18
   q1.unhighlight();
-  av.umsg(Frames.addQuestion("q21"));
+  av.umsg(Frames.addQuestion("q2"));
   q2.highlight();
   av.step();
 
-  //frame 22
-  av.umsg(Frames.addQuestion("q22"));
+  // Frame 19
+  av.umsg(Frames.addQuestion("q2final"));
   av.step();
 
-  //frame 23
+  // Frame 20
   q2.unhighlight();
-  av.umsg(Frames.addQuestion("q23"));
+  av.umsg(Frames.addQuestion("q3"));
   q3.highlight();
   av.step();
 
-  //frame 24
-  av.umsg(Frames.addQuestion("q24"));
+  // Frame 21
+  av.umsg(Frames.addQuestion("q3final"));
   av.step();
 
-  //frame 25
+  // Frame 22
   q3.unhighlight();
-  av.umsg(Frames.addQuestion("q25"));
+  av.umsg(Frames.addQuestion("q4"));
   q4.highlight();
   av.step();
 
-  //frame 26
-  av.umsg(Frames.addQuestion("q26"));
+  // Frame 23
+  av.umsg(Frames.addQuestion("q4final"));
   av.step();
 
-  //frame 27
+  // Frame 24
   q4.unhighlight();
-  av.umsg(Frames.addQuestion("q27"));
+  av.umsg(Frames.addQuestion("q5"));
   q5.highlight();
   av.step();
 
-  //frame 28
-  av.umsg(Frames.addQuestion("q28"));
+  // Frame 25
+  av.umsg(Frames.addQuestion("q5final"));
   av.step();
 
-  //frame 29
+  // Frame 26
   q5.highlight();
   toggleFinal(DFA1, q5);
-  av.umsg("The resulting DFA is the DFA for $L_1 \\backslash L_2$");
+  av.umsg("The resulting DFA (after we update the final state set as determined by the algorithm) is $M' = L_1 \\backslash L_2$");
   av.step();
 
-  //frame 30
-  av.umsg("The formal proof is:<br/>For each state $i$ do<br/>$\\ \\ \\ \\ $Make $i$ the start state that represent $L_i\\prime$<br/>$\\ \\ \\ \\ $if $L_i\\prime \\cap L_2 \\ne \\emptyset$ then<br/>$\\ \\ \\ \\ \\ \\ \\ \\ $put $i$ in $F\\prime$ in $M\\prime$<br/>where $M\\prime$ is the DFA for $L_1 \\backslash L_2$")
+  // Frame 27
+  av.umsg("A full statement of the algorithm is:<br/>For each state $i$ do<br/>$\\ \\ \\ \\ $Make $i$ the start state that represents $L_i'$<br/>$\\ \\ \\ \\ $if $L_i' \\cap L_2 \\ne \\emptyset$ then<br/>$\\ \\ \\ \\ \\ \\ \\ \\ $put $i$ in $F'$ of $M'$<br/>where $M'$ is the DFA for $L_1 \\backslash L_2$")
   DFA1.hide();
   av.step();
 
-  //frame 31
-  av.umsg(Frames.addQuestion("q31"));
+  // Frame 28
+  av.umsg(Frames.addQuestion("lastex"));
   av.step();
 
-  //frame 32
-  av.umsg("Exactly. Here is the DFA for $L_1$")
+  // Frame 29
+  av.umsg("Here is the DFA for $L_1 = \\{a^*baa^*\\}$.")
   var DFA2 = new av.ds.FA({left: 50, top:50, height: 500, url: "../../../AV/OpenFLAP/machines/Regular/RightQuotient2.jff"});
   var q00 = DFA2.getNodeWithValue("q0");
   var q11 = DFA2.getNodeWithValue("q1");
@@ -158,31 +146,31 @@ $(document).ready(function () {
   var q33 = DFA2.getNodeWithValue("q3");
   av.step();
 
-  //frame 33
+  // Frame 30
   q00.highlight();
-  av.umsg(Frames.addQuestion("q33"));
+  av.umsg(Frames.addQuestion("q0again"));
   av.step();
 
-  //frame 34
+  // Frame 31
   q00.unhighlight();
   q11.highlight();
-  av.umsg(Frames.addQuestion("q34"));
+  av.umsg(Frames.addQuestion("q1again"));
   av.step();
 
-  //frame 35
+  // Frame 32
   q11.unhighlight();
   q22.highlight();
   toggleFinal(DFA2, q11);
-  av.umsg(Frames.addQuestion("q35"));
+  av.umsg(Frames.addQuestion("q2again"));
   av.step();
 
-  //frame 36
+  // Frame 33
   q22.unhighlight();
   q33.highlight();
-  av.umsg(Frames.addQuestion("q36"));
+  av.umsg(Frames.addQuestion("q3again"));
   av.step();
 
-  //frame 37
-  av.step("Completed.");
+  // Frame 34
+  av.umsg("Congratulations! Frameset completed.");
   av.recorded();
 });  
