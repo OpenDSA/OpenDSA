94606e0815210a71092d7c895ae886a2cd54800e
Arinjoy-Basak <arinjoyb@vt.edu>
2022-03-30 12:30:14 -0400

    adding sample problem for discussion

---
+++ Exercises/DeformsTesting/algebrademo-1-PPROsolution.js
@@ -0,0 +1,20 @@
+const solution = {
+    0 : {
+        type: "number",
+        solution : 4,
+        unit: "N",
+    },
+    1 : {
+        type: "number",
+        solution : -1,
+        unit: "N",
+    },
+    2 : {
+        type: "number",
+        solution : -2,
+        unit: "N",
+    },
+};
+// Find a way to specify sets of equations being solved at any stage,
+// and the values that are assigned to variables in them at that time.
+const TRAINING=true;
