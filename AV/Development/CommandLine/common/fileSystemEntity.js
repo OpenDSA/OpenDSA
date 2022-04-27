cf6bd4f0697fba22a4fa47e3764467e11b267a2e
Ryan Buxton <ryanbuxton523@vt.edu>
2022-04-27 14:30:44 -0400

    Created command line visualization

---
+++ AV/Development/CommandLine/common/fileSystemEntity.js
@@ -0,0 +1,127 @@
+let count = 0;
+
+class FileSystemEntity {
+  constructor(name) {
+    this.name = name;
+    this.parent = undefined;
+    this.id = ++count;
+  }
+
+  getPath() {
+    const pathNames = [];
+
+    let curr = this;
+
+    do {
+      pathNames.unshift(curr.name);
+      curr = curr.parent;
+    } while (curr);
+
+    return pathNames.join("/");
+  }
+
+  getChildByPath(path) {
+    const pathNames = path.split("/");
+
+    let currentDir = this;
+
+    pathNames.every((name) => {
+      if (name === "" || name === ".") {
+        //continue
+        return true;
+      } else if (name === "..") {
+        //go up one
+        // continue;
+        currentDir = currentDir.parent;
+      } else {
+        currentDir = currentDir.contents.find(
+          (content) => content.name === name
+        );
+      }
+      return Boolean(currentDir);
+    });
+
+    return currentDir;
+  }
+}
+
+class File extends FileSystemEntity {
+  constructor(name) {
+    super(name);
+  }
+
+  copy() {
+    const newFile = new File(this.name);
+    newFile.parent = this.parent;
+    return newFile;
+  }
+
+  mapToD3() {
+    return {
+      name: this.name,
+      id: this.id,
+      isDirectory: false,
+    };
+  }
+}
+
+class Directory extends FileSystemEntity {
+  constructor(name) {
+    super(name);
+    this.contents = [];
+  }
+
+  copy() {
+    const newDirectory = new Directory(this.name);
+    newDirectory.parent = this.parent;
+    newDirectory.contents = this.contents.map((content) => content.copy());
+    return newDirectory;
+  }
+
+  mapToD3() {
+    const newEntity = {
+      name: this.name,
+      id: this.id,
+      isDirectory: true,
+    };
+
+    newEntity.children = this.contents.map((content) => content.mapToD3());
+
+    return newEntity;
+  }
+
+  insert(fileSystemEntity) {
+    this.contents.push(fileSystemEntity);
+    fileSystemEntity.parent = this;
+  }
+
+  find(name) {
+    if (name === ".") {
+      return this;
+    } else if (name === "..") {
+      return this.parent;
+    }
+    return this.contents.find(
+      (fileSystemEntity) => fileSystemEntity.name === name
+    );
+  }
+
+  remove(name) {
+    const toRemove = this.find(name);
+    if (toRemove) {
+      toRemove.parent = undefined;
+      this.contents = this.contents.filter(
+        (fileSystemEntity) => fileSystemEntity.name !== name
+      );
+      return toRemove;
+    }
+  }
+}
+
+function splitPath(path) {
+  let pathNames = path.split("/");
+  const lastName = pathNames.splice(-1)[0];
+  return [lastName, pathNames.join("/")];
+}
+
+export { FileSystemEntity, File, Directory, splitPath };
---
+++ RST/en/Development/Git.rst
@@ -0,0 +1,19 @@
+.. This file is part of the OpenDSA eTextbook project. See
+.. http://opendsa.org for more details.
+.. Copyright (c) 2012-2020 by the OpenDSA Project Contributors, and
+.. distributed under an MIT open source license.
+
+.. avmetadata::
+   :author: Ryan Buxton 
+   :satisfies: Git 
+   :topic: Tour
+
+Git
+======================
+
+Clone
+-----
+
+Branch
+------
+
