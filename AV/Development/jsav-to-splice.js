8e3b176f7c9a70c4cefa33a4752c4ff883572372
Adeyemi Aina <babs@Adeyemis-MacBook-Pro.local>
2024-06-20 16:07:45 -0400

    update listener for splice protocol

--- AV/Development/jsav-to-splice.js
+++ AV/Development/jsav-to-splice.js
@@ -1,7 +1,20 @@
 $("body").on("jsav-log-event", function(event, eventData) {
-    if (eventData.type === 'jsav-exercise-grade-change') {
-      SPLICE.reportScoreAndState(eventData.av, eventData.score.correct / eventData.score.total, { currentStep: eventData.currentStep })
-    }
-    // still logging scores and state to console
-    console.log(eventData);
-  });
+  if (eventData.type === 'jsav-exercise-grade-change' || eventData.type === 'jsav-exercise-grade' || eventData.type === 'jsav-exercise-step-fixed') {
+      SPLICE.reportScoreAndState(eventData.av, eventData.score.correct / eventData.score.total, { currentStep: eventData.currentStep });
+  }
+  // still logging scores and state to console
+  // console.log(eventData);
+});
+
+
+
+
+
+
+// $("body").on("jsav-log-event", function(event, eventData) {
+//     if (eventData.type === 'jsav-exercise-grade-change') {
+//       SPLICE.reportScoreAndState(eventData.av, eventData.score.correct / eventData.score.total, { currentStep: eventData.currentStep })
+//     }
+//     // still logging scores and state to console
+//     console.log(eventData);
+//   });
