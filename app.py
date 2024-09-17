829e8a0146084c34a89fca2f0a2090b49cb3732b
Alex Hicks <awh4kc@vt.edu>
2024-09-17 11:54:05 -0400

    Remove dotgit hiding

--- app.py
+++ app.py
@@ -17,14 +17,6 @@ def redirect_to_rendered_index(book):
     '''Quickly access a book instead of browsing to the exact file'''
     return redirect(f"/Books/{book}/html/index.html", code=302)
 
-@app.route('/.git/')
-def block_dotgit():
-    return redirect('/Books/', code=302)
-
-@app.route('/.git/<path>/')
-def block_dotgit_dir(path):
-    return redirect('/Books/', code=302)
-
 ################################## API calls ###############################
 
 def run_command(cmd: str):
