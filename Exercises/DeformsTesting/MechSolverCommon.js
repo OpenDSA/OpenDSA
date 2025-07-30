631f677655f0ba4f70582d3bb862f7502b49b709
Nakul Kumar <nak.ak2015@gmail.com>
2025-07-30 12:04:03 -0400

    Cleaned Up commented

--- Exercises/DeformsTesting/MechSolverCommon.js
+++ Exercises/DeformsTesting/MechSolverCommon.js
@@ -720,17 +720,7 @@ requirejs(["./mathjs.js"], function(){});
                 console.log(master_vars)
             })
         }
-        // else master_vars = null; // We will generate and store this.
-        
-        // TODO: Work on this to update this with
-        // 1. Schedule event flushing to server immediately when attempt is submitted
-        // 2. Setup calls to event logger without context from HTML files.
-        // $("body").on("jsav-log-event", function(event, eventData) {
-            // console.log(eventData);
-            // if(window.parent.ODSA != undefined)
-	        //     window.parent.ODSA.UTILS.logUserAction(eventData.type,eventData.desc)
-        // });
-        
+        // else master_vars = null; // We will generate and store this.        
         // Setting up the button for training mode, if not inside Canvas.
         if(window.parent.ODSA == undefined)
         {
